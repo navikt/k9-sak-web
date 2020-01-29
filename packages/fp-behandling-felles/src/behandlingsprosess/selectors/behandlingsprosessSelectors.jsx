c0c13df0333396d9cf5748c5ca046d30469006e0
Erlend Vige <erlend.vige@gmail.com>
2020-01-29 10:17:49 +0100

    Overstyrbar fix (#64)
    * Remove filtering of overstyrbar for determining readonly in vilkar
    
    fixes #63
    
    * remove pull_request_template from fpsak

--- packages/fp-behandling-felles/src/behandlingsprosess/selectors/behandlingsprosessSelectors.jsx
+++ packages/fp-behandling-felles/src/behandlingsprosess/selectors/behandlingsprosessSelectors.jsx
@@ -17,48 +17,78 @@ const getCommonBehandlingsprosessSelectors = (
   getOverrideBehandlingspunkter,
   getRettigheter,
 ) => {
-  const getBehandlingspunkter = createSelector([getBehandlingspunkterProps], (bpProps = []) => bpProps.map((p) => p.code));
-
-  const getBehandlingspunkterStatus = createSelector(
-    [getBehandlingspunkterProps], (bpProps) => arrayToObject(bpProps, (bpProp) => bpProp.code, (bpProp) => bpProp.status),
+  const getBehandlingspunkter = createSelector([getBehandlingspunkterProps], (bpProps = []) =>
+    bpProps.map(p => p.code),
   );
 
-  const getBehandlingspunkterTitleCodes = createSelector(
-    [getBehandlingspunkterProps], (bpProps) => arrayToObject(bpProps, (bpProp) => bpProp.code, (bpProp) => bpProp.titleCode),
+  const getBehandlingspunkterStatus = createSelector([getBehandlingspunkterProps], bpProps =>
+    arrayToObject(
+      bpProps,
+      bpProp => bpProp.code,
+      bpProp => bpProp.status,
+    ),
   );
 
-  const getBehandlingspunktAksjonspunkterNames = createSelector(
-    [getBehandlingspunkterProps], (bpProps) => arrayToObject(bpProps, (bpProp) => bpProp.code, (bpProp) => bpProp.apCodes),
+  const getBehandlingspunkterTitleCodes = createSelector([getBehandlingspunkterProps], bpProps =>
+    arrayToObject(
+      bpProps,
+      bpProp => bpProp.code,
+      bpProp => bpProp.titleCode,
+    ),
   );
 
-  const getBehandlingspunkterVilkar = createSelector(
-    [getBehandlingspunkterProps], (bpProps) => arrayToObject(bpProps, (bpProp) => bpProp.code, (bpProp) => bpProp.vilkarene),
+  const getBehandlingspunktAksjonspunkterNames = createSelector([getBehandlingspunkterProps], bpProps =>
+    arrayToObject(
+      bpProps,
+      bpProp => bpProp.code,
+      bpProp => bpProp.apCodes,
+    ),
+  );
+
+  const getBehandlingspunkterVilkar = createSelector([getBehandlingspunkterProps], bpProps =>
+    arrayToObject(
+      bpProps,
+      bpProp => bpProp.code,
+      bpProp => bpProp.vilkarene,
+    ),
   );
 
   const getBehandlingspunktAksjonspunkter = createSelector(
     [getAksjonspunkter, getBehandlingspunktAksjonspunkterNames],
     (aksjonspunkter = [], behandlingspunktAksjonspunkterNames = {}) => {
-      const valueFunction = (bp) => behandlingspunktAksjonspunkterNames[bp]
-        .map((apCode) => aksjonspunkter.find((ap) => ap.definisjon.kode === apCode))
-        .filter((ap) => ap);
-      return arrayToObject(Object.keys(behandlingspunktAksjonspunkterNames), (bp) => bp, valueFunction);
+      const valueFunction = bp =>
+        behandlingspunktAksjonspunkterNames[bp]
+          .map(apCode => aksjonspunkter.find(ap => ap.definisjon.kode === apCode))
+          .filter(ap => ap);
+      return arrayToObject(Object.keys(behandlingspunktAksjonspunkterNames), bp => bp, valueFunction);
     },
   );
 
-  const getBehandlingspunkterWithOpenAksjonspunkter = createSelector([getBehandlingspunktAksjonspunkter], (punkter = {}) => (
-    Object.keys(punkter)
-      .filter((p) => punkter[p].some((ap) => isAksjonspunktOpen(ap.status.kode) && ap.kanLoses))
-  ));
+  const getBehandlingspunkterWithOpenAksjonspunkter = createSelector(
+    [getBehandlingspunktAksjonspunkter],
+    (punkter = {}) =>
+      Object.keys(punkter).filter(p => punkter[p].some(ap => isAksjonspunktOpen(ap.status.kode) && ap.kanLoses)),
+  );
 
   const getVedtakBehandlingspunkt = createSelector(
     [getBehandlingspunkter, getBehandlingspunkterStatus],
-    (bps = [], bpStatus = {}) => bps.find((bp) => bp === behandlingspunktCodes.VEDTAK && bpStatus[bp] !== vilkarUtfallType.IKKE_VURDERT),
+    (bps = [], bpStatus = {}) =>
+      bps.find(bp => bp === behandlingspunktCodes.VEDTAK && bpStatus[bp] !== vilkarUtfallType.IKKE_VURDERT),
   );
 
   const getSelectableBehandlingspunkter = createSelector(
-    [getBehandlingspunkter, getBehandlingspunkterWithOpenAksjonspunkter, getBehandlingspunkterStatus, getOverrideBehandlingspunkter],
-    (bps = [], bpsWithOpenAps = [], bpStatus = {}, overriddenBps = []) => bps
-      .filter((bp) => !(bpStatus[bp] === vilkarUtfallType.IKKE_VURDERT && !bpsWithOpenAps.includes(bp)) || overriddenBps.includes(bp)),
+    [
+      getBehandlingspunkter,
+      getBehandlingspunkterWithOpenAksjonspunkter,
+      getBehandlingspunkterStatus,
+      getOverrideBehandlingspunkter,
+    ],
+    (bps = [], bpsWithOpenAps = [], bpStatus = {}, overriddenBps = []) =>
+      bps.filter(
+        bp =>
+          !(bpStatus[bp] === vilkarUtfallType.IKKE_VURDERT && !bpsWithOpenAps.includes(bp)) ||
+          overriddenBps.includes(bp),
+      ),
   );
 
   const getDefaultBehandlingspunkt = createSelector(
@@ -68,93 +98,107 @@ const getCommonBehandlingsprosessSelectors = (
 
   const getSelectedBehandlingspunkt = createSelector(
     [getSelectableBehandlingspunkter, getDefaultBehandlingspunkt, getSelectedBehandlingspunktNavn],
-    (selectableBps = [], defaultBehandlingspunkt = null, selectedBpNavn = null) => (selectedBpNavn === DEFAULT_BEHANDLINGSPROSESS
-      ? selectableBps.find((bp) => bp === defaultBehandlingspunkt)
-      : selectableBps.find((bp) => bp === selectedBpNavn)),
+    (selectableBps = [], defaultBehandlingspunkt = null, selectedBpNavn = null) =>
+      selectedBpNavn === DEFAULT_BEHANDLINGSPROSESS
+        ? selectableBps.find(bp => bp === defaultBehandlingspunkt)
+        : selectableBps.find(bp => bp === selectedBpNavn),
   );
-
 
   const getIsSelectedBehandlingspunktOverridden = createSelector(
     [getSelectedBehandlingspunkt, getOverrideBehandlingspunkter],
-    (selectedBehandlingspunkt, overriddenBehandlingspunkter = []) => overriddenBehandlingspunkter.includes(selectedBehandlingspunkt),
+    (selectedBehandlingspunkt, overriddenBehandlingspunkter = []) =>
+      overriddenBehandlingspunkter.includes(selectedBehandlingspunkt),
   );
 
   const getSelectedBehandlingspunktStatus = createSelector(
     [getSelectedBehandlingspunkt, getBehandlingspunkterStatus],
-    (selectedBehandlingspunkt, behandlingspunkterStatus = {}) => (behandlingspunkterStatus[selectedBehandlingspunkt]
-      ? behandlingspunkterStatus[selectedBehandlingspunkt] : ''),
+    (selectedBehandlingspunkt, behandlingspunkterStatus = {}) =>
+      behandlingspunkterStatus[selectedBehandlingspunkt] ? behandlingspunkterStatus[selectedBehandlingspunkt] : '',
   );
 
   const getSelectedBehandlingspunktTitleCode = createSelector(
     [getSelectedBehandlingspunkt, getBehandlingspunkterTitleCodes],
-    (selectedBehandlingspunkt, behandlingspunkterTitleCodes = {}) => (behandlingspunkterTitleCodes[selectedBehandlingspunkt]
-      ? behandlingspunkterTitleCodes[selectedBehandlingspunkt] : {}),
+    (selectedBehandlingspunkt, behandlingspunkterTitleCodes = {}) =>
+      behandlingspunkterTitleCodes[selectedBehandlingspunkt]
+        ? behandlingspunkterTitleCodes[selectedBehandlingspunkt]
+        : {},
   );
 
   const getSelectedBehandlingspunktAksjonspunkter = createSelector(
     [getSelectedBehandlingspunkt, getBehandlingspunktAksjonspunkter],
-    (selectedBehandlingspunkt, behandlingspunktAksjonspunkter = {}) => (behandlingspunktAksjonspunkter[selectedBehandlingspunkt]
-      ? behandlingspunktAksjonspunkter[selectedBehandlingspunkt] : []),
+    (selectedBehandlingspunkt, behandlingspunktAksjonspunkter = {}) =>
+      behandlingspunktAksjonspunkter[selectedBehandlingspunkt]
+        ? behandlingspunktAksjonspunkter[selectedBehandlingspunkt]
+        : [],
   );
 
   const getSelectedBehandlingspunktVilkar = createSelector(
     [getSelectedBehandlingspunkt, getBehandlingspunkterVilkar],
-    (selectedBehandlingspunkt, behandlingspunktVilkar = {}) => (behandlingspunktVilkar[selectedBehandlingspunkt]
-      ? behandlingspunktVilkar[selectedBehandlingspunkt] : []),
+    (selectedBehandlingspunkt, behandlingspunktVilkar = {}) =>
+      behandlingspunktVilkar[selectedBehandlingspunkt] ? behandlingspunktVilkar[selectedBehandlingspunkt] : [],
   );
 
   const getNotAcceptedByBeslutter = createSelector(
     [getSelectedBehandlingspunkt, getBehandlingspunktAksjonspunkterNames, getAllMerknaderFraBeslutter],
     (selectedBehandlingspunkt, aksjonspunkter = {}, merknaderFraBeslutter = {}) => {
       const ap = aksjonspunkter[selectedBehandlingspunkt];
-      return ap !== undefined
-        && ap.length > 0
-        && !!merknaderFraBeslutter[ap[0]]
-        && merknaderFraBeslutter[ap[0]].notAccepted;
+      return (
+        ap !== undefined && ap.length > 0 && !!merknaderFraBeslutter[ap[0]] && merknaderFraBeslutter[ap[0]].notAccepted
+      );
     },
   );
 
-  const findApStatus = (punktAksjonspunkter) => (punktAksjonspunkter.length > 0
-    ? punktAksjonspunkter.some((ap) => isAksjonspunktOpen(ap.status.kode) && ap.kanLoses) : false);
+  const findApStatus = punktAksjonspunkter =>
+    punktAksjonspunkter.length > 0
+      ? punktAksjonspunkter.some(ap => isAksjonspunktOpen(ap.status.kode) && ap.kanLoses)
+      : false;
 
   const getAksjonspunkterOpenStatus = createSelector(
     [getBehandlingspunktAksjonspunkter, getOverrideBehandlingspunkter],
-    (behandlingspunktAksjonspunkter, overriddenBehandlingspunkter) => arrayToObject(
-      Object.keys(behandlingspunktAksjonspunkter), (key) => key,
-      (key) => (overriddenBehandlingspunkter.includes(key) ? true : findApStatus(behandlingspunktAksjonspunkter[key])),
-    ),
+    (behandlingspunktAksjonspunkter, overriddenBehandlingspunkter) =>
+      arrayToObject(
+        Object.keys(behandlingspunktAksjonspunkter),
+        key => key,
+        key => (overriddenBehandlingspunkter.includes(key) ? true : findApStatus(behandlingspunktAksjonspunkter[key])),
+      ),
   );
 
-  const hasNonActiveAksjonspunkterOrNonOverstyrbarVilkar = createSelector(
+  const hasInactiveAksjonspunkterSelector = createSelector(
     [getSelectedBehandlingspunktAksjonspunkter, getSelectedBehandlingspunktVilkar],
-    (aksjonspunkter, vilkarList) => aksjonspunkter.some((ap) => !ap.erAktivt) || vilkarList.some((v) => !v.overstyrbar),
+    aksjonspunkter => aksjonspunkter.some(ap => !ap.erAktivt),
   );
 
   const isSelectedBehandlingspunktReadOnly = createSelector(
-    [getRettigheter, getBehandlingIsOnHold, hasReadOnlyBehandling, hasNonActiveAksjonspunkterOrNonOverstyrbarVilkar],
-    (rettigheter, behandlingIsOnHold, isBehandlingReadOnly, hasNonActivOrNonOverstyrbar) => !rettigheter.writeAccess.isEnabled
-      || behandlingIsOnHold || isBehandlingReadOnly || hasNonActivOrNonOverstyrbar,
+    [getRettigheter, getBehandlingIsOnHold, hasReadOnlyBehandling, hasInactiveAksjonspunkterSelector],
+    (rettigheter, behandlingIsOnHold, isBehandlingReadOnly, hasInactiveAksjonspunkter) => {
+      return (
+        !rettigheter.writeAccess.isEnabled || behandlingIsOnHold || isBehandlingReadOnly || hasInactiveAksjonspunkter
+      );
+    },
   );
 
   const isSelectedBehandlingspunktOverrideReadOnly = createSelector(
-    [getRettigheter, hasNonActiveAksjonspunkterOrNonOverstyrbarVilkar, isBehandlingStatusReadOnly],
-    (rettigheter, hasNonActivOrNonOverstyrbar, isStatusReadOnly) => !rettigheter.kanOverstyreAccess.isEnabled
-      || isStatusReadOnly || hasNonActivOrNonOverstyrbar,
+    [getRettigheter, hasInactiveAksjonspunkterSelector, isBehandlingStatusReadOnly],
+    (rettigheter, hasInactiveAksjonspunkter, isStatusReadOnly) =>
+      !rettigheter.kanOverstyreAccess.isEnabled || isStatusReadOnly || hasInactiveAksjonspunkter,
   );
 
   const hasBehandlingspunktAtLeastOneOpenAksjonspunkt = createSelector(
     [getSelectedBehandlingspunktAksjonspunkter],
-    (aksjonspunkter = []) => aksjonspunkter
-      .filter((ap) => ap.definisjon.kode !== aksjonspunktCodes.FORESLA_VEDTAK)
-      .some((ap) => isAksjonspunktOpen(ap.status.kode)),
+    (aksjonspunkter = []) =>
+      aksjonspunkter
+        .filter(ap => ap.definisjon.kode !== aksjonspunktCodes.FORESLA_VEDTAK)
+        .some(ap => isAksjonspunktOpen(ap.status.kode)),
   );
 
   const isBehandlingspunktAksjonspunkterSolvable = createSelector(
-    [getSelectedBehandlingspunktAksjonspunkter], (aksjonspunkter = []) => aksjonspunkter.some((ap) => ap.kanLoses),
+    [getSelectedBehandlingspunktAksjonspunkter],
+    (aksjonspunkter = []) => aksjonspunkter.some(ap => ap.kanLoses),
   );
 
   const getBehandlingspunktAksjonspunkterCodes = createSelector(
-    [getSelectedBehandlingspunktAksjonspunkter], (aksjonspunkter = []) => aksjonspunkter.map((ap) => ap.definisjon.kode),
+    [getSelectedBehandlingspunktAksjonspunkter],
+    (aksjonspunkter = []) => aksjonspunkter.map(ap => ap.definisjon.kode),
   );
 
   const isBehandlingspunkterAksjonspunkterNotSolvableOrVilkarIsOppfylt = createSelector(
@@ -176,7 +220,7 @@ const getCommonBehandlingsprosessSelectors = (
     getSelectedBehandlingspunktVilkar,
     getNotAcceptedByBeslutter,
     getAksjonspunkterOpenStatus,
-    hasNonActiveAksjonspunkterOrNonOverstyrbarVilkar,
+    hasInactiveAksjonspunkterSelector,
     isSelectedBehandlingspunktReadOnly,
     isSelectedBehandlingspunktOverrideReadOnly,
     hasBehandlingspunktAtLeastOneOpenAksjonspunkt,
