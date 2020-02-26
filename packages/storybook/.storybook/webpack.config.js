6c8948fd44af9ffd593dd3338221c65f05238b54
Hallvard Andreas Stark <hallvard@scel.to>
2020-02-26 13:07:31 +0100

    oppdatert css-loader

--- packages/storybook/.storybook/webpack.config.js
+++ packages/storybook/.storybook/webpack.config.js
@@ -65,15 +65,17 @@ module.exports = async ({ config, mode }) => {
           loader: 'css-loader',
           options: {
             importLoaders: 1,
-            modules: true,
-            localIdentName: '[name]_[local]_[contenthash:base64:5]',
+            modules: {
+              localIdentName: '[name]_[local]_[contenthash:base64:5]',
+            },
           },
         },
         {
           loader: 'less-loader',
           options: {
-            modules: true,
-            localIdentName: '[name]_[local]_[contenthash:base64:5]',
+            modules: {
+              localIdentName: '[name]_[local]_[contenthash:base64:5]',
+            },
             modifyVars: {
               nodeModulesPath: '~',
               coreModulePath: '~',
