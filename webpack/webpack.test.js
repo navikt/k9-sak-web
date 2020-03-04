1bceef022690c9629d9a9ddfadfc65ae6cee61f1
Erlend Vige <erlend.vige@gmail.com>
2020-03-04 11:13:05 +0100

    fix failing tests due to removed webpack.test.js

---
+++ webpack/webpack.test.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const commonTest = require('./webpack.common.test.js');
+
+const PACKAGES_DIR = path.join(__dirname, '../packages');
+const ESLINT_CONFIG_PATH = path.resolve(__dirname, '../eslint/eslintrc.test.js');
+
+const config = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(tsx?|ts?|jsx?)$/,
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        options: {
+          failOnWarning: true,
+          failOnError: true,
+          configFile: ESLINT_CONFIG_PATH,
+          fix: false,
+          cache: true,
+        },
+        include: [PACKAGES_DIR],
+      },
+    ],
+  },
+};
+
+module.exports = merge(commonTest, config);
