ead5e3f1fd0d5dad7768298fc8341eceaa4644c1
Hallvard Andreas Stark <hallvard@scel.to>
2020-03-20 12:35:30 +0100

    la til ny test

--- packages/fakta-omsorgen-for/src/components/SokerinfoTable.spec.jsx
+++ packages/fakta-omsorgen-for/src/components/SokerinfoTable.spec.jsx
@@ -38,4 +38,43 @@ describe('<SokerinfoTable>', () => {
     expect(table.text()).to.contain(tekstForForhold(forhold[0]));
     expect(table.text()).to.contain(tekstForForhold(forhold[1]));
   });
+
+  it('skal ikke vise rad i tabell dersom omsorgen for ikke er definert', () => {
+    const header = 'Overskrift';
+    const forhold = [
+      {
+        forholdstekst: 'Omsorgen for',
+        erOppfylt: null,
+        link: {
+          to: 'https://www.vg.no',
+          text: 'SØ',
+        },
+      },
+      {
+        forholdstekst: 'Mor eller far',
+        erOppfylt: true,
+        link: {
+          to: 'https://www.vg.no',
+          text: 'FR',
+        },
+      },
+      {
+        forholdstekst: 'Samme bosted',
+        erOppfylt: true,
+        link: {
+          to: 'https://www.vg.no',
+          text: 'FR',
+        },
+      },
+    ];
+
+    const tekstForForhold = etForhold =>
+      `${etForhold.forholdstekst}${etForhold.erOppfylt ? 'Ja' : 'Nei'}${etForhold.link.text}`;
+
+    const table = mountWithIntl(<SokerinfoTable header={header} forhold={forhold} />);
+    expect(table.find(Table)).has.length(1);
+    expect(table.text()).to.not.contain(tekstForForhold(forhold[0]));
+    expect(table.text()).to.contain(tekstForForhold(forhold[1]));
+    expect(table.text()).to.contain(tekstForForhold(forhold[2]));
+  });
 });
