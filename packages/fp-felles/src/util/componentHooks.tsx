d9dac54929a2636957b813732bbd617e075b05ed
Anders Kristiansen <anders.kristiansen@nav.no>
2020-04-20 13:16:42 +0200

    TSF-240 inkluderer value i hook deps

--- packages/fp-felles/src/util/componentHooks.tsx
+++ packages/fp-felles/src/util/componentHooks.tsx
@@ -4,7 +4,7 @@ const usePrevious = <T extends any>(value: T): T | undefined => {
   const ref = useRef<T>();
   useEffect(() => {
     ref.current = value;
-  });
+  }, [value]);
   return ref.current;
 };
 
