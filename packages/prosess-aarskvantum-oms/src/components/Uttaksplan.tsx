a9850787fa0c63542d1e1499cffca9b8e5b8e051
Anders Kristiansen <anders.kristiansen@nav.no>
2020-04-24 11:09:36 +0200

    TSF-246 tester konvertering av duration

--- packages/prosess-aarskvantum-oms/src/components/Uttaksplan.tsx
+++ packages/prosess-aarskvantum-oms/src/components/Uttaksplan.tsx
@@ -1,32 +1,19 @@
 import React, { FunctionComponent, ReactNode } from 'react';
 import { FormattedMessage } from 'react-intl';
-import moment from 'moment';
 import { Undertittel, Element } from 'nav-frontend-typografi';
 import { TableColumn, TableRow, Table, Image } from '@fpsak-frontend/shared-components/index';
 import kalender from '@fpsak-frontend/assets/images/kalender.svg';
-import { DDMMYYYY_DATE_FORMAT, ISO_DATE_FORMAT } from '@fpsak-frontend/utils';
 import BorderedContainer from './BorderedContainer';
 import Aktivitet from '../dto/Aktivitet';
+import { durationTilTimerMed7ogEnHalvTimesDagsbasis, formatDate, storForbokstav } from './utils';
 
 interface UttaksplanProps {
   aktiviteter: Aktivitet[];
 }
 
-const storForbokstav = (string: string) => string.charAt(0).toUpperCase() + string.substr(1);
-
-const formatDate = (date: string): string => moment(date, ISO_DATE_FORMAT).format(DDMMYYYY_DATE_FORMAT);
-
 const periodevisning = (periode: string): string => {
   const [fom, tom] = periode.split('/');
   return `${formatDate(fom)} - ${formatDate(tom)}`;
-};
-
-const durationTilTimerMed7ogEnHalvTimesDagsbasis = (delvisFravær: string): number => {
-  const antallTimerMedFulleDager = moment.duration(delvisFravær).asHours();
-  const resttimer = antallTimerMedFulleDager % 24;
-  const heleDager = Math.floor(antallTimerMedFulleDager / 24);
-
-  return heleDager * 7.5 + resttimer;
 };
 
 const formaterDelvisFravær = (delvisFravær?: string): ReactNode => {
