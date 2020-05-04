8f9a591964695901ff43dc3eeda51d830c03dbf0
Geir Roger Moen <geir.roger.moen@nav.no>
2020-05-04 15:14:47 +0200

    NOJIRA Renamet feature-toggles for tilbakekreving i k9

--- packages/fp-felles/src/featureToggle.jsx
+++ packages/fp-felles/src/featureToggle.jsx
@@ -1,10 +1,10 @@
 const featureToggle = {
-  AKTIVER_TILBAKEKREVINGBEHANDLING: 'fpsak.aktiver-tilbakekrevingbehandling',
-  SIMULER_VARSELTEKST: 'fpsak.simuler-oppdrag-varseltekst',
+  AKTIVER_TILBAKEKREVINGBEHANDLING: 'k9sak.aktiver-tilbakekrevingbehandling',
+  SIMULER_VARSELTEKST: 'k9sak.simuler-oppdrag-varseltekst',
   MARKER_UTENLANDSSAK: 'fpsak.aksjonspunkt-marker-utenlandssak',
   OVERSTYR_BEREGNINGSGRUNNLAG: 'fpsak.overstyr_beregningsgrunnlag',
-  BESLUTT_TILBAKEKREVING: 'fpsak.beslutt-tilbakekreving',
-  AKTIVER_KLAGEBEHANDLING: 'fpsak.aktiver-klagebehandling'
+  BESLUTT_TILBAKEKREVING: 'k9sak.beslutt-tilbakekreving',
+  AKTIVER_KLAGEBEHANDLING: 'fpsak.aktiver-klagebehandling',
 };
 
 export default featureToggle;
--- webpack/mocks/feature-toggles.js
+++ webpack/mocks/feature-toggles.js
@@ -4,10 +4,10 @@ const toggles = {
   featureToggles: {
     'fpsak.aksjonspunkt-marker-utenlandssak': false,
     'fpsak.overstyr_beregningsgrunnlag': false,
-    'fpsak.simuler-oppdrag-varseltekst': false,
-    'fpsak.aktiver-tilbakekrevingbehandling': false,
-    'fpsak.beslutt-tilbakekreving': false,
-    'fpsak.aktiver-klagebehandling': false
+    'k9sak.simuler-oppdrag-varseltekst': false,
+    'k9sak.aktiver-tilbakekrevingbehandling': false,
+    'k9sak.beslutt-tilbakekreving': false,
+    'fpsak.aktiver-klagebehandling': false,
   },
 };
 if (process.env.FEATURE_TOGGLES) {
@@ -15,8 +15,8 @@ if (process.env.FEATURE_TOGGLES) {
     toggles.featureToggles[key.trim()] = true;
   });
 }
-module.exports = function(app) {
-  app.all('/k9/sak/api/feature-toggle', function(req, res) {
+module.exports = function (app) {
+  app.all('/k9/sak/api/feature-toggle', function (req, res) {
     res.json(toggles);
   });
 };
