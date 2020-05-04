b903416dc79ed5e2d509d7bb89c9684a81cd3203
Petter Ekern <petter.ekern@nav.no>
2020-05-04 15:11:10 +0200

    FIX: Fjerner gammel featuretoggle for beregning

--- webpack/mocks/feature-toggles.js
+++ webpack/mocks/feature-toggles.js
@@ -3,11 +3,10 @@ require('dotenv').config();
 const toggles = {
   featureToggles: {
     'fpsak.aksjonspunkt-marker-utenlandssak': false,
-    'fpsak.overstyr_beregningsgrunnlag': false,
     'fpsak.simuler-oppdrag-varseltekst': false,
     'fpsak.aktiver-tilbakekrevingbehandling': false,
     'fpsak.beslutt-tilbakekreving': false,
-    'fpsak.aktiver-klagebehandling': false
+    'fpsak.aktiver-klagebehandling': false,
   },
 };
 if (process.env.FEATURE_TOGGLES) {
@@ -15,8 +14,8 @@ if (process.env.FEATURE_TOGGLES) {
     toggles.featureToggles[key.trim()] = true;
   });
 }
-module.exports = function(app) {
-  app.all('/k9/sak/api/feature-toggle', function(req, res) {
+module.exports = function (app) {
+  app.all('/k9/sak/api/feature-toggle', function (req, res) {
     res.json(toggles);
   });
 };
