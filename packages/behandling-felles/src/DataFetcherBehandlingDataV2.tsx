3365881f3239ca9aab103b01aef8b41c7bb08b6d
Erlend Vige <erlend.vige@gmail.com>
2020-05-13 10:59:38 +0200

    remove unused endpoints, add new rel for oppgitt-opptjening

--- packages/behandling-felles/src/DataFetcherBehandlingDataV2.tsx
+++ packages/behandling-felles/src/DataFetcherBehandlingDataV2.tsx
@@ -1,13 +1,15 @@
-import React, {
-  FunctionComponent, useEffect, useRef, useState,
-} from 'react';
+import React, { FunctionComponent, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { LoadingPanel } from '@fpsak-frontend/shared-components';
 import { EndpointOperations } from '@fpsak-frontend/rest-api-redux';
 import { isObjectEmpty } from '@fpsak-frontend/utils';
 
-const format = (name) => name.toLowerCase().replace(/_([a-z])/g, (m) => m.toUpperCase()).replace(/_/g, '');
+const format = name =>
+  name
+    .toLowerCase()
+    .replace(/_([a-z])/g, m => m.toUpperCase())
+    .replace(/_/g, '');
 
 interface OwnProps {
   behandlingVersion: number;
@@ -30,66 +32,86 @@ const DataFetcherBehandlingData: FunctionComponent<OwnProps> = ({
     return null;
   }
 
+  // eslint-disable-next-line no-param-reassign
+  endpoints = endpoints.filter(e => e !== undefined && e !== null);
+
   const [fetchingData, setFetchingData] = useState({
     hasFinishedFetching: false,
     data: {},
   });
 
-  const endpointData = endpoints.reduce((acc, e) => ({
-    ...acc,
-    [e.name]: {
-      formattedName: format(e.name),
-      data: useSelector((state) => e.getRestApiData()(state)),
-      isFinished: useSelector((state) => e.getRestApiFinished()(state)),
-      cacheParams: useSelector((state) => e.getRestApiCacheParams()(state)),
-    },
-  }), {});
+  const endpointData = endpoints.reduce(
+    (acc, e) => ({
+      ...acc,
+      [e.name]: {
+        formattedName: format(e.name),
+        data: useSelector(state => e.getRestApiData()(state)),
+        isFinished: useSelector(state => e.getRestApiFinished()(state)),
+        cacheParams: useSelector(state => e.getRestApiCacheParams()(state)),
+      },
+    }),
+    {},
+  );
 
-  const activeEndpointsData = endpoints.filter((e) => e.isEndpointEnabled()).map((e) => ({
-    endpoint: e,
-    ...endpointData[e.name],
-  }));
+  const activeEndpointsData = endpoints
+    .filter(e => e.isEndpointEnabled())
+    .map(e => ({
+      endpoint: e,
+      ...endpointData[e.name],
+    }));
 
   const dispatch = useDispatch();
   const ref = useRef<number>();
   useEffect(() => {
-    setFetchingData((oldState) => ({
+    setFetchingData(oldState => ({
       hasFinishedFetching: false,
       data: oldState.data,
     }));
 
     ref.current = behandlingVersion;
-    const endpointsToFetchFrom = activeEndpointsData.filter((e) => !e.cacheParams || e.cacheParams.behandlingVersion !== behandlingVersion);
+    const endpointsToFetchFrom = activeEndpointsData.filter(
+      e => !e.cacheParams || e.cacheParams.behandlingVersion !== behandlingVersion,
+    );
 
     const meta = {
       keepData: showOldDataWhenRefetching,
       cacheParams: { behandlingVersion },
     };
 
-    Promise.all(endpointsToFetchFrom.map((e) => {
-      const params = endpointParams ? endpointParams[e.endpoint.name] : undefined;
-      return dispatch(e.endpoint.makeRestApiRequest()(params, meta));
-    })).then((data) => {
+    Promise.all(
+      endpointsToFetchFrom.map(e => {
+        const params = endpointParams ? endpointParams[e.endpoint.name] : undefined;
+        return dispatch(e.endpoint.makeRestApiRequest()(params, meta));
+      }),
+    ).then(data => {
       setFetchingData({
         hasFinishedFetching: true,
-        data: endpointsToFetchFrom.reduce((acc, endpoint, index) => ({
-          ...acc,
-          [endpoint.formattedName]: data[index].payload,
-        }), {}),
+        data: endpointsToFetchFrom.reduce(
+          (acc, endpoint, index) => ({
+            ...acc,
+            [endpoint.formattedName]: data[index].payload,
+          }),
+          {},
+        ),
       });
     });
   }, [behandlingVersion]);
 
   const hasChanged = ref.current !== behandlingVersion;
-  const hasFinishedFetching = fetchingData.hasFinishedFetching && activeEndpointsData.every((e) => e.isFinished);
+  const hasFinishedFetching = fetchingData.hasFinishedFetching && activeEndpointsData.every(e => e.isFinished);
 
   if (hasFinishedFetching && !hasChanged) {
-    return render(activeEndpointsData.reduce((acc, endpoint) => ({
-      ...acc,
-      [endpoint.formattedName]: endpoint.data,
-    }), fetchingData.data), true);
+    return render(
+      activeEndpointsData.reduce(
+        (acc, endpoint) => ({
+          ...acc,
+          [endpoint.formattedName]: endpoint.data,
+        }),
+        fetchingData.data,
+      ),
+      true,
+    );
   }
-
 
   if ((!hasFinishedFetching || hasChanged) && showOldDataWhenRefetching && !isObjectEmpty(fetchingData.data)) {
     return render(fetchingData.data, false);
