a65ba73fb8896f780219a4fb3eddbb8581800c8b
Hallvard Andreas Stark <hallvard@scel.to>
2020-05-25 13:37:54 +0200

    laget egen komponent av avslagsårsakliste filtrerer bort duplikate avslagskoder

--- packages/prosess-vedtak/src/components/VedtakAvslagPanel.jsx
+++ packages/prosess-vedtak/src/components/VedtakAvslagPanel.jsx
@@ -1,43 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { FormattedMessage, injectIntl } from 'react-intl';
-import { Normaltekst, Undertekst } from 'nav-frontend-typografi';
-import { connect } from 'react-redux';
-
-import kodeverkTyper from '@fpsak-frontend/kodeverk/src/kodeverkTyper';
-import { VerticalSpacer } from '@fpsak-frontend/shared-components';
 import { getKodeverknavnFn } from '@fpsak-frontend/fp-felles';
-import vilkarUtfallType from '@fpsak-frontend/kodeverk/src/vilkarUtfallType';
 import behandlingStatus from '@fpsak-frontend/kodeverk/src/behandlingStatus';
 import fagsakYtelseType from '@fpsak-frontend/kodeverk/src/fagsakYtelseType';
-
-import { findAvslagResultatText, findTilbakekrevingText, hasIkkeOppfyltSoknadsfristvilkar } from './VedtakHelper';
-import VedtakFritekstPanel from './VedtakFritekstPanel';
+import kodeverkTyper from '@fpsak-frontend/kodeverk/src/kodeverkTyper';
+import { VerticalSpacer } from '@fpsak-frontend/shared-components';
+import { Normaltekst, Undertekst } from 'nav-frontend-typografi';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { injectIntl } from 'react-intl';
+import { connect } from 'react-redux';
 import vedtakVarselPropType from '../propTypes/vedtakVarselPropType';
-
-export const getAvslagArsak = (vilkar, aksjonspunkter, vedtakVarsel, getKodeverknavn) => {
-  const avslatteVilkar = vilkar.filter(v =>
-    v.perioder.some(periode => periode.vilkarStatus.kode === vilkarUtfallType.IKKE_OPPFYLT),
-  );
-  if (avslatteVilkar.length === 0) {
-    return <FormattedMessage id="VedtakForm.UttaksperioderIkkeGyldig" />;
-  }
-
-  return avslatteVilkar.map(avslåttVilkår => (
-    <Normaltekst key={avslåttVilkår.vilkarType.kode}>
-      {`${getKodeverknavn(avslåttVilkår.vilkarType)}: ${getKodeverknavn(
-        { kode: avslåttVilkår.perioder[0].avslagKode, kodeverk: 'AVSLAGSARSAK' },
-        avslåttVilkår.vilkarType.kode,
-      )}`}
-    </Normaltekst>
-  ));
-};
+import AvslagsårsakListe from './AvslagsårsakListe';
+import VedtakFritekstPanel from './VedtakFritekstPanel';
+import { findAvslagResultatText, findTilbakekrevingText, hasIkkeOppfyltSoknadsfristvilkar } from './VedtakHelper';
 
 export const VedtakAvslagPanelImpl = ({
   intl,
   behandlingStatusKode,
   vilkar,
-  aksjonspunkter,
   behandlingsresultat,
   sprakkode,
   readOnly,
@@ -61,13 +40,12 @@ export const VedtakAvslagPanelImpl = ({
         {tilbakekrevingText && `. ${intl.formatMessage({ id: tilbakekrevingText })}`}
       </Normaltekst>
       <VerticalSpacer sixteenPx />
-      {getAvslagArsak(vilkar, aksjonspunkter, vedtakVarsel, getKodeverknavn) && (
-        <div>
-          <Undertekst>{intl.formatMessage({ id: 'VedtakForm.ArsakTilAvslag' })}</Undertekst>
-          {getAvslagArsak(vilkar, aksjonspunkter, vedtakVarsel, getKodeverknavn)}
-          <VerticalSpacer sixteenPx />
-        </div>
-      )}
+
+      <div>
+        <Undertekst>{intl.formatMessage({ id: 'VedtakForm.ArsakTilAvslag' })}</Undertekst>
+        <AvslagsårsakListe vilkar={vilkar} getKodeverknavn={getKodeverknavn} />
+        <VerticalSpacer sixteenPx />
+      </div>
       {(fritekstfeltForSoknadsfrist || vedtakVarsel.avslagsarsakFritekst || beregningErManueltFastsatt) && (
         <VedtakFritekstPanel
           readOnly={readOnly}
@@ -84,7 +62,6 @@ VedtakAvslagPanelImpl.propTypes = {
   intl: PropTypes.shape().isRequired,
   behandlingStatusKode: PropTypes.string.isRequired,
   vilkar: PropTypes.arrayOf(PropTypes.shape()).isRequired,
-  aksjonspunkter: PropTypes.arrayOf(PropTypes.shape()).isRequired,
   behandlingsresultat: PropTypes.shape().isRequired,
   sprakkode: PropTypes.shape().isRequired,
   readOnly: PropTypes.bool.isRequired,
