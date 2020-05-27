8d66dc1ca74fd69f95eeda6749fc65ac72c34b6e
Qadeer Ahmad Khan <qadeer.ahmad.khan@nav.no>
2020-05-27 11:43:44 +0200

    resetter SAVE_KLAGE_VURDERING api for at finish flagget skal bli false igjen. Ellers blir den værende true selv påfølgende SAVE_KLAGE_VURDERING kall ikke er ferdige. Dette gjør at forhåndsvisning alltid skjer etter SAVE_KLAGE_VURDERING.
    Endringer etter QA

--- packages/prosess-klagevurdering/src/components/felles/TempsaveAndPreviewKlageLink.tsx
+++ packages/prosess-klagevurdering/src/components/felles/TempsaveAndPreviewKlageLink.tsx
@@ -1,99 +1,116 @@
-import classNames from "classnames";
-import {FormattedMessage} from "react-intl";
-import React from "react";
+import classNames from 'classnames';
+import { FormattedMessage } from 'react-intl';
+import React from 'react';
 import klageVurderingType from '@fpsak-frontend/kodeverk/src/klageVurdering';
-import dokumentMalType from "@fpsak-frontend/kodeverk/src/dokumentMalType";
-import klageApi from "@fpsak-frontend/behandling-klage/src/data/klageBehandlingApi";
-import {connect} from "react-redux";
-import styles from "./tempsaveAndPreviewKlageLink.less";
-
+import dokumentMalType from '@fpsak-frontend/kodeverk/src/dokumentMalType';
+import klageApi from '@fpsak-frontend/behandling-klage/src/data/klageBehandlingApi';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import styles from './tempsaveAndPreviewKlageLink.less';
 
 const transformValues = (values, aksjonspunktCode) => ({
-  klageMedholdArsak: (values.klageVurdering === klageVurderingType.MEDHOLD_I_KLAGE
-    || values.klageVurdering === klageVurderingType.OPPHEVE_YTELSESVEDTAK) ? values.klageMedholdArsak : null,
-  klageVurderingOmgjoer: values.klageVurdering === klageVurderingType.MEDHOLD_I_KLAGE ? values.klageVurderingOmgjoer : null,
+  klageMedholdArsak:
+    values.klageVurdering === klageVurderingType.MEDHOLD_I_KLAGE ||
+    values.klageVurdering === klageVurderingType.OPPHEVE_YTELSESVEDTAK
+      ? values.klageMedholdArsak
+      : null,
+  klageVurderingOmgjoer:
+    values.klageVurdering === klageVurderingType.MEDHOLD_I_KLAGE ? values.klageVurderingOmgjoer : null,
   klageVurdering: values.klageVurdering,
   fritekstTilBrev: values.fritekstTilBrev,
   begrunnelse: values.begrunnelse,
   kode: aksjonspunktCode,
 });
 
-const getBrevData = (tekst) => {
+const getBrevData = tekst => {
   return {
     fritekst: tekst || '',
     dokumentMal: dokumentMalType.UTLED_KLAGE,
   };
 };
 
-
 interface OwnProps {
-  formValues;
-  saveKlage: (params: {}) => undefined;
+  formValues: any;
+  saveKlage: (params: any) => void;
   aksjonspunktCode: string;
   readOnly: boolean;
-  previewCallback: (brevData: {}) => undefined;
+  previewCallback: (brevData: any) => void;
   hasFinishedSaveKlage: boolean;
-  hasStartedSaveKlage: boolean;
+  resetSaveKlage: () => void;
 }
 
 interface StateProps {
-  skalForhaandsvise: boolean
+  skalForhaandsvise: boolean;
 }
-
 
 export class TempsaveAndPreviewKlageLink extends React.Component<OwnProps, StateProps> {
   constructor(props) {
     super(props);
     this.state = {
-      skalForhaandsvise: false
-    }
+      skalForhaandsvise: false,
+    };
   }
 
-  tempSave = (event) => {
-    const {saveKlage, formValues, aksjonspunktCode} = this.props;
+  componentDidUpdate = () => {
+    const { readOnly, hasFinishedSaveKlage } = this.props;
+    const { skalForhaandsvise } = this.state;
+    if (!readOnly && hasFinishedSaveKlage && skalForhaandsvise) {
+      this.forhåndsvisKlage();
+      this.resettVerdier();
+    }
+  };
+
+  private tempSave = event => {
+    const { saveKlage, formValues, aksjonspunktCode } = this.props;
     saveKlage(transformValues(formValues, aksjonspunktCode));
-    this.setState({skalForhaandsvise: true});
+    this.setState({ skalForhaandsvise: true });
     event.preventDefault();
   };
 
-  forhåndsvisKlage = () => {
-    const {previewCallback, formValues} = this.props;
+  private forhåndsvisKlage = () => {
+    const { previewCallback, formValues } = this.props;
     previewCallback(getBrevData(formValues.fritekstTilBrev));
-    this.setState({skalForhaandsvise: false})
   };
 
+  private resettVerdier() {
+    this.setState({ skalForhaandsvise: false });
+    const { resetSaveKlage } = this.props;
+    resetSaveKlage();
+  }
 
   render() {
-    const {
-      readOnly,
-      hasFinishedSaveKlage,
-      hasStartedSaveKlage
-    } = this.props;
-
-    const {skalForhaandsvise} = this.state;
-
+    const { readOnly } = this.props;
     return (
-      <div> {!readOnly && (
-        <a
-          href=""
-          onClick={(e) => {
-            this.tempSave(e);
-          }}
-          onKeyDown={(e) => (e.keyCode === 13 ? this.tempSave(e) : null)}
-          className={classNames(styles.previewLink, 'lenke lenke--frittstaende')}
-        >
-          <FormattedMessage id="Klage.ResolveKlage.TempSaveAndPreviewButton"/>
-        </a>)
-      }
-        {!readOnly && !hasStartedSaveKlage && hasFinishedSaveKlage && skalForhaandsvise && this.forhåndsvisKlage()}
+      <div>
+        {' '}
+        {!readOnly && (
+          <a
+            href=""
+            onClick={e => {
+              this.tempSave(e);
+            }}
+            onKeyDown={e => (e.keyCode === 13 ? this.tempSave(e) : null)}
+            className={classNames(styles.previewLink, 'lenke lenke--frittstaende')}
+          >
+            <FormattedMessage id="Klage.ResolveKlage.TempSaveAndPreviewButton" />
+          </a>
+        )}
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   hasFinishedSaveKlage: !!klageApi.SAVE_KLAGE_VURDERING.getRestApiFinished()(state),
-  hasStartedSaveKlage: !!klageApi.SAVE_KLAGE_VURDERING.getRestApiStarted()(state),
 });
 
-export default connect(mapStateToProps)(TempsaveAndPreviewKlageLink);
+const mapDispatchToProps = dispatch => ({
+  ...bindActionCreators(
+    {
+      resetSaveKlage: klageApi.SAVE_KLAGE_VURDERING.resetRestApi(),
+    },
+    dispatch,
+  ),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TempsaveAndPreviewKlageLink);
