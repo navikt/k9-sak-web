a528e3037bd6409d755c200366bf4a9451b315b3
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2020-05-28 13:40:04 +0200

    lagre og preview (#456)
    * lagre og preview
    
    * resetter SAVE_KLAGE_VURDERING api for at finish flagget skal bli false igjen. Ellers blir den værende true selv påfølgende SAVE_KLAGE_VURDERING kall ikke er ferdige. Dette gjør at forhåndsvisning alltid skjer etter SAVE_KLAGE_VURDERING.
    
    Endringer etter QA
    
    * oppdatert tsconfig og kjørt prettier
    
    * bruker react hooks istedenfor og legger inne type informasjon
    
    * kjørt prettier på endrede filer - ingen funksjonelle endringer
    
    * endringer etter pr.

---
+++ packages/prosess-klagevurdering/src/components/felles/TempSaveAndPreviewKlageLink.tsx
@@ -0,0 +1,105 @@
+import classNames from 'classnames';
+import { FormattedMessage } from 'react-intl';
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import klageVurderingType from '@fpsak-frontend/kodeverk/src/klageVurdering';
+import dokumentMalType from '@fpsak-frontend/kodeverk/src/dokumentMalType';
+import klageApi from '@fpsak-frontend/behandling-klage/src/data/klageBehandlingApi';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import styles from './tempsaveAndPreviewKlageLink.less';
+
+const transformValues = (values: any, aksjonspunktCode: string) => ({
+  klageMedholdArsak:
+    values.klageVurdering === klageVurderingType.MEDHOLD_I_KLAGE ||
+    values.klageVurdering === klageVurderingType.OPPHEVE_YTELSESVEDTAK
+      ? values.klageMedholdArsak
+      : null,
+  klageVurderingOmgjoer:
+    values.klageVurdering === klageVurderingType.MEDHOLD_I_KLAGE ? values.klageVurderingOmgjoer : null,
+  klageVurdering: values.klageVurdering,
+  fritekstTilBrev: values.fritekstTilBrev,
+  begrunnelse: values.begrunnelse,
+  kode: aksjonspunktCode,
+});
+
+const getBrevData = (tekst: string) => {
+  return { fritekst: tekst || '', dokumentMal: dokumentMalType.UTLED_KLAGE };
+};
+
+interface OwnProps {
+  formValues: any;
+  saveKlage: (params: any) => void;
+  aksjonspunktCode: string;
+  readOnly: boolean;
+  previewCallback: (brevData: any) => void;
+  hasFinishedSaveKlage: boolean;
+  resetSaveKlage: () => void;
+}
+
+const useForhaandsvise = (
+  readOnly: boolean,
+  hasFinishedSaveKlage: boolean,
+  previewCallback: (data) => void,
+  formValues: any,
+  resetSaveKlage: () => void,
+) => {
+  const [skalForhaandsvise, setSkalForhaandsvise] = useState(false);
+  useEffect(() => {
+    if (!readOnly && hasFinishedSaveKlage && skalForhaandsvise) {
+      previewCallback(getBrevData(formValues.fritekstTilBrev));
+      setSkalForhaandsvise(false);
+      resetSaveKlage();
+    }
+  }, [skalForhaandsvise, hasFinishedSaveKlage, readOnly, formValues.fritekstTilBrev, previewCallback, resetSaveKlage]);
+
+  return setSkalForhaandsvise;
+};
+
+export const TempSaveAndPreviewKlageLink: FunctionComponent<OwnProps> = ({
+  formValues,
+  saveKlage,
+  aksjonspunktCode,
+  readOnly,
+  previewCallback,
+  hasFinishedSaveKlage,
+  resetSaveKlage,
+}) => {
+  const setSkalForhaandsvise = useForhaandsvise(
+    readOnly,
+    hasFinishedSaveKlage,
+    previewCallback,
+    formValues,
+    resetSaveKlage,
+  );
+
+  const tempSave = event => {
+    saveKlage(transformValues(formValues, aksjonspunktCode));
+    setSkalForhaandsvise(true);
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      {!readOnly && (
+        <a
+          href=""
+          onClick={tempSave}
+          onKeyDown={e => (e.keyCode === 13 ? tempSave(e) : null)}
+          className={classNames(styles.previewLink, 'lenke lenke--frittstaende')}
+        >
+          <FormattedMessage id="Klage.ResolveKlage.TempSaveAndPreviewButton" />
+        </a>
+      )}
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  hasFinishedSaveKlage: !!klageApi.SAVE_KLAGE_VURDERING.getRestApiFinished()(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  ...bindActionCreators({ resetSaveKlage: klageApi.SAVE_KLAGE_VURDERING.resetRestApi() }, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TempSaveAndPreviewKlageLink);
