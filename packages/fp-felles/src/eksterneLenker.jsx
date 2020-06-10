7c59f1e790b8ac808b342e0f15a0497c433a4482
Hallvard Andreas Stark <hallvard@scel.to>
2020-06-10 08:35:06 +0200

    endret url for lenke til systemrutine

--- packages/fp-felles/src/eksterneLenker.jsx
+++ packages/fp-felles/src/eksterneLenker.jsx
@@ -3,4 +3,4 @@ export const LINK_TIL_BESTE_BEREGNING_REGNEARK =
   'https://navno.sharepoint.com/sites/fag-og-ytelser-familie-foreldrepenger';
 /* eslint-disable max-len */
 export const SYSTEMRUTINE_URL =
-  'https://navno.sharepoint.com/sites/fag-og-ytelser-fagsystemer/SitePages/K9-sak--Rutiner-for-omsorgspenger.aspx';
+  'https://navno.sharepoint.com/sites/fag-og-ytelser-fagsystemer/SitePages/K9-saksbehandlingsløsningen%20for%20pleiepenger,%20omsorgspenger,%20opplæringspenger%20og%20pleie%20av%20nærstående.aspx';
