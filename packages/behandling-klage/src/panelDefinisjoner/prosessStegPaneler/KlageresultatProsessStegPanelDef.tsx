a81d335729ca0088c91d0af91d71872bd9d81c29
Tomas Engebretsen <tomas.engebretsen@nav.no>
2020-06-25 11:24:44 +0200

    Fjerner getOverstyrtStatus

--- packages/behandling-klage/src/panelDefinisjoner/prosessStegPaneler/KlageresultatProsessStegPanelDef.tsx
+++ packages/behandling-klage/src/panelDefinisjoner/prosessStegPaneler/KlageresultatProsessStegPanelDef.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 
-import aksjonspunktStatus from '@fpsak-frontend/kodeverk/src/aksjonspunktStatus';
-import vilkarUtfallType from '@fpsak-frontend/kodeverk/src/vilkarUtfallType';
 import VedtakKlageProsessIndex from '@fpsak-frontend/prosess-vedtak-klage';
 import { prosessStegCodes } from '@k9-sak-web/konstanter';
 import aksjonspunktCodes from '@fpsak-frontend/kodeverk/src/aksjonspunktCodes';
 import { ProsessStegDef, ProsessStegPanelDef } from '@fpsak-frontend/behandling-felles';
 
-const getVedtakStatus = aksjonspunkter => {
-  const harApentAksjonpunkt = aksjonspunkter.some(ap => ap.status.kode === aksjonspunktStatus.OPPRETTET);
-  return aksjonspunkter.length === 0 || harApentAksjonpunkt ? vilkarUtfallType.IKKE_VURDERT : vilkarUtfallType.OPPFYLT;
-};
-
 class PanelDef extends ProsessStegPanelDef {
   getKomponent = props => <VedtakKlageProsessIndex {...props} />;
 
   getOverstyrVisningAvKomponent = () => true;
-
-  getOverstyrtStatus = ({ aksjonspunkterForSteg }) =>
-    getVedtakStatus(aksjonspunkterForSteg);
 
   getAksjonspunktKoder = () => [
     aksjonspunktCodes.FORESLA_VEDTAK,
