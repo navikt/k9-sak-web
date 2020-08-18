d2b30f4baa89ca1989f2fbff5dee4f4bc7ff9f78
Petter <petter.ruud@nav.no>
2020-08-18 11:30:51 +0200

    Tsf 1109 fikse source maps i sentry i prod (#731)
    * Tar i bruk sentry realse på github actions
    
    * fjerner sjekk på localhost da denne blir nå filtrert i Sentry + oppdaterer sentry browser plugin
    
    * kommenterer ut endring i promote da den mangler en secret for nå
    
    * legger til isdevelopment igjen

--- webpack/webpack.common.dev_and_prod.js
+++ webpack/webpack.common.dev_and_prod.js
@@ -7,8 +7,6 @@ const webpack = require('webpack');
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const PACKAGE = require('./../package.json');
-const VERSION = PACKAGE.version;
 
 const ROOT_DIR = path.resolve(__dirname, '../public/client');
 const CORE_DIR = path.resolve(__dirname, '../node_modules');
@@ -169,10 +167,6 @@ const config = {
       filename: 'index.html',
       favicon: path.join(ROOT_DIR, 'favicon.ico'),
       template: path.join(ROOT_DIR, 'index.html'),
-      version: VERSION,
-    }),
-    new webpack.DefinePlugin({
-      VERSION: JSON.stringify(VERSION),
     }),
     new CopyWebpackPlugin([
       {
