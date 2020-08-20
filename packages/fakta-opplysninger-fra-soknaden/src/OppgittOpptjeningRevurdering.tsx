16bb21ce7c8c791d0813c724d7b64b30ef874976
Erlend Vige <erlend.vige@gmail.com>
2020-08-20 10:19:45 +0200

    Frisinn aksjonspunkt-fix (#716)
    * Frisinn aksjonspunkt-fix
    
    * Re-revert preutfylling
    
    Co-authored-by: Petter <petter.ruud@nav.no>

--- packages/fakta-opplysninger-fra-soknaden/src/OppgittOpptjeningRevurdering.tsx
+++ packages/fakta-opplysninger-fra-soknaden/src/OppgittOpptjeningRevurdering.tsx
@@ -3,9 +3,9 @@ import * as React from 'react';
 import { createIntl, createIntlCache, RawIntlProvider } from 'react-intl';
 import OpplysningerFraSøknaden from '@k9-sak-web/types/src/opplysningerFraSoknaden';
 import { InjectedFormProps } from 'redux-form';
+import aksjonspunktCodes from '@fpsak-frontend/kodeverk/src/aksjonspunktCodes';
 import messages from '../i18n/nb_NO.json';
 import OppgittOpptjeningRevurderingForm from './OppgittOpptjeningRevurderingForm';
-
 
 const cache = createIntlCache();
 
@@ -28,13 +28,33 @@ interface Props {
   oppgittOpptjening: OpplysningerFraSøknaden;
 }
 
+function harRelevantAksjonspunktSomKanLøses(aksjonspunkter) {
+  const aksjonspunkt = (aksjonspunkter || []).find(
+    ap => ap.definisjon.kode === aksjonspunktCodes.OVERSTYRING_FRISINN_OPPGITT_OPPTJENING,
+  );
+  if (!aksjonspunkt) {
+    return false;
+  }
+  return aksjonspunkt.kanLoses === true;
+}
+
 const OppgittOpptjeningRevurdering = (props: Props & InjectedFormProps) => {
   const {
     behandling: { id, versjon },
+    kanEndrePåSøknadsopplysninger,
+    aksjonspunkter,
   } = props;
   return (
     <RawIntlProvider value={intlConfig}>
-      <OppgittOpptjeningRevurderingForm behandlingId={id} behandlingVersjon={versjon} {...props} />
+      <OppgittOpptjeningRevurderingForm
+        behandlingId={id}
+        behandlingVersjon={versjon}
+        {...props}
+        kanEndrePåSøknadsopplysninger={
+          kanEndrePåSøknadsopplysninger && harRelevantAksjonspunktSomKanLøses(aksjonspunkter)
+        }
+        aksjonspunkter={aksjonspunkter}
+      />
     </RawIntlProvider>
   );
 };
