e661498cf172a904c4bba52716c94d3c245ba28c
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-08-24 08:54:23 +0200

    fix: endrer headre oppsett for å ta headere fra backend hvis er satt,… (#777)
    * fix: endrer headre oppsett for å ta headere fra backend hvis er satt, ellers fra nginx
    
    * fix: fjerner cache-control fra /
    
    * fix: resetter default header
    
    * fix: fjerner ugyldig request header ref
    https://tools.ietf.org/html/rfc7234#section-5.2.1

--- packages/rest-api/src/axios/initRestMethods.ts
+++ packages/rest-api/src/axios/initRestMethods.ts
@@ -21,7 +21,7 @@ const cancellable = (axiosInstance, config) => {
 };
 
 const defaultHeaders = {
-  'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+  'Cache-Control': 'no-store, no-cache, max-age=0',
   'Access-Control-Allow-Headers': 'Location',
   Pragma: 'no-cache',
   Expires: 0,
