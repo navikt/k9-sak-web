1cf5b24b07c0e8a341366c0b106d95da262757a0
Erlend Vige <erlend.vige@gmail.com>
2020-08-28 09:15:09 +0200

    Update URLs to los + Systemrutine

--- packages/konstanter/src/eksterneLenker.ts
+++ packages/konstanter/src/eksterneLenker.ts
@@ -2,5 +2,4 @@ export const RETTSKILDE_URL = 'https://lovdata.no/pro/sso/login/nav';
 export const LINK_TIL_BESTE_BEREGNING_REGNEARK =
   'https://navno.sharepoint.com/sites/fag-og-ytelser-familie-foreldrepenger';
 /* eslint-disable max-len */
-export const SYSTEMRUTINE_URL =
-  'https://navno.sharepoint.com/sites/fag-og-ytelser-fagsystemer/SitePages/K9-saksbehandlingsløsningen%20for%20pleiepenger,%20omsorgspenger,%20opplæringspenger%20og%20pleie%20av%20nærstående.aspx';
+export const SYSTEMRUTINE_URL = 'https://navno.sharepoint.com/sites/fag-og-ytelser-fagsystemer';
