9b2b6853ac32fd1da11529257b098af64802353b
TomasEng <tomas.engebretsen@nav.no>
2020-09-08 12:51:12 +0200

    Verifiserer årsaker til redusert utbetaling (#887)
    * Verifiserer årsaker til redusert utbetaling
    
    * Fikser versjon av nav-frontend-skjema
    
    Co-authored-by: Petter <petter.ruud@nav.no>

--- packages/prosess-vedtak/src/components/revurdering/VedtakRedusertUtbetalingArsaker.jsx
+++ packages/prosess-vedtak/src/components/revurdering/VedtakRedusertUtbetalingArsaker.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { CheckboxField } from '@fpsak-frontend/form';
 import PropTypes from 'prop-types';
+import { CheckboxGruppe } from "nav-frontend-skjema";
 import styles from './vedtakRedusertUtbetalingArsaker.less';
 import redusertUtbetalingArsak from '../../kodeverk/redusertUtbetalingArsak';
 import vedtakVarselPropType from '../../propTypes/vedtakVarselPropType';
 
-const VedtakRedusertUtbetalingArsaker = ({ readOnly, values, vedtakVarsel }) => {
+const VedtakRedusertUtbetalingArsaker = ({ intl, readOnly, values, vedtakVarsel, erSendtInnUtenArsaker }) => {
+  const ingenArsakErValgt = !Array.from(values.values()).includes(true);
   return (
-    <div className={styles.wrapper}>
+    <CheckboxGruppe
+      className={styles.wrapper}
+      feil={erSendtInnUtenArsaker && ingenArsakErValgt && intl.formatMessage({id: 'VedtakForm.RedusertUtbetalingArsaker.IkkeSatt'})}
+    >
       {Object.values(redusertUtbetalingArsak).map(name => (
         <CheckboxField
           name={name}
@@ -17,14 +22,16 @@ const VedtakRedusertUtbetalingArsaker = ({ readOnly, values, vedtakVarsel }) => 
           checked={readOnly ? vedtakVarsel.redusertUtbetalingÅrsaker?.some(key => key === name) : values.get(name)}
         />
       ))}
-    </div>
+    </CheckboxGruppe>
   );
 };
 
 VedtakRedusertUtbetalingArsaker.propTypes = {
+  intl: PropTypes.shape().isRequired,
   readOnly: PropTypes.bool.isRequired,
   values: PropTypes.instanceOf(Map),
   vedtakVarsel: vedtakVarselPropType,
+  erSendtInnUtenArsaker: PropTypes.bool.isRequired,
 };
 
 export default VedtakRedusertUtbetalingArsaker;
