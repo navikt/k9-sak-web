3364952268b59d131f1eb5db92e00accd5ce778a
Petter <petter.ruud@nav.no>
2020-09-24 09:10:45 +0200

    Legger til slik at uttak blir striplet hvis perioden er delvis innvilget (#957)
    * Legger til slik at uttak blir striplet hvis perioden er delvis innvilget
    
    * Fikser NP
    
    * Flytter types ut til fellespakke
    
    * Update package.json
    
    * Update ProsessStegUtledet.tsx
    
    * fikser imports i nøkkelcontainer

---
+++ packages/types/src/omsorgspenger/Uttaksperiode.ts
@@ -0,0 +1,40 @@
+import stringEnum from '../tsUtils';
+
+export const VilkårEnum = stringEnum({
+  OMSORGSVILKÅRET: 'OMSORGSVILKÅRET',
+  NOK_DAGER: 'NOK_DAGER',
+  INNGANGSVILKÅR: 'INNGANGSVILKÅR',
+  ALDERSVILKÅR_SØKER: 'ALDERSVILKÅR_SØKER',
+  ALDERSVILKÅR_BARN: 'ALDERSVILKÅR_BARN',
+  SMITTEVERN: 'SMITTEVERN',
+  UIDENTIFISERT_RAMMEVEDTAK: 'UIDENTIFISERT_RAMMEVEDTAK',
+  ARBEIDSFORHOLD: 'ARBEIDSFORHOLD',
+});
+
+export type Vilkår = typeof VilkårEnum[keyof typeof VilkårEnum];
+
+export const UtfallEnum = stringEnum({
+  INNVILGET: 'INNVILGET',
+  AVSLÅTT: 'AVSLÅTT',
+  UAVKLART: 'UAVKLART',
+});
+
+export type Utfalltype = typeof UtfallEnum[keyof typeof UtfallEnum];
+
+export type Map<Key extends string | number, Value> = {
+  [key in Key]?: Value;
+};
+
+export type VurderteVilkår = Map<Vilkår, Utfalltype>;
+
+export interface Uttaksperiode {
+  periode: string; // fom/tom
+  delvisFravær?: string; // Duration
+  utfall: Utfalltype;
+  utbetalingsgrad: number;
+  vurderteVilkår: {
+    vilkår: VurderteVilkår;
+  };
+  hjemler: string[];
+}
+export default Uttaksperiode;
