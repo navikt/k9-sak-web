d7ed72f492b71094424c534bcf9302eba424c929
Erlend Vige <erlend.edvardsen.vige@nav.no>
2020-10-08 10:45:22 +0200

    Remove unused parameters

--- packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
+++ packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
@@ -1,20 +1,18 @@
 import * as React from 'react';
 import { MicroFrontend } from '@fpsak-frontend/utils';
 
-const initializeMedisinskVilkår = (elementId, behandling, submitCallback) => {
-  (window as any).renderMedisinskVilkarApp(elementId, {
-    onSubmit: submitCallback,
-  });
+const initializeMedisinskVilkår = elementId => {
+  (window as any).renderMedisinskVilkarApp(elementId);
 };
 
 const medisinskVilkårAppID = 'medisinskVilkårApp';
-export default ({ behandling, submitCallback }) => {
+export default () => {
   return (
     <MicroFrontend
       id={medisinskVilkårAppID}
       jsSrc="https://medisinsk-vilkar-frontend.dev.adeo.no/1.0.0/app.js"
       jsIntegrity="sha384-Cx0ZWt3Nlv09Ony0GC7wFYGTqxg9B4U9ViGnCxMoI+eY6njwkccIi0FtBgLgSXBo"
-      onReady={() => initializeMedisinskVilkår(medisinskVilkårAppID, behandling, submitCallback)}
+      onReady={() => initializeMedisinskVilkår(medisinskVilkårAppID)}
       onError={() => {}}
     />
   );
