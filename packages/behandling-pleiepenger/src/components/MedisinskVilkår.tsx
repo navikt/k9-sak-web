64c6e669166ba42fdff4f350d11d24c59c4d8f00
Erlend Vige <erlend.edvardsen.vige@nav.no>
2020-10-22 16:41:19 +0200

    Add medisinsk vilkar microfrontend ingress to CSP

--- packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
+++ packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
@@ -10,8 +10,10 @@ export default () => {
   return (
     <MicroFrontend
       id={medisinskVilkårAppID}
-      jsSrc="https://medisinsk-vilkar-frontend.dev.adeo.no/1.0.2/app.js"
-      jsIntegrity="sha384-Cx0ZWt3Nlv09Ony0GC7wFYGTqxg9B4U9ViGnCxMoI+eY6njwkccIi0FtBgLgSXBo"
+      jsSrc="https://medisinsk-vilkar-frontend.dev.adeo.no/1.0.5/app.js"
+      jsIntegrity="sha384-0Pe12ZEVxOKf8QBu7ELWNNSBV3kOByyQVkGmRHRiwr6hW9IPS2iUQlKIWdCIVwos"
+      stylesheetSrc="https://medisinsk-vilkar-frontend.dev.adeo.no/1.0.5/styles.css"
+      stylesheetIntegrity="sha384-pcc0nZEnb08jMuxXkCc2evZ98N4xhD2Pusa5oUUOoKXCe0bCu/wtMCYAngdZXpqu"
       onReady={() => initializeMedisinskVilkår(medisinskVilkårAppID)}
       onError={() => {}}
     />
