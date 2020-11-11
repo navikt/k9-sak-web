8e0f858bf41e198722114ae2723a812b60706cf1
Erlend Vige <erlend.edvardsen.vige@nav.no>
2020-11-11 08:50:50 +0100

    bump medisinsk vilkår version (#1169)

--- packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
+++ packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
@@ -10,9 +10,9 @@ export default () => {
   return (
     <MicroFrontend
       id={medisinskVilkårAppID}
-      jsSrc="/k9/microfrontend/medisinsk-vilkar/1.2.7/app.js"
-      jsIntegrity="sha384-+fPzNRJWt649DQ2zeJ+o6BWAVcm3U8gc2Jh3sJPJmj/2oRPneGpT391x1fq7lOT1"
-      stylesheetSrc="/k9/microfrontend/medisinsk-vilkar/1.2.7/styles.css"
+      jsSrc="/k9/microfrontend/medisinsk-vilkar/1.2.9/app.js"
+      jsIntegrity="sha384-zTZOHy+icP1959vJmLfNltEiJ71a8PYaJFmG+uiUfCv3SET82YxEaeE2dHzAYiX3"
+      stylesheetSrc="/k9/microfrontend/medisinsk-vilkar/1.2.9/styles.css"
       stylesheetIntegrity="sha384-gG2QA/rYN+9l6GpiVzIZVspHGHD4c2vTW5ITdIUVBznVIGJqliuZfizW+Vk6Ltgq"
       onReady={() => initializeMedisinskVilkår(medisinskVilkårAppID)}
       onError={() => {}}
