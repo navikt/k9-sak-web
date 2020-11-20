1b166155fd43421d4de079d9202ee80e933cf113
Petter <petterruud@gmail.com>
2020-11-20 10:32:30 +0100

    Legger til skjermlenkekode for unntak

--- packages/behandlings-unntak/src/panelDefinisjoner/faktaPanelDefinisjoner.ts
+++ packages/behandlings-unntak/src/panelDefinisjoner/faktaPanelDefinisjoner.ts
@@ -1,14 +1,24 @@
 import ArbeidsforholdFaktaPanelDef from './faktaPaneler/ArbeidsforholdFaktaPanelDef';
+import VergeFaktaPanelDef from './faktaPaneler/VergeFaktaPanelDef';
 import MedlemskapsvilkaretFaktaPanelDef from './faktaPaneler/MedlemskapsvilkaretFaktaPanelDef';
 import OpptjeningsvilkaretFaktaPanelDef from './faktaPaneler/OpptjeningsvilkaretFaktaPanelDef';
+import BeregningFaktaPanelDef from './faktaPaneler/BeregningFaktaPanelDef';
 import FordelBeregningPanelDef from './faktaPaneler/FordelBeregningPanelDef';
+import UttakFaktaPanelDef from './faktaPaneler/UttakFaktaPanelDef';
+import BarnFaktaPanelDef from './faktaPaneler/BarnFaktaPanelDef';
+import NøkkeltallFaktaPanelDef from './faktaPaneler/NøkkeltallFaktaPanelDef';
 import InntektOgYtelserFaktaPanelDef from './faktaPaneler/InntektOgYtelserFaktaPanelDef';
 
 const faktaPanelDefinisjoner = [
   new ArbeidsforholdFaktaPanelDef(),
+  new VergeFaktaPanelDef(),
   new MedlemskapsvilkaretFaktaPanelDef(),
   new OpptjeningsvilkaretFaktaPanelDef(),
+  new UttakFaktaPanelDef(),
+  new BarnFaktaPanelDef(),
+  new BeregningFaktaPanelDef(),
   new FordelBeregningPanelDef(),
+  new NøkkeltallFaktaPanelDef(),
   new InntektOgYtelserFaktaPanelDef(),
 ];
 
