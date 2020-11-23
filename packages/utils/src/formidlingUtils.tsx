68977781dac530c5b209f196f128e1855d6898db
Qadeer Ahmad Khan <qadeer.ahmad.khan@nav.no>
2020-11-23 23:19:43 +0100

    Fjerner deprekert felt som er erstattet av eksternReferanse

--- packages/utils/src/formidlingUtils.tsx
+++ packages/utils/src/formidlingUtils.tsx
@@ -1,11 +1,11 @@
-import {Behandling, FagsakInfo} from '@k9-sak-web/types';
-import avsenderApplikasjon from "@fpsak-frontend/kodeverk/src/avsenderApplikasjon";
-import BehandlingType from "@fpsak-frontend/kodeverk/src/behandlingType";
-import fagsakYtelseType from "@fpsak-frontend/kodeverk/src/fagsakYtelseType";
-import ForhåndsvisRequest from "@k9-sak-web/types/src/formidlingTsType";
+import { Behandling, FagsakInfo } from '@k9-sak-web/types';
+import avsenderApplikasjon from '@fpsak-frontend/kodeverk/src/avsenderApplikasjon';
+import BehandlingType from '@fpsak-frontend/kodeverk/src/behandlingType';
+import fagsakYtelseType from '@fpsak-frontend/kodeverk/src/fagsakYtelseType';
+import ForhåndsvisRequest from '@k9-sak-web/types/src/formidlingTsType';
 
 function mapFagsakYtelseType(type: any): string {
-  return type.kode === fagsakYtelseType.OMSORGSPENGER ? "OMSORGSPENGER" : type.kode;
+  return type.kode === fagsakYtelseType.OMSORGSPENGER ? 'OMSORGSPENGER' : type.kode;
 }
 
 export function bestemAvsenderApp(type: string) {
@@ -13,17 +13,16 @@ export function bestemAvsenderApp(type: string) {
 }
 
 const lagForhåndsvisRequest = (behandling: Behandling, fagsak: FagsakInfo, data: any): ForhåndsvisRequest => {
-  const {dokumentMal, dokumentdata} = data
+  const { dokumentMal, dokumentdata } = data;
   return {
-    behandlingUuid: behandling.uuid,
     eksternReferanse: behandling.uuid,
     ytelseType: mapFagsakYtelseType(fagsak.fagsakYtelseType),
     saksnummer: fagsak.saksnummer,
     aktørId: fagsak.fagsakPerson.aktørId,
     avsenderApplikasjon: bestemAvsenderApp(behandling.type.kode),
     dokumentMal,
-    dokumentdata
-  }
-}
+    dokumentdata,
+  };
+};
 
-export default lagForhåndsvisRequest
+export default lagForhåndsvisRequest;
