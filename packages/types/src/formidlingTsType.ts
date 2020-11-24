d52b5b0bc679d7ebe9f203bf9f9d807897136d2c
Qadeer Ahmad Khan <qadeer.ahmad.khan@nav.no>
2020-11-24 11:30:03 +0100

    Fjerner ubrukt felt.

--- packages/types/src/formidlingTsType.ts
+++ packages/types/src/formidlingTsType.ts
@@ -1,14 +1,13 @@
 import Kodeverk from './kodeverkTsType';
 
 type ForhåndsvisRequest = Readonly<{
-  behandlingUuid?: string,
-  eksternReferanse: string,
-  ytelseType: any,
-  saksnummer: string,
-  aktørId: string,
-  avsenderApplikasjon: Kodeverk,
-  dokumentMal: Kodeverk
-  dokumentdata?: any
+  eksternReferanse: string;
+  ytelseType: any;
+  saksnummer: string;
+  aktørId: string;
+  avsenderApplikasjon: Kodeverk;
+  dokumentMal: Kodeverk;
+  dokumentdata?: any;
 }>;
 
 export default ForhåndsvisRequest;
