49cb6464cd0496388b817b3c39cbd3f6f0bc7e4e
Saikat Halder <45660476+haldersaikat@users.noreply.github.com>
2020-11-27 14:39:15 +0100

    PFP-9604 kopierte endringen fra fp-frontend.Nå historikkMalType4 kan vise spesiell tegn ordentlig (#1228)

--- packages/sak-historikk/src/components/maler/historikkMalType4.jsx
+++ packages/sak-historikk/src/components/maler/historikkMalType4.jsx
@@ -8,19 +8,22 @@ import BubbleText from './felles/bubbleText';
 
 const HistorikkMalType4 = ({ historikkinnslagDeler, getKodeverknavn }) => (
   <div>
-    {
-      historikkinnslagDeler.map((del, delIndex) => (
-        <div key={
+    {historikkinnslagDeler.map((del, delIndex) => (
+      <div
+        key={
           `del${delIndex}` // eslint-disable-line react/no-array-index-key
         }
-        >
-          <Element className="snakkeboble-panel__tekst">{findHendelseText(del.hendelse, getKodeverknavn)}</Element>
-          {del.aarsak && <Normaltekst>{getKodeverknavn(del.aarsak)}</Normaltekst>}
-          {del.begrunnelse && <BubbleText bodyText={getKodeverknavn(del.begrunnelse)} className="snakkeboble-panel__tekst" />}
-          {del.begrunnelseFritekst && <BubbleText bodyText={del.begrunnelseFritekst} className="snakkeboble-panel__tekst" />}
-        </div>
-      ))
-    }
+      >
+        <Element className="snakkeboble-panel__tekst">{findHendelseText(del.hendelse, getKodeverknavn)}</Element>
+        {del.aarsak && <Normaltekst>{getKodeverknavn(del.aarsak)}</Normaltekst>}
+        {del.begrunnelse && (
+          <BubbleText bodyText={getKodeverknavn(del.begrunnelse)} className="snakkeboble-panel__tekst" />
+        )}
+        {del.begrunnelseFritekst && (
+          <BubbleText bodyText={decodeHtmlEntity(del.begrunnelseFritekst)} className="snakkeboble-panel__tekst" />
+        )}
+      </div>
+    ))}
   </div>
 );
 
