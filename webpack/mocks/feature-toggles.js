5e7a738a973428fefd74933b1bb361c6e0428a9a
Magnus Bergman <magnus.bergman@nav.no>
2020-12-01 15:48:20 +0100

    TSF-1445 Fjerne 'aktiver-tilbakekrevingbehandling'

--- webpack/mocks/feature-toggles.js
+++ webpack/mocks/feature-toggles.js
@@ -4,7 +4,6 @@ const toggles = {
   featureToggles: {
     'fpsak.aksjonspunkt-marker-utenlandssak': false,
     'k9sak.simuler-oppdrag-varseltekst': false,
-    'k9sak.aktiver-tilbakekrevingbehandling': false,
     'k9sak.aktiver-klagebehandling': false,
     'k9sak.frontend.uttak.aksjonspunkt': false,
     'k9sak.aktiver-dokumentdata': false,
