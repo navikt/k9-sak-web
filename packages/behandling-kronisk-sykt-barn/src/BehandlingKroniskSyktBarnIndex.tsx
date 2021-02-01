5841f6da61978f1aee155846a5bed07ef56a7616
Tomas Engebretsen <tomas.engebretsen@nav.no>
2021-02-01 14:54:20 +0100

    Grunnlag for behandling av saker knyttet til kronisk sykt barn

---
+++ packages/behandling-kronisk-sykt-barn/src/BehandlingKroniskSyktBarnIndex.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react';
+
+interface OwnProps {}
+
+const BehandlingKroniskSyktBarnIndex: FunctionComponent<OwnProps> = ({}) => {
+
+  return <></>;
+};
+
+export default BehandlingKroniskSyktBarnIndex;
