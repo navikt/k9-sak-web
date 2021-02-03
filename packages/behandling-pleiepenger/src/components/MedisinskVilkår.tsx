0f268a05c61669d3b42f18b46e3b4b6bfc287ee4
Erlend Vige <erlend.edvardsen.vige@nav.no>
2021-02-03 10:22:49 +0100

    Microfrontend dev

--- packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
+++ packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import { MicroFrontend } from '@fpsak-frontend/utils';
+import { useRestApiErrorDispatcher } from '../../../rest-api-hooks';
+import EventType from '../../../rest-api/src/requestApi/eventType';
+import findEndpointsForMedisinskVilkårFrontend from '../microfrontend/utils/findEndpointsForMedisinskVilkårFrontend';
+import SimpleEndpoints from '../microfrontend/types/SimpleEndpoints';
 
-const initializeMedisinskVilkår = elementId => {
+const initializeMedisinskVilkår = (elementId, httpErrorHandler, endpoints: SimpleEndpoints, behandlingUuid: string) => {
   (window as any).renderMedisinskVilkarApp(elementId, {
     onVurderingValgt: vurdering => {
       if (vurdering !== null) {
@@ -11,19 +15,53 @@ const initializeMedisinskVilkår = elementId => {
       }
     },
     vurdering: new URLSearchParams(`?${window.location.hash.substr(1)}`).get('vurdering'),
+    httpErrorHandler,
+    endpoints,
+    behandlingUuid,
   });
 };
 
 const medisinskVilkårAppID = 'medisinskVilkårApp';
-export default () => {
+export default ({ behandling: { links, uuid } }) => {
+  const { addErrorMessage } = useRestApiErrorDispatcher();
+  const httpErrorHandler = (status: number, locationHeader?: string) => {
+    if (status === 403) {
+      addErrorMessage({ type: EventType.REQUEST_FORBIDDEN });
+    } else if (status === 401) {
+      if (locationHeader) {
+        const currentPath = encodeURIComponent(window.location.pathname + window.location.search);
+        window.location.href = `${locationHeader}?redirectTo=${currentPath}`;
+      } else {
+        addErrorMessage({ type: EventType.REQUEST_UNAUTHORIZED });
+      }
+    }
+  };
+
   return (
     <MicroFrontend
       id={medisinskVilkårAppID}
-      jsSrc="/k9/microfrontend/medisinsk-vilkar/1.4.2/app.js"
+      jsSrc="http://localhost:8081/main.js"
       jsIntegrity="sha384-iNIBcJsYevOG/6mMde96Zy76+n0IarHJehHRWuBmVxn6fGK5sHlm4fRVIeLyXp3S"
-      stylesheetSrc="/k9/microfrontend/medisinsk-vilkar/1.4.2/styles.css"
+      stylesheetSrc="http://localhost:8081/styles.css"
       stylesheetIntegrity="sha384-Ns3um5ypN0Dx4jWK7OT/rD9piP7up1qj4/bP3AYwhQtLHhc2SOMBTStumAyR0IXu"
-      onReady={() => initializeMedisinskVilkår(medisinskVilkårAppID)}
+      onReady={() =>
+        initializeMedisinskVilkår(
+          medisinskVilkårAppID,
+          httpErrorHandler,
+          findEndpointsForMedisinskVilkårFrontend(links, [
+            { rel: 'sykdom-vurdering-oversikt-ktp', desiredName: 'vurderingsoversiktKontinuerligTilsynOgPleie' },
+            { rel: 'sykdom-vurdering-oversikt-too', desiredName: 'vurderingsoversiktBehovForToOmsorgspersoner' },
+            { rel: 'sykdom-vurdering-direkte', desiredName: 'hentVurdering' },
+            { rel: 'sykdom-vurdering-opprettelse', desiredName: 'opprettVurdering' },
+            { rel: 'sykdom-vurdering-endring', desiredName: 'endreVurdering' },
+            { rel: 'sykdom-dokument-oversikt', desiredName: 'dokumentoversikt' },
+            { rel: 'sykdom-innleggelse-endring', desiredName: 'innleggelsesperioder'},
+            { rel: 'sykdom-innleggelse', desiredName: 'lagreInnleggelsesperioder'},
+            { rel: 'sykdom-diagnosekoder-endring', desiredName: 'diagnosekoder'}
+          ]),
+          uuid,
+        )
+      }
       onError={() => {}}
     />
   );
