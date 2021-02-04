167098190be669b0ee75b7cfcf987858899fa931
tor-nav <tor.roneid@nav.no>
2021-02-04 12:15:25 +0100

    FIX div

--- packages/sak-app/src/app/util/access.spec.ts
+++ packages/sak-app/src/app/util/access.spec.ts
@@ -130,6 +130,7 @@ describe('access', () => {
 
     forEachFagsakAndBehandlingStatus((fagsakStatus, behandlingStatus) => {
       const expected = validFagsakStatuser.includes(fagsakStatus) && validBehandlingStatuser.includes(behandlingStatus);
+
       // eslint-disable-next-line jest/valid-title
       it(getTestName('tilgang til å overstyre', expected, fagsakStatus, behandlingStatus), () => {
         const access = kanOverstyreAccess(
