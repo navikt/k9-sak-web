ef2b887e6b7b89000f5c0b62a6ec6a3dad770b97
eras-dev <emma.rasmusson@nav.no>
2021-02-12 16:37:19 +0100

    Lagt til Delt bostedoversettelse, ikon for hjem og DeltBosted komponent

---
+++ packages/fakta-barn-og-overfoeringsdager/src/components/DeltBosted/DeltBosted.tsx
@@ -0,0 +1,76 @@
+import React, { FunctionComponent } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { Element, Normaltekst } from 'nav-frontend-typografi';
+import Panel from 'nav-frontend-paneler';
+import styles from './deltBosted.less';
+import Barn from '../../types/Barn';
+import formaterDato from '../utils';
+
+interface BarnInputProps {
+  barnet: Barn;
+  index: number;
+}
+
+const DeltBosted: FunctionComponent<BarnInputProps> = ({ barnet, index }) => {
+  const { aleneomsorg, kroniskSykdom, fosterbarn, utenlandskBarn, fødselsnummer } = barnet;
+
+  return (
+    <Panel border className={styles.barnInput}>
+      <div className={styles.header}>
+        <h4>
+          <FormattedMessage id="FaktaRammevedtak.BarnVisningNummer" values={{ nummer: index + 1 }} />
+        </h4>
+        <span className={styles.italic}>{fødselsnummer}</span>
+      </div>
+      {(aleneomsorg || kroniskSykdom || fosterbarn || utenlandskBarn) && (
+        <div className={styles.rammevedtak}>
+          <div>
+            <Element>
+              <FormattedMessage id="FaktaRammevedtak.Barn.Rammevedtak" />
+            </Element>
+            {kroniskSykdom && (
+              <Normaltekst>
+                <FormattedMessage id="FaktaRammevedtak.Barn.UtvidetRett" />
+              </Normaltekst>
+            )}
+            {aleneomsorg && (
+              <Normaltekst>
+                <FormattedMessage id="FaktaRammevedtak.Barn.Aleneomsorg" />
+              </Normaltekst>
+            )}
+            {fosterbarn && (
+              <Normaltekst>
+                <FormattedMessage id="FaktaRammevedtak.Barn.Fosterbarn" />
+              </Normaltekst>
+            )}
+            {utenlandskBarn && (
+              <Normaltekst>
+                <FormattedMessage id="FaktaRammevedtak.Barn.UtenlandskBarn" />
+              </Normaltekst>
+            )}
+          </div>
+          <div>
+            <Element>
+              <FormattedMessage id="FaktaRammevedtak.Barn.FOM" />
+            </Element>
+            {kroniskSykdom && <Normaltekst>{formaterDato(kroniskSykdom.fom)}</Normaltekst>}
+            {aleneomsorg && <Normaltekst>{formaterDato(aleneomsorg.fom)}</Normaltekst>}
+            {fosterbarn && <Normaltekst>{formaterDato(fosterbarn.fom)}</Normaltekst>}
+            {utenlandskBarn && <Normaltekst>{formaterDato(utenlandskBarn.fom)}</Normaltekst>}
+          </div>
+          <div>
+            <Element>
+              <FormattedMessage id="FaktaRammevedtak.Barn.TOM" />
+            </Element>
+            {kroniskSykdom && <Normaltekst>{formaterDato(kroniskSykdom.tom)}</Normaltekst>}
+            {aleneomsorg && <Normaltekst>{formaterDato(aleneomsorg.tom)}</Normaltekst>}
+            {fosterbarn && <Normaltekst>{formaterDato(fosterbarn.tom)}</Normaltekst>}
+            {utenlandskBarn && <Normaltekst>{formaterDato(utenlandskBarn.tom)}</Normaltekst>}
+          </div>
+        </div>
+      )}
+    </Panel>
+  );
+};
+
+export default DeltBosted;
