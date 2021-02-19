1f917c4ab3fe33fa5afd224864e1df33e8d98612
Erlend Vige <erlend.edvardsen.vige@nav.no>
2021-02-19 10:38:11 +0100

    Bump version of psb uttak microfrontend

--- packages/behandling-pleiepenger/src/components/Uttak.tsx
+++ packages/behandling-pleiepenger/src/components/Uttak.tsx
@@ -17,9 +17,9 @@ const uttakAppID = 'uttakApp';
 export default ({ uuid, uttaksperioder }: UttakProps) => (
   <MicroFrontend
     id={uttakAppID}
-    jsSrc="https://psb-uttak-frontend.dev.adeo.no/1.0.2/app.js"
-    jsIntegrity="sha384-vac17jZlI7vGTGemPYieQO/Eg7P5QxyVYIyBuQMamhlJPxFncX/plPfLQgo11/zx"
-    stylesheetSrc="https://psb-uttak-frontend.dev.adeo.no/1.0.2/styles.css"
+    jsSrc="https://psb-uttak-frontend.dev.adeo.no/1.0.3/app.js"
+    jsIntegrity="sha384-hxzgxGbIY1bpkc3y5/e35ShHYIMPDGjsu4Zm1iuwT3q8ihhPGwE2FHNbp6nOFOoo"
+    stylesheetSrc="https://psb-uttak-frontend.dev.adeo.no/1.0.3/styles.css"
     stylesheetIntegrity="sha384-7Azm1w5ns5abNtQPahMeU8MZHrMNVwrX7yL4TmjT31DeqH1/sYzR6XGJSWB2lA+8"
     onReady={() => initializeUttak(uttakAppID, uttaksperioder, uuid)}
   />
