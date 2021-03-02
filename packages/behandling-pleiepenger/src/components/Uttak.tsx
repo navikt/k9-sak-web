45b6829b7f2aad08ab127c1c680b0cc369664663
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2021-03-02 08:54:42 +0100

    ny versjon av uttak microfrontend (#1592)

--- packages/behandling-pleiepenger/src/components/Uttak.tsx
+++ packages/behandling-pleiepenger/src/components/Uttak.tsx
@@ -17,9 +17,9 @@ const uttakAppID = 'uttakApp';
 export default ({ uuid, uttaksperioder }: UttakProps) => (
   <MicroFrontend
     id={uttakAppID}
-    jsSrc="/k9/microfrontend/psb-uttak/1.0.7/app.js"
-    jsIntegrity="sha384-gFrPjxJvpki3sC+sMCesTXUB5KCi28j7RdmaQJn7WlgjNohIJ0XpMbnUnIpDvPFY"
-    stylesheetSrc="/k9/microfrontend/psb-uttak/1.0.7/styles.css"
+    jsSrc="/k9/microfrontend/psb-uttak/1.0.8/app.js"
+    jsIntegrity="sha384-lD7nv0amb1/R2nxutp+jnlG8ipI5I+Nf2Rj7R1w3C3BLWnay1TePe4yKtpJWZj+k"
+    stylesheetSrc="/k9/microfrontend/psb-uttak/1.0.8/styles.css"
     stylesheetIntegrity="sha384-swIF7lzCWHWd+i8uFRiEeuBvScZUV4ze6R32bzl6S+L8Gy25SW8zpmaF2CuRbTIG"
     onReady={() => initializeUttak(uttakAppID, uttaksperioder, uuid)}
   />
