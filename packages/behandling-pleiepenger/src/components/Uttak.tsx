3a1eac70a7dc33d3f7dfb97396853d0f3fd2b97f
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2021-03-03 14:25:21 +0100

    ny versjon av uttak (#1613)

--- packages/behandling-pleiepenger/src/components/Uttak.tsx
+++ packages/behandling-pleiepenger/src/components/Uttak.tsx
@@ -17,10 +17,10 @@ const uttakAppID = 'uttakApp';
 export default ({ uuid, uttaksperioder }: UttakProps) => (
   <MicroFrontend
     id={uttakAppID}
-    jsSrc="/k9/microfrontend/psb-uttak/1.0.8/app.js"
-    jsIntegrity="sha384-lD7nv0amb1/R2nxutp+jnlG8ipI5I+Nf2Rj7R1w3C3BLWnay1TePe4yKtpJWZj+k"
-    stylesheetSrc="/k9/microfrontend/psb-uttak/1.0.8/styles.css"
-    stylesheetIntegrity="sha384-swIF7lzCWHWd+i8uFRiEeuBvScZUV4ze6R32bzl6S+L8Gy25SW8zpmaF2CuRbTIG"
+    jsSrc="/k9/microfrontend/psb-uttak/1.0.9/app.js"
+    jsIntegrity="sha384-h1K5E6zjfVetyJO9mEViqmGvld6FvhDEHke7BDCZcdR+vzzMAE/K641Bylv7o+jQ"
+    stylesheetSrc="/k9/microfrontend/psb-uttak/1.0.9/styles.css"
+    stylesheetIntegrity="sha384-XwcNDA26KHmidkAIWeaxDvZdYiQ/jchQaPPdwGDDYGM7UK/a8kxb+eNNuNkei7vs"
     onReady={() => initializeUttak(uttakAppID, uttaksperioder, uuid)}
   />
 );
