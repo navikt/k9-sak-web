b11c118d95d4b18eac2c205e7314485a0c42585d
eras-dev <emma.rasmusson@nav.no>
2021-03-12 09:55:49 +0100

    namnbyte for import med

--- packages/behandling-utvidet-rett/src/panelDefinisjoner/prosessStegPaneler/VedtakProsessStegPanelDef.tsx
+++ packages/behandling-utvidet-rett/src/panelDefinisjoner/prosessStegPaneler/VedtakProsessStegPanelDef.tsx
@@ -3,7 +3,7 @@ import VedtakProsessIndex from '@fpsak-frontend/prosess-vedtak';
 import { prosessStegCodes } from '@k9-sak-web/konstanter';
 import aksjonspunktCodes from '@fpsak-frontend/kodeverk/src/aksjonspunktCodes';
 import { ProsessStegDef, ProsessStegPanelDef } from '@k9-sak-web/behandling-felles';
-import findStatusForVedtak from '@fpsak-frontend/utils/src/finnStatusForVedtak';
+import findStatusForVedtak from '@fpsak-frontend/utils/src/findStatusForVedtak';
 import { UtvidetRettBehandlingApiKeys } from '../../data/utvidetRettBehandlingApi';
 
 class PanelDef extends ProsessStegPanelDef {
