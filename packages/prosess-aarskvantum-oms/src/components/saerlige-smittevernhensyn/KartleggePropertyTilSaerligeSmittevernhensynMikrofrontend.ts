53e1651b3e15504357afbd241c1f4edb121b92af
eras-dev <emma.rasmusson@nav.no>
2021-03-24 15:01:41 +0100

    Lagt til mikrofrontend for uttak

---
+++ packages/prosess-aarskvantum-oms/src/components/saerlige-smittevernhensyn/KartleggePropertyTilSaerligeSmittevernhensynMikrofrontend.ts
@@ -0,0 +1,46 @@
+import aksjonspunktCodes from '@fpsak-frontend/kodeverk/src/aksjonspunktCodes';
+import behandlingStatus from '@fpsak-frontend/kodeverk/src/behandlingStatus';
+import { Aksjonspunkt } from '@k9-sak-web/types';
+import MikrofrontendKomponenter from './types/MikrofrontendKomponenter';
+import { SaerligSmittevernhensynProps } from './types/SaerligSmittevernhensynProps';
+
+const KartleggePropertyTilSaerligeSmittevernhensynMikrofrontend = (
+  submitCallback,
+  behandling,
+  aksjonspunkter: Aksjonspunkt[],
+  isAksjonspunktOpen,
+) => {
+  let objektTilMikrofrontend = {};
+  const aksjonspunkt = aksjonspunkter[0];
+  console.log(aksjonspunkter);
+
+  if (aksjonspunkt && aksjonspunkt.definisjon.kode === aksjonspunktCodes.VURDER_ÅRSKVANTUM_DOK) {
+    const aksjonspunktLost = behandling.status.kode === behandlingStatus.BEHANDLING_UTREDES && !isAksjonspunktOpen;
+
+    objektTilMikrofrontend = {
+      visKomponent: MikrofrontendKomponenter.KORRIGERE_PERIODER,
+      props: {
+        aksjonspunktLost,
+        lesemodus: !isAksjonspunktOpen,
+        årsakFraSoknad: 'Årsak',
+        informasjonTilLesemodus: {
+          begrunnelse: aksjonspunkt.begrunnelse,
+          vilkarOppfylt: true,
+        },
+        losAksjonspunkt: (fravaerGrunnetSmittevernhensynEllerStengt, begrunnelse) => {
+          submitCallback([
+            {
+              kode: aksjonspunkt.definisjon.kode,
+              innvilgePeriodene: fravaerGrunnetSmittevernhensynEllerStengt,
+              begrunnelse,
+              fortsettBehandling: true,
+            },
+          ]);
+        },
+      } as SaerligSmittevernhensynProps,
+    };
+  }
+  return objektTilMikrofrontend;
+};
+
+export default KartleggePropertyTilSaerligeSmittevernhensynMikrofrontend;
