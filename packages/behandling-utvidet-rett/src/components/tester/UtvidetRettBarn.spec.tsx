a92b396aa031f0e6838b59884d4d069671bdf69a
eras-dev <emma.rasmusson@nav.no>
2021-04-09 16:47:00 +0200

    Oppdatert tester

--- packages/behandling-utvidet-rett/src/components/tester/UtvidetRettBarn.spec.tsx
+++ packages/behandling-utvidet-rett/src/components/tester/UtvidetRettBarn.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallowWithIntl } from '@fpsak-frontend/utils-test/src/intl-enzyme-test-helper';
 import FagsakYtelseType from '@fpsak-frontend/kodeverk/src/fagsakYtelseType';
-import Seksjon from '@k9-sak-web/fakta-barn-og-overfoeringsdager/src/components/Seksjon';
+import FaktaBarnIndex from '@k9-sak-web/fakta-barn-oms';
 import UtvidetRettBarnFakta from '../UtvidetRettBarnFakta/UtvidetRettBarnFakta';
 
 describe('<UtvidetRettBarnFakta>', () => {
@@ -12,38 +12,12 @@ describe('<UtvidetRettBarnFakta>', () => {
         barnSoktFor: [],
       },
       fagsaksType: FagsakYtelseType.OMSORGSPENGER_MIDLERTIDIG_ALENE,
+      rammevedtak: [],
     };
 
     const wrapper = shallowWithIntl(<UtvidetRettBarnFakta {...objektTilKomponent} />);
 
-    const elementerMedTypeOchText = (nodeType, tekst) =>
-      wrapper.findWhere(node => node.name() === nodeType && node.text() === tekst);
-
-    const overskrift = wrapper.find(Seksjon).prop('title').id;
-    const fodselsnummer = elementerMedTypeOchText('span', objektTilKomponent.personopplysninger.barn[0].fnr);
-
-    expect(overskrift).toBe('UtvidetRett.Barn.MidlertidigAlene.Titel');
-    expect(fodselsnummer).toHaveLength(1);
-  });
-
-  it('skal rendre barn faktapanel korrekt for kronisk sykt barn', () => {
-    const objektTilKomponent = {
-      personopplysninger: {
-        barn: [],
-        barnSoktFor: [{ fnr: '101112' }],
-      },
-      fagsaksType: FagsakYtelseType.OMSORGSPENGER_KRONISK_SYKT_BARN,
-    };
-
-    const wrapper = shallowWithIntl(<UtvidetRettBarnFakta {...objektTilKomponent} />);
-
-    const elementerMedTypeOchText = (nodeType, tekst) =>
-      wrapper.findWhere(node => node.name() === nodeType && node.text() === tekst);
-
-    const overskrift = wrapper.find(Seksjon).prop('title').id;
-    const fodselsnummer = elementerMedTypeOchText('span', objektTilKomponent.personopplysninger.barnSoktFor[0].fnr);
-
-    expect(overskrift).toBe('UtvidetRett.Barn.KroniskSyktBarn.Titel');
-    expect(fodselsnummer).toHaveLength(1);
+    const faktaBarnIndex = wrapper.find(FaktaBarnIndex);
+    expect(faktaBarnIndex).toHaveLength(1);
   });
 });
