3eba57a370052c8dd4fa7a8b8ccff6bcedae95ad
Hallvard Andreas Stark <hallvard@scel.to>
2021-04-27 08:10:33 +0200

    henter statuskode fra enum

--- packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
+++ packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { useRestApiErrorDispatcher } from '@k9-sak-web/rest-api-hooks';
 import { MicroFrontend } from '@fpsak-frontend/utils';
 import aksjonspunktCodes from '@fpsak-frontend/kodeverk/src/aksjonspunktCodes';
+import aksjonspunktStatus from '@fpsak-frontend/kodeverk/src/aksjonspunktStatus';
 import findEndpointsForMicrofrontend from '../microfrontend/utils/findEndpointsForMicrofrontend';
 import SimpleEndpoints from '../microfrontend/types/SimpleEndpoints';
 import findAksjonspunkt from '../microfrontend/utils/findAksjonspunkt';
@@ -35,7 +36,7 @@ export default ({ behandling: { links, uuid }, submitCallback, aksjonspunkter, r
   const medisinskVilkårAksjonspunkt = findAksjonspunkt(aksjonspunkter, aksjonspunktCodes.MEDISINSK_VILKAAR);
   const medisinskVilkårAksjonspunktkode = medisinskVilkårAksjonspunkt?.definisjon.kode;
   const medisinskVilkårAksjonspunktstatus = medisinskVilkårAksjonspunkt?.status.kode;
-  const visFortsettknapp = medisinskVilkårAksjonspunktstatus === 'OPPR';
+  const visFortsettknapp = medisinskVilkårAksjonspunktstatus === aksjonspunktStatus.OPPRETTET;
 
   const løsAksjonspunkt = () =>
     submitCallback([{ kode: medisinskVilkårAksjonspunktkode, begrunnelse: 'Sykdom er behandlet' }]);
