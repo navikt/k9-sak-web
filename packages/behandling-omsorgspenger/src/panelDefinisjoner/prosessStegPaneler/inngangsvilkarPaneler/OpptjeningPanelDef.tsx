2861b5faa333de8c5468dd121f9d8ffcada957de
Magnus Bergman <magnus.bergman@nav.no>
2021-04-27 12:47:50 +0200

    Endre rekkefølge på inngangsvilkår i oms

---
+++ packages/behandling-omsorgspenger/src/panelDefinisjoner/prosessStegPaneler/inngangsvilkarPaneler/OpptjeningPanelDef.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import vilkarType from '@fpsak-frontend/kodeverk/src/vilkarType';
+import OpptjeningVilkarProsessIndex from '@fpsak-frontend/prosess-vilkar-opptjening-oms';
+import aksjonspunktCodes from '@fpsak-frontend/kodeverk/src/aksjonspunktCodes';
+import { ProsessStegPanelDef, ProsessStegOverstyringPanelDef } from '@k9-sak-web/behandling-felles';
+
+import { OmsorgspengerBehandlingApiKeys } from '../../../data/omsorgspengerBehandlingApi';
+
+class OpptjeningPanelDef extends ProsessStegPanelDef {
+  getId = () => 'OPPTJENING';
+
+  getTekstKode = () => 'Behandlingspunkt.Opptjening';
+
+  getKomponent = props => <OpptjeningVilkarProsessIndex {...props} />;
+
+  getAksjonspunktKoder = () => [aksjonspunktCodes.VURDER_OPPTJENINGSVILKARET];
+
+  getVilkarKoder = () => [vilkarType.OPPTJENINGSVILKARET];
+
+  getEndepunkter = () => [OmsorgspengerBehandlingApiKeys.OPPTJENING];
+
+  getData = ({ vilkarForSteg }) => ({
+    lovReferanse: vilkarForSteg[0].lovReferanse,
+  });
+
+  getOverstyringspanelDef = () =>
+    new ProsessStegOverstyringPanelDef(this, aksjonspunktCodes.OVERSTYRING_AV_OPPTJENINGSVILKARET);
+}
+
+export default OpptjeningPanelDef;
