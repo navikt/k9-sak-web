60f3b531ec201c34f5f415858e0a758ea1b8e928
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2021-04-28 14:38:59 +0200

    ny versjon av sykdom og uttak (#1899)

--- packages/behandling-pleiepenger/src/components/Uttak.tsx
+++ packages/behandling-pleiepenger/src/components/Uttak.tsx
@@ -17,9 +17,9 @@ const uttakAppID = 'uttakApp';
 export default ({ uuid, uttaksperioder }: UttakProps) => (
   <MicroFrontend
     id={uttakAppID}
-    jsSrc="/k9/microfrontend/psb-uttak/1.0.14/app.js"
-    jsIntegrity="sha384-BMiXZSYFHgzqDbj4WMpxeZ2MWPQqJgWnIc+Ba0YHgET4sI7wsho8HHGsKr4uWDcW"
-    stylesheetSrc="/k9/microfrontend/psb-uttak/1.0.14/styles.css"
+    jsSrc="/k9/microfrontend/psb-uttak/1.0.15/app.js"
+    jsIntegrity="sha384-cluofH7W6hyb4mayIPJApd16omPLYMZnqYkLl+R8oJJGPE7lNgHCYD1J8bSJeX+w"
+    stylesheetSrc="/k9/microfrontend/psb-uttak/1.0.15/styles.css"
     stylesheetIntegrity="sha384-hhKDiE6Kpc5DT489hk2o+rS12mO9j2aMSUcKliD8r8M6p9rtu5FlvT7P6b7wEWUd"
     onReady={() => initializeUttak(uttakAppID, uttaksperioder, uuid)}
   />
