c0a7e717ece51646ef9055b20ca6a816c884d96c
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2021-04-30 10:54:33 +0200

    Update packages/utils/src/formidlingUtils.tsx

--- packages/utils/src/formidlingUtils.tsx
+++ packages/utils/src/formidlingUtils.tsx
@@ -39,7 +39,7 @@ export function lagVisningsnavnForMottaker(
 }
 
 function vedtaksbrevmaler(tilgjengeligeVedtaksbrev: TilgjengeligeVedtaksbrev) {
-  return tilgjengeligeVedtaksbrev ? Object.keys(tilgjengeligeVedtaksbrev?.vedtaksbrevmaler) : [];
+  return tilgjengeligeVedtaksbrev?.vedtaksbrevmaler ? Object.keys(tilgjengeligeVedtaksbrev.vedtaksbrevmaler) : [];
 }
 
 export function finnesTilgjengeligeVedtaksbrev(tilgjengeligeVedtaksbrev: TilgjengeligeVedtaksbrev): boolean {
