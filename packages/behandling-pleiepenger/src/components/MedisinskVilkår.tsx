584881e8ff09576ed09a09bf54ddea9f1a6606e2
Hallvard Andreas Stark <hallvard@scel.to>
2021-05-10 12:30:30 +0200

    ny versjon av sykdom

--- packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
+++ packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
@@ -48,9 +48,9 @@ export default ({ behandling: { links, uuid }, submitCallback, aksjonspunkter, r
   return (
     <MicroFrontend
       id={medisinskVilkårAppID}
-      jsSrc="/k9/microfrontend/medisinsk-vilkar/1.8.2/app.js"
-      jsIntegrity="sha384-vPthqRtUr/18p8WgsKz0s3KXk2G3e7/vQCrjLx4LnicmRFOtq+zL2dh+DoNvH2tZ"
-      stylesheetSrc="/k9/microfrontend/medisinsk-vilkar/1.8.2/styles.css"
+      jsSrc="/k9/microfrontend/medisinsk-vilkar/1.8.3/app.js"
+      jsIntegrity="sha384-OanLmcQPdiO8baJeLHCZ5eEkhRRWVuWNfl/gOqoq0LfTd49HvDfvzwi5JPwTmdFu"
+      stylesheetSrc="/k9/microfrontend/medisinsk-vilkar/1.8.3/styles.css"
       stylesheetIntegrity="sha384-jgynT0LSuHE36k4/mSrYA4w/mr7aHL+DZvguC9Ct0YEhbYu29QCHk3urP3/XcBpp"
       onReady={() =>
         initializeMedisinskVilkår(
