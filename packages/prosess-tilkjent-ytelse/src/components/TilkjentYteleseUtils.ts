464eb3760e7cde6d270b2e7d5d281e499e1d0be5
Hallvard Andreas Stark <hallvard@scel.to>
2021-05-20 14:48:48 +0200

    viser arbeidsgivers navn i tab dersom tilgjengelig

--- packages/prosess-tilkjent-ytelse/src/components/TilkjentYteleseUtils.ts
+++ packages/prosess-tilkjent-ytelse/src/components/TilkjentYteleseUtils.ts
@@ -11,6 +11,11 @@ const getEndCharFromId = id => (id ? `...${id.substring(id.length - 4, id.length
 export const createVisningsnavnForAndel = (andel, getKodeverknavn, arbeidsgiverOpplysningerPerId) => {
   if (!andel) return '';
 
+  const arbeidsgiver = arbeidsgiverOpplysningerPerId[andel.arbeidsgiverOrgnr];
+  if (arbeidsgiver) {
+    return arbeidsgiver.navn;
+  }
+
   if (!andel.arbeidsgiver || !(andel.arbeidsgiver.arbeidsgiverOrgnr || andel.arbeidsgiver.identifikator)) {
     return andel.aktivitetStatus ? getKodeverknavn(andel.aktivitetStatus) : '';
   }
