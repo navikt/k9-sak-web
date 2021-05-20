898c3db64ba9930c3da96751d0936caf955f67b6
eras-dev <emma.rasmusson@nav.no>
2021-05-20 15:45:23 +0200

    Ny versjon

--- packages/behandling-utvidet-rett/src/panelDefinisjoner/prosessStegPaneler/utvidetRettPanel/utvidetRettMikrofrontend/UtvidetRettMikrofrontend.tsx
+++ packages/behandling-utvidet-rett/src/panelDefinisjoner/prosessStegPaneler/utvidetRettPanel/utvidetRettMikrofrontend/UtvidetRettMikrofrontend.tsx
@@ -29,9 +29,9 @@ const hentVersjonInformasjon = () => {
     stylesheetIntegrity: 'sha384-LC4FE5IBLroddA6Ew0fDNUxK+oapnpHA8pFrMSZ7Q67tIbZTe8hn8P/ktKJRojwr',
   };
   const preprodVersjon = {
-    versjon: '1.5.40',
-    jsIntegrity: 'sha384-dcM4G69O4KLmvKOBXwvUV0K0zfTFaJNo27mC8L4VaCTyalpuzO4IKy5KuuLIxQ3U',
-    stylesheetIntegrity: 'sha384-LC4FE5IBLroddA6Ew0fDNUxK+oapnpHA8pFrMSZ7Q67tIbZTe8hn8P/ktKJRojwr',
+    versjon: '1.5.42',
+    jsIntegrity: 'sha384-kEbTBWQyc6dzyhXgAPDf18waJ89qi+knpAUGpSSWrtxTmNRJ88lNDq2VGkKecfTO',
+    stylesheetIntegrity: 'sha384-z3CWkmyvqam9PcRhhOUowe+0T/tRb3QTAjg2skXgGVkthjCXvN0/XDxmBwQVzDyz',
   };
   return sjekkHvisErIProduksjon() ? produksjonsVersjon : preprodVersjon;
 };
