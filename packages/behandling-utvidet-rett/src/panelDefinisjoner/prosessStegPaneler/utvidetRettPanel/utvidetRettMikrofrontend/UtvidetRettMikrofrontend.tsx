65d157384e43ae8b869fe5940bd9fea80532af8d
eras-dev <emma.rasmusson@nav.no>
2021-06-21 15:51:28 +0200

    Ny versjon av utvidet rett

--- packages/behandling-utvidet-rett/src/panelDefinisjoner/prosessStegPaneler/utvidetRettPanel/utvidetRettMikrofrontend/UtvidetRettMikrofrontend.tsx
+++ packages/behandling-utvidet-rett/src/panelDefinisjoner/prosessStegPaneler/utvidetRettPanel/utvidetRettMikrofrontend/UtvidetRettMikrofrontend.tsx
@@ -28,8 +28,8 @@ const hentVersjonInformasjon = () => {
     stylesheetIntegrity: 'sha384-3iUtet323prriMT769mdhUmWxrtoD2sTbqMwOZV0tKNwjCvRz+tNgmCtOq2Poocv',
   };
   const preprodVersjon = {
-    versjon: '2.0.5',
-    jsIntegrity: 'sha384-i+oKtHuy2bCkvqa9QJSxqOB805ca9Vi5H84KF6pKSuADH80RR2QdlLS43uZWK6TX',
+    versjon: '2.0.6',
+    jsIntegrity: 'sha384-/hMQ3cBTBSD1W8UFKndpCYgu+VSlhaqMkGqjXJ+QNKMqNtY0r+TCHl2T2/4ezGMs',
     stylesheetIntegrity: 'sha384-zrAzgRd84XQvBESFpgMsjglo0FQbW2KE2+3r0qio1lA6y5G/CHD8JWz938HuirHQ',
   };
   return sjekkHvisErIProduksjon() ? produksjonsVersjon : preprodVersjon;
