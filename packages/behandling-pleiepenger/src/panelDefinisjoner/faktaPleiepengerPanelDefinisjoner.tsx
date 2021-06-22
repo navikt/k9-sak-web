53907eb7223505082c56a8dd61380deebe6121f6
Hallvard Andreas Stark <hallvard@scel.to>
2021-06-22 14:33:46 +0200

    microfrontend-oppsett for om barnet

--- packages/behandling-pleiepenger/src/panelDefinisjoner/faktaPleiepengerPanelDefinisjoner.tsx
+++ packages/behandling-pleiepenger/src/panelDefinisjoner/faktaPleiepengerPanelDefinisjoner.tsx
@@ -7,8 +7,10 @@ import OmsorgenForFaktaPanelDef from './faktaPaneler/OmsorgenForFaktaPanelDef';
 import InntektsmeldingFaktaPanelDef from './faktaPaneler/InntektsmeldingFaktaPanelDef';
 import EtablertTilsynFaktaPanelDef from './faktaPaneler/EtablertTilsynFaktaPanelDef';
 import FordelBeregningPanelDef from './faktaPaneler/FordelBeregningPanelDef';
+import OmBarnetFaktaPanelDef from './faktaPaneler/OmBarnetFaktaPanelDef';
 
 const faktaPanelDefinisjoner = [
+  new OmBarnetFaktaPanelDef(),
   new ArbeidsforholdFaktaPanelDef(),
   new OmsorgenForFaktaPanelDef(),
   new MedisinskVilkarFaktaPanelDef2(),
