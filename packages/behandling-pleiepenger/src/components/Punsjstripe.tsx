7421acfde14e3dbba3e34b876dbf4ba82ad38e9b
Hallvard Andreas Stark <hallvard@scel.to>
2021-08-11 14:47:02 +0200

    henter liste med åpne oppgaver i punsj proxy-instillinger

---
+++ packages/behandling-pleiepenger/src/components/Punsjstripe.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
+import axios from 'axios';
+
+const Punsjstripe = ({ aktørId }) => {
+  const [punsjoppgaver, setPunsjoppgaver] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get(`/k9-punsj/journalpost/uferdig/${aktørId}`)
+      .then(({ data }) => {
+        console.log(data);
+        setPunsjoppgaver(data);
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  if (punsjoppgaver?.length === 0) {
+    return null;
+  }
+  return <AlertStripeAdvarsel>{`Du har ${punsjoppgaver.length} uløste oppgaver i Punsj`}</AlertStripeAdvarsel>;
+};
+export default Punsjstripe;
