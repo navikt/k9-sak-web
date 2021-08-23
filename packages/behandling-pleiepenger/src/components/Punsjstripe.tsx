570ae141e01cbdc487b176d39c1ea781d0d6346e
Hallvard Andreas Stark <hallvard@scel.to>
2021-08-23 12:18:43 +0200

    prøver punsjstripe på nytt

--- packages/behandling-pleiepenger/src/components/Punsjstripe.tsx
+++ packages/behandling-pleiepenger/src/components/Punsjstripe.tsx
@@ -1,20 +1,21 @@
-import * as React from 'react';
-import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
 import axios from 'axios';
+import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
+import * as React from 'react';
 
 const Punsjstripe = ({ aktørId }) => {
   const [punsjoppgaver, setPunsjoppgaver] = React.useState([]);
+  const [error, setError] = React.useState(null);
 
   React.useEffect(() => {
     axios
-      .get(`/k9-punsj/journalpost/uferdig/${aktørId}`)
+      .get(`/k9/sak/api/punsj/journalpost/uferdig?aktoerId=${aktørId}`)
       .then(({ data }) => {
         setPunsjoppgaver(data);
       })
-      .catch();
+      .catch(err => setError(err));
   }, []);
 
-  if (!punsjoppgaver || punsjoppgaver?.length === 0) {
+  if (!punsjoppgaver || punsjoppgaver?.length === 0 || error) {
     return null;
   }
   return <AlertStripeAdvarsel>{`Du har ${punsjoppgaver?.length} uløste oppgaver i Punsj`}</AlertStripeAdvarsel>;
