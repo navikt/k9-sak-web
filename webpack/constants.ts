6dcc6fcf0568e73cad98abdab2447890cd2c9a50
Magnus Bergman <magnus.bergman@nav.no>
2021-08-25 10:05:56 +0200

    Rydde opp webpack config, fikse react-refresh/hmr

---
+++ webpack/constants.ts
@@ -0,0 +1,2 @@
+export const IS_DEV = JSON.stringify(process.env.NODE_ENV) === '"development"';
+export const PUBLIC_PATH = '/k9/web/';
---
+++ webpack/webpack.dev.ts
@@ -0,0 +1,36 @@
+import webpack from 'webpack';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+
+import { PUBLIC_ROOT, APP_DIR } from './paths';
+
+import module from './common/module';
+import resolve from './common/resolve';
+import externals from './common/externals';
+import cache from './common/cache';
+import plugins from './common/plugins';
+import devServer from './common/devServer';
+
+import { PUBLIC_PATH } from './constants';
+
+export default {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: [APP_DIR + '/index.tsx'],
+  output: {
+    path: PUBLIC_ROOT,
+    publicPath: PUBLIC_PATH,
+    filename: '[name].js',
+  },
+  module,
+  resolve,
+  externals,
+  cache,
+  plugins: [...plugins, new ReactRefreshWebpackPlugin()],
+  optimization: {
+    moduleIds: 'named',
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  devServer,
+};
