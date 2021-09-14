5f8c6b1d762dd6becec6c2e9c0ee9fe62c37be9d
Magnus Bergman <magnus.bergman@nav.no>
2021-09-14 13:36:23 +0200

    Oppdatere tester

--- packages/prosess-aarskvantum-oms/src/components/utils.spec.ts
+++ packages/prosess-aarskvantum-oms/src/components/utils.spec.ts
@@ -15,15 +15,15 @@ it('finner ut om en periode er i smittevernsperioden', () => {
   const slutterFørsteDag = '2020-03-10/2020-04-20';
   expect(periodeErISmittevernsperioden(slutterFørsteDag)).toBe(true);
 
-  const slutterSisteDag = '2020-12-10/2020-12-31';
+  const slutterSisteDag = '2020-09-10/2020-09-30';
   expect(periodeErISmittevernsperioden(slutterSisteDag)).toBe(true);
 
-  const slutterDagenEtter = '2020-12-10/2021-01-01';
+  const slutterDagenEtter = '2021-09-10/2021-10-01';
   expect(periodeErISmittevernsperioden(slutterDagenEtter)).toBe(true);
 
-  const kunDagenEtter = '2021-01-01/2021-01-01';
+  const kunDagenEtter = '2021-10-01/2021-10-01';
   expect(periodeErISmittevernsperioden(kunDagenEtter)).toBe(false);
 
-  const begynnerDagenEtter = '2021-01-01/2021-01-12';
+  const begynnerDagenEtter = '2021-10-01/2021-10-12';
   expect(periodeErISmittevernsperioden(begynnerDagenEtter)).toBe(false);
 });
