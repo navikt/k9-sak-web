ea3f360d39988db90b7f96674bd9fb7b18ee3e56
vebnor <vebjorn.nordby@nav.no>
2021-11-23 19:33:01 +0100

    legger til tekster i historikk

--- packages/sak-historikk/src/kodeverk/historikkEndretFeltVerdiTypeCodes.ts
+++ packages/sak-historikk/src/kodeverk/historikkEndretFeltVerdiTypeCodes.ts
@@ -857,6 +857,14 @@ const historikkEndretFeltVerdiTypeCodes = {
     kode: 'INGEN_INNVIRKNING',
     verdiId: 'HistorikkEndretFeltVerdiType.IngenInnvirkning',
   },
+  FORTSETT: {
+    kode: 'FORTSETT',
+    verdiId: 'HistorikkEndretFeltVerdiType.Fortsett',
+  },
+  MANGLER_GRUNNLAG: {
+    kode: 'MANGLER_GRUNNLAG',
+    verdiId: 'HistorikkEndretFeltVerdiType.MangerGrunnlag',
+  },
 };
 
 export default historikkEndretFeltVerdiTypeCodes;
