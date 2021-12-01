e2e17fa7e666974c93520e9f0a4224bae42bdfbd
eras-dev <76997730+eras-dev@users.noreply.github.com>
2021-12-01 10:44:28 +0100

    viser alltid sidemeny med perioder for inngangsvilkår (#2845)
    * viser alltid sidemeny for opptjening
    
    * fjernet for söknadsfrist og alderpanel med
    
    * oppdatert testerna

--- packages/prosess-vilkar-overstyring/src/VilkarresultatMedOverstyringProsessIndex.spec.tsx
+++ packages/prosess-vilkar-overstyring/src/VilkarresultatMedOverstyringProsessIndex.spec.tsx
@@ -8,7 +8,7 @@ import { Behandling } from '@k9-sak-web/types';
 import VilkarresultatMedOverstyringProsessIndex from './VilkarresultatMedOverstyringProsessIndex';
 
 describe('<VilkarresultatMedOverstyringForm>', () => {
-  it('skal ikke rendre tabs dersom bare en periode', () => {
+  it('skal rendre tabs dersom bare en periode', () => {
     const wrapper = shallow(
       <VilkarresultatMedOverstyringProsessIndex
         behandling={
@@ -64,7 +64,7 @@ describe('<VilkarresultatMedOverstyringForm>', () => {
     );
 
     const tabs = wrapper.find(SideMenu);
-    expect(tabs).toHaveLength(0);
+    expect(tabs).toHaveLength(1);
   });
 
   it('skal rendre tabs dersom mer enn en periode', () => {
