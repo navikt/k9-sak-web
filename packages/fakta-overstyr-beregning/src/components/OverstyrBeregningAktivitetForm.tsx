375eb5a3d49917c6936398a9239e8be4149f03e6
Thomas Hamre Wiberg <thomas.wiberg@nav.no>
2021-12-09 15:03:33 +0100

    Diverse fiks i overstyr input for beregning skjema

--- packages/fakta-overstyr-beregning/src/components/OverstyrBeregningAktivitetForm.tsx
+++ packages/fakta-overstyr-beregning/src/components/OverstyrBeregningAktivitetForm.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import { injectIntl, WrappedComponentProps } from 'react-intl';
 
 import { TableColumn, TableRow } from '@fpsak-frontend/shared-components';
-import { Field } from "formik";
+import { parseCurrencyInput } from '@fpsak-frontend/utils';
+import { Field, useFormikContext } from "formik";
 import { Input } from "nav-frontend-skjema";
 import styles from './OverstyrBeregningFaktaForm.less';
+import { OverstyrInputForBeregningDto } from "../types/OverstyrInputForBeregningDto";
 
 interface Props {
     key: string;
     periodeIndex: number;
     aktivitetIndex: number;
     firmaNavn: string;
+    readOnly: boolean;
 }
 
 const OverstyrBeregningAktivitetForm: React.FC<Props & WrappedComponentProps> = ({
@@ -18,39 +21,58 @@ const OverstyrBeregningAktivitetForm: React.FC<Props & WrappedComponentProps> = 
     periodeIndex,
     aktivitetIndex,
     firmaNavn,
+    readOnly,
     intl
-}) => (
-    <TableRow key={key}>
-        <TableColumn>
-            <span className={styles.firmaNavn}>
-                {firmaNavn}
-            </span>
-        </TableColumn>
-        <TableColumn>
-            <Field name={`perioder.${periodeIndex}.aktivitetliste.${aktivitetIndex}.inntektPrAar`}>
-                {({ field, meta }) => <Input
-                    id={`perioder-${periodeIndex}-aktivitetliste-${aktivitetIndex}-inntekt`}
-                    type="number"
-                    placeholder={intl.formatMessage({ id: 'OverstyrInputForm.InntektPrAar' })}
-                    feil={meta.touched && meta.error ? meta.error : false}
-                    value={field.value}
-                    {...field}
-                />}
-            </Field>
-        </TableColumn>
-        <TableColumn>
-            <Field name={`perioder.${periodeIndex}.aktivitetliste.${aktivitetIndex}.refusjonPrAar`}>
-                {({ field, meta }) => <Input
-                    id={`perioder-${periodeIndex}-aktivitetliste-${aktivitetIndex}-refusjon`}
-                    type="number"
-                    placeholder={intl.formatMessage({ id: 'OverstyrInputForm.RefusjonPrAar' })}
-                    feil={meta.touched && meta.error ? meta.error : false}
-                    value={field.value}
-                    {...field} />}
-            </Field>
-        </TableColumn>
-    </TableRow>
-)
+}) => {
+    const { setFieldValue } = useFormikContext<OverstyrInputForBeregningDto>();
+    return (
+        <TableRow key={key}>
+            <TableColumn>
+                <span className={styles.firmaNavn}>
+                    {firmaNavn}
+                </span>
+            </TableColumn>
+            <TableColumn>
+                <Field name={`perioder.${periodeIndex}.aktivitetliste.${aktivitetIndex}.inntektPrAar`}>
+                    {({ field, meta }) => (
+                        <Input
+                            {...field}
+                            id={`perioder-${periodeIndex}-aktivitetliste-${aktivitetIndex}-inntekt`}
+                            type="text"
+                            placeholder={intl.formatMessage({ id: 'OverstyrInputForm.InntektPrAar' })}
+                            onChange={(e) => {
+                                const tallverdi: number = parseInt(e.target.value.replace(/[^0-9]/g, ''), 10);
+                                setFieldValue(field.name, tallverdi);
+                            }}
+                            maxLength={10}
+                            value={parseCurrencyInput(field.value)}
+                            feil={meta.touched && meta.error ? meta.error : false}
+                            disabled={readOnly}
+                        />
+                    )}
+                </Field>
+            </TableColumn>
+            <TableColumn>
+                <Field name={`perioder.${periodeIndex}.aktivitetliste.${aktivitetIndex}.refusjonPrAar`}>
+                    {({ field, meta }) => (<Input
+                        {...field}
+                        id={`perioder-${periodeIndex}-aktivitetliste-${aktivitetIndex}-refusjon`}
+                        type="text"
+                        placeholder={intl.formatMessage({ id: 'OverstyrInputForm.RefusjonPrAar' })}
+                        onChange={(e) => {
+                            const tallverdi: number = parseInt(e.target.value.replace(/[^0-9]/g, ''), 10);
+                            setFieldValue(field.name, tallverdi);
+                        }}
+                        maxLength={10}
+                        value={parseCurrencyInput(field.value)}
+                        feil={meta.touched && meta.error ? meta.error : false}
+                        disabled={readOnly}
+                    />)}
+                </Field>
+            </TableColumn>
+        </TableRow>
+    )
+};
 
 export default injectIntl(OverstyrBeregningAktivitetForm);
 
