cbd263a41e80c4bec5ccaf2616099f77f82aaba6
eras-dev <emma.rasmusson@nav.no>
2021-12-22 08:50:07 +0100

    Legger til slik att ap vises som åpen dersom vilkårsperioden vurderes i behandlingen og om informasjon fra form dersom perioden skal vurderes

--- packages/prosess-vilkar-opptjening-oms/src/components/OpptjeningVilkarAksjonspunktPanel.tsx
+++ packages/prosess-vilkar-opptjening-oms/src/components/OpptjeningVilkarAksjonspunktPanel.tsx
@@ -103,7 +103,7 @@ export const OpptjeningVilkarAksjonspunktPanelImpl = ({
   return (
     <ProsessPanelTemplate
       title={intl.formatMessage({ id: 'OpptjeningVilkarAksjonspunktPanel.Opptjeningsvilkaret' })}
-      isAksjonspunktOpen={isApOpen}
+      isAksjonspunktOpen={isApOpen && vilkårPerioder[periodeIndex].vurdersIBehandlingen}
       formName={formProps.form}
       handleSubmit={formProps.handleSubmit}
       isDirty={dirty}
@@ -134,7 +134,7 @@ export const OpptjeningVilkarAksjonspunktPanelImpl = ({
 
       <VilkarFields
         erOmsorgspenger={erOmsorgspenger}
-        erVilkarOk={erVilkarOk}
+        erVilkarOk={vilkårPerioder[periodeIndex].vurdersIBehandlingen ? erVilkarOk : originalErVilkarOk}
         readOnly={readOnly || !vilkårPerioder[periodeIndex].vurdersIBehandlingen}
         fieldPrefix={`vilkarFields[${periodeIndex}]`}
       />
