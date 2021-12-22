4c521d35c3dd8d6f879db261c55d4190c5a0daf4
eras-dev <76997730+eras-dev@users.noreply.github.com>
2021-12-22 10:13:23 +0100

    Revert/midlertidig revert slik att prodsetting ikke blir blokkert under ferie (#2955)
    * Revert "Fix/opptjening perioder handtere automatiskt håndterte perioder (#2953)"
    
    This reverts commit 78aab8ebde67b445a8b8af8e73794110c821a860.
    
    * Revert "Sjekker bara om begrunnelse finns dersom vilkarField er relevant I Be… (#2943)"
    
    This reverts commit b33e46034f2b90696f693991101bfb36976296a8.

--- packages/prosess-vilkar-opptjening-oms/src/components/OpptjeningVilkarAksjonspunktPanel.tsx
+++ packages/prosess-vilkar-opptjening-oms/src/components/OpptjeningVilkarAksjonspunktPanel.tsx
@@ -23,7 +23,6 @@ interface VilkårField {
   erVilkarOk: string | boolean;
   begrunnelse: string;
   innvilgelseMerknadKode: string;
-  vurdersIBehandlingen: boolean;
 }
 
 interface OpptjeningVilkarAksjonspunktPanelImplProps {
@@ -45,11 +44,6 @@ interface OpptjeningVilkarAksjonspunktPanelImplProps {
 
 interface StateProps {
   erVilkarOk: string | boolean;
-}
-
-const hentErVilkarOK = (aksjonspunkter: Aksjonspunkt[], vilkårPerioder: Vilkarperiode[], periodeIndex: number, status: string) => {
-  const isOpenAksjonspunkt = aksjonspunkter.some(ap => isAksjonspunktOpen(ap.status.kode));
-  return isOpenAksjonspunkt && vilkårPerioder[periodeIndex].vurdersIBehandlingen ? undefined : vilkarUtfallType.OPPFYLT === status;
 }
 
 /**
@@ -87,7 +81,7 @@ export const OpptjeningVilkarAksjonspunktPanelImpl = ({
   const isFormComplete = () => {
     const isAllTabsCreated = Array.isArray(vilkårPerioder) && vilkårPerioder.length === vilkarFields?.length;
     return isAllTabsCreated
-      ? !vilkarFields.some(vilkarField => vilkarField.vurdersIBehandlingen && (!vilkarField.begrunnelse || vilkarField.erVilkarOk === undefined))
+      ? !vilkarFields.some(vilkarField => !vilkarField.begrunnelse || vilkarField.erVilkarOk === undefined)
       : false;
   };
 
@@ -98,12 +92,13 @@ export const OpptjeningVilkarAksjonspunktPanelImpl = ({
     fagsakType === FagsakYtelseType.OMSORGSPENGER_MIDLERTIDIG_ALENE;
 
   const erPleiepenger = fagsakType === FagsakYtelseType.PLEIEPENGER;
-  const originalErVilkarOk = hentErVilkarOK(aksjonspunkter, vilkårPerioder, periodeIndex, status);
+  const isOpenAksjonspunkt = aksjonspunkter.some(ap => isAksjonspunktOpen(ap.status.kode));
+  const originalErVilkarOk = isOpenAksjonspunkt ? undefined : vilkarUtfallType.OPPFYLT === status;
 
   return (
     <ProsessPanelTemplate
       title={intl.formatMessage({ id: 'OpptjeningVilkarAksjonspunktPanel.Opptjeningsvilkaret' })}
-      isAksjonspunktOpen={isApOpen && vilkårPerioder[periodeIndex].vurdersIBehandlingen}
+      isAksjonspunktOpen={isApOpen}
       formName={formProps.form}
       handleSubmit={formProps.handleSubmit}
       isDirty={dirty}
@@ -134,7 +129,7 @@ export const OpptjeningVilkarAksjonspunktPanelImpl = ({
 
       <VilkarFields
         erOmsorgspenger={erOmsorgspenger}
-        erVilkarOk={vilkårPerioder[periodeIndex].vurdersIBehandlingen ? erVilkarOk : originalErVilkarOk}
+        erVilkarOk={erVilkarOk}
         readOnly={readOnly || !vilkårPerioder[periodeIndex].vurdersIBehandlingen}
         fieldPrefix={`vilkarFields[${periodeIndex}]`}
       />
@@ -147,10 +142,9 @@ export const buildInitialValues = createSelector(
     (ownProps: OpptjeningVilkarAksjonspunktPanelImplProps) => ownProps.aksjonspunkter,
     (ownProps: OpptjeningVilkarAksjonspunktPanelImplProps) => ownProps.vilkårPerioder,
     (ownProps: OpptjeningVilkarAksjonspunktPanelImplProps) => ownProps.status,
-    (ownProps: OpptjeningVilkarAksjonspunktPanelImplProps) => ownProps.periodeIndex,
   ],
-  (aksjonspunkter, vilkårPerioder, status, periodeIndex) => ({
-    ...VilkarFields.buildInitialValues(aksjonspunkter, vilkårPerioder, hentErVilkarOK(aksjonspunkter, vilkårPerioder, periodeIndex, status)),
+  (aksjonspunkter, vilkårPerioder, status) => ({
+    ...VilkarFields.buildInitialValues(aksjonspunkter, vilkårPerioder, status),
     ...ProsessStegBegrunnelseTextField.buildInitialValues(aksjonspunkter),
   }),
 );
