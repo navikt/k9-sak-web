7620f1a6cb1770de27e68d2bdcb8a92df476665b
eras-dev <emma.rasmusson@nav.no>
2022-01-10 14:21:57 +0100

    fjernet kommentar

--- packages/utils/src/micro-frontends/sjekkHvisErIProduksjon.ts
+++ packages/utils/src/micro-frontends/sjekkHvisErIProduksjon.ts
@@ -5,9 +5,6 @@ const sjekkHvisErIProduksjon = () => {
 export default sjekkHvisErIProduksjon;
 
 export const hentURLTilILivetsSluttfase = () => {
-  // eslint-disable-next-line
-  console.log(process.env.PLEIEPENGER_I_LIVETS_SLUTTFASE_URL);
-
   if (process.env.NODE_ENV === 'development') {
     return 'http://localhost:8182';
   }
