361855767a31d62fc4f052cd059405dd20e15673
vebnor <vebjorn.nordby@nav.no>
2022-01-13 12:53:26 +0100

    kommentar

--- packages/mocks/handlers.js
+++ packages/mocks/handlers.js
@@ -1,7 +1,11 @@
 /* eslint-disable import/no-mutable-exports */
 import { testHandlers } from './testHandlers';
 
-// eslint-disable-next-line import/prefer-default-export
+// Alle handlers som ligger direkte i dette arrayet vil gjelde
+// selv om k9-sak-web startes uten env spesielle env-variabler.
+// Requesten treffer handlerne i stedet for eventuelle eksisterende APIer
+// f.eks hvis vi har handlere til alle APIene vi bruker her, vil vi aldri treffe den faktiske backenden når vi kjører opp lokalt.
+// Derfor burde nok ting kun legges i dette arrayet midlertidig
 let handlers = [];
 
 if (process.env.MSW_MODE === 'test') {
