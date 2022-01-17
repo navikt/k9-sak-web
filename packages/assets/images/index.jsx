4f6ba9251d2f00904eff9478f172c5f59404ab2f
Hallvard Andreas Stark <hallvard@scel.to>
2022-01-17 10:09:59 +0100

    Fjernet ubrukt kode

--- packages/assets/images/index.jsx
+++ packages/assets/images/index.jsx
@@ -97,6 +97,5 @@ export { default as zoomIn } from './zoom_in.svg';
 export { default as zoomInFilled } from './zoom_in_filled.svg';
 export { default as zoomOut } from './zoom_out.svg';
 export { default as zoomOutFilled } from './zoom_out_filled.svg';
-export { default as bin } from './bin.svg';
 export { default as chevronBlueRight } from './chevron_blue_right.svg';
 export { default as calendar2 } from './calendar-2.svg';
