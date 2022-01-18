d98c872f113f05b7049f31bda967c82c339971c7
Thomas Hamre Wiberg <thomas.wiberg@nav.no>
2022-01-18 18:46:34 +0100

    fix Storybook

--- packages/storybook/decorators/withReduxAndRouter.tsx
+++ packages/storybook/decorators/withReduxAndRouter.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
 
 import configureStore from '@k9-sak-web/sak-app/src/configureStore';
-
-const history = createBrowserHistory({
-  basename: '/k9/web/',
-});
 
 const withReduxAndRouterProvider = Story => {
   const store = configureStore();
