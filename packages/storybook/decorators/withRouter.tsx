ba301d61bc0e953e08dd32918c56842ab3b0d3f3
Thomas Hamre Wiberg <thomas.wiberg@nav.no>
2022-01-18 18:46:34 +0100

    fix Storybook

--- packages/storybook/decorators/withRouter.tsx
+++ packages/storybook/decorators/withRouter.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-
-const history = createBrowserHistory({
-  basename: '/k9/web/',
-});
 
 const withRouterProvider = Story => (
   <MemoryRouter>
