d06b72973ede3d27f545042a3d2a901ea62b2847
vebnor <vebjorn.nordby@nav.no>
2022-01-21 09:26:33 +0100

    Revert "skrevet flere tester for behandlingvelger (#3038)"
    This reverts commit dfee75212ceb90ced563ccaba8522e8ddee7194b.

--- packages/sak-behandling-velger/src/components/BehandlingPicker.tsx
+++ packages/sak-behandling-velger/src/components/BehandlingPicker.tsx
@@ -71,7 +71,7 @@ const renderListItems = ({
       return activeFilters.includes(behandling.type.kode);
     })
     .map(behandling => (
-      <li data-testid="behandling" key={behandling.id}>
+      <li key={behandling.id}>
         <NavLink
           onClick={() => setValgtBehandlingId(behandling.id)}
           className={styles.linkToBehandling}
@@ -207,7 +207,7 @@ const BehandlingPicker = ({
           </div>
           <ul className={styles.behandlingList}>
             {noExistingBehandlinger && (
-              <Normaltekst data-testid="ingenBehandlinger">
+              <Normaltekst>
                 <FormattedMessage id="BehandlingList.ZeroBehandlinger" />
               </Normaltekst>
             )}
