8bd0835bcb6fd2bc495925f0067690d9d3cf9c23
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2022-01-27 09:41:38 +0100

    Revert "Revert behandlingsvelger 2 (#3064)"
    This reverts commit 59a9c294933a803292927c6e5dc58bdaafba7280.

---
+++ packages/types/src/behandlingPerioderType.ts
@@ -0,0 +1,21 @@
+import Periode from './periodeTsType';
+import PerioderMedAarsak from './PerioderMedAarsak';
+export default interface BehandlingPerioderType {
+  perioderTilVurdering: Periode[];
+  perioderMedÅrsak: PerioderMedAarsak[];
+  dokumenterTilBehandling: DokumenterTilBehandling[];
+}
+
+interface DokumenterTilBehandling {
+  journalpostId: string;
+  innsendingsTidspunkt: string;
+  type: string;
+  søktePerioder: SoktePerioder[];
+}
+
+interface SoktePerioder {
+  periode: Periode;
+  type: null;
+  arbeidsgiver: null;
+  arbeidsforholdRef: null;
+}
