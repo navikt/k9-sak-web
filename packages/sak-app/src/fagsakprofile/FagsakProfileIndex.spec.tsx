b1d3cfa1914bfcdead2087f28728b65bb4be945a
Hallvard Andreas Stark <hallvard@scel.to>
2022-01-31 09:38:46 +0100

    Revert "Revert "Revert behandlingsvelger 2 (#3064)" (#3067)"
    This reverts commit 9242c5fa7c5f748bab1ed7ed0f527373431c4542.

--- packages/sak-app/src/fagsakprofile/FagsakProfileIndex.spec.tsx
+++ packages/sak-app/src/fagsakprofile/FagsakProfileIndex.spec.tsx
@@ -85,6 +85,7 @@ describe('<FagsakProfileIndex>', () => {
 
     const behandlingVelger = fagsakProfile.renderProp('renderBehandlingVelger')().find(BehandlingVelgerSakIndex);
     expect(behandlingVelger).toHaveLength(1);
+    expect(behandlingVelger.prop('showAll')).toBe(true);
   });
 
   it('skal ikke vise alle behandlinger når behandling er valgt', () => {
@@ -111,5 +112,6 @@ describe('<FagsakProfileIndex>', () => {
 
     const behandlingVelger = fagsakProfile.renderProp('renderBehandlingVelger')().find(BehandlingVelgerSakIndex);
     expect(behandlingVelger).toHaveLength(1);
+    expect(behandlingVelger.prop('showAll')).toBe(false);
   });
 });
