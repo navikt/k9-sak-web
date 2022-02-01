d6fb1582f39c97aa81df9592d9cbef1ec0e5207a
eras-dev <emma.rasmusson@nav.no>
2022-02-01 14:12:09 +0100

    reverserar bug

--- packages/utils/src/hentAktivePerioderFraVilkar.ts
+++ packages/utils/src/hentAktivePerioderFraVilkar.ts
@@ -3,6 +3,7 @@ import {Vilkar} from "@k9-sak-web/types";
 const hentAktivePerioderFraVilkar = (vilkar: Vilkar[], visAllePerioder: boolean) => {
   const [activeVilkår] = vilkar;
  return activeVilkår.perioder.filter(periode => (visAllePerioder && !periode.vurdersIBehandlingen)
+    || (periode.vurdersIBehandlingen && activeVilkår.perioder.length === 1)
     || (periode.vurdersIBehandlingen && !visAllePerioder));
 };
 
