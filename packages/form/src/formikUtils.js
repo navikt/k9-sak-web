a9b7d650d71541c6c90e96e3ee88559142c348fd
vebnor <vebjorn.nordby@nav.no>
2022-02-04 12:19:49 +0100

    kommentar

--- packages/form/src/formikUtils.js
+++ packages/form/src/formikUtils.js
@@ -1,6 +1,7 @@
+// tar inn array med valideringsfunksjoner og validerer verdien
 // eslint-disable-next-line import/prefer-default-export
-export const validateAll = (validations, value) => {
-  const errorList = validations.map(func => func(value)).find(Boolean);
+export const validateAll = (valideringsfunksjoner, value) => {
+  const errorList = valideringsfunksjoner.map(func => func(value)).find(Boolean);
 
   if (Array.isArray(errorList) && errorList.length) {
     return errorList[0];
