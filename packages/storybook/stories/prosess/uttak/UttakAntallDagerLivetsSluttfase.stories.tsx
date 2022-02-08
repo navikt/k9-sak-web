d86791042774a3dc7ff6e4f9c1f9ec3f3f4d0637
eras-dev <emma.rasmusson@nav.no>
2022-02-08 15:17:41 +0100

    storybook

---
+++ packages/storybook/stories/prosess/uttak/UttakAntallDagerLivetsSluttfase.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { withKnobs } from '@storybook/addon-knobs';
+import AntallDagerLivetsSluttfaseIndex from '@k9-sak-web/prosess-uttak-antall-dager-sluttfase/src/AntallDagerLivetsSluttfaseIndex';
+
+export default {
+  title: 'prosess/antall-dager-livets-sluttfase',
+  component: AntallDagerLivetsSluttfaseIndex,
+  decorators: [withKnobs],
+};
+
+export const antallDagerLivetsSluttfaseIndex = () => <AntallDagerLivetsSluttfaseIndex maxAntallDager={60} antallDagerInnvilgetForPleietrengendeHittil={40} sistePleiedag="2021.01.02" />;
