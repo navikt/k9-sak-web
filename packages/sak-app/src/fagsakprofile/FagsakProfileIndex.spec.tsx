c54d5538786ca75980d56a3e026223616d3c44c3
Thomas H. Wiberg <thomas@thawi.no>
2022-02-11 09:13:18 +0100

    debug github actions test

--- packages/sak-app/src/fagsakprofile/FagsakProfileIndex.spec.tsx
+++ packages/sak-app/src/fagsakprofile/FagsakProfileIndex.spec.tsx
@@ -212,6 +212,7 @@ describe('<FagsakProfileIndex>', () => {
     expect(screen.queryByText('Se alle behandlinger')).toBeInTheDocument();
     expect(screen.queryAllByText('Avslått').length).toBe(2);
     expect(screen.queryByText('Opprettet:')).toBeInTheDocument();
+    console.log(await screen.debug());
     expect(screen.queryByText('31.01.2020')).toBeInTheDocument();
   });
 });
