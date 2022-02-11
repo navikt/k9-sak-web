82fc4d68d198e517a8efc27d35c3c4f5ea796ee2
Thomas H. Wiberg <thomas@thawi.no>
2022-02-11 22:21:08 +0100

    linting

--- packages/types/src/behandlingPerioderType.ts
+++ packages/types/src/behandlingPerioderType.ts
@@ -1,5 +1,6 @@
 import Periode from './periodeTsType';
 import PerioderMedAarsak from './PerioderMedAarsak';
+
 export default interface BehandlingPerioderType {
   perioderTilVurdering: Periode[];
   perioderMedÅrsak: PerioderMedAarsak[];
