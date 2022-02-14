ca6d55159a648e1dd9789dab1391430c04ad9ba6
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2022-02-14 10:21:19 +0100

    Revert behandlingsvelger 4 (#3157)
    * Revert "Filtrerer ut alle behandlinger som har perioder med årsak (#3151)"
    
    This reverts commit fc040d6d674064e0ae139122c7ee78f78ccba288.
    
    * Revert "Legger inne behandlingsvelger igjen (#3137)"
    
    This reverts commit a59091ff808d88caef7f39f8a36e851228428bf6.

--- packages/sak-behandling-velger/src/BehandlingVelgerSakIndex.tsx
+++ packages/sak-behandling-velger/src/BehandlingVelgerSakIndex.tsx
@@ -1,11 +1,11 @@
-import { BehandlingAppKontekst, Kodeverk, KodeverkMedNavn, Fagsak } from '@k9-sak-web/types';
-import { Location } from 'history';
 import React from 'react';
 import { createIntl, createIntlCache, RawIntlProvider } from 'react-intl';
-import fagsakYtelseType from '@fpsak-frontend/kodeverk/src/fagsakYtelseType';
-import messages from '../i18n/nb_NO.json';
+import { Location } from 'history';
+
+import { BehandlingAppKontekst, KodeverkMedNavn, Kodeverk } from '@k9-sak-web/types';
+
 import BehandlingPicker from './components/BehandlingPicker';
-import BehandlingPickerOld from './components/BehandlingPickerOld';
+import messages from '../i18n/nb_NO.json';
 
 const cache = createIntlCache();
 
@@ -22,42 +22,30 @@ interface OwnProps {
   getBehandlingLocation: (behandlingId: number) => Location;
   noExistingBehandlinger: boolean;
   behandlingId?: number;
-  getKodeverkFn: (kodeverk: Kodeverk, behandlingType?: Kodeverk) => KodeverkMedNavn;
   showAll: boolean;
   toggleShowAll: () => void;
-  fagsak: Fagsak;
+  getKodeverkFn: (kodeverk: Kodeverk, behandlingType?: Kodeverk) => KodeverkMedNavn;
 }
 
 const BehandlingVelgerSakIndex = ({
   behandlinger,
   getBehandlingLocation,
   noExistingBehandlinger,
-  getKodeverkFn,
   behandlingId,
   showAll,
   toggleShowAll,
-  fagsak,
+  getKodeverkFn,
 }: OwnProps) => (
   <RawIntlProvider value={intl}>
-    {fagsak.sakstype.kode === fagsakYtelseType.FRISINN ? (
-      <BehandlingPickerOld
-        behandlinger={behandlinger}
-        getBehandlingLocation={getBehandlingLocation}
-        noExistingBehandlinger={noExistingBehandlinger}
-        behandlingId={behandlingId}
-        showAll={showAll}
-        toggleShowAll={toggleShowAll}
-        getKodeverkFn={getKodeverkFn}
-      />
-    ) : (
-      <BehandlingPicker
-        behandlinger={behandlinger}
-        getBehandlingLocation={getBehandlingLocation}
-        noExistingBehandlinger={noExistingBehandlinger}
-        getKodeverkFn={getKodeverkFn}
-        behandlingId={behandlingId}
-      />
-    )}
+    <BehandlingPicker
+      behandlinger={behandlinger}
+      getBehandlingLocation={getBehandlingLocation}
+      noExistingBehandlinger={noExistingBehandlinger}
+      behandlingId={behandlingId}
+      showAll={showAll}
+      toggleShowAll={toggleShowAll}
+      getKodeverkFn={getKodeverkFn}
+    />
   </RawIntlProvider>
 );
 
