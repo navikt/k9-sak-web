5e6ba808502b39b82e9305ac02ae03e92652eb42
eras-dev <emma.rasmusson@nav.no>
2022-02-15 13:03:40 +0100

    Revert "Revert revert undertrykking av brev (#3163)"
    This reverts commit 3de4e47e79abaae1feefc6547ba631b0a7b3d129.

--- packages/prosess-vedtak/src/components/VedtakFritekstPanel.jsx
+++ packages/prosess-vedtak/src/components/VedtakFritekstPanel.jsx
@@ -1,33 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Column, Row } from 'nav-frontend-grid';
+import { Undertekst } from 'nav-frontend-typografi';
 
 import { VerticalSpacer } from '@fpsak-frontend/shared-components';
-import { decodeHtmlEntity, getLanguageFromSprakkode, hasValidText, maxLength, minLength } from '@fpsak-frontend/utils';
-import TextAreaFormik from '../../../form/src/TextAreaFormik';
+import { TextAreaField } from '@fpsak-frontend/form';
+import {
+  decodeHtmlEntity,
+  getLanguageCodeFromSprakkode,
+  hasValidText,
+  maxLength,
+  minLength,
+  requiredIfNotPristine,
+} from '@fpsak-frontend/utils';
 
 import styles from './vedtakAvslagPanel.less';
 
 const maxLength100000 = maxLength(100000);
 const minLength3 = minLength(3);
 
-const VedtakFritekstPanelImpl = ({ begrunnelse, begrunnelseFieldName, sprakkode, readOnly, label, intl }) => (
+const VedtakFritekstPanelImpl = ({ begrunnelse, begrunnelseFieldName, sprakkode, readOnly, label }) => (
   <>
     {!readOnly && (
       <Row>
         <VerticalSpacer sixteenPx />
-        <Column xs="12">
-          <TextAreaFormik
+        <Column xs="8">
+          <TextAreaField
             name={begrunnelseFieldName}
             label={label}
-            validate={[minLength3, maxLength100000, hasValidText]}
+            validate={[requiredIfNotPristine, minLength3, maxLength100000, hasValidText]}
             maxLength={100000}
             readOnly={readOnly}
             badges={[
               {
                 type: 'fokus',
-                text: getLanguageFromSprakkode(sprakkode),
-                title: intl.formatMessage({ id: 'Malform.Beskrivelse' }),
+                textId: getLanguageCodeFromSprakkode(sprakkode),
+                title: 'Malform.Beskrivelse',
               },
             ]}
           />
@@ -37,6 +45,7 @@ const VedtakFritekstPanelImpl = ({ begrunnelse, begrunnelseFieldName, sprakkode,
     {readOnly && begrunnelse !== null && (
       <span>
         <VerticalSpacer twentyPx />
+        <Undertekst>{intl.formatMessage({ id: labelTextCode })}</Undertekst>
         <VerticalSpacer eightPx />
         <div className={styles.fritekstItem}>{decodeHtmlEntity(begrunnelse)}</div>
       </span>
@@ -55,6 +64,5 @@ VedtakFritekstPanelImpl.propTypes = {
   sprakkode: PropTypes.shape().isRequired,
   readOnly: PropTypes.bool.isRequired,
   label: PropTypes.string.isRequired,
-  intl: PropTypes.shape(),
 };
 export default VedtakFritekstPanelImpl;
