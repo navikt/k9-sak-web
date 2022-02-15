446459c7409468a45295665cc3f6e9cfeaeaf0f0
eras-dev <emma.rasmusson@nav.no>
2022-02-15 12:57:42 +0100

    Revert "Revert "Tsf 2413 undertrykke brev i behandling (#3044)" (#3156)"
    This reverts commit 8a4b70f2dad2a07edc7cbf13462213e7e5114438.

---
+++ packages/prosess-vedtak/src/kodeverk/redusertUtbetalingArsak.tsx
@@ -0,0 +1,15 @@
+export type redusertUtbetalingArsakType = {
+  [key: string]: string;
+};
+
+const redusertUtbetalingArsak = [
+  'INNTEKT_REDUSERT_2019',
+  'INNTEKT_REDUSERT_2020_FØR_MARS',
+  'INNTEKT_I_SØKNADSPERIODE_ENDRET',
+  'SØKNADSPERIODENS_LENGDE_ENDRET',
+  'DATO_FOR_NYOPPSTARTET_SN_ENDRET',
+  'SØKNAD_GJELDER_IKKE_LENGER_FL',
+  'SØKNAD_GJELDER_IKKE_LENGER_SN',
+  'KONTROLL_INNTEKT_2019',
+];
+export default redusertUtbetalingArsak;
