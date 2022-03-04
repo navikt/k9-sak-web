710288bfe5ee6cf9a59d93e5c41b2db0fb41f8a8
vebnor <vebjorn.nordby@nav.no>
2022-03-04 15:36:16 +0100

    også stories da

--- packages/behandling-pleiepenger/src/components/Utenlandsopphold.spec.tsx
+++ packages/behandling-pleiepenger/src/components/Utenlandsopphold.spec.tsx
@@ -6,35 +6,16 @@ import userEvent from '@testing-library/user-event';
 import { renderWithIntl } from '@fpsak-frontend/utils-test/src/test-utils';
 
 import Utenlandsopphold from './Utenlandsopphold';
-import utenlandsoppholdMock from '../../../mocks/mockdata/utenlandsoppholdMock';
+import utenlandsoppholdMock, { utenlandsoppholdÅrsakMock } from '../../../mocks/mockdata/utenlandsoppholdMock';
 
-const kodeverk = {
-  UtenlandsoppholdÅrsak: [
-    {
-      kode: 'BARNET_INNLAGT_I_HELSEINSTITUSJON_FOR_NORSK_OFFENTLIG_REGNING',
-      navn: 'Barnet er innlagt i helseinstitusjon for norsk offentlig regning (mottar pleiepenger som i Norge, telles ikke i 8 uker)',
-      kodeverk: 'UTENLANDSOPPHOLD_ÅRSAK',
-    },
-    {
-      kode: 'BARNET_INNLAGT_I_HELSEINSTITUSJON_DEKKET_ETTER_AVTALE_MED_ET_ANNET_LAND_OM_TRYGD',
-      navn: 'Barnet er innlagt i helseinstitusjon dekket etter avtale med annet land om trygd (mottar pleiepenger som i Norge, telles ikke i 8 uker)',
-      kodeverk: 'UTENLANDSOPPHOLD_ÅRSAK',
-    },
-    {
-      kode: 'INGEN',
-      navn: 'Ingen av årsakene over (kan motta pleiepenger i 8 uker)',
-      kodeverk: 'UTENLANDSOPPHOLD_ÅRSAK',
-    },
-  ],
-};
 describe('Utenlandsopphold', () => {
   test('har utenlandsopphold som tittel', () => {
-    renderWithIntl(<Utenlandsopphold utenlandsopphold={utenlandsoppholdMock} kodeverk={kodeverk} />);
+    renderWithIntl(<Utenlandsopphold utenlandsopphold={utenlandsoppholdMock} kodeverk={utenlandsoppholdÅrsakMock} />);
     screen.getByText('Utenlandsopphold');
   });
 
   test('kan kan toggle på hjelpetekst', () => {
-    renderWithIntl(<Utenlandsopphold utenlandsopphold={utenlandsoppholdMock} kodeverk={kodeverk} />);
+    renderWithIntl(<Utenlandsopphold utenlandsopphold={utenlandsoppholdMock} kodeverk={utenlandsoppholdÅrsakMock} />);
     const infoboksTekst =
       'Opphold innenfor EØS likestilles med opphold i Norge, og det er ingen tidsbegrensning på hvor lenge søker kan motta pleiepenger.';
     expect(screen.queryByText(infoboksTekst)).toBeFalsy();
@@ -44,7 +25,10 @@ describe('Utenlandsopphold', () => {
 
   test('viser land, tilhørighet til EØS og årsak for utenlandsopphold', () => {
     renderWithIntl(
-      <Utenlandsopphold utenlandsopphold={{ perioder: [utenlandsoppholdMock.perioder[0]] }} kodeverk={kodeverk} />,
+      <Utenlandsopphold
+        utenlandsopphold={{ perioder: [utenlandsoppholdMock.perioder[0]] }}
+        kodeverk={utenlandsoppholdÅrsakMock}
+      />,
     );
 
     expect(screen.getByText('Land')).toBeVisible();
