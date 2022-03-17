c79f277364296910858e21f494f2628fc436bbfb
Vebjørn Nordby <vebjorn.nordby@nav.no>
2022-03-17 16:01:59 +0100

    Endringslogg (#3357)
    * legger til endringslogg-komponent
    
    * legger til urls for endringslogg i prod og dev
    
    * småpirk i visningen
    
    * fjerner dotenv
    
    * legger inn farge for header i dev igjen
    
    * formattering
    
    * fjerner unødvendig fragment

--- webpack/webpack.prod.ts
+++ webpack/webpack.prod.ts
@@ -1,3 +1,4 @@
+import webpack from 'webpack';
 import path from 'path';
 
 import TerserPlugin from 'terser-webpack-plugin';
@@ -26,7 +27,10 @@ export default {
   },
   cache,
   module,
-  plugins,
+  plugins: [
+    ...plugins,
+    new webpack.EnvironmentPlugin({ ENDRINGSLOGG_URL: 'https://familie-endringslogg.intern.nav.no' }),
+  ],
   externals,
   resolve,
   optimization: {
