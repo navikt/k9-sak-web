46710031a21eed90e2e198a2c382cbc0ece0871d
eras-dev <emma.rasmusson@nav.no>
2022-03-28 08:44:52 +0200

    Revert "legger in period (#3396)"
    This reverts commit fe1744c77ba03c5678b5a5fb3cf63e9f79cb4961.

--- packages/behandling-utvidet-rett/src/panelDefinisjoner/prosessStegPaneler/UtvidetRettOmsorgenForMikrofrontendFelles.ts
+++ packages/behandling-utvidet-rett/src/panelDefinisjoner/prosessStegPaneler/UtvidetRettOmsorgenForMikrofrontendFelles.ts
@@ -1,7 +1,6 @@
 import { Vilkar } from '@k9-sak-web/types';
 import vilkarUtfallType from '@fpsak-frontend/kodeverk/src/vilkarUtfallType';
 import { InformasjonOmVurdertVilkar } from '../../types/utvidetRettMikrofrontend/InformasjonOmVurdertVilkar';
-import { formatereLukketPeriode } from '@fpsak-frontend/utils';
 
 export const generereInfoForVurdertVilkar = (
   skalVilkarsUtfallVises: boolean,
@@ -14,7 +13,6 @@ export const generereInfoForVurdertVilkar = (
     navnPåAksjonspunkt,
     vilkarOppfylt: false,
     vilkar: 'Vilkar ikke funnet.',
-    periode: '',
   } as InformasjonOmVurdertVilkar;
 
   if (skalVilkarsUtfallVises && vilkar.perioder[0]) {
@@ -23,7 +21,6 @@ export const generereInfoForVurdertVilkar = (
     vurdertVilkar.navnPåAksjonspunkt = navnPåAksjonspunkt;
     vurdertVilkar.vilkarOppfylt = periode.vilkarStatus.kode === vilkarUtfallType.OPPFYLT;
     vurdertVilkar.vilkar = vilkar.lovReferanse;
-    vurdertVilkar.periode = formatereLukketPeriode(`${periode.periode.fom}/${periode.periode.tom}`);
   }
   return vurdertVilkar;
 };
