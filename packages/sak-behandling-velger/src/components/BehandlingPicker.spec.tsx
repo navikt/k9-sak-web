f03980b2603fb7db76ece4e464aa567837cbb116
Hallvard Andreas Stark <hallvard@scel.to>
2022-04-07 14:11:01 +0200

    fikser test

--- packages/sak-behandling-velger/src/components/BehandlingPicker.spec.tsx
+++ packages/sak-behandling-velger/src/components/BehandlingPicker.spec.tsx
@@ -1,3 +1,4 @@
+import behandlingStatus from '@fpsak-frontend/kodeverk/src/behandlingStatus';
 import { renderWithIntl } from '@fpsak-frontend/utils-test/src/test-utils';
 import { BehandlingAppKontekst } from '@k9-sak-web/types';
 import { screen, waitFor } from '@testing-library/react';
@@ -20,8 +21,8 @@ describe('<BehandlingPicker>', () => {
       kodeverk: '',
     },
     status: {
-      kode: 'FVED',
-      kodeverk: '',
+      kode: behandlingStatus.AVSLUTTET,
+      kodeverk: 'BEHANDLING_STATUS',
     },
     opprettet: '15.10.2017',
     behandlendeEnhetId: '1242424',
@@ -63,12 +64,14 @@ describe('<BehandlingPicker>', () => {
         },
       });
       renderWithIntl(
-        <BehandlingPicker
-          noExistingBehandlinger
-          behandlinger={[]}
-          getBehandlingLocation={() => locationMock}
-          getKodeverkFn={sinon.spy()}
-        />,
+        <BrowserRouter>
+          <BehandlingPicker
+            noExistingBehandlinger
+            behandlinger={[]}
+            getBehandlingLocation={() => locationMock}
+            getKodeverkFn={sinon.spy()}
+          />
+        </BrowserRouter>,
         {
           locale: 'nb-NO',
           messages,
