63d938622e9784c59feefd20f2d6407498a2cfd0
Vebjørn Nordby <vebjorn.nordby@nav.no>
2022-04-21 12:42:43 +0200

    TSF-1991 FormattedTime viser to timer tidligere enn den skal (#3482)

--- packages/shared-components/src/DateTimeLabel.tsx
+++ packages/shared-components/src/DateTimeLabel.tsx
@@ -16,22 +16,24 @@ interface OwnProps {
  * <DateTimeLabel dateTimeString="2017-08-02T00:54:25.455" />
  * ```
  */
-const DateTimeLabel = ({ dateTimeString, useNewFormat = false }: OwnProps) => (
-  <>
-    <FormattedDate day="2-digit" month="2-digit" year="numeric" value={new Date(dateTimeString)} />
-    {!useNewFormat && (
-      <>
-        -
-        <FormattedTime value={new Date(dateTimeString)} />
-      </>
-    )}
-    {useNewFormat && (
-      <>
-        <FormattedMessage id="DateTimeLabel.Kl" />
-        <FormattedTime value={new Date(dateTimeString)} hour="numeric" minute="numeric" second="numeric" />
-      </>
-    )}
-  </>
-);
+const DateTimeLabel = ({ dateTimeString, useNewFormat = false }: OwnProps) => {
+  const date = new Date(dateTimeString);
+  return (
+    <>
+      <FormattedDate day="2-digit" month="2-digit" year="numeric" value={date} />
+      {!useNewFormat && (
+        <>
+          -<span>{`${date.getHours()}:${date.getMinutes()}`}</span>
+        </>
+      )}
+      {useNewFormat && (
+        <>
+          <FormattedMessage id="DateTimeLabel.Kl" />
+          <FormattedTime value={date} hour="numeric" minute="numeric" second="numeric" />
+        </>
+      )}
+    </>
+  );
+};
 
 export default DateTimeLabel;
