d37fa1528d351ce4c9de933cfd4677002e3ec14c
vebnor <vebjorn.nordby@nav.no>
2022-05-13 13:39:13 +0200

    filtrerer ut felter i formik som ikke skal lagres når man hopper mellom prosesssteg

--- packages/prosess-vedtak/src/components/LagreFormikStateLokalt.tsx
+++ packages/prosess-vedtak/src/components/LagreFormikStateLokalt.tsx
@@ -1,15 +1,26 @@
-import { useFormikContext } from 'formik';
+import { FormikValues, useFormikContext } from 'formik';
 import { useEffect, useContext } from 'react';
 
 import { VedtakFormContext } from '@k9-sak-web/behandling-felles/src/components/ProsessStegContainer';
+import { fieldnames } from '../konstanter';
 
 export default function LagreFormikStateLokalt() {
   const vedtakContext = useContext(VedtakFormContext);
 
-  const { values } = useFormikContext();
+  const { values }: { values: FormikValues } = useFormikContext();
+  const verdierSomSkalBeholdesVedNavigasjonVekkFraVedtak = [
+    fieldnames.OVERSKRIFT,
+    fieldnames.BRØDTEKST,
+    fieldnames.OVERSTYRT_MOTTAKER,
+    fieldnames.BEGRUNNELSE,
+  ];
   useEffect(
     () => () => {
-      vedtakContext?.setVedtakFormState(values);
+      vedtakContext?.setVedtakFormState(
+        Object.keys(values)
+          .filter(key => verdierSomSkalBeholdesVedNavigasjonVekkFraVedtak.includes(key))
+          .reduce((obj, key) => ({ ...obj, [key]: values[key] }), {}),
+      );
     },
     [values],
   );
