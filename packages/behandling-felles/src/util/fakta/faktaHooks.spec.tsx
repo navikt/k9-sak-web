9a4b5b4a751ba6dc69c54ed16bbf6d0ac6e46781
Thomas H. Wiberg <thomas@thawi.no>
2022-05-20 15:16:55 +0200

    Dependabot bumps
    react-redux -> ^8.0.1
    dotenv -> ^16.0.1
    sinon -> ^14.0.0

--- packages/behandling-felles/src/util/fakta/faktaHooks.spec.tsx
+++ packages/behandling-felles/src/util/fakta/faktaHooks.spec.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 import React from 'react';
 import { IntlShape } from 'react-intl';
 import { shallow } from 'enzyme';
--- packages/behandling-felles/src/util/fakta/faktaUtils.spec.tsx
+++ packages/behandling-felles/src/util/fakta/faktaUtils.spec.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 import React from 'react';
 import sinon from 'sinon';
 import { IntlShape } from 'react-intl';
