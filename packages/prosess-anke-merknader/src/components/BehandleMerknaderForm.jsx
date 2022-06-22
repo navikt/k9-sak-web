0adb574fe429a25308b90fa9315a16dfeb5c5c71
Thomas H. Wiberg <thomas@thawi.no>
2022-06-22 10:44:58 +0200

    Kodeverk: prosess anke merknader

--- packages/prosess-anke-merknader/src/components/BehandleMerknaderForm.jsx
+++ packages/prosess-anke-merknader/src/components/BehandleMerknaderForm.jsx
@@ -124,7 +124,7 @@ const buildInitialValues = createSelector([ownProps => ownProps.ankeVurderingRes
 }));
 
 const mapStateToPropsFactory = (initialState, initialOwnProps) => {
-  const aksjonspunktCode = initialOwnProps.aksjonspunkter[0].definisjon.kode;
+  const aksjonspunktCode = initialOwnProps.aksjonspunkter[0].definisjon;
   const onSubmit = values => initialOwnProps.submitCallback([transformValues(values, aksjonspunktCode)]);
   return (state, ownProps) => ({
     aksjonspunktCode,
