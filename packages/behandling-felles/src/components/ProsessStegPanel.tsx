23c455fe0e8690b7239cff74923b17790459d0ca
Espen Velsvik <espenjv@users.noreply.github.com>
2022-06-28 13:04:09 +0200

    Flytter hook over conditional (#3736)

--- packages/behandling-felles/src/components/ProsessStegPanel.tsx
+++ packages/behandling-felles/src/components/ProsessStegPanel.tsx
@@ -15,133 +15,132 @@ import prosessStegHooks from '../util/prosessSteg/prosessStegHooks';
 import { ProsessStegUtledet } from '../util/prosessSteg/ProsessStegUtledet';
 
 interface OwnProps {
-    fagsak: Fagsak;
-    behandling: Behandling;
-    alleKodeverk: { [key: string]: KodeverkMedNavn[] };
-    valgtProsessSteg?: ProsessStegUtledet;
-    apentFaktaPanelInfo?: { urlCode: string; textCode: string };
-    oppdaterProsessStegOgFaktaPanelIUrl?: (punktnavn?: string, faktanavn?: string) => void;
-    lagringSideeffekterCallback: (
-        aksjonspunktModeller: [{ kode: string; isVedtakSubmission?: boolean; sendVarsel?: boolean }],
-    ) => any;
-    lagreAksjonspunkter: (params: any, keepData?: boolean) => Promise<any>;
-    lagreOverstyrteAksjonspunkter?: (params: any, keepData?: boolean) => Promise<any>;
-    useMultipleRestApi: (endpoints: EndpointData[], options: Options) => RestApiData<any>;
-    featureToggles?: FeatureToggles;
+  fagsak: Fagsak;
+  behandling: Behandling;
+  alleKodeverk: { [key: string]: KodeverkMedNavn[] };
+  valgtProsessSteg?: ProsessStegUtledet;
+  apentFaktaPanelInfo?: { urlCode: string; textCode: string };
+  oppdaterProsessStegOgFaktaPanelIUrl?: (punktnavn?: string, faktanavn?: string) => void;
+  lagringSideeffekterCallback: (
+    aksjonspunktModeller: [{ kode: string; isVedtakSubmission?: boolean; sendVarsel?: boolean }],
+  ) => any;
+  lagreAksjonspunkter: (params: any, keepData?: boolean) => Promise<any>;
+  lagreOverstyrteAksjonspunkter?: (params: any, keepData?: boolean) => Promise<any>;
+  useMultipleRestApi: (endpoints: EndpointData[], options: Options) => RestApiData<any>;
+  featureToggles?: FeatureToggles;
 }
 
 const ProsessStegPanel = ({
-                              valgtProsessSteg,
-                              fagsak,
-                              behandling,
-                              alleKodeverk,
-                              apentFaktaPanelInfo,
-                              oppdaterProsessStegOgFaktaPanelIUrl,
-                              lagringSideeffekterCallback,
-                              lagreAksjonspunkter,
-                              lagreOverstyrteAksjonspunkter,
-                              useMultipleRestApi,
-                              featureToggles,
-                          }: OwnProps) => {
-    const erHenlagtOgVedtakStegValgt =
-        behandling.behandlingHenlagt && valgtProsessSteg && valgtProsessSteg.getUrlKode() === prosessStegCodes.VEDTAK;
+  valgtProsessSteg,
+  fagsak,
+  behandling,
+  alleKodeverk,
+  apentFaktaPanelInfo,
+  oppdaterProsessStegOgFaktaPanelIUrl,
+  lagringSideeffekterCallback,
+  lagreAksjonspunkter,
+  lagreOverstyrteAksjonspunkter,
+  useMultipleRestApi,
+  featureToggles,
+}: OwnProps) => {
+  const erHenlagtOgVedtakStegValgt =
+    behandling.behandlingHenlagt && valgtProsessSteg && valgtProsessSteg.getUrlKode() === prosessStegCodes.VEDTAK;
 
-    const panelKeys = valgtProsessSteg
-        ? valgtProsessSteg
-            .getDelPaneler()[0]
-            .getProsessStegDelPanelDef()
-            .getEndepunkter(featureToggles)
-            .map(e => ({key: e}))
-        : [];
+  const panelKeys = valgtProsessSteg
+    ? valgtProsessSteg
+        .getDelPaneler()[0]
+        .getProsessStegDelPanelDef()
+        .getEndepunkter(featureToggles)
+        .map(e => ({ key: e }))
+    : [];
 
-    const suspendRequest = !!(
-        panelKeys.length === 0 ||
-        erHenlagtOgVedtakStegValgt ||
-        !valgtProsessSteg ||
-        (!valgtProsessSteg.getErStegBehandlet() && valgtProsessSteg.getUrlKode())
-    );
+  const suspendRequest = !!(
+    panelKeys.length === 0 ||
+    erHenlagtOgVedtakStegValgt ||
+    !valgtProsessSteg ||
+    (!valgtProsessSteg.getErStegBehandlet() && valgtProsessSteg.getUrlKode())
+  );
 
-    const {data, state: hentDataState} = useMultipleRestApi(panelKeys, {
-        keepData: true,
-        isCachingOn: true,
-        suspendRequest,
-        updateTriggers: [behandling?.versjon, suspendRequest, valgtProsessSteg],
-    });
+  const { data, state: hentDataState } = useMultipleRestApi(panelKeys, {
+    keepData: true,
+    isCachingOn: true,
+    suspendRequest,
+    updateTriggers: [behandling?.versjon, suspendRequest, valgtProsessSteg],
+  });
 
-    const bekreftAksjonspunktCallback = prosessStegHooks.useBekreftAksjonspunkt(
-        fagsak,
-        behandling,
-        lagringSideeffekterCallback,
-        lagreAksjonspunkter,
-        lagreOverstyrteAksjonspunkter,
-        valgtProsessSteg,
-    );
+  const bekreftAksjonspunktCallback = prosessStegHooks.useBekreftAksjonspunkt(
+    fagsak,
+    behandling,
+    lagringSideeffekterCallback,
+    lagreAksjonspunkter,
+    lagreOverstyrteAksjonspunkter,
+    valgtProsessSteg,
+  );
 
-
-    if (erHenlagtOgVedtakStegValgt) {
-        return <BehandlingHenlagtPanel/>;
+  const [formData, setFormData] = useState({});
+  useEffect(() => {
+    if (formData) {
+      setFormData(undefined);
     }
-    if (!valgtProsessSteg) {
-        return null;
-    }
-    if (!valgtProsessSteg.getErStegBehandlet() && valgtProsessSteg.getUrlKode()) {
-        return <ProsessStegIkkeBehandletPanel/>;
-    }
+  }, [behandling.versjon]);
 
-    const harHentetData = panelKeys.length === 0 || hentDataState === RestApiState.SUCCESS;
-    const delPaneler = valgtProsessSteg.getDelPaneler();
+  if (erHenlagtOgVedtakStegValgt) {
+    return <BehandlingHenlagtPanel />;
+  }
+  if (!valgtProsessSteg) {
+    return null;
+  }
+  if (!valgtProsessSteg.getErStegBehandlet() && valgtProsessSteg.getUrlKode()) {
+    return <ProsessStegIkkeBehandletPanel />;
+  }
 
-    const [formData, setFormData] = useState({});
-    useEffect(() => {
-        if (formData) {
-            setFormData(undefined);
-        }
-    }, [behandling.versjon]);
+  const harHentetData = panelKeys.length === 0 || hentDataState === RestApiState.SUCCESS;
+  const delPaneler = valgtProsessSteg.getDelPaneler();
 
-    return (
-        // eslint-disable-next-line react/jsx-no-useless-fragment
-        <>
-            {valgtProsessSteg.getErStegBehandlet() && (
-                <MargMarkering
-                    behandlingStatus={behandling.status}
-                    aksjonspunkter={valgtProsessSteg.getAksjonspunkter()}
-                    isReadOnly={valgtProsessSteg.getErReadOnly()}
-                    visAksjonspunktMarkering={delPaneler.length === 1}
-                >
-                    {delPaneler.length === 1 && (
-                        <FadingPanel>
-                            {!harHentetData && <LoadingPanel/>}
-                            {harHentetData && (
-                                <>
-                                    {delPaneler[0].getProsessStegDelPanelDef().getKomponent({
-                                        behandling,
-                                        featureToggles,
-                                        alleKodeverk,
-                                        formData,
-                                        setFormData,
-                                        submitCallback: bekreftAksjonspunktCallback,
-                                        ...delPaneler[0].getKomponentData(),
-                                        ...data,
-                                    })}
-                                </>
-                            )}
-                        </FadingPanel>
-                    )}
-                    {delPaneler.length > 1 && (
-                        <InngangsvilkarPanel
-                            behandling={behandling}
-                            alleKodeverk={alleKodeverk}
-                            prosessStegData={delPaneler}
-                            submitCallback={bekreftAksjonspunktCallback}
-                            apentFaktaPanelInfo={apentFaktaPanelInfo}
-                            oppdaterProsessStegOgFaktaPanelIUrl={oppdaterProsessStegOgFaktaPanelIUrl}
-                            useMultipleRestApi={useMultipleRestApi}
-                        />
-                    )}
-                </MargMarkering>
-            )}
-        </>
-    );
+  return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      {valgtProsessSteg.getErStegBehandlet() && (
+        <MargMarkering
+          behandlingStatus={behandling.status}
+          aksjonspunkter={valgtProsessSteg.getAksjonspunkter()}
+          isReadOnly={valgtProsessSteg.getErReadOnly()}
+          visAksjonspunktMarkering={delPaneler.length === 1}
+        >
+          {delPaneler.length === 1 && (
+            <FadingPanel>
+              {!harHentetData && <LoadingPanel />}
+              {harHentetData && (
+                <>
+                  {delPaneler[0].getProsessStegDelPanelDef().getKomponent({
+                    behandling,
+                    featureToggles,
+                    alleKodeverk,
+                    formData,
+                    setFormData,
+                    submitCallback: bekreftAksjonspunktCallback,
+                    ...delPaneler[0].getKomponentData(),
+                    ...data,
+                  })}
+                </>
+              )}
+            </FadingPanel>
+          )}
+          {delPaneler.length > 1 && (
+            <InngangsvilkarPanel
+              behandling={behandling}
+              alleKodeverk={alleKodeverk}
+              prosessStegData={delPaneler}
+              submitCallback={bekreftAksjonspunktCallback}
+              apentFaktaPanelInfo={apentFaktaPanelInfo}
+              oppdaterProsessStegOgFaktaPanelIUrl={oppdaterProsessStegOgFaktaPanelIUrl}
+              useMultipleRestApi={useMultipleRestApi}
+            />
+          )}
+        </MargMarkering>
+      )}
+    </>
+  );
 };
 
 export default ProsessStegPanel;
