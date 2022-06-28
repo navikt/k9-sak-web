718fd034f3d96d2afd11dd2baf53ebfbfa8cefc8
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2022-06-28 15:15:56 +0200

    Veiledervisning for behandlingsmeny (#3737)

---
+++ packages/sak-app/src/behandlingmenu/BehandlingMenuVeiledervisning.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Button } from '@navikt/ds-react';
+import MenyMarkerBehandling from '@k9-sak-web/sak-meny-marker-behandling';
+import { MerknadFraLos, FeatureToggles } from '@k9-sak-web/types';
+
+interface VeiledervisningProps {
+  behandlingUuid: string;
+  featureToggles: FeatureToggles;
+  markerBehandling: (values: any) => Promise<any>;
+  merknaderFraLos: MerknadFraLos;
+}
+
+const BehandlingMenuVeiledervisning: React.FC<VeiledervisningProps> = ({
+  behandlingUuid,
+  featureToggles,
+  markerBehandling,
+  merknaderFraLos,
+}) => {
+  const [visMarkerBehandlingModal, setVisMarkerBehandlingModal] = useState(false);
+
+  if (!featureToggles?.LOS_MARKER_BEHANDLING) {
+    return null;
+  }
+
+  return (
+    <>
+      <Button size="small" variant="secondary" onClick={() => setVisMarkerBehandlingModal(!visMarkerBehandlingModal)}>
+        Marker behandling
+      </Button>
+      {visMarkerBehandlingModal && (
+        <MenyMarkerBehandling
+          behandlingUuid={behandlingUuid}
+          markerBehandling={markerBehandling}
+          lukkModal={() => setVisMarkerBehandlingModal(!visMarkerBehandlingModal)}
+          brukHastekøMarkering
+          merknaderFraLos={merknaderFraLos}
+        />
+      )}
+    </>
+  );
+};
+
+export default BehandlingMenuVeiledervisning;
