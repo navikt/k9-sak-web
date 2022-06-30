93dab1dce700e5ed71fad7bd669e9e68a3825406
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2022-06-30 08:23:38 +0200

    Flytter plugin for module federation til dev config (#3751)
    * Flytter plugin for module federation til dev config
    
    * Endrer sjekk på om vi er i dev

--- webpack/webpack.dev.ts
+++ webpack/webpack.dev.ts
@@ -8,8 +8,12 @@ import externals from './common/externals';
 import cache from './common/cache';
 import plugins from './common/plugins';
 import devServer from './common/devServer';
+import pck from '../package.json';
 
 import { PUBLIC_PATH } from './constants';
+
+const deps = pck.dependencies;
+const { ModuleFederationPlugin } = webpack.container;
 
 export default {
   mode: 'development',
@@ -26,6 +30,23 @@ export default {
   cache,
   plugins: [
     ...plugins,
+    new ModuleFederationPlugin({
+      name: 'ft_frontend_saksbehandling',
+      remotes: {
+        ft_prosess_beregningsgrunnlag:
+          'ft_prosess_beregningsgrunnlag@http://localhost:9008/remoteEntry.js?[(new Date).getTime()]',
+      },
+      shared: {
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: deps['react-dom'],
+        },
+      },
+    }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.EnvironmentPlugin({
       MSW_MODE: 'development',
