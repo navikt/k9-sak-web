2bdb2f9f7cba39e75b6ab70cf040671868b14992
Hallvard Andreas Stark <hallvard@scel.to>
2022-06-30 14:20:49 +0200

    Bruker ft-plattform-komponenter istedet for k9-react-components

--- packages/behandling-felles/src/components/ProsessStegContainer.tsx
+++ packages/behandling-felles/src/components/ProsessStegContainer.tsx
@@ -1,7 +1,6 @@
 import React, { useMemo, ReactNode, useState } from 'react';
 import { injectIntl, WrappedComponentProps } from 'react-intl';
-import { ProcessMenu } from '@navikt/k9-react-components';
-
+import { ProcessMenu } from '@navikt/ft-plattform-komponenter';
 import ProsessStegMenyRad from '../types/prosessStegMenyRadTsType';
 
 import styles from './prosessStegContainer.less';
@@ -33,15 +32,17 @@ const ProsessStegContainer = ({
   // Byttet ut redux-form med formik uten å tenke på at staten forsvinner når man bytter panel
   // Dette er en fiks for det, men bør på sikt erstattes med hyppig mellomlagring i vedtak
   const [vedtakFormState, setVedtakFormState] = useState(null);
+  const vedtakFormContextValue = useMemo(
+    () => ({ vedtakFormState, setVedtakFormState }),
+    [vedtakFormState, setVedtakFormState],
+  );
 
   return (
     <div className={styles.container}>
       <div className={styles.meny}>
         <ProcessMenu steps={steg} onClick={velgProsessStegPanelCallback} />
       </div>
-      <VedtakFormContext.Provider value={{ vedtakFormState, setVedtakFormState }}>
-        {children}
-      </VedtakFormContext.Provider>
+      <VedtakFormContext.Provider value={vedtakFormContextValue}>{children}</VedtakFormContext.Provider>
     </div>
   );
 };
