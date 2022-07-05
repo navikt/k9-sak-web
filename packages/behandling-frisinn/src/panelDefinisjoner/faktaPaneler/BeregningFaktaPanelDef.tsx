001e0c31353dbb76906385502cccd2ab599b951a
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2022-07-05 14:42:22 +0200

    Fakta beregning mikrofrontend (#3756)
    * Fakta beregning mikrofrontend
    
    * Bump pakkeversjon

--- packages/behandling-frisinn/src/panelDefinisjoner/faktaPaneler/BeregningFaktaPanelDef.tsx
+++ packages/behandling-frisinn/src/panelDefinisjoner/faktaPaneler/BeregningFaktaPanelDef.tsx
@@ -3,7 +3,17 @@ import React from 'react';
 import { faktaPanelCodes } from '@k9-sak-web/konstanter';
 import aksjonspunktCodes from '@fpsak-frontend/kodeverk/src/aksjonspunktCodes';
 import BeregningFaktaIndex from '@fpsak-frontend/fakta-beregning';
-import { FaktaPanelDef } from '@k9-sak-web/behandling-felles';
+import { FaktaPanelDef, DynamicLoader } from '@k9-sak-web/behandling-felles';
+import { konverterKodeverkTilKode } from '@fpsak-frontend/utils';
+import vilkarType from '@fpsak-frontend/kodeverk/src/vilkarType';
+
+const FaktaBeregningsgrunnlag = React.lazy(() => import('@navikt/ft-fakta-beregning'));
+
+const FaktaBeregningsgrunnlagMF =
+  process.env.NODE_ENV !== 'development'
+    ? undefined
+    : // eslint-disable-next-line import/no-unresolved
+      () => import('ft_fakta_beregning/FaktaBeregning');
 
 class BeregningFaktaPanelDef extends FaktaPanelDef {
   getUrlKode = () => faktaPanelCodes.BEREGNING;
@@ -12,7 +22,27 @@ class BeregningFaktaPanelDef extends FaktaPanelDef {
 
   getAksjonspunktKoder = () => [aksjonspunktCodes.VURDER_FAKTA_FOR_ATFL_SN];
 
-  getKomponent = props => <BeregningFaktaIndex {...props} />;
+  getKomponent = props => {
+    if (props.featureToggles.NY_BEREGNING_FAKTA_ENABLED) {
+      const deepCopyProps = JSON.parse(JSON.stringify(props));
+      konverterKodeverkTilKode(deepCopyProps);
+      const bgVilkaret = deepCopyProps.vilkar.find(v => v.vilkarType === vilkarType.BEREGNINGSGRUNNLAGVILKARET);
+      return (
+        <DynamicLoader<React.ComponentProps<typeof FaktaBeregningsgrunnlag>>
+          packageCompFn={() => import('@navikt/ft-fakta-beregning')}
+          federatedCompFn={FaktaBeregningsgrunnlagMF}
+          {...props}
+          arbeidsgiverOpplysningerPerId={deepCopyProps.arbeidsgiverOpplysningerPerId}
+          submitCallback={props.submitCallback}
+          formData={props.formData}
+          setFormData={props.setFormData}
+          vilkar={bgVilkaret}
+        />
+      );
+    }
+
+    return <BeregningFaktaIndex {...props} />;
+  };
 
   getOverstyrVisningAvKomponent = ({ beregningsgrunnlag }) => !!beregningsgrunnlag;
 
@@ -20,7 +50,7 @@ class BeregningFaktaPanelDef extends FaktaPanelDef {
     erOverstyrer: false,
     beregningsgrunnlag: beregningsgrunnlag ? [beregningsgrunnlag[0]] : [], // FRISINN skal kun vise ett beregningsgrunnlag
     arbeidsgiverOpplysningerPerId,
-    vilkar
+    vilkar,
   });
 }
 
