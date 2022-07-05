84ce5b9fa23a2fbd76e54f937de4db741e34d2d9
Hallvard Andreas Stark <hallvard@scel.to>
2022-07-05 16:21:13 +0200

    Hotfix av sjekk på feature toggle

--- packages/behandling-frisinn/src/panelDefinisjoner/faktaPaneler/BeregningFaktaPanelDef.tsx
+++ packages/behandling-frisinn/src/panelDefinisjoner/faktaPaneler/BeregningFaktaPanelDef.tsx
@@ -23,7 +23,7 @@ class BeregningFaktaPanelDef extends FaktaPanelDef {
   getAksjonspunktKoder = () => [aksjonspunktCodes.VURDER_FAKTA_FOR_ATFL_SN];
 
   getKomponent = props => {
-    if (props.featureToggles.NY_BEREGNING_FAKTA_ENABLED) {
+    if (props.featureToggles?.NY_BEREGNING_FAKTA_ENABLED) {
       const deepCopyProps = JSON.parse(JSON.stringify(props));
       konverterKodeverkTilKode(deepCopyProps);
       const bgVilkaret = deepCopyProps.vilkar.find(v => v.vilkarType === vilkarType.BEREGNINGSGRUNNLAGVILKARET);
