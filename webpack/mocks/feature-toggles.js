99c5d79c998237f412f88803d5668a4f0f23de5e
Thomas H. Wiberg <thomas@thawi.no>
2022-07-08 22:46:25 +0200

    Revert "Legge til AP 9014 (#3782)"
    This reverts commit d36077fe925e15c78e89a744b0fb09c58aad9f26.

--- webpack/mocks/feature-toggles.js
+++ webpack/mocks/feature-toggles.js
@@ -45,10 +45,6 @@ const featureToggles = [
     key: 'NY_BEREGNING_FAKTA_FORDEL_ENABLED',
     value: process.env.NY_BEREGNING_FAKTA_FORDEL_ENABLED,
   },
-  {
-    key: 'AKSJONSPUNKT_9014',
-    value: process.env.AKSJONSPUNKT_9014,
-  },
 ];
 
 module.exports = function (app) {
