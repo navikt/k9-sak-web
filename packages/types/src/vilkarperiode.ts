13c8e5a204b051bf95c132a9c0f5e73d79eb7537
Espen Johansen Velsvik <espen.velsvik@nav.no>
2022-08-02 15:13:15 +0200

    Rename vurdersIBehandlingen -> vurderesIBehandlingen

--- packages/types/src/vilkarperiode.ts
+++ packages/types/src/vilkarperiode.ts
@@ -4,7 +4,6 @@ export type Vilkarperiode = Readonly<{
   avslagKode?: string;
   begrunnelse?: string;
   vurderesIBehandlingen?: boolean;
-  vurderesIBehandlingen?: boolean;
   merknad?: Kodeverk;
   merknadParametere: { [name: string]: string };
   periode: { fom: string; tom: string };
