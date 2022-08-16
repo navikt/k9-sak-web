f82737a9e9d3326f0ff82b7d0a4650cd34d1bc5f
Vebjørn Nordby <vebjorn.nordby@nav.no>
2022-08-16 16:26:08 +0200

    Tsf 2650 visning av permisjoner under arbeidsforhold under saksopplysninger (#3797)
    * TSF-2650 fikser overflow og fjerner yrkestittel
    
    * visning av permisjon
    
    * TSF-2650@
    
    * TSF-2650
    
    * viser kun siste fire siffer i arbeidsforholdId
    
    * lint
    
    * henter permisjonstyper fra kodeverk
    
    * viser hele arbeidsforholdId
    
    * Rendre tom TableColumn dersom vi ikke har permisjoner, ellers blir raden kortere enn rader med permisjoner
    
    * tester
    
    * tester

--- packages/fakta-arbeidsforhold/src/components/arbeidsforholdDetaljer/PermisjonerInfo.tsx
+++ packages/fakta-arbeidsforhold/src/components/arbeidsforholdDetaljer/PermisjonerInfo.tsx
@@ -2,51 +2,60 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Normaltekst } from 'nav-frontend-typografi';
 
-import { VerticalSpacer, PeriodLabel } from '@fpsak-frontend/shared-components';
-
 import ArbeidsforholdV2 from '@k9-sak-web/types/src/arbeidsforholdV2TsType';
+import { VerticalSpacer, PeriodLabel } from '@fpsak-frontend/shared-components';
+import { KodeverkMedNavn } from '@k9-sak-web/types';
+import { getKodeverknavnFn } from '@fpsak-frontend/utils';
+import kodeverkTyper from '@fpsak-frontend/kodeverk/src/kodeverkTyper';
+
 import styles from './permisjonPeriode.less';
 
 const utledPeriodeLabelKey = (id, index) => id + index;
 
 interface OwnProps {
   arbeidsforhold: ArbeidsforholdV2;
+  alleKodeverk: { [key: string]: KodeverkMedNavn[] };
 }
 
-const PermisjonerInfo = ({ arbeidsforhold }: OwnProps) => (
-  // eslint-disable-next-line react/jsx-no-useless-fragment
-  <>
-    {arbeidsforhold.permisjoner && arbeidsforhold.permisjoner.length > 0 && (
-      <div>
-        <VerticalSpacer sixteenPx />
-        {arbeidsforhold.permisjoner.map((permisjon, index) => (
-          <div>
-            <VerticalSpacer sixteenPx />
-            <div className={styles.container} key={utledPeriodeLabelKey(arbeidsforhold.id, index)}>
-              <Normaltekst>
-                <FormattedMessage id="PersonArbeidsforholdDetailForm.PermisjonPeriode" />
-              </Normaltekst>
-              <div className={styles.type}>
-                <PeriodLabel
-                  dateStringFom={permisjon.permisjonFom}
-                  dateStringTom={permisjon.permisjonTom ? permisjon.permisjonTom : ''}
-                />
+const PermisjonerInfo = ({ arbeidsforhold, alleKodeverk }: OwnProps) => {
+  const getKodeverknavn = getKodeverknavnFn(alleKodeverk, kodeverkTyper);
+  return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      {arbeidsforhold.permisjoner && arbeidsforhold.permisjoner.length > 0 && (
+        <div>
+          <VerticalSpacer sixteenPx />
+          {arbeidsforhold.permisjoner.map((permisjon, index) => (
+            <div>
+              <VerticalSpacer sixteenPx />
+              <div key={utledPeriodeLabelKey(arbeidsforhold.id, index)}>
+                <Normaltekst>
+                  <FormattedMessage id="PersonArbeidsforholdDetailForm.PermisjonPeriode" />
+                </Normaltekst>
+                <div className={styles.type}>
+                  <PeriodLabel
+                    dateStringFom={permisjon.permisjonFom}
+                    dateStringTom={permisjon.permisjonTom ? permisjon.permisjonTom : ''}
+                  />
+                </div>
+                <Normaltekst>
+                  <FormattedMessage id="PersonArbeidsforholdDetailForm.Permisjonype" />
+                </Normaltekst>
+                <Normaltekst className={styles.type}>
+                  {permisjon.type.kode === '-' ? 'Ukjent' : getKodeverknavn(permisjon.type)}
+                </Normaltekst>
+                <Normaltekst>
+                  <FormattedMessage id="PersonArbeidsforholdDetailForm.Prosent" />
+                </Normaltekst>
+                <Normaltekst className={styles.type}>{permisjon.permisjonsprosent}</Normaltekst>
               </div>
-              <Normaltekst className={styles.div}>
-                <FormattedMessage id="PersonArbeidsforholdDetailForm.Permisjonype" />
-              </Normaltekst>
-              <Normaltekst className={styles.type}>{permisjon.type.kode}</Normaltekst>
-              <Normaltekst className={styles.div}>
-                <FormattedMessage id="PersonArbeidsforholdDetailForm.Prosent" />
-              </Normaltekst>
-              <Normaltekst className={styles.type}>{permisjon.permisjonsprosent}</Normaltekst>
             </div>
-          </div>
-        ))}
-        <VerticalSpacer sixteenPx />
-      </div>
-    )}
-  </>
-);
+          ))}
+          <VerticalSpacer sixteenPx />
+        </div>
+      )}
+    </>
+  );
+};
 
 export default PermisjonerInfo;
