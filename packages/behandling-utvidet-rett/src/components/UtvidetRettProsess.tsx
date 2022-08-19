3d98bd5ff92adb6d6675f84d93d751495b99e045
Thomas H. Wiberg <thomas@thawi.no>
2022-08-19 15:02:46 +0200

    Lagt til aksjonspunkt 9015 (#3834)
    * Lagt til aksjonspunkt 9015
    
    Aldersvillkår i rammevedtak
    
    * yarn.lock
    
    * Lint
    
    * Justeringer

--- packages/behandling-utvidet-rett/src/components/UtvidetRettProsess.tsx
+++ packages/behandling-utvidet-rett/src/components/UtvidetRettProsess.tsx
@@ -29,17 +29,17 @@ const forhandsvis = (data: any) => {
 
 const getForhandsvisTilbakeCallback =
   (forhandsvisTilbakekrevingMelding: (data: any) => Promise<any>, fagsak: Fagsak, behandling: Behandling) =>
-  (mottaker: string, brevmalkode: string, fritekst: string, saksnummer: string) => {
-    const data = {
-      behandlingUuid: behandling.uuid,
-      fagsakYtelseType: fagsak.sakstype,
-      varseltekst: fritekst || '',
-      mottaker,
-      brevmalkode,
-      saksnummer,
+    (mottaker: string, brevmalkode: string, fritekst: string, saksnummer: string) => {
+      const data = {
+        behandlingUuid: behandling.uuid,
+        fagsakYtelseType: fagsak.sakstype,
+        varseltekst: fritekst || '',
+        mottaker,
+        brevmalkode,
+        saksnummer,
+      };
+      return forhandsvisTilbakekrevingMelding(data).then(response => forhandsvis(response));
     };
-    return forhandsvisTilbakekrevingMelding(data).then(response => forhandsvis(response));
-  };
 
 const getForhandsvisCallback =
   (
@@ -48,10 +48,10 @@ const getForhandsvisCallback =
     fagsakPerson: FagsakPerson,
     behandling: Behandling,
   ) =>
-  (data: any) => {
-    const request = lagForhåndsvisRequest(behandling, fagsak, fagsakPerson, data);
-    return forhandsvisMelding(request).then(response => forhandsvis(response));
-  };
+    (data: any) => {
+      const request = lagForhåndsvisRequest(behandling, fagsak, fagsakPerson, data);
+      return forhandsvisMelding(request).then(response => forhandsvis(response));
+    };
 
 const getLagringSideeffekter =
   (
@@ -62,43 +62,43 @@ const getLagringSideeffekter =
     opneSokeside,
     lagreDokumentdata,
   ) =>
-  async aksjonspunktModels => {
-    const erRevurderingsaksjonspunkt = aksjonspunktModels.some(
-      apModel =>
-        (apModel.kode === aksjonspunktCodes.VARSEL_REVURDERING_MANUELL ||
-          apModel.kode === aksjonspunktCodes.VARSEL_REVURDERING_ETTERKONTROLL) &&
-        apModel.sendVarsel,
-    );
-    const visIverksetterVedtakModal =
-      aksjonspunktModels[0].isVedtakSubmission &&
-      [aksjonspunktCodes.VEDTAK_UTEN_TOTRINNSKONTROLL, aksjonspunktCodes.FATTER_VEDTAK].includes(
-        aksjonspunktModels[0].kode,
+    async aksjonspunktModels => {
+      const erRevurderingsaksjonspunkt = aksjonspunktModels.some(
+        apModel =>
+          (apModel.kode === aksjonspunktCodes.VARSEL_REVURDERING_MANUELL ||
+            apModel.kode === aksjonspunktCodes.VARSEL_REVURDERING_ETTERKONTROLL) &&
+          apModel.sendVarsel,
       );
-    const visFatterVedtakModal =
-      aksjonspunktModels[0].isVedtakSubmission && aksjonspunktModels[0].kode === aksjonspunktCodes.FORESLA_VEDTAK;
+      const visIverksetterVedtakModal =
+        aksjonspunktModels[0].isVedtakSubmission &&
+        [aksjonspunktCodes.VEDTAK_UTEN_TOTRINNSKONTROLL, aksjonspunktCodes.FATTER_VEDTAK].includes(
+          aksjonspunktModels[0].kode,
+        );
+      const visFatterVedtakModal =
+        aksjonspunktModels[0].isVedtakSubmission && aksjonspunktModels[0].kode === aksjonspunktCodes.FORESLA_VEDTAK;
 
-    if (erRevurderingsaksjonspunkt) {
-      toggleOppdatereFagsakContext(false);
-    }
-
-    if (aksjonspunktModels[0].isVedtakSubmission) {
-      const dokumentdata = lagDokumentdata(aksjonspunktModels[0]);
-      if (dokumentdata) await lagreDokumentdata(dokumentdata);
-    }
-
-    // Returner funksjon som blir kjørt etter lagring av aksjonspunkt(er)
-    return () => {
-      if (visFatterVedtakModal) {
-        toggleFatterVedtakModal(true);
-      } else if (visIverksetterVedtakModal) {
-        toggleIverksetterVedtakModal(true);
-      } else if (erRevurderingsaksjonspunkt) {
-        opneSokeside();
-      } else {
-        oppdaterProsessStegOgFaktaPanelIUrl('default', 'default');
+      if (erRevurderingsaksjonspunkt) {
+        toggleOppdatereFagsakContext(false);
       }
+
+      if (aksjonspunktModels[0].isVedtakSubmission) {
+        const dokumentdata = lagDokumentdata(aksjonspunktModels[0]);
+        if (dokumentdata) await lagreDokumentdata(dokumentdata);
+      }
+
+      // Returner funksjon som blir kjørt etter lagring av aksjonspunkt(er)
+      return () => {
+        if (visFatterVedtakModal) {
+          toggleFatterVedtakModal(true);
+        } else if (visIverksetterVedtakModal) {
+          toggleIverksetterVedtakModal(true);
+        } else if (erRevurderingsaksjonspunkt) {
+          opneSokeside();
+        } else {
+          oppdaterProsessStegOgFaktaPanelIUrl('default', 'default');
+        }
+      };
     };
-  };
 
 const UtvidetRettProsess = ({
   data,
@@ -164,7 +164,10 @@ const UtvidetRettProsess = ({
   );
 
   const [prosessStegPaneler, valgtPanel, formaterteProsessStegPaneler] = prosessStegHooks.useProsessStegPaneler(
-    prosessStegUtvidetRettPanelDefinisjoner(fagsak.sakstype === fagsakYtelseType.OMSORGSPENGER_ALENE_OM_OMSORGEN),
+    prosessStegUtvidetRettPanelDefinisjoner(
+      fagsak.sakstype === fagsakYtelseType.OMSORGSPENGER_ALENE_OM_OMSORGEN,
+      featureToggles,
+    ),
     dataTilUtledingAvFpPaneler,
     fagsak,
     rettigheter,
--- packages/behandling-utvidet-rett/src/panelDefinisjoner/prosessStegPaneler/inngangsvilkarPaneler/omsorgenForMikrofrontend/KartleggePropertyTilOmsorgenForMikrofrontendKomponent.ts
+++ packages/behandling-utvidet-rett/src/panelDefinisjoner/prosessStegPaneler/inngangsvilkarPaneler/omsorgenForMikrofrontend/KartleggePropertyTilOmsorgenForMikrofrontendKomponent.ts
@@ -4,12 +4,15 @@ import vilkarUtfallType from '@fpsak-frontend/kodeverk/src/vilkarUtfallType';
 import { Behandling } from '@k9-sak-web/types';
 import fagsakTsType from '@k9-sak-web/types/src/fagsakTsType';
 import { FormStateType } from '@fpsak-frontend/form/src/types/FormStateType';
+import vilkarType from '@fpsak-frontend/kodeverk/src/vilkarType';
 import UtvidetRettMikrofrontendVisning from '../../../../types/MikrofrontendKomponenter';
 import { generereInfoForVurdertVilkar } from '../../UtvidetRettOmsorgenForMikrofrontendFelles';
 import { OmsorgenForProps } from '../../../../types/utvidetRettMikrofrontend/OmsorgProps';
 import { InformasjonTilLesemodus } from '../../../../types/utvidetRettMikrofrontend/informasjonTilLesemodus';
-import { AksjonspunktInformasjon, VilkarInformasjon, } from '../../../../types/utvidetRettMikrofrontend/KartleggePropertyTilMikrofrontendTypes';
-import vilkarType from '@fpsak-frontend/kodeverk/src/vilkarType';
+import {
+  AksjonspunktInformasjon,
+  VilkarInformasjon,
+} from '../../../../types/utvidetRettMikrofrontend/KartleggePropertyTilMikrofrontendTypes';
 
 interface PropTypes {
   isReadOnly: boolean;
@@ -32,7 +35,7 @@ const KartleggePropertyTilOmsorgenForMikrofrontendKomponent = ({
   vilkarInformasjon,
   fagsaksType,
   FormState,
-  harBarnSoktForRammevedtakOmKroniskSyk
+  harBarnSoktForRammevedtakOmKroniskSyk,
 }: PropTypes) => {
   const { aksjonspunkter, isAksjonspunktOpen } = aksjonspunktInformasjon;
   const { vilkar, status } = vilkarInformasjon;
@@ -46,7 +49,8 @@ const KartleggePropertyTilOmsorgenForMikrofrontendKomponent = ({
 
   const vilkaretVurderesManuelltMedAksjonspunkt = aksjonspunkt && omsorgenForVilkar;
   // Vilkåret kan kun bli automatisk innvilget. Dersom det blir automatiskt avslått resulterer det i manuell vurdering via aksjonspunkt.
-  const vilkaretErAutomatiskInnvilget = !aksjonspunkt && omsorgenForVilkar && omsorgenForVilkar.perioder[0]?.vilkarStatus === vilkarUtfallType.OPPFYLT;
+  const vilkaretErAutomatiskInnvilget =
+    !aksjonspunkt && omsorgenForVilkar && omsorgenForVilkar.perioder[0]?.vilkarStatus === vilkarUtfallType.OPPFYLT;
 
   if (vilkaretVurderesManuelltMedAksjonspunkt) {
     const skalVilkarsUtfallVises = behandling.status === behandlingStatus.AVSLUTTET;
@@ -83,7 +87,7 @@ const KartleggePropertyTilOmsorgenForMikrofrontendKomponent = ({
         },
         formState: FormState,
       } as OmsorgenForProps,
-    }
+    };
   }
 
   if (vilkaretErAutomatiskInnvilget) {
@@ -96,15 +100,10 @@ const KartleggePropertyTilOmsorgenForMikrofrontendKomponent = ({
         barn: angitteBarn.map(barn => barn.personIdent),
         harBarnSoktForRammevedtakOmKroniskSyk,
         vedtakFattetVilkarOppfylt: true,
-        informasjonOmVilkar: generereInfoForVurdertVilkar(
-          true,
-          omsorgenForVilkar,
-          '',
-          'Omsorgen for',
-        ),
+        informasjonOmVilkar: generereInfoForVurdertVilkar(true, omsorgenForVilkar, '', 'Omsorgen for'),
         formState: FormState,
       } as OmsorgenForProps,
-    }
+    };
   }
 
   return {};
