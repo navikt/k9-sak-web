ce70f9ea0140869197bda5e8196f59a1214b7299
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2022-08-26 15:11:44 +0200

    Kjører devbygg i Github actions da formålet er å se om bygget kjører (#3853)
    Og at de sjekkes for circular dependencies

--- webpack/common/plugins.ts
+++ webpack/common/plugins.ts
@@ -13,54 +13,55 @@ import { PUBLIC_ROOT, LANG_DIR } from '../paths';
 
 const PACKAGES_DIR = path.resolve(__dirname, '../packages');
 
-const pluginConfig = [
-  new ESLintPlugin({
-    lintDirtyModulesOnly: IS_DEV,
-    context: PACKAGES_DIR,
-    extensions: ['tsx', 'ts'],
-    failOnWarning: false,
-    failOnError: !IS_DEV,
-    fix: IS_DEV,
-    overrideConfigFile: path.resolve(
-      __dirname,
-      IS_DEV ? '../../eslint/eslintrc.dev.js' : '../../eslint/eslintrc.prod.js',
-    ),
-    // cache: true,
-  }),
-  new MiniCssExtractPlugin({
-    filename: 'style_[contenthash].css',
-    ignoreOrder: true,
-  }),
-  new HtmlWebpackPlugin({
-    filename: 'index.html',
-    favicon: path.join(PUBLIC_ROOT, 'favicon.ico'),
-    template: path.join(PUBLIC_ROOT, 'index.html'),
-  }),
-  new CopyWebpackPlugin({
-    patterns: [
-      {
-        from: LANG_DIR,
-        to: 'sprak/[name][ext]',
-        force: true,
-        transform: {
-          transformer: (content, path) => content,
-          cache: {
-            keys: {
-              key: '[contenthash]',
+export default function (env?) {
+  const isCIBuild = env?.ci === 'true';
+  return [
+    new ESLintPlugin({
+      lintDirtyModulesOnly: IS_DEV,
+      context: PACKAGES_DIR,
+      extensions: ['tsx', 'ts'],
+      failOnWarning: false,
+      failOnError: !IS_DEV,
+      fix: IS_DEV,
+      overrideConfigFile: path.resolve(
+        __dirname,
+        IS_DEV ? '../../eslint/eslintrc.dev.js' : '../../eslint/eslintrc.prod.js',
+      ),
+      // cache: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style_[contenthash].css',
+      ignoreOrder: true,
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      favicon: path.join(PUBLIC_ROOT, 'favicon.ico'),
+      template: path.join(PUBLIC_ROOT, 'index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: LANG_DIR,
+          to: 'sprak/[name][ext]',
+          force: true,
+          transform: {
+            transformer: (content, path) => content,
+            cache: {
+              keys: {
+                key: '[contenthash]',
+              },
             },
           },
         },
-      },
-    ],
-  }),
-  new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /nb/),
-  IS_PROD
-    ? new CircularDependencyPlugin({
-        exclude: /node_modules/,
-        failOnError: true,
-      })
-    : false,
-  new ExternalTemplateRemotesPlugin(),
-].filter(Boolean);
-
-export default pluginConfig;
+      ],
+    }),
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /nb/),
+    IS_PROD || isCIBuild
+      ? new CircularDependencyPlugin({
+          exclude: /node_modules/,
+          failOnError: true,
+        })
+      : false,
+    new ExternalTemplateRemotesPlugin(),
+  ].filter(Boolean);
+}
