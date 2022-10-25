be906b2dc22badd29cac4a50170020a2096c6833
Thomas H. Wiberg <thomas@thawi.no>
2022-10-25 23:52:32 +0200

    Ubrukt

--- packages/prosess-vedtak/src/components/VedtakSubmit.tsx
+++ packages/prosess-vedtak/src/components/VedtakSubmit.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import behandlingStatusCode from '@fpsak-frontend/kodeverk/src/behandlingStatus';
 import { FormattedMessage, useIntl } from 'react-intl';
