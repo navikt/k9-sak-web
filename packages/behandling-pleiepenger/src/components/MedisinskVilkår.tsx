d0238100e2d8aae6118eddd24e1a4d6acf6207be
Hallvard Andreas Stark <hallvard@scel.to>
2022-11-10 12:51:11 +0100

    Fikser feil i medisinsk vilkår

--- packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
+++ packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
@@ -62,8 +62,8 @@ export default ({
   return (
     <MicroFrontend
       id={medisinskVilkårAppID}
-      jsSrc="http://localhost:8081/main.js"
-      stylesheetSrc="http://localhost:8081/styles.css"
+      jsSrc="/k9/microfrontend/medisinsk-vilkar/1/app.js"
+      stylesheetSrc="/k9/microfrontend/medisinsk-vilkar/1/styles.css"
       noCache
       onReady={() =>
         initializeMedisinskVilkår(
