d2b9520b91a0638c8648b59f21b601d7775951ae
vebnor <vebjorn.nordby@nav.no>
2022-12-14 14:47:59 +0100

    knapper og greier

--- packages/fakta-opplaering/src/gjennomgaaOpplaering/GjennomgaaOpplaeringForm.tsx
+++ packages/fakta-opplaering/src/gjennomgaaOpplaering/GjennomgaaOpplaeringForm.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
-import { Period } from '@navikt/k9-period-utils';
-import { Box, Margin, DetailView, LabelledContent, Form } from '@navikt/ft-plattform-komponenter';
+import { Box, Margin, DetailView, LabelledContent } from '@navikt/ft-plattform-komponenter';
 import { TextAreaFormik } from '@fpsak-frontend/form';
+import { PeriodpickerList } from '@navikt/k9-form-utils';
 import { Calender } from '@navikt/ds-icons';
 
 import { FaktaOpplaeringContext } from '@k9-sak-web/behandling-opplaeringspenger/src/panelDefinisjoner/faktaPaneler/OpplaeringFaktaPanelDef';
@@ -10,10 +10,14 @@ import { required } from '@fpsak-frontend/utils';
 import { Formik } from 'formik';
 import RadioGroupFormik from '@fpsak-frontend/form/src/RadioGroupFormik';
 import { Button } from '@navikt/ds-react';
+import { Period } from '@navikt/k9-period-utils';
+import DeleteButton from './delete-button/DeleteButton';
+import AddButton from './add-button/AddButton';
 
 enum fieldname {
   BEGRUNNELSE = 'BEGRUNNELSE',
   GODKJENT_OPPLAERING = 'GODKJENT_OPPLAERING',
+  PERIODER = 'PERIODER',
 }
 
 enum RadioOptions {
@@ -64,7 +68,7 @@ const GjennomgaaOpplaeringForm = ({ vurdering, avbrytRedigering, erRedigering }:
         initialValues={initialValues}
         onSubmit={values => løsAksjonspunktGjennomgåOpplæring(mapValuesTilAksjonspunktPayload(values))}
       >
-        {({ handleSubmit, isSubmitting }) => (
+        {({ handleSubmit, isSubmitting, values }) => (
           <>
             <div>
               <Calender /> <span>{vurdering.opplæring.prettifyPeriod()}</span>
@@ -83,6 +87,7 @@ const GjennomgaaOpplaeringForm = ({ vurdering, avbrytRedigering, erRedigering }:
                 legend="Er det opplæring i perioden?"
                 options={[
                   { value: RadioOptions.JA, label: 'Ja' },
+                  { value: RadioOptions.DELVIS, label: 'Ja, i deler av perioden' },
                   { value: RadioOptions.NEI, label: 'Nei' },
                 ]}
                 validate={[required]}
@@ -90,6 +95,34 @@ const GjennomgaaOpplaeringForm = ({ vurdering, avbrytRedigering, erRedigering }:
                 disabled={readOnly}
               />
             </Box>
+            <Box marginTop={Margin.xLarge}>
+              <RadioGroupFormik
+                legend="I hvilken periode er det opplæring?"
+                options={[
+                  { value: RadioOptions.JA, label: 'Ja' },
+                  { value: RadioOptions.DELVIS, label: 'Ja, i deler av perioden' },
+                  { value: RadioOptions.NEI, label: 'Nei' },
+                ]}
+                validate={[required]}
+                name={fieldname.GODKJENT_OPPLAERING}
+                disabled={readOnly}
+              />
+            </Box>
+            {values[fieldname.GODKJENT_OPPLAERING] === RadioOptions.DELVIS && <Box marginTop={Margin.xLarge} />}
+            {/*             {perioderUtenBehovForNattevåk.length > 0 && (
+              <Box marginTop={Margin.xLarge}>
+                <AlertStripeInfo>
+                  <LabelledContent
+                    label="Resterende perioder har søkeren ikke behov for nattevåk:"
+                    content={perioderUtenBehovForNattevåk.map(periode => (
+                      <p key={`${periode.fom}-${periode.tom}`} style={{ margin: 0 }}>
+                        {periode.prettifyPeriod()}
+                      </p>
+                    ))}
+                  />
+                </AlertStripeInfo>
+              </Box>
+            )} */}
             <Box marginTop={Margin.xLarge}>
               <Button
                 size="small"
