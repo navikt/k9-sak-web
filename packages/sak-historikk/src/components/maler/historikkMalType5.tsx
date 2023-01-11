48d69dca5212ea20734736ed16a9ec9a704be498
Espen Velsvik <espenjv@users.noreply.github.com>
2023-01-11 09:01:18 +0100

    Historikk for 5067 (#4353)

--- packages/sak-historikk/src/components/maler/historikkMalType5.tsx
+++ packages/sak-historikk/src/components/maler/historikkMalType5.tsx
@@ -86,10 +86,7 @@ const formatChangedField = (
   const fromValue = findEndretFeltVerdi(endretFelt, endretFelt.fraVerdi, intl, getKodeverknavn);
   const toValue = findEndretFeltVerdi(endretFelt, endretFelt.tilVerdi, intl, getKodeverknavn);
 
-  if (
-    endretFelt.fraVerdi !== null &&
-    endretFelt.endretFeltNavn.kode !== historikkEndretFeltTypeCodes.FORDELING_FOR_NY_ANDEL.kode
-  ) {
+  if (endretFelt.fraVerdi !== null) {
     return (
       <FormattedMessage
         id="Historikk.Template.5.ChangedFromTo"
