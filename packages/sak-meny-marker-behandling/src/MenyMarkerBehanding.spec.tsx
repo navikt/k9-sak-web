c9d3c6a204fda2ed84ababc41af9c777a406e17e
vebnor <vebjorn.nordby@nav.no>
2023-05-09 15:05:50 +0200

    fikse test

--- packages/sak-meny-marker-behandling/src/MenyMarkerBehanding.spec.tsx
+++ packages/sak-meny-marker-behandling/src/MenyMarkerBehanding.spec.tsx
@@ -1,10 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
+import { K9sakApiKeys, requestApi } from '@k9-sak-web/sak-app/src/data/k9sakApi';
+import { act } from 'react-dom/test-utils';
 import MenyMarkerBehandling from './MenyMarkerBehandling';
 
 describe('<MenyMarkerBehandling', () => {
-  it('skal vise inputfelt for tekst gitt at checkbox er valgt', () => {
+  it('skal vise inputfelt for tekst gitt at checkbox er valgt', async () => {
+    requestApi.mock(K9sakApiKeys.FEATURE_TOGGLE, [
+      {
+        key: 'LOS_MARKER_BEHANDLING_SUBMIT',
+        value: 'true',
+      },
+    ]);
+
     render(
       <MenyMarkerBehandling
         behandlingUuid="123"
@@ -15,7 +24,10 @@ describe('<MenyMarkerBehandling', () => {
       />,
     );
     expect(screen.queryByLabelText('Kommentar')).toBe(null);
-    userEvent.click(screen.getByLabelText('Behandlingen er hastesak'));
+    await act(async () => {
+      userEvent.click(screen.getByLabelText('Behandlingen er hastesak'));
+    });
+
     expect(screen.getByLabelText('Kommentar')).toBeInTheDocument();
   });
 });
