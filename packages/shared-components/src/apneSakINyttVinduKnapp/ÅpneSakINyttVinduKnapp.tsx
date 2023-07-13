601b62684f4e2ea430e4fcf0eeb6a7855f34ce75
Thomas H. Wiberg <thomas@thawi.no>
2023-07-13 09:35:02 +0200

    Nytt vindu (#4826)
    * Legg til knapp for å åpne sak i nytt vindu
    
    * Justere størrelser på elementer i editor
    
    * Props & children

---
+++ packages/shared-components/src/apneSakINyttVinduKnapp/ÅpneSakINyttVinduKnapp.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { ExternalLink } from '@navikt/ds-icons';
+import { Button, ButtonProps } from '@navikt/ds-react';
+
+import { useHref, useLocation } from 'react-router-dom';
+
+type ÅpneSakINyttVinduProps = {
+  value?: string;
+  overridePath?: string;
+  target?: string;
+};
+
+const ÅpneSakINyttVindu: React.FC<ButtonProps & ÅpneSakINyttVinduProps> = ({
+  children,
+  variant = 'tertiary',
+  size = 'small',
+  disabled,
+  loading,
+  icon = <ExternalLink aria-hidden />,
+  iconPosition,
+  value = 'Åpne sak i ny fane',
+  overridePath,
+  target = '_blank',
+}) => {
+  const { pathname } = useLocation();
+  const href = useHref(pathname);
+
+  const handleClick = () => {
+    if (pathname.length > 0) {
+      window.open(overridePath || href, target);
+    }
+  };
+
+  return (
+    <Button
+      variant={variant}
+      onClick={handleClick}
+      icon={icon}
+      size={size}
+      iconPosition={iconPosition}
+      disabled={disabled === undefined ? pathname.length < 1 : disabled}
+      loading={loading}
+    >
+      {children && { children }}
+      {!children && { value }}
+    </Button>
+  );
+};
+
+export default ÅpneSakINyttVindu;
