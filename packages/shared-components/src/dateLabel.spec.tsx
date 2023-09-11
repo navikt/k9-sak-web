5959dcbe7a2442447199eabf69652a31f30884a5
Hallvard Andreas Stark <hallvard@scel.to>
2023-09-11 13:50:31 +0200

    prøver å fikse test

--- packages/shared-components/src/dateLabel.spec.tsx
+++ packages/shared-components/src/dateLabel.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { format } from 'date-fns';
 
 import DateLabel from './DateLabel';
@@ -7,11 +7,11 @@ import DateLabel from './DateLabel';
 describe('<DateLabel>', () => {
   it('skal ha en FormattedDate-komponent', () => {
     render(<DateLabel dateString="2017-10-10" />);
-    expect(screen.queryByText('10.10.2017')).toBeInTheDocument();
+    waitFor(() => expect(screen.queryByText('10.10.2017')).toBeInTheDocument());
   });
 
   it('skal sjekke at dato blir formatert korrekt', () => {
     render(<DateLabel dateString="2017-10-10" />);
-    expect(screen.queryByText(format(new Date('10.10.2017'), 'dd.MM.yyyy'))).toBeInTheDocument();
+    waitFor(() => expect(screen.queryByText(format(new Date('10.10.2017'), 'dd.MM.yyyy'))).toBeInTheDocument());
   });
 });
