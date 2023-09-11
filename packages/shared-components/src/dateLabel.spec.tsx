4e82ca3a5a089cdbd30373260478fa70b853f4e0
Hallvard Andreas Stark <hallvard@scel.to>
2023-09-11 13:25:55 +0200

    Fix test

--- packages/shared-components/src/dateLabel.spec.tsx
+++ packages/shared-components/src/dateLabel.spec.tsx
@@ -6,12 +6,12 @@ import DateLabel from './DateLabel';
 
 describe('<DateLabel>', () => {
   it('skal ha en FormattedDate-komponent', () => {
-    render(<DateLabel dateString="10.10.2017" />);
+    render(<DateLabel dateString="2017-10-10" />);
     expect(screen.queryByText('10.10.2017')).toBeInTheDocument();
   });
 
   it('skal sjekke at dato blir formatert korrekt', () => {
-    render(<DateLabel dateString="10.10.2017" />);
+    render(<DateLabel dateString="2017-10-10" />);
     expect(screen.queryByText(format(new Date('10.10.2017'), 'dd.MM.yyyy'))).toBeInTheDocument();
   });
 });
