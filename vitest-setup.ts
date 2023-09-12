b04cfcd69cb8814796080af48e5f9e04a3ca9e24
Hallvard Andreas Stark <hallvard@scel.to>
2023-09-12 11:11:59 +0200

    fix stub

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -3,10 +3,8 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import '@testing-library/jest-dom/vitest';
 import { switchOnTestMode } from '@k9-sak-web/rest-api';
 
-// vi.spyOn(window.URL, 'createObjectURL').mockImplementation(() => 'http://fake.url');
-if (typeof window?.URL?.createObjectURL === 'undefined') {
-  window.URL.createObjectURL = vi.fn();
-}
+vi.stubGlobal('URL', { createObjectURL: vi.fn() });
+
 vi.stubGlobal('open', vi.fn());
 
 switchOnTestMode();
