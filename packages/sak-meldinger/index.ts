a80e777957ad4286c9df7e5dacae26b5c08b7ac5
Qadeer Ahmad Khan <qadeer.ahmad.khan@nav.no>
2023-09-21 13:53:19 +0200

    testfix

--- packages/sak-meldinger/index.ts
+++ packages/sak-meldinger/index.ts
@@ -1,2 +1,3 @@
 export { default } from './src/MeldingerSakIndex';
 export { default as MessagesModalSakIndex } from './src/MessagesModalSakIndex';
+export type { FormValues } from './src/components/MessagesMedMedisinskeTypeBrevmal';
