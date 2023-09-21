dbebea5f347f93e74abefd24839a5c86233dacd7
Qadeer Ahmad Khan <qadeer.ahmad.khan@nav.no>
2023-09-21 13:44:09 +0200

    lintfix

--- packages/sak-meldinger/index.ts
+++ packages/sak-meldinger/index.ts
@@ -1,3 +1,2 @@
 export { default } from './src/MeldingerSakIndex';
 export { default as MessagesModalSakIndex } from './src/MessagesModalSakIndex';
-export type { FormValues } from './src/components/Messages';
