3162a6b5e7e52cd0493b33f4d0cb64a8251c37fc
Hallvard Andreas Stark <hallvard@scel.to>
2023-09-25 13:35:01 +0200

    fikset kjøring av bun

--- packages/sak-app/src/app/paths.spec.ts
+++ packages/sak-app/src/app/paths.spec.ts
@@ -2,6 +2,10 @@ import { getPathToFplos } from './paths';
 
 describe('paths', () => {
   it('skal hente url til Fplos gitt Fpsak url', () => {
+    Object.defineProperty(globalThis, 'window', {
+      value: { location: { href: 'https://example.com/es' } },
+      writable: true,
+    });
     expect(getPathToFplos()).toEqual(null);
   });
 });
