072d26d4d26d8efa05f1f3e15b2c31c169949773
Hallvard Andreas Stark <hallvard@scel.to>
2023-10-03 19:10:56 +0200

    forsøk på oppgradering til react 18

--- packages/utils-test/src/setup-test-env.ts
+++ packages/utils-test/src/setup-test-env.ts
@@ -1,6 +1,6 @@
 import 'raf/polyfill';
 import { configure as configureEnzyme } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 import configure from '@commercetools/enzyme-extensions';
 import ShallowWrapper from 'enzyme/ShallowWrapper';
 import { switchOnTestMode } from '@k9-sak-web/rest-api';
