63ee616339cb32226dc11c2d5b3bf45e64aaca26
Hallvard Andreas Stark <hallvard@scel.to>
2023-10-03 20:33:07 +0200

    Forsøker å fikse tester

--- packages/utils-test/src/setup-test-env.ts
+++ packages/utils-test/src/setup-test-env.ts
@@ -4,9 +4,11 @@ import Adapter from '@cfaester/enzyme-adapter-react-18';
 import configure from '@commercetools/enzyme-extensions';
 import ShallowWrapper from 'enzyme/ShallowWrapper';
 import { switchOnTestMode } from '@k9-sak-web/rest-api';
+import { TextEncoder, TextDecoder } from 'util';
 
 configureEnzyme({ adapter: new Adapter() });
 
 configure(ShallowWrapper);
 
 switchOnTestMode();
+Object.assign(global, { TextDecoder, TextEncoder });
