0bcc17f775895098b1719bd65b62f2ae025e9f2f
Hallvard Andreas Stark <hallvard@scel.to>
2023-10-03 14:15:49 +0200

    fix import

--- packages/utils/src/hentAktivePerioderFraVilkar.ts
+++ packages/utils/src/hentAktivePerioderFraVilkar.ts
@@ -1,5 +1,5 @@
 import { Vilkar } from '@k9-sak-web/types';
-import { dateStringSorter } from '@navikt/k9-date-utils';
+import { dateStringSorter } from '@navikt/k9-fe-date-utils';
 
 const hentAktivePerioderFraVilkar = (vilkar: Vilkar[], visAllePerioder: boolean) => {
   const [activeVilkår] = vilkar;
