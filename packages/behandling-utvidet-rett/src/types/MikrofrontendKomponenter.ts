d51fdea90b774201625b2d6e354d286780835c79
Hallvard Andreas Stark <hallvard@scel.to>
2023-11-08 22:30:32 +0100

    fjernet ubrukte komponenter og typer

--- packages/behandling-utvidet-rett/src/types/MikrofrontendKomponenter.ts
+++
@@ -1,8 +0,0 @@
-enum UtvidetRettMikrofrontendVisning {
-  KORRIGERE_PERIODER = 'KorrigerePerioder',
-  VILKAR_KRONISK_SYKT_BARN = 'VilkarKroniskSyktBarn',
-  VILKAR_MIDLERTIDIG_ALENE = 'VilkarMidlertidigAlene',
-  VILKAR_ALENE_OM_OMSORGEN = 'AleneOmOmsorgen',
-  OMSORG = 'Omsorg',
-}
-export default UtvidetRettMikrofrontendVisning;
