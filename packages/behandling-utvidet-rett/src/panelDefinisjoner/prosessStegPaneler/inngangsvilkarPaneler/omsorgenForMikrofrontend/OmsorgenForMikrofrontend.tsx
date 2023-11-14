5145866df94d880ad75d2b8484ea71b0e787c9f8
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2023-11-14 21:28:54 +0100

    Fikset feil innsending av props (#5258)

--- packages/behandling-utvidet-rett/src/panelDefinisjoner/prosessStegPaneler/inngangsvilkarPaneler/omsorgenForMikrofrontend/OmsorgenForMikrofrontend.tsx
+++ packages/behandling-utvidet-rett/src/panelDefinisjoner/prosessStegPaneler/inngangsvilkarPaneler/omsorgenForMikrofrontend/OmsorgenForMikrofrontend.tsx
@@ -3,11 +3,29 @@ import { Omsorgsdager } from '@navikt/k9-fe-omsorgsdager';
 import React from 'react';
 import KartleggePropertyTilOmsorgenForMikrofrontendKomponent from './KartleggePropertyTilOmsorgenForMikrofrontendKomponent';
 
-export default props => (
+export default ({
+  isReadOnly,
+  aksjonspunkter,
+  isAksjonspunktOpen,
+  submitCallback,
+  behandling,
+  status,
+  vilkar,
+  angitteBarn,
+  fagsaksType,
+  harBarnSoktForRammevedtakOmKroniskSyk,
+}) => (
   <Omsorgsdager
     containerData={KartleggePropertyTilOmsorgenForMikrofrontendKomponent({
-      ...props,
+      isReadOnly,
+      submitCallback,
+      behandling,
+      angitteBarn,
+      aksjonspunktInformasjon: { aksjonspunkter, isAksjonspunktOpen },
+      vilkarInformasjon: { vilkar, status },
+      fagsaksType,
       FormState,
+      harBarnSoktForRammevedtakOmKroniskSyk,
     })}
   />
 );
