5b1d462aa9c54b2b69a106202c3a5a849332904f
Hallvard Andreas Stark <hallvard@scel.to>
2023-11-28 21:31:25 +0100

    Fikser to feil der data prøver å hentes ut fra undefined objekt

---
+++ packages/fakta-inntekt-og-ytelser/src/InntektType.ts
@@ -0,0 +1,12 @@
+import { Kodeverk } from '@k9-sak-web/types';
+
+export interface Inntekt {
+  fom: string;
+  tom: string;
+  utbetaler: string;
+  inntektspostType: Kodeverk;
+  ytelseType: Kodeverk;
+  belop: number;
+  ytelse: boolean;
+  navn: string;
+}
