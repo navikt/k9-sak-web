265167a13586620ff1f23c55095efcf3892ee9b5
Hallvard Andreas Stark <hallvard@scel.to>
2024-01-04 14:42:14 +0100

    Bumper ft-pakker Fjerner gammel fakta-beregning Fjerner feature-toggle

--- packages/behandling-opplaeringspenger/src/panelDefinisjoner/faktaPaneler/BeregningFaktaPanelDef.tsx
+++ packages/behandling-opplaeringspenger/src/panelDefinisjoner/faktaPaneler/BeregningFaktaPanelDef.tsx
@@ -5,8 +5,7 @@ import aksjonspunktCodes from '@fpsak-frontend/kodeverk/src/aksjonspunktCodes';
 import { FaktaPanelDef } from '@k9-sak-web/behandling-felles';
 import { konverterKodeverkTilKode, mapVilkar, transformBeregningValues } from '@fpsak-frontend/utils';
 import vilkarType from '@fpsak-frontend/kodeverk/src/vilkarType';
-import { BeregningFaktaIndex } from '@navikt/ft-fakta-beregning';
-import { BeregningFaktaIndex as BeregningFaktaIndexRedesign } from '@navikt/ft-fakta-beregning-redesign';
+import { BeregningFaktaIndex } from '@navikt/ft-fakta-beregning-redesign';
 import { OpplaeringspengerBehandlingApiKeys } from '../../data/opplaeringspengerBehandlingApi';
 
 class BeregningFaktaPanelDef extends FaktaPanelDef {
@@ -31,22 +30,7 @@ class BeregningFaktaPanelDef extends FaktaPanelDef {
     const deepCopyProps = JSON.parse(JSON.stringify(props));
     konverterKodeverkTilKode(deepCopyProps);
     const bgVilkaret = deepCopyProps.vilkar.find(v => v.vilkarType === vilkarType.BEREGNINGSGRUNNLAGVILKARET);
-    if (props.featureToggles?.FAKTA_BEREGNING_REDESIGN) {
-      return (
-        <BeregningFaktaIndexRedesign
-          {...deepCopyProps}
-          kodeverkSamling={deepCopyProps.alleKodeverk}
-          beregningsgrunnlag={deepCopyProps.beregningsgrunnlag}
-          arbeidsgiverOpplysningerPerId={deepCopyProps.arbeidsgiverOpplysningerPerId}
-          submitCallback={aksjonspunktData => props.submitCallback(transformBeregningValues(aksjonspunktData))}
-          formData={props.formData}
-          setFormData={props.setFormData}
-          vilkar={mapVilkar(bgVilkaret, props.beregningreferanserTilVurdering)}
-          skalKunneOverstyreAktiviteter={false}
-          skalKunneAvbryteOverstyring
-        />
-      );
-    }
+
     return (
       <BeregningFaktaIndex
         {...deepCopyProps}
