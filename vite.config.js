1edf2eafbe06986415caaef35cdc7197281fce63
Hallvard Andreas Stark <hallvard@scel.to>
2024-01-26 14:43:27 +0100

    fjerner proxy for mikrofrontends

--- vite.config.js
+++ vite.config.js
@@ -49,50 +49,9 @@ export default ({ mode }) => {
         '/k9/oppdrag': createProxy(process.env.APP_URL_K9OPPDRAG || 'http://localhost:8070'),
         'k9/klage': createProxy(process.env.APP_URL_KLAGE || 'http://localhost:8701'),
         'k9/tilbake': createProxy(process.env.APP_URL_K9TILBAKE || 'http://localhost:8030'),
-
         'k9/diagnosekoder/': createProxy(process.env.APP_URL_DIAGNOSEKODER || 'http://localhost:8300', {
           '^/k9/diagnosekoder/': '/diagnosekoder',
         }),
-
-        '/k9/microfrontend/omsorgsdager': createProxy(
-          process.env.OMSORGSDAGER_FRONTEND_URL || 'http://localhost:8088',
-          {
-            '^/k9/microfrontend/omsorgsdager': '',
-          },
-        ),
-        '/k9/microfrontend/medisinsk-vilkar': createProxy(
-          process.env.MEDISINSK_VILKAR_FRONTEND_URL || 'http://localhost:8081',
-          {
-            '^/k9/microfrontend/medisinsk-vilkar': '',
-          },
-        ),
-        '/k9/microfrontend/omsorgen-for': createProxy(
-          process.env.OMSORGEN_FOR_FRONTEND_URL || 'http://localhost:8282',
-          {
-            '^/k9/microfrontend/omsorgen-for': '',
-          },
-        ),
-        '/k9/microfrontend/psb-om-barnet': createProxy(
-          process.env.PSB_OM_BARNET_FRONTEND_URL || 'http://localhost:8585',
-          {
-            '^/k9/microfrontend/psb-om-barnet': '',
-          },
-        ),
-        '/k9/microfrontend/psb-etablert-tilsyn': createProxy(
-          process.env.PSB_ETABLERT_TILSYN_FRONTEND_URL || 'http://localhost:8484',
-          {
-            '^/k9/microfrontend/psb-etablert-tilsyn': '',
-          },
-        ),
-        '/k9/microfrontend/psb-uttak': createProxy(process.env.PSB_UTTAK_FRONTEND_URL || 'http://localhost:8181', {
-          '^/k9/microfrontend/psb-uttak': '',
-        }),
-        '/k9/microfrontend/psb-inntektsmelding': createProxy(
-          process.env.PSB_INNTEKTSMELDING_FRONTEND_URL || 'http://localhost:8383',
-          {
-            '^/k9/microfrontend/psb-inntektsmelding': '',
-          },
-        ),
         'k9/endringslogg': createProxy(
           process.env.ENDRINGSLOGG_URL || 'https://familie-endringslogg.intern.dev.nav.no',
           {
