2c5583141e7e45568bb20d596a7b91a751995896
Hallvard Andreas Stark <hallvard@scel.to>
2024-01-30 14:29:31 +0100

    Skrevet om tester for sak-support-meny og sak-sok

--- packages/sak-sok/src/components/SearchForm.spec.tsx
+++ packages/sak-sok/src/components/SearchForm.spec.tsx
@@ -1,35 +1,47 @@
-import React from 'react';
-import sinon from 'sinon';
-
+import { renderWithIntlAndReduxForm } from '@fpsak-frontend/utils-test';
 import { reduxFormPropsMock } from '@fpsak-frontend/utils-test/src/redux-form-test-helper';
-
+import { act, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { reduxForm } from 'redux-form';
+import sinon from 'sinon';
+import { intlMock } from '../../i18n/index';
+import messages from '../../i18n/nb_NO.json';
 import { SearchForm } from './SearchForm';
-import shallowWithIntl, { intlMock } from '../../i18n/index';
 
 describe('<Search>', () => {
   it('skal ha et søkefelt og en søkeknapp', () => {
-    const wrapper = shallowWithIntl(
-      <SearchForm intl={intlMock} searchString="" searchStarted {...reduxFormPropsMock} />,
+    const MockForm = reduxForm({ form: 'mock', onSubmit: vi.fn() })(({ children }) => <div>{children}</div>);
+    renderWithIntlAndReduxForm(
+      <MockForm>
+        <SearchForm intl={intlMock} searchString="" searchStarted {...reduxFormPropsMock} />{' '}
+      </MockForm>,
+      { messages },
     );
-
-    expect(wrapper.find('InputField')).toHaveLength(1);
-    expect(wrapper.find('Hovedknapp')).toHaveLength(1);
+    expect(screen.getByLabelText('Saksnummer eller fødselsnummer/D-nummer')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Laster' })).toBeInTheDocument();
   });
 
-  it('skal utføre søk når en trykker på søkeknapp', () => {
+  it('skal utføre søk når en trykker på søkeknapp', async () => {
+    const MockForm = reduxForm({ form: 'mock', onSubmit: vi.fn() })(({ children }) => <div>{children}</div>);
     const onButtonClick = sinon.spy();
 
-    const wrapper = shallowWithIntl(
-      <SearchForm intl={intlMock} searchString="" searchStarted {...reduxFormPropsMock} handleSubmit={onButtonClick} />,
+    renderWithIntlAndReduxForm(
+      <MockForm>
+        <SearchForm
+          intl={intlMock}
+          searchString="test"
+          searchStarted={false}
+          {...reduxFormPropsMock}
+          handleSubmit={onButtonClick}
+        />{' '}
+      </MockForm>,
+      { messages },
     );
 
-    const form = wrapper.find('form');
-    form.simulate('submit', {
-      preventDefault() {
-        return undefined;
-      },
+    await act(async () => {
+      await userEvent.click(screen.getByRole('button', { name: 'Søk' }));
     });
-
     expect(onButtonClick).toHaveProperty('callCount', 1);
   });
 });
