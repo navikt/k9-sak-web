75b9c2a5d1dd949b2dc3026b753a24cda3ea6e0c
Hallvard Andreas Stark <hallvard@scel.to>
2024-02-06 14:14:09 +0100

    rettet feil i sortering av importer

--- packages/sak-app/src/bootstrap.tsx
+++ packages/sak-app/src/bootstrap.tsx
@@ -13,10 +13,10 @@ import { BrowserRouter } from 'react-router-dom';
  * - https://bugs.chromium.org/p/chromium/issues/detail?id=1215606&q=norwegian&can=2
  * - https://github.com/formatjs/formatjs/issues/3066
  */
-import '@formatjs/intl-datetimeformat/locale-data/nb';
 import '@formatjs/intl-datetimeformat/polyfill-force';
-import '@formatjs/intl-numberformat/locale-data/nb';
+import '@formatjs/intl-datetimeformat/locale-data/nb';
 import '@formatjs/intl-numberformat/polyfill-force';
+import '@formatjs/intl-numberformat/locale-data/nb';
 
 import AppIndex from './app/AppIndex';
 import configureStore from './configureStore';
@@ -74,7 +74,7 @@ const renderFunc = Component => {
   }
 
   const prepare = async (): Promise<void> => {
-    if (process.env.NODE_ENV === 'development') {
+    if (process.env.NODE_ENV === 'test') {
       // eslint-disable-next-line import/no-relative-packages
       const { worker } = await import('../../mocks/browser');
       worker.start({ onUnhandledRequest: 'bypass' });
