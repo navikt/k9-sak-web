4fc707787d0d9ef086725f9b0a7e0ab60c9b37f5
Jostein Stuhaug <135307852+josstn@users.noreply.github.com>
2024-02-27 11:37:31 +0100

    Bruk vite_ env variabler for feature toggles. (#5624)
    Så sleppe vi å hardkode dei i package.json.

--- _mocks/featureToggles.js
+++ _mocks/featureToggles.js
@@ -1,66 +1,66 @@
 export default [
   {
     key: 'KLAGE_KABAL',
-    value: process.env.KLAGE_KABAL,
+    value: process.env.VITE_KLAGE_KABAL,
   },
   {
     key: 'VARSELTEKST',
-    value: process.env.VARSELTEKST,
+    value: process.env.VITE_VARSELTEKST,
   },
   {
     key: 'DOKUMENTDATA',
-    value: process.env.DOKUMENTDATA,
+    value: process.env.VITE_DOKUMENTDATA,
   },
   {
     key: 'UNNTAKSBEHANDLING',
-    value: process.env.UNNTAKSBEHANDLING,
+    value: process.env.VITE_UNNTAKSBEHANDLING,
   },
   {
     key: 'SOKNADPERIODESTRIPE',
-    value: process.env.SOKNADPERIODESTRIPE,
+    value: process.env.VITE_SOKNADPERIODESTRIPE,
   },
   {
     key: 'TYPE_MEDISINSKE_OPPLYSNINGER_BREV',
-    value: process.env.TYPE_MEDISINSKE_OPPLYSNINGER_BREV,
+    value: process.env.VITE_TYPE_MEDISINSKE_OPPLYSNINGER_BREV,
   },
   {
     key: 'LOS_MARKER_BEHANDLING',
-    value: process.env.LOS_MARKER_BEHANDLING,
+    value: process.env.VITE_LOS_MARKER_BEHANDLING,
   },
   {
     key: 'LOS_MARKER_BEHANDLING_SUBMIT',
-    value: process.env.LOS_MARKER_BEHANDLING_SUBMIT,
+    value: process.env.VITE_LOS_MARKER_BEHANDLING_SUBMIT,
   },
   {
     key: 'AKSJONSPUNKT_9014',
-    value: process.env.AKSJONSPUNKT_9014,
+    value: process.env.VITE_AKSJONSPUNKT_9014,
   },
   {
     key: 'AKSJONSPUNKT_9015',
-    value: process.env.AKSJONSPUNKT_9015,
+    value: process.env.VITE_AKSJONSPUNKT_9015,
   },
   {
     key: 'FRITEKST_REDIGERING',
-    value: process.env.FRITEKST_REDIGERING,
+    value: process.env.VITE_FRITEKST_REDIGERING,
   },
   {
     key: 'SKJUL_AVSLUTTET_ARBEIDSGIVER',
-    value: process.env.SKJUL_AVSLUTTET_ARBEIDSGIVER,
+    value: process.env.VITE_SKJUL_AVSLUTTET_ARBEIDSGIVER,
   },
   {
     key: 'OMS_PUNSJSTRIPE',
-    value: process.env.OMS_PUNSJSTRIPE,
+    value: process.env.VITE_OMS_PUNSJSTRIPE,
   },
   {
     key: 'OMSORGEN_FOR_PERIODISERT',
-    value: process.env.OMSORGEN_FOR_PERIODISERT,
+    value: process.env.VITE_OMSORGEN_FOR_PERIODISERT,
   },
   {
     key: 'OVERSTYR_BEREGNING',
-    value: process.env.OVERSTYR_BEREGNING,
+    value: process.env.VITE_OVERSTYR_BEREGNING,
   },
   {
     key: 'OVERSTYRING_UTTAK',
-    value: process.env.OVERSTYRING_UTTAK,
+    value: process.env.VITE_OVERSTYRING_UTTAK,
   },
 ];
