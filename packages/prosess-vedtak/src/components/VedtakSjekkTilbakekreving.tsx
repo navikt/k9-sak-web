4429dae64c00566488bfb40bff06d879c4a365e9
Thomas H. Wiberg <thomas@thawi.no>
2024-03-01 13:26:50 +0100

    Redaksjonelle endringer, 5085

--- packages/prosess-vedtak/src/components/VedtakSjekkTilbakekreving.tsx
+++ packages/prosess-vedtak/src/components/VedtakSjekkTilbakekreving.tsx
@@ -31,13 +31,18 @@ export const VedtakSjekkTilbakekreving: React.FC<Props> = ({
     <>
       <Alert className={styles.aksjonspunktAlert} variant="warning" size="medium">
         <Heading spacing size="small" level="3">
-          Har åpen tilbakekrevingssak som kan bli påvirket
+          Saken har en åpen ytelsesbehandling og en tilbakekrevingssak. Ytelsesbehandlingen kan påvirke resultatet av
+          den åpne tilbakekrevingssaken.
         </Heading>
         <BodyLong>Vurder om tilbakekrevingssaken skal behandles først.</BodyLong>
         <VerticalSpacer twentyPx />
-        <RadioGroup legend="Behandle tilbakekrevingssaken først?" onChange={handleChange} disabled={readOnly}>
-          <Radio value="ja">Ja</Radio>
-          <Radio value="nei">Nei</Radio>
+        <RadioGroup
+          legend="Ønsker du å behandle tilbakekrevingssaken først?"
+          onChange={handleChange}
+          disabled={readOnly}
+        >
+          <Radio value="ja">Ja, sett denne behandlingen på vent (må gjøres via behandlingsmenyen)</Radio>
+          <Radio value="nei">Nei, behandle denne behandlingen videre</Radio>
         </RadioGroup>
         <VerticalSpacer twentyPx />
         <Button variant="primary" onClick={handleSubmit} type="button" disabled={deaktiverBekreftKnapp}>
