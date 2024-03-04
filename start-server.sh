8d03e6b1fc80d500d9b6ce095ff0503ca0859b9a
vebnor <vebjorn.nordby@nav.no>
2024-03-04 12:32:12 +0100

    debug

--- start-server.sh
+++ start-server.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-_shutdown_() {
-  # https://github.com/kubernetes/contrib/issues/1140
-  # https://github.com/kubernetes/kubernetes/issues/43576
-  # https://github.com/kubernetes/kubernetes/issues/64510
-  # https://nav-it.slack.com/archives/C5KUST8N6/p1543497847341300
-  echo "shutdown initialized, allowing incoming requests for 5 seconds before continuing"
-  sleep 5
-  nginx -s quit
-  wait "$pid"
-}
-trap _shutdown_ SIGTERM
-[ -d /tmp/k9/feature-toggle ] && echo "Feature toggle-directory finnes fra før, tilbakestiller" && rm -r /tmp/k9/feature-toggle/* || mkdir -p  /tmp/k9/feature-toggle
+
 envsubst < /etc/nginx/conf.d/feature-toggles.json > /tmp/k9/feature-toggle/toggles.json
 
 export APP_HOSTNAME="${HOSTNAME:-localhost}"
