8e2ac92d9a72fd18e094c8dd73a7fc632c34cc0b
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-03-08 15:00:55 +0100

    Fiks nullpointer i gammal MeldingBackendClient når gitt orgnr ikkje blir funne.
    Sidan server endepunktet vart endra for å gje betre openapi spesifikasjon returnerer det no null istadenfor tomt objekt når gitt orgnr ikkje blir funne. Endrer derfor den gamle klientkoden til å handtere dette.

--- packages/sak-app/src/behandlingsupport/melding/MeldingBackendClient.ts
+++ packages/sak-app/src/behandlingsupport/melding/MeldingBackendClient.ts
@@ -5,7 +5,7 @@ import { K9sakApiKeys, requestApi } from '../../data/k9sakApi';
 export default class MeldingBackendClient implements BackendApi {
   async getBrevMottakerinfoEreg(organisasjonsnr: string): Promise<EregOrganizationLookupResponse> {
     const resp = await requestApi.startRequest(K9sakApiKeys.BREV_MOTTAKER_ORGANISASJON, { organisasjonsnr });
-    if (resp.payload.navn) {
+    if (resp.payload?.navn) {
       // Fant namn på gitt organisasjonsnr
       return { name: resp.payload.navn };
     }
