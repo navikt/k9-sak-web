f541aa12107dde384396d8a9ecc09ff1a9b2a85b
Thomas H. Wiberg <thomas@thawi.no>
2024-03-20 14:51:44 +0100

    Kodeverk: (Pakke) fakta-bosted-soker

--- packages/fakta-bosted-soker/src/BostedSokerFaktaIndex.tsx
+++ packages/fakta-bosted-soker/src/BostedSokerFaktaIndex.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { createIntl, createIntlCache, RawIntlProvider } from 'react-intl';
-
-import { KodeverkMedNavn } from '@k9-sak-web/types';
-import kodeverkTyper from '@fpsak-frontend/kodeverk/src/kodeverkTyper';
-
+import { PersonopplysningAdresse } from '@k9-sak-web/types';
 import BostedSokerView from './components/BostedSokerView';
 import messages from '../i18n/nb_NO.json';
 
@@ -19,17 +16,7 @@ const intl = createIntl(
 
 export type BostedSokerPersonopplysninger = {
   navn: string;
-  adresser: [
-    {
-      adresseType: string;
-      adresselinje1: string;
-      adresselinje2?: string;
-      adresselinje3?: string;
-      postNummer?: string;
-      poststed?: string;
-      land?: string;
-    },
-  ];
+  adresser: PersonopplysningAdresse[];
   sivilstand: string;
   region: string;
   personstatus: string;
@@ -41,22 +28,14 @@ export type BostedSokerPersonopplysninger = {
 interface OwnProps {
   personopplysninger: BostedSokerPersonopplysninger;
   sokerTypeTextId?: string;
-  alleKodeverk: { [key: string]: KodeverkMedNavn[] };
 }
 
 const BostedSokerFaktaIndex = ({
   personopplysninger,
   sokerTypeTextId = 'BostedSokerFaktaIndex.Soker',
-  alleKodeverk,
 }: OwnProps): JSX.Element => (
   <RawIntlProvider value={intl}>
-    <BostedSokerView
-      personopplysninger={personopplysninger}
-      sokerTypeTextId={sokerTypeTextId}
-      regionTypes={alleKodeverk[kodeverkTyper.REGION]}
-      sivilstandTypes={alleKodeverk[kodeverkTyper.SIVILSTAND_TYPE]}
-      personstatusTypes={alleKodeverk[kodeverkTyper.PERSONSTATUS_TYPE]}
-    />
+    <BostedSokerView personopplysninger={personopplysninger} sokerTypeTextId={sokerTypeTextId} />
   </RawIntlProvider>
 );
 
--- packages/fakta-bosted-soker/src/components/BostedSokerView.tsx
+++ packages/fakta-bosted-soker/src/components/BostedSokerView.tsx
@@ -3,26 +3,21 @@ import { FormattedMessage, WrappedComponentProps, injectIntl } from 'react-intl'
 import Etikettfokus from 'nav-frontend-etiketter';
 import { Column, Row } from 'nav-frontend-grid';
 import { Element, Normaltekst, Undertekst } from 'nav-frontend-typografi';
-
-import { KodeverkMedNavn } from '@k9-sak-web/types';
 import opplysningAdresseType from '@fpsak-frontend/kodeverk/src/opplysningAdresseType';
 import personstatusType from '@fpsak-frontend/kodeverk/src/personstatusType';
 import Region from '@fpsak-frontend/kodeverk/src/region';
 import { getAddresses } from '@fpsak-frontend/utils';
-
+import { useKodeverkV2 } from '@k9-sak-web/gui/kodeverk/hooks/useKodeverk.js';
+import { KodeverkType } from '@k9-sak-web/lib/types/KodeverkType.js';
 import { BostedSokerPersonopplysninger } from '../BostedSokerFaktaIndex';
-
 import styles from './bostedSokerView.module.css';
 
 interface OwnProps {
   personopplysninger: BostedSokerPersonopplysninger;
   sokerTypeTextId: string;
-  regionTypes: KodeverkMedNavn[];
-  sivilstandTypes: KodeverkMedNavn[];
-  personstatusTypes: KodeverkMedNavn[];
 }
 
-const getAdresse = adresser => {
+const getAdresse = (adresser: BostedSokerPersonopplysninger['adresser']) => {
   const adresseListe = getAddresses(adresser);
   const adresse = adresseListe[opplysningAdresseType.POSTADRESSE] || adresseListe[opplysningAdresseType.BOSTEDSADRESSE];
   return adresse || '-';
@@ -36,64 +31,60 @@ const getUtlandsadresse = adresser => {
   return utlandsAdresse || '-';
 };
 
-const getPersonstatus = personopplysning =>
+const getPersonstatus = (personopplysning: BostedSokerPersonopplysninger) =>
   personopplysning.avklartPersonstatus && personopplysning.avklartPersonstatus.overstyrtPersonstatus
     ? personopplysning.avklartPersonstatus.overstyrtPersonstatus
     : personopplysning.personstatus;
 
-export const BostedSokerView = ({
-  intl,
-  personopplysninger,
-  sokerTypeTextId,
-  regionTypes,
-  sivilstandTypes,
-  personstatusTypes,
-}: OwnProps & WrappedComponentProps) => (
-  <div className={styles.defaultBostedSoker}>
-    <Row>
-      <Column xs="8">
-        <Undertekst>
-          <FormattedMessage id={sokerTypeTextId} />
-        </Undertekst>
-        <Element>{personopplysninger.navn ? personopplysninger.navn : '-'}</Element>
-        <Normaltekst className={styles.paddingBottom}>{getAdresse(personopplysninger.adresser)}</Normaltekst>
-        <Undertekst>
-          <FormattedMessage id="BostedSokerView.ForeignAddresse" />
-        </Undertekst>
-        <Normaltekst>{getUtlandsadresse(personopplysninger.adresser)}</Normaltekst>
-      </Column>
-      <Column xs="4">
-        {getPersonstatus(personopplysninger) && (
-          <div className={styles.etikettMargin}>
-            <Etikettfokus
-              className={getPersonstatus(personopplysninger).kode === personstatusType.DOD ? styles.dodEtikett : ''}
-              type="fokus"
-              typo="undertekst"
-              title={intl.formatMessage({ id: 'Personstatus.Hjelpetekst' })}
-            >
-              {getPersonstatus(personopplysninger).kode === personstatusType.UDEFINERT
-                ? intl.formatMessage({ id: 'Personstatus.Ukjent' })
-                : personstatusTypes.find(s => s.kode === getPersonstatus(personopplysninger)).navn}
-            </Etikettfokus>
-          </div>
-        )}
-        {personopplysninger.sivilstand && (
-          <div className={styles.etikettMargin}>
-            <Etikettfokus type="fokus" typo="undertekst" title={intl.formatMessage({ id: 'Sivilstand.Hjelpetekst' })}>
-              {sivilstandTypes.find(s => s.kode === personopplysninger.sivilstand).navn}
-            </Etikettfokus>
-          </div>
-        )}
-        {personopplysninger.region && personopplysninger.region !== Region.UDEFINERT && (
-          <div className={styles.etikettMargin}>
-            <Etikettfokus type="fokus" typo="undertekst" title={intl.formatMessage({ id: 'BostedSokerView.Region' })}>
-              {regionTypes.find(r => r.kode === personopplysninger.region).navn}
-            </Etikettfokus>
-          </div>
-        )}
-      </Column>
-    </Row>
-  </div>
-);
+export const BostedSokerView = ({ intl, personopplysninger, sokerTypeTextId }: OwnProps & WrappedComponentProps) => {
+  const { kodeverkNavnFraKode } = useKodeverkV2();
+  return (
+    <div className={styles.defaultBostedSoker}>
+      <Row>
+        <Column xs="8">
+          <Undertekst>
+            <FormattedMessage id={sokerTypeTextId} />
+          </Undertekst>
+          <Element>{personopplysninger.navn ? personopplysninger.navn : '-'}</Element>
+          <Normaltekst className={styles.paddingBottom}>{getAdresse(personopplysninger.adresser)}</Normaltekst>
+          <Undertekst>
+            <FormattedMessage id="BostedSokerView.ForeignAddresse" />
+          </Undertekst>
+          <Normaltekst>{getUtlandsadresse(personopplysninger.adresser)}</Normaltekst>
+        </Column>
+        <Column xs="4">
+          {getPersonstatus(personopplysninger) && (
+            <div className={styles.etikettMargin}>
+              <Etikettfokus
+                className={getPersonstatus(personopplysninger) === personstatusType.DOD ? styles.dodEtikett : ''}
+                type="fokus"
+                typo="undertekst"
+                title={intl.formatMessage({ id: 'Personstatus.Hjelpetekst' })}
+              >
+                {getPersonstatus(personopplysninger) === personstatusType.UDEFINERT
+                  ? intl.formatMessage({ id: 'Personstatus.Ukjent' })
+                  : kodeverkNavnFraKode(getPersonstatus(personopplysninger), KodeverkType.PERSONSTATUS_TYPE)}
+              </Etikettfokus>
+            </div>
+          )}
+          {personopplysninger.sivilstand && (
+            <div className={styles.etikettMargin}>
+              <Etikettfokus type="fokus" typo="undertekst" title={intl.formatMessage({ id: 'Sivilstand.Hjelpetekst' })}>
+                {kodeverkNavnFraKode(personopplysninger.sivilstand, KodeverkType.SIVILSTAND_TYPE)}
+              </Etikettfokus>
+            </div>
+          )}
+          {personopplysninger.region && personopplysninger.region !== Region.UDEFINERT && (
+            <div className={styles.etikettMargin}>
+              <Etikettfokus type="fokus" typo="undertekst" title={intl.formatMessage({ id: 'BostedSokerView.Region' })}>
+                {kodeverkNavnFraKode(personopplysninger.region, KodeverkType.REGION)}
+              </Etikettfokus>
+            </div>
+          )}
+        </Column>
+      </Row>
+    </div>
+  );
+};
 
 export default injectIntl(BostedSokerView);
