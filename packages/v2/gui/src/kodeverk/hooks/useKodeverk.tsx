1d21f391cbb63b6c9f87a668d88550dacf64e532
Thomas H. Wiberg <thomas@thawi.no>
2024-03-22 15:32:12 +0100

    Kodeverk: Forsøk på ulike kodeverkoppslag

--- packages/v2/gui/src/kodeverk/hooks/useKodeverk.tsx
+++ packages/v2/gui/src/kodeverk/hooks/useKodeverk.tsx
@@ -3,11 +3,47 @@ import { utledKodeverkNavnFraKode, utledKodeverkNavnFraUndertypeKode } from '@k9
 import { KodeverkType } from '@k9-sak-web/lib/types/KodeverkType.js';
 import { KodeverkResponse } from '@k9-sak-web/lib/types/KodeverkResponse.js';
 import { AlleKodeverk } from '@k9-sak-web/lib/types/AlleKodeverk.js';
+import { useState } from 'react';
 
 export const useKodeverkV2 = () => {
-  const alleKodeverk = restApiHooks.useGlobalStateRestApiData<KodeverkResponse>(K9sakApiKeys.KODEVERK);
+  const [alleKodeverk, setAlleKodeverk] = useState<KodeverkResponse | undefined>(undefined);
+  const [alleKodeverkTilbake, setAlleKodeverkTilbake] = useState<KodeverkResponse | undefined>(undefined);
+  const [alleKodeverkKlage, setAlleKodeverkKlage] = useState<KodeverkResponse | undefined>(undefined);
 
-  const kodeverkNavnFraKode = (kode: string, kodeverkType: KodeverkType, undertypeKode?: string) => {
+  const hentAlleKodeverk = async (kodeverkKilde: 'kodeverk' | 'kodeverkTilbake' | 'kodeverkKlage') => {
+    let kodeverkResponse;
+    switch (kodeverkKilde) {
+      case 'kodeverkTilbake':
+        kodeverkResponse = await restApiHooks.useGlobalStateRestApiData<KodeverkResponse>(
+          K9sakApiKeys.KODEVERK_TILBAKE,
+        );
+        setAlleKodeverkTilbake(kodeverkResponse);
+        break;
+      case 'kodeverkKlage':
+        kodeverkResponse = await restApiHooks.useGlobalStateRestApiData<KodeverkResponse>(K9sakApiKeys.KODEVERK_KLAGE);
+        setAlleKodeverk(kodeverkResponse);
+        break;
+      default:
+        kodeverkResponse = await restApiHooks.useGlobalStateRestApiData<KodeverkResponse>(K9sakApiKeys.KODEVERK);
+        setAlleKodeverkKlage(kodeverkResponse);
+        break;
+    }
+  };
+
+  /*
+   * Note to self / TODO
+   * Har her lagt opp til alle kodeverk i ett oppslag, men det er mulig det må legges til en mulighet for
+   * flere ulike oppslag. Da det kanskje er forskjellige sett med kodeverk, for eksempel for TILBAKE.
+   */
+
+  const kodeverkNavnFraKode = async (
+    kode: string,
+    kodeverkType: KodeverkType,
+    undertypeKode?: string,
+    kilde: 'kodeverk' | 'kodeverkTilbake' | 'kodeverkKlage' = 'kodeverk',
+  ) => {
+    if (!alleKodeverk) await hentAlleKodeverk(kilde);
+
     if (alleKodeverk) {
       if (undertypeKode) {
         if (alleKodeverk[kodeverkType]) {
@@ -27,5 +63,5 @@ export const useKodeverkV2 = () => {
     return [];
   };
 
-  return { kodeverkNavnFraKode, hentKodeverkForType };
+  return { kodeverkNavnFraKode, hentKodeverkForType, alleKodeverk, alleKodeverkKlage, alleKodeverkTilbake };
 };
