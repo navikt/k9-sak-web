1aeacfd38c0de5b90446da3e129e7bd201f88614
Jostein Stuhaug <135307852+josstn@users.noreply.github.com>
2024-04-12 11:13:47 +0200

    Fjerner redux-logger. (#5803)
    Bakgrunn: Meir irriterande enn nyttig å ha den der.

--- packages/sak-app/src/configureStore.ts
+++ packages/sak-app/src/configureStore.ts
@@ -1,6 +1,5 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import { IS_DEV } from './constants';
 
@@ -10,9 +9,6 @@ const configureStore = () => {
   const middleware = [thunkMiddleware];
   let enhancer;
   if (isDevelopment) {
-    const logger = createLogger();
-    middleware.push(logger);
-
     /* eslint-disable-next-line no-underscore-dangle */
     const composeEnhancers = (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
     enhancer = composeEnhancers(applyMiddleware(...middleware));
