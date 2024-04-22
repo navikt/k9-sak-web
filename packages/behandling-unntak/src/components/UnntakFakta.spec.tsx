4fa46b55b1d3aa617dcf41a74fbabf3bfcf0b105
Hallvard Andreas Stark <hallvard@scel.to>
2024-04-22 12:35:47 +0200

    Fiks test

--- packages/behandling-unntak/src/components/UnntakFakta.spec.tsx
+++ packages/behandling-unntak/src/components/UnntakFakta.spec.tsx
@@ -149,7 +149,7 @@ describe('<UnntakFakta>', () => {
       />,
     );
 
-    expect(screen.getByRole('button', { name: 'Arbeidsforhold' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Arbeidsforhold Aksjonspunkt' })).toBeInTheDocument();
     expect(screen.getByRole('button', { name: 'Inntekt og ytelser' })).toBeInTheDocument();
     expect(screen.getByText('Avklar om arbeidsforholdene skal benyttes i behandlingen')).toBeInTheDocument();
   });
@@ -181,7 +181,7 @@ describe('<UnntakFakta>', () => {
     );
 
     await act(async () => {
-      await userEvent.click(screen.getByRole('button', { name: 'Arbeidsforhold' }));
+      await userEvent.click(screen.getByRole('button', { name: 'Arbeidsforhold Aksjonspunkt' }));
     });
 
     const { calls } = oppdaterProsessStegOgFaktaPanelIUrl.mock;
