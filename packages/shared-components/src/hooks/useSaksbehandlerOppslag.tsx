9defc9bf8ae00affb24a9325d8c49a84fdf2f7b1
vebnor <vebjorn.nordby@nav.no>
2024-04-26 10:31:30 +0200

    fikse tester

--- packages/shared-components/src/hooks/useSaksbehandlerOppslag.tsx
+++ packages/shared-components/src/hooks/useSaksbehandlerOppslag.tsx
@@ -3,10 +3,10 @@ import { K9sakApiKeys } from '@k9-sak-web/sak-app/src/data/k9sakApi';
 import { SaksbehandlereInfo } from '@k9-sak-web/types';
 
 export const useSaksbehandlerOppslag = () => {
-  const { saksbehandlere } = useGlobalStateRestApiData<SaksbehandlereInfo>(K9sakApiKeys.HENT_SAKSBEHANDLERE);
+  const data = useGlobalStateRestApiData<SaksbehandlereInfo>(K9sakApiKeys.HENT_SAKSBEHANDLERE);
+  const saksbehandlere = data?.saksbehandlere;
 
   const hentSaksbehandlerNavn = (saksbehandler: string): string => saksbehandlere[saksbehandler] || saksbehandler;
-
   return { saksbehandlere, hentSaksbehandlerNavn };
 };
 
