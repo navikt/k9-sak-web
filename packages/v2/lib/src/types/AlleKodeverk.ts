d31beac520127f3e7541839ad19c20d056e1dc4b
Thomas H. Wiberg <thomas@thawi.no>
2024-04-26 15:17:52 +0200

    Manglende type

--- packages/v2/lib/src/types/AlleKodeverk.ts
+++ packages/v2/lib/src/types/AlleKodeverk.ts
@@ -1,9 +1,9 @@
-import { KodeverkType, KodeverkV2, KodeverkKlageType } from '.';
+import { KodeverkType, KodeverkV2, KodeverkKlageType, KodeverkTilbakeType } from '.';
 
 export type KodeverkMedUndertype = {
   [key: string]: KodeverkV2[];
 };
 
 export type AlleKodeverk = {
-  [key in KodeverkType | KodeverkKlageType]?: KodeverkV2[] | KodeverkMedUndertype;
+  [key in KodeverkType | KodeverkKlageType | KodeverkTilbakeType]?: KodeverkV2[] | KodeverkMedUndertype;
 };
