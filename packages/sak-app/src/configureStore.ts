b740a1f1e05549994ff79330443b4ee0c6cced89
Thomas H. Wiberg <thomas@thawi.no>
2024-05-07 13:30:45 +0200

    Oppdater redux & co

--- packages/sak-app/src/configureStore.ts
+++ packages/sak-app/src/configureStore.ts
@@ -1,19 +1,18 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import thunkMiddleware from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 import { IS_DEV } from './constants';
 
 const isDevelopment = IS_DEV;
 
 const configureStore = () => {
-  const middleware = [thunkMiddleware];
   let enhancer;
   if (isDevelopment) {
     /* eslint-disable-next-line no-underscore-dangle */
     const composeEnhancers = (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-    enhancer = composeEnhancers(applyMiddleware(...middleware));
+    enhancer = composeEnhancers(applyMiddleware(thunk));
   } else {
-    enhancer = compose(applyMiddleware(...middleware));
+    enhancer = compose(applyMiddleware(thunk));
   }
 
   const allReducers = combineReducers({
