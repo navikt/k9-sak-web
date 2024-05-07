db7c7c4ef8613442d47a50e8b3989a0f4a89718a
Hallvard Andreas Stark <hallvard@scel.to>
2024-05-07 12:18:04 +0200

    Fikser typings

--- packages/behandling-opplaeringspenger/src/FaktaOpplaeringContext.ts
+++ packages/behandling-opplaeringspenger/src/FaktaOpplaeringContext.ts
@@ -1,6 +1,13 @@
 import React from 'react';
 
-import { Aksjonspunkt, NoedvendighetPerioder, NoedvendighetVurdering } from '@k9-sak-web/types';
+import { ReisetidVurdering } from '@k9-sak-web/fakta-opplaering/src/reisetid/ReisetidTypes';
+import {
+  Aksjonspunkt,
+  GjennomgaaOpplaeringVurdering,
+  NoedvendighetPerioder,
+  NoedvendighetVurdering,
+  Periode,
+} from '@k9-sak-web/types';
 import Dokument from '@k9-sak-web/types/src/sykdom/Dokument';
 
 export const FaktaOpplaeringContext = React.createContext<FaktaOpplaeringContextTypes>(null);
@@ -10,9 +17,19 @@ export interface FaktaOpplaeringContextTypes {
   readOnly: boolean;
   løsAksjonspunktGjennomgåOpplæring: (v: any) => void;
   løsAksjonspunktNødvendighet: (v: any) => void;
+  løsAksjonspunktReisetid: (v: any) => void;
   nødvendigOpplæring: {
     vurderinger: NoedvendighetVurdering[];
     perioder: NoedvendighetPerioder[];
   };
   opplaeringDokumenter: Dokument[];
+  reisetid: {
+    perioder: {
+      opplæringsperiode: Periode;
+    }[];
+    vurderinger: ReisetidVurdering[];
+  };
+  gjennomgåttOpplæring: {
+    vurderinger: GjennomgaaOpplaeringVurdering[];
+  };
 }
