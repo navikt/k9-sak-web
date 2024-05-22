4c3dda26f3c510f97570bdce51e69baae2893c0a
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-05-22 09:38:35 +0200

    Fjern ubrukt behandlingstype dokumentinnsyn.
    Bruken av denne vart fjerna i master.

--- packages/v2/backend/src/combined/kodeverk/behandling/BehandlingType.ts
+++ packages/v2/backend/src/combined/kodeverk/behandling/BehandlingType.ts
@@ -8,23 +8,10 @@ import {
 } from '../../../k9klage/kodeverk/behandling/BehandlingType.js';
 import type { Kodeverk } from '../../../shared/Kodeverk.ts';
 
-// DOKUMENTINNSYN - Denne er sannsynlegvis ikkje i bruk og bør fjernast. Ikkje definert i nokon backend kodebase.
-// Leggast derfor til manuelt her midlerditig til vi har funne ut om den kan fjernast.
-export type DeprecatedBehandlingType = 'BT-006';
-type DeprecatedBehandlingTypeName = 'DOKUMENTINNSYN';
-
-export const deprecatedBehandlingType: Readonly<Record<DeprecatedBehandlingTypeName, DeprecatedBehandlingType>> = {
-  DOKUMENTINNSYN: 'BT-006',
-};
-
-export type BehandlingType = BehandlingTypeK9Sak | BehandlingTypeK9Klage | DeprecatedBehandlingType;
+export type BehandlingType = BehandlingTypeK9Sak | BehandlingTypeK9Klage;
 
 export type BehandlingTypeKodeverk = Kodeverk<BehandlingType, 'BEHANDLING_TYPE'>;
 
-const values: string[] = [
-  ...Object.values(behandlingTypeK9Sak),
-  ...Object.values(behandlingTypeK9Klage),
-  ...Object.values(deprecatedBehandlingType),
-];
+const values: string[] = [...Object.values(behandlingTypeK9Sak), ...Object.values(behandlingTypeK9Klage)];
 
 export const isBehandlingType = (t: string): t is BehandlingType => values.includes(t);
