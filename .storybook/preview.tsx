e18b672a3ac93c2b661b30614511da7e5a8d4416
Hallvard Andreas Stark <hallvard@scel.to>
2024-05-24 22:12:07 +0200

    Venter på at msw skal være klar før stories laster

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,7 +3,7 @@ import configureStore from '@k9-sak-web/sak-app/src/configureStore';
 import '@navikt/ds-css';
 import '@navikt/ft-plattform-komponenter/dist/style.css';
 import { Preview } from '@storybook/react';
-import { getWorker, initialize, mswLoader } from 'msw-storybook-addon';
+import { initialize, mswLoader } from 'msw-storybook-addon';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
@@ -33,7 +33,55 @@ const preview: Preview = {
       );
     },
   ],
-  loaders: [mswLoader, () => getWorker().start()],
+  loaders: [
+    async context => {
+      await mswLoader(context);
+      await waitForActivatedServiceWorker();
+    },
+  ],
+};
+
+const waitForActivatedServiceWorker = async () => {
+  // Wait for the worker to be loaded
+  const serviceWorker =
+    navigator.serviceWorker.controller ||
+    (await new Promise((resolve, reject) => {
+      let triesLeft = 10;
+
+      const fn = () => {
+        if (navigator.serviceWorker.controller) {
+          resolve(navigator.serviceWorker.controller);
+        } else {
+          triesLeft -= 1;
+
+          if (triesLeft === 0) {
+            reject(new Error('Timed out waiting for service worker'));
+          } else {
+            setTimeout(fn, 100);
+          }
+        }
+      };
+
+      setTimeout(fn, 100);
+    }));
+
+  if (!serviceWorker) {
+    throw new Error('No service worker found');
+  }
+
+  // Make sure the worker is actually ready to go
+  if (serviceWorker.state !== 'activated') {
+    await new Promise<void>(resolve => {
+      const fn = (e: Event) => {
+        if (e.target && 'state' in e.target && e.target.state === 'activated') {
+          serviceWorker.removeEventListener('statechange', fn);
+          resolve();
+        }
+      };
+
+      serviceWorker.addEventListener('statechange', fn);
+    });
+  }
 };
 
 export default preview;
