5c2afb96a824599b8eeb14986459015ee796ecfe
Hallvard Andreas Stark <hallvard@scel.to>
2024-05-31 11:12:41 +0200

    endrer filnavn

--- packages/v2/gui/src/storybook/decorators/withStoryReload.tsx
+++ packages/v2/gui/src/storybook/decorators/withStoryReload.tsx
@@ -6,7 +6,6 @@ import React, { useEffect } from 'react';
  * Reloads the current URL before showing the story.
  * If you use MSW and edit the mock data in the story this decorator will reset those changes when you navigate out of the story.
  *
- * @returns Story component
  */
 
 const withStoryReload = (): Decorator => Story => {
