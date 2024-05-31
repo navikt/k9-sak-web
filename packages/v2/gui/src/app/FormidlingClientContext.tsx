a3ad7908b92d8b92ca1628cae28d44d0bf412551
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-05-31 14:53:52 +0200

    Fiks url paths i formidling klient.
    Hadde misforstått korleis URL konstruktør virka med base url angitt.

--- packages/v2/gui/src/app/FormidlingClientContext.tsx
+++ packages/v2/gui/src/app/FormidlingClientContext.tsx
@@ -5,5 +5,5 @@ import { FormidlingClient } from '@k9-sak-web/backend/k9formidling/client/Formid
  * This shall be a top level context providing the FormidlingClient instance that will be used for communicating with backend server.
  */
 export const FormidlingClientContext = createContext(
-  new FormidlingClient(new URL('/k9/formidling/api', window.location.origin)),
+  new FormidlingClient(new URL('/k9/formidling/api/', window.location.origin)),
 );
