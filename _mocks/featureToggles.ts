c4a1e465aa097041a71cb9fe07a137127f0582ac
Audun Eltvik <audun.eltvik@nav.no>
2024-06-18 14:47:28 +0200

    Legg til feature toggle

--- _mocks/featureToggles.ts
+++ _mocks/featureToggles.ts
@@ -73,4 +73,8 @@ export const featureTogglesFactory = () => [
     key: 'AVSLAGSAARSAK_ALENEOMSORG',
     value: process.env.VITE_AVSLAGSAARSAK_ALENEOMSORG,
   },
+  {
+    key: 'NYE_NOKKELTALL',
+    value: process.env.VITE_NYE_NOKKELTALL,
+  }
 ];
--- packages/prosess-aarskvantum-oms/src/components/nokkeltall/DagerNavKanUtbetale.tsx
+++ packages/prosess-aarskvantum-oms/src/components/nokkeltall/DagerNavKanUtbetale.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { useFeatureToggles } from "@fpsak-frontend/shared-components";
 import Nokkeltall from './Nokkeltall';
 import styles from './nokkeltall.module.css';
 
@@ -19,34 +20,38 @@ const DagerNavKanUtbetale = ({
   antallDagerFraværRapportertSomNyoppstartet,
   visDetaljer,
   viserDetaljer,
-}: DagerNavKanUtbetaleProps) => (
-  <Nokkeltall
-    overskrift={{ antallDager: dagerNavKanUtbetale, overskrifttekstId: 'Nøkkeltall.DagerNavKanUtbetale' }}
-    detaljer={[
-      {
-        antallDager: dagerRettPå,
-        overskrifttekstId: 'Nøkkeltall.TotaltAntallDager',
-        infotekstContent: <FormattedMessage id="Nøkkeltall.TotaltAntallDager.InfoText" />,
-      },
-      {
-        antallDager: -antallDagerArbeidsgiverDekker,
-        overskrifttekstId: 'Nøkkeltall.Ventetid',
-        infotekstContent: (
-          <FormattedMessage id="Nøkkeltall.Ventetid.InfoText" values={{ dager: antallDagerArbeidsgiverDekker }} />
-        ),
-      },
-      {
-        antallDager: antallDagerFraværRapportertSomNyoppstartet,
-        overskrifttekstId: 'Nøkkeltall.AntallDagerFraværRapportertSomNyoppstartet',
-        infotekstContent: (
-          <FormattedMessage id="Nøkkeltall.AntallDagerFraværRapportertSomNyoppstartet.InfoText" />
-        )
-      },
-    ]}
-    viserDetaljer={viserDetaljer}
-    visDetaljer={visDetaljer}
-    className={styles.dagerNavKanUtbetale}
-  />
-);
+}: DagerNavKanUtbetaleProps) => {
+  const featureToggles = useFeatureToggles();
+
+  return (
+    <Nokkeltall
+      overskrift={{antallDager: dagerNavKanUtbetale, overskrifttekstId: 'Nøkkeltall.DagerNavKanUtbetale'}}
+      detaljer={[
+        {
+          antallDager: dagerRettPå,
+          overskrifttekstId: 'Nøkkeltall.TotaltAntallDager',
+          infotekstContent: <FormattedMessage id="Nøkkeltall.TotaltAntallDager.InfoText"/>,
+        },
+        {
+          antallDager: -antallDagerArbeidsgiverDekker,
+          overskrifttekstId: 'Nøkkeltall.Ventetid',
+          infotekstContent: (
+            <FormattedMessage id="Nøkkeltall.Ventetid.InfoText" values={{dager: antallDagerArbeidsgiverDekker}}/>
+          ),
+        },
+        (featureToggles?.AVSLAGSAARSAK_ALENEOMSORG && {
+          antallDager: antallDagerFraværRapportertSomNyoppstartet,
+          overskrifttekstId: 'Nøkkeltall.AntallDagerFraværRapportertSomNyoppstartet',
+          infotekstContent: (
+            <FormattedMessage id="Nøkkeltall.AntallDagerFraværRapportertSomNyoppstartet.InfoText"/>
+          )
+        }),
+      ]}
+      viserDetaljer={viserDetaljer}
+      visDetaljer={visDetaljer}
+      className={styles.dagerNavKanUtbetale}
+    />
+  );
+}
 
 export default DagerNavKanUtbetale;
