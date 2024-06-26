edc1789c18a63536b7a9b6ac04b2725e63f80e6b
Jostein Stuhaug <135307852+josstn@users.noreply.github.com>
2024-06-26 16:10:41 +0200

    Tsff 230 meldinger omskriving 3 (#6309)
    * Endra til å bruke FileSearchIcon for forhåndsvisning knapp i meldingspanel.
    
    Ønske om at det skal vere likt ikon som allereie er brukt i vedtakssteget.
    
    * Endra til å nytt FileSearchIcon for PreviewLink.tsx
    
    Istadenfor gammalt ikon frå ds-icons.
    
    * Bruk separat checkbox for å aktivere sending til tredjepart.
    
    Som før, istadenfor å ha det som ein del av mottaker valg boks.
    
    * Reaktivitet og reset justering.
    
    * Fiks storybook for meldingspanel, fritekst valg.
    
    Fake api backend stemde ikkje med api definisjon lenger, så det vart aldri returnert innhold til brevmal i storybook.
    
    * Bedre handtering av send til tredjepart checkbox.
    
    Deaktiver og resett verdier når det trengs.
    
    * Rydding i FakeMessagesBackendApi.ts ingen reell endring.
    
    * Fjern duplikat storybook addon-controls config.
    
    * Messages: Endra request abort, start på bruk av state reducer.
    
    Avbrutt forespørsel vil returnere signaltype istadenfor å kaste error. Vil gjere det mulig å handtere avbrudd (pga debounce) utan å få feilmelding i konsoll.
    
    Starter og omskriving til å bruke state reducer i Messages.tsx for å unngå unødvendig re-rendering og få ein meir robust state handtering i komponenten.
    
    * Messages: state reducer v0 implementert.
    
    * Messages: state reducer v0 implementert.
    
    * Flytta litt state logikk inn i messagesStateReducer.
    
    Bygde og messagesStateReducer om til switch på type for å få typescript sjekk på at alle mulige typer er handtert, og gjere koden meir lesbar.
    
    * Endre namn på RequestAborted til RequestIntentionallyAborted.
    
    For å betre signalisere meininga med den.
    
    Fjerner samtidig gammal Error med samme namn, den er ikkje brukt lenger.
    
    * Unngå unødvendig serverkall for tredjepartsmottaker søk.
    
    Ikkje noko poeng i å kalle server viss gitt orgnr ikkje er 9 siffer.

--- packages/v2/gui/src/storybook/mocks/FakeMessagesBackendApi.ts
+++ packages/v2/gui/src/storybook/mocks/FakeMessagesBackendApi.ts
@@ -1,60 +1,57 @@
 import type { ForhåndsvisDto } from '@k9-sak-web/backend/k9formidling/models/ForhåndsvisDto.js';
 import type { FritekstbrevDokumentdata } from '@k9-sak-web/backend/k9formidling/models/FritekstbrevDokumentdata.js';
 import type { BestillBrevDto } from '@k9-sak-web/backend/k9sak/generated';
-import { fagsakYtelsesType } from '@k9-sak-web/backend/k9sak/kodeverk/FagsakYtelsesType.js';
 import type { EregOrganizationLookupResponse } from '@k9-sak-web/gui/sak/meldinger/EregOrganizationLookupResponse.js';
 import type { BackendApi } from '@k9-sak-web/gui/sak/meldinger/Messages.js';
 import { action } from '@storybook/addon-actions';
+import type { AvsenderApplikasjon } from "@k9-sak-web/backend/k9formidling/models/AvsenderApplikasjon.ts";
+import { requestIntentionallyAborted, type RequestIntentionallyAborted } from "@k9-sak-web/backend/shared/RequestIntentionallyAborted.ts";
 import { fakePdf } from './fakePdf.js';
-
-// XXX Should be moved out somewhere so other fake implementations can use it
-const sleep = (timeMs: number) =>
-  new Promise(resolve => {
-    setTimeout(resolve, timeMs);
-  });
+import { delay } from "../../utils/delay.js";
 
 export class FakeMessagesBackendApi implements BackendApi {
+  public static readonly dummyMalinnhold = [
+    { tittel: 'Varsel nr 1', fritekst: 'Hei, du må sende inn ditt og datt før frist.' },
+    { tittel: 'Varsel nr 2', fritekst: 'Brev tekst forslag nr 2.' },
+  ];
+
   async hentInnholdBrevmal(
     sakstype: string,
     eksternReferanse: string,
+    avsenderApplikasjon: AvsenderApplikasjon,
     maltype: string,
   ): Promise<FritekstbrevDokumentdata[]> {
-    // @ts-ignore
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const x = eksternReferanse;
-    if (sakstype === fagsakYtelsesType.OMP && maltype === 'VARSEL_FRITEKST') {
-      return [
-        { tittel: 'Varsel nr 1', fritekst: 'Hei, du må sende inn ditt og datt før frist.' },
-        { tittel: 'Varsel nr 2', fritekst: 'Brev tekst forslag nr 2.' },
-      ];
+    const x = eksternReferanse + sakstype + avsenderApplikasjon; // For å unngå unused variable feil
+    if (x !== null && maltype === 'INNHENT_MEDISINSKE_OPPLYSNINGER') {
+      return FakeMessagesBackendApi.dummyMalinnhold
     }
     return [];
   }
 
-  async getBrevMottakerinfoEreg(orgnr: string, abort?: AbortSignal): Promise<EregOrganizationLookupResponse> {
-    abort?.addEventListener('abort', () => {
-      throw abort.reason;
-    });
-    if (orgnr.length === 9) {
-      if (Number.isFinite(Number(orgnr))) {
-        if (orgnr === '000000000') {
+  async getBrevMottakerinfoEreg(orgnr: string, abort?: AbortSignal): Promise<EregOrganizationLookupResponse | RequestIntentionallyAborted> {
+    if(abort?.aborted) {
+      return requestIntentionallyAborted
+    }
+    if (orgnr.trim().length === 9) {
+      if (Number.isFinite(Number(orgnr.trim()))) {
+        if (orgnr.trim() === '000000000') {
           // To test what happens when orgnr is not found
           return { notFound: true };
         }
-        return { name: `Fake storybook org (${orgnr})` };
+        return { name: `Fake storybook org (${orgnr.trim()})` };
       }
     }
     return { invalidOrgnum: true };
   }
 
   async bestillDokument(bestilling: BestillBrevDto): Promise<void> {
-    await sleep(1_400);
+    await delay(1_400);
     action('bestillDokument')(bestilling);
   }
 
   async lagForhåndsvisningPdf(data: ForhåndsvisDto): Promise<Blob> {
     action('lag pdf data')(data);
-    await sleep(1_000);
+    await delay(1_000);
     return fakePdf();
   }
 }
