927b3c6c637ea1276f9e82c69b80d3dfc116ce62
Thomas H. Wiberg <thomas@thawi.no>
2024-06-28 16:05:44 +0200

    Kodeverk: kommentarer

--- packages/v2/gui/src/kodeverk/hooks/useKodeverkContext.tsx
+++ packages/v2/gui/src/kodeverk/hooks/useKodeverkContext.tsx
@@ -54,7 +54,6 @@ export const useKodeverkContext = () => {
     kilde = undefined,
     ukjentTekst = undefined,
   ) => {
-    console.log('kodeverk oppslag', kode, kodeverkType, kilde);
     const kodeverkForType = hentKodeverkForKode(kodeverkType, kilde);
     if (kodeverkForType) return utledKodeverkNavnFraKode(kode, kodeverkForType);
     return ukjentTekst || 'Ukjent kode';
