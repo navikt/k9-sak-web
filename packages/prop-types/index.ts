bb270911c24e14c28b0f209f2c820302d3231f5c
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-07-08 14:56:47 +0200

    Rydding i packages/prop-types. Ingen funksjonell endring.
    Fjern ubrukt beregningresultatMedUttaksplanPropType.jsx og fagsakPropType.jsx

--- packages/prop-types/index.ts
+++ packages/prop-types/index.ts
@@ -1,12 +1,5 @@
 export { default as aksjonspunktPropType } from './src/aksjonspunktPropType';
-export {
-  uttakPropType,
-  beregningsresultatPeriodeAndelPropType,
-  beregningsresultatPeriodePropType,
-  beregningresultatMedUttaksplanPropType,
-} from './src/beregningresultatMedUttaksplanPropType';
 
-export { default as fagsakPropType } from './src/fagsakPropType';
 export { kodeverkPropType, kodeverkObjektPropType } from './src/kodeverkPropType';
 export { default as navAnsattPropType } from './src/navAnsattPropType';
 export { default as personopplysningPropType } from './src/personopplysningPropType';
