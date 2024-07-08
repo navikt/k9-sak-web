21410679f949e7c41876e7c1187ba7ef07039e91
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-07-08 15:01:31 +0200

    Rydding i packages/prop-types. Ingen funksjonell endring.
    Fjern ubrukt navAnsattPropType.jsx, personopplysningPropType.jsx og rettighetPropType.jsx

--- packages/prop-types/index.ts
+++ packages/prop-types/index.ts
@@ -1,7 +1,4 @@
 export { default as aksjonspunktPropType } from './src/aksjonspunktPropType';
 
 export { kodeverkPropType, kodeverkObjektPropType } from './src/kodeverkPropType';
-export { default as navAnsattPropType } from './src/navAnsattPropType';
-export { default as personopplysningPropType } from './src/personopplysningPropType';
-export { default as rettighetPropType } from './src/rettighetPropType';
 export { stonadskontoPropType, stonadskontoerPropType } from './src/stonadskontoPropType';
