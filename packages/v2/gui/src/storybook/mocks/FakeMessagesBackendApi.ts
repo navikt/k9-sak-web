ada8acc1efcaaccc85c265f6224e4dd41e041aee
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-07-29 14:09:07 +0200

    Litt meir storybook-testing av Messages.tsx
    Tester at bestilling kaller api med forventa data for nokre scenario.
    
    Fjerner tidsforsinkelse, så test ikkje tek unødvendig lang tid.
    
    Forbetra typescript sjekk (bruker satisifies på story meta definisjon)

--- packages/v2/gui/src/storybook/mocks/FakeMessagesBackendApi.ts
+++ packages/v2/gui/src/storybook/mocks/FakeMessagesBackendApi.ts
@@ -4,16 +4,33 @@ import type { BestillBrevDto } from '@k9-sak-web/backend/k9sak/generated';
 import type { EregOrganizationLookupResponse } from '@k9-sak-web/gui/sak/meldinger/EregOrganizationLookupResponse.js';
 import type { BackendApi } from '@k9-sak-web/gui/sak/meldinger/Messages.js';
 import { action } from '@storybook/addon-actions';
-import type { AvsenderApplikasjon } from "@k9-sak-web/backend/k9formidling/models/AvsenderApplikasjon.ts";
-import { requestIntentionallyAborted, type RequestIntentionallyAborted } from "@k9-sak-web/backend/shared/RequestIntentionallyAborted.ts";
+import type { AvsenderApplikasjon } from '@k9-sak-web/backend/k9formidling/models/AvsenderApplikasjon.ts';
+import {
+  requestIntentionallyAborted,
+  type RequestIntentionallyAborted,
+} from '@k9-sak-web/backend/shared/RequestIntentionallyAborted.ts';
 import { fakePdf } from './fakePdf.js';
-import { delay } from "../../utils/delay.js";
+import { delay } from '../../utils/delay.js';
 
 export class FakeMessagesBackendApi implements BackendApi {
   public static readonly dummyMalinnhold = [
     { tittel: 'Varsel nr 1', fritekst: 'Hei, du må sende inn ditt og datt før frist.' },
     { tittel: 'Varsel nr 2', fritekst: 'Brev tekst forslag nr 2.' },
   ];
+
+  #sisteBestilling: BestillBrevDto | undefined;
+
+  constructor(private delayMillis = 0) {
+    // Intentionally empty
+  }
+
+  setDelayMillis(delayMillis: number) {
+    this.delayMillis = delayMillis;
+  }
+
+  get sisteBestilling() {
+    return this.#sisteBestilling;
+  }
 
   async hentInnholdBrevmal(
     sakstype: string,
@@ -23,14 +40,17 @@ export class FakeMessagesBackendApi implements BackendApi {
   ): Promise<FritekstbrevDokumentdata[]> {
     const x = eksternReferanse + sakstype + avsenderApplikasjon; // For å unngå unused variable feil
     if (x !== null && maltype === 'INNHENT_MEDISINSKE_OPPLYSNINGER') {
-      return FakeMessagesBackendApi.dummyMalinnhold
+      return FakeMessagesBackendApi.dummyMalinnhold;
     }
     return [];
   }
 
-  async getBrevMottakerinfoEreg(orgnr: string, abort?: AbortSignal): Promise<EregOrganizationLookupResponse | RequestIntentionallyAborted> {
-    if(abort?.aborted) {
-      return requestIntentionallyAborted
+  async getBrevMottakerinfoEreg(
+    orgnr: string,
+    abort?: AbortSignal,
+  ): Promise<EregOrganizationLookupResponse | RequestIntentionallyAborted> {
+    if (abort?.aborted) {
+      return requestIntentionallyAborted;
     }
     if (orgnr.trim().length === 9) {
       if (Number.isFinite(Number(orgnr.trim()))) {
@@ -45,13 +65,18 @@ export class FakeMessagesBackendApi implements BackendApi {
   }
 
   async bestillDokument(bestilling: BestillBrevDto): Promise<void> {
-    await delay(1_400);
+    if (this.delayMillis > 0) {
+      await delay(this.delayMillis);
+    }
+    this.#sisteBestilling = structuredClone(bestilling);
     action('bestillDokument')(bestilling);
   }
 
   async lagForhåndsvisningPdf(data: ForhåndsvisDto): Promise<Blob> {
     action('lag pdf data')(data);
-    await delay(1_000);
+    if (this.delayMillis > 0) {
+      await delay(this.delayMillis);
+    }
     return fakePdf();
   }
 }
