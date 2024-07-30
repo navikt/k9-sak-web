6359b12f92509eeb0471afa0534f95b751377298
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-07-30 16:21:54 +0200

    Litt forbetring av EditorJSWrapper og FritekstEditor.
    Laga story som tester FritekstEditor komponent, og generelt forbetra EditorJSWrapper og FritekstEditor litt.

---
+++ packages/prosess-vedtak/src/components/FritekstRedigering/FritekstEditor.stories.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import type { Decorator, Meta, StoryObj } from '@storybook/react';
+import { fn, expect, userEvent, waitFor } from '@storybook/test';
+import { createIntl, IntlShape, RawIntlProvider } from 'react-intl';
+import FritekstEditor from './FritekstEditor.js';
+import messages from '../../../i18n/nb_NO.json';
+
+const withRawIntlProvider =
+  (intl: IntlShape): Decorator =>
+  Story => (
+    <RawIntlProvider value={intl}>
+      <Story />
+    </RawIntlProvider>
+  );
+
+const intl = createIntl({
+  locale: 'nb-NO',
+  messages,
+});
+
+const meta = {
+  title: 'prosess/prosess-vedtak/FritekstRedigering',
+  component: FritekstEditor,
+  decorators: [withRawIntlProvider(intl)],
+} satisfies Meta<typeof FritekstEditor>;
+
+export default meta;
+
+export const Default: StoryObj<typeof meta> = {
+  args: {
+    kanInkludereKalender: true,
+    skalBrukeOverstyrendeFritekstBrev: false,
+    readOnly: false,
+    redigerbartInnholdKlart: true,
+    redigerbartInnhold: 'Storybook default scenario',
+    originalHtml: 'OriginalHtml',
+    prefiksInnhold: '',
+    suffiksInnhold: '',
+    brevStiler: '',
+    handleSubmit: fn(),
+    lukkEditor: fn(),
+    handleForhåndsvis: fn(),
+    oppdaterFormFelt: fn(),
+    setFieldValue: fn(),
+  },
+};
+
+const customizedContent = 'Storybook customized scenario';
+const addedContent = ' Added text';
+export const AvansertMedPlayTest: StoryObj<typeof FritekstEditor> = {
+  args: {
+    ...Default.args,
+    redigerbartInnhold: customizedContent,
+    originalHtml: 'Storybook customized scenario originalt innhold',
+    prefiksInnhold: 'Prefiks',
+    suffiksInnhold: 'Suffiks',
+  },
+  play: async ({ canvas, args, canvasElement }) => {
+    const prefixEl = canvas.getByText('Prefiks');
+    expect(prefixEl).toBeInTheDocument();
+    const suffixEl = canvas.getByText('Suffiks');
+    expect(suffixEl).toBeInTheDocument();
+    const contentBlock = canvasElement.querySelector('#rediger-brev');
+    await expect(contentBlock).toBeInTheDocument();
+
+    const submitBtn = canvas.getByRole('button', { name: 'Lagre og lukk' });
+    expect(submitBtn).toBeInTheDocument();
+    await waitFor(() => expect(args.oppdaterFormFelt).toHaveBeenCalled());
+    await userEvent.click(submitBtn, { delay: 100 });
+    await waitFor(() => expect(args.handleSubmit).toHaveBeenCalledWith(`<p>${customizedContent}</p>`));
+    const para = contentBlock.querySelector('.ce-paragraph.cdx-block');
+    await userEvent.type(para, addedContent);
+    await userEvent.click(submitBtn);
+    await waitFor(() => expect(args.handleSubmit).toHaveBeenCalledWith(`<p>${customizedContent}${addedContent}</p>`));
+  },
+};
