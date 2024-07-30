e006dbc51a2bdc8932dc38c4da7c0d1f0a6fd5f7
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-07-30 16:31:56 +0200

    Litt modernisering av VedtakProsessIndexTilbakekreving.stories.tsx

--- packages/prosess-vedtak/src/VedtakProsessIndexTilbakekreving.stories.tsx
+++ packages/prosess-vedtak/src/VedtakProsessIndexTilbakekreving.stories.tsx
@@ -1,18 +1,17 @@
-import { action } from '@storybook/addon-actions';
-import React from 'react';
-
 import klageBehandlingArsakType from '@fpsak-frontend/kodeverk/src/behandlingArsakType';
 import behandlingResultatType from '@fpsak-frontend/kodeverk/src/behandlingResultatType';
 import behandlingStatus from '@fpsak-frontend/kodeverk/src/behandlingStatus';
 import behandlingType from '@fpsak-frontend/kodeverk/src/behandlingType';
 import alleKodeverk from '@k9-sak-web/gui/storybook/mocks/alleKodeverk.json';
+import { expect, fn, userEvent } from '@storybook/test';
+import { Meta, StoryObj } from '@storybook/react';
 import VedtakProsessIndex from './VedtakProsessIndex';
 
 const behandling = {
   id: 1,
   versjon: 1,
   type: {
-    kode: behandlingType.FORSTEGANGSSOKNAD,
+    kode: behandlingType.SOKNAD,
     kodeverk: '',
   },
   status: {
@@ -45,60 +44,53 @@ const behandling = {
   ],
 };
 
-export default {
+const meta: Meta<typeof VedtakProsessIndex> = {
   title: 'prosess/prosess-vedtak',
+  component: VedtakProsessIndex,
 };
 
-export const visSjekkTilbakekreving = args => {
-  const aksjonspunkt5085 = {
-    aksjonspunktType: { kode: 'MANU', kodeverk: 'AKSJONSPUNKT_TYPE' },
-    begrunnelse: null,
-    besluttersBegrunnelse: null,
-    definisjon: {
-      kode: '5085',
-      kodeverk: 'AKSJONSPUNKT_DEF',
-    },
-    erAktivt: true,
-    fristTid: null,
-    kanLoses: true,
-    status: { kode: 'OPPR', kodeverk: 'AKSJONSPUNKT_STATUS' },
-    toTrinnsBehandling: false,
-    toTrinnsBehandlingGodkjent: null,
-    vilkarType: null,
-    vurderPaNyttArsaker: null,
-    venteårsak: { kode: '-', kodeverk: 'VENT_AARSAK' },
-  };
+export default meta;
 
-  return (
-    <VedtakProsessIndex
-      behandling={{
-        ...behandling,
-        type: {
-          kode: behandlingType.SOKNAD,
-          kodeverk: '',
-        },
-        behandlingsresultat: {
-          vedtaksbrev: {
-            kode: 'FRITEKST',
-          },
-          type: {
-            kode: behandlingResultatType.IKKE_FASTSATT,
-          },
-        },
-      }}
-      vilkar={[]}
-      medlemskap={{ fom: '2019-01-01' }}
-      aksjonspunkter={[aksjonspunkt5085]}
-      previewCallback={action('button-click')}
-      submitCallback={action('button-click')}
-      alleKodeverk={alleKodeverk}
-      {...args}
-    />
-  );
+const aksjonspunkt5085 = {
+  aksjonspunktType: { kode: 'MANU', kodeverk: 'AKSJONSPUNKT_TYPE' },
+  begrunnelse: null,
+  besluttersBegrunnelse: null,
+  definisjon: {
+    kode: '5085',
+    kodeverk: 'AKSJONSPUNKT_DEF',
+  },
+  erAktivt: true,
+  fristTid: null,
+  kanLoses: true,
+  status: { kode: 'OPPR', kodeverk: 'AKSJONSPUNKT_STATUS' },
+  toTrinnsBehandling: false,
+  toTrinnsBehandlingGodkjent: null,
+  vilkarType: null,
+  vurderPaNyttArsaker: null,
+  venteårsak: { kode: '-', kodeverk: 'VENT_AARSAK' },
 };
 
-visSjekkTilbakekreving.args = {
-  sendVarselOmRevurdering: false,
-  employeeHasAccess: false,
-  isReadOnly: false,
+export const visSjekkTilbakekreving: StoryObj<typeof meta> = {
+  args: {
+    behandling,
+    sendVarselOmRevurdering: false,
+    employeeHasAccess: false,
+    isReadOnly: false,
+    vilkar: [],
+    medlemskap: { fom: '2019-01-01' },
+    aksjonspunkter: [aksjonspunkt5085],
+    alleKodeverk,
+    previewCallback: fn(),
+    submitCallback: fn(),
+  },
+  play: async ({ args, canvas, step }) => {
+    await step('Test å svare nei', async () => {
+      const neiRadio = canvas.getByText('Nei, behandle denne behandlingen videre');
+      expect(neiRadio).toBeInTheDocument();
+      await userEvent.click(neiRadio);
+      const bekreftBtn = canvas.getByText('Bekreft');
+      await userEvent.click(bekreftBtn);
+      await expect(args.submitCallback).toHaveBeenCalledWith([{ kode: aksjonspunkt5085.definisjon.kode }]);
+    });
+  },
 };
