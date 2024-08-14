1391eeac6195c66e908e2df452add1772e774a44
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-08-14 17:23:22 +0200

    Skriver om NotFoundPage komponent til å bruke ny BigError komponent.
    Lager også separat, moderne storybook fil for den, og flytter test inn i storybook.

--- packages/sak-infosider/src/InfosiderSakIndex.stories.tsx
+++ packages/sak-infosider/src/InfosiderSakIndex.stories.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import IngenBehandlingValgtPanel from './IngenBehandlingValgtPanel';
-import NotFoundPage from './NotFoundPage';
 import UnauthorizedPage from './UnauthorizedPage';
 
 export default {
   title: 'sak/sak-infosider',
 };
-
-export const visPanelForSideIkkeFunnet = () => <NotFoundPage />;
 
 export const visPanelForIkkeInnloggetBruker = () => <UnauthorizedPage />;
 
