f1b25f041d57f88e9a88ca12b460cca4b5f6ba16
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-08-15 10:12:31 +0200

    Skriver om IngenBehandlingValgtPanel komponent til å ikkje bruke intl lenger
    Lager også separat, moderne storybook fil for den, og flytter test inn i storybook.

--- packages/sak-infosider/src/IngenBehandlingValgtPanel.tsx
+++ packages/sak-infosider/src/IngenBehandlingValgtPanel.tsx
@@ -1,24 +1,10 @@
 import { Alert, BodyShort } from '@navikt/ds-react';
 import React from 'react';
-import { FormattedMessage, RawIntlProvider, createIntl, createIntlCache } from 'react-intl';
-import messages from '../i18n/nb_NO.json';
 
 import styles from './ingenBehandlingValgtPanel.module.css';
 
-const cache = createIntlCache();
-
-const intl = createIntl(
-  {
-    locale: 'nb-NO',
-    messages,
-  },
-  cache,
-);
-
 const getMessage = (numBehandlinger: number): string =>
-  numBehandlinger === 0
-    ? 'IngenBehandlingValgtPanel.ZeroBehandlinger'
-    : 'IngenBehandlingValgtPanel.PleaseSelectBehandling';
+  numBehandlinger === 0 ? 'Ingen behandlinger er opprettet' : 'Velg behandling';
 
 interface OwnProps {
   numBehandlinger: number;
@@ -30,15 +16,11 @@ interface OwnProps {
  * Presentasjonskomponent. Vises når ingen behandling er valgt på en fagsak
  */
 const IngenBehandlingValgtPanel = ({ numBehandlinger }: OwnProps) => (
-  <RawIntlProvider value={intl}>
-    <div className={styles.noSelectedBehandlingPanel} data-testid="IngenBehandlingValgtPanel">
-      <Alert size="small" variant="info">
-        <BodyShort size="small">
-          <FormattedMessage id={getMessage(numBehandlinger)} />
-        </BodyShort>
-      </Alert>
-    </div>
-  </RawIntlProvider>
+  <div className={styles.noSelectedBehandlingPanel} data-testid="IngenBehandlingValgtPanel">
+    <Alert size="small" variant="info">
+      <BodyShort size="small">{getMessage(numBehandlinger)}</BodyShort>
+    </Alert>
+  </div>
 );
 
 export default IngenBehandlingValgtPanel;
