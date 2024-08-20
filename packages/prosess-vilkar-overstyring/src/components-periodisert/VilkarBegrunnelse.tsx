089d94fe3c75616395971dc5c7fa916f9a6f2d38
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2024-08-20 12:26:30 +0200

    Refaktor prosess-vilkar overstyring (#6468)
    * prosess-vilkar overstyring
    Skriver bort redux-form
    
    * Rydding
    
    * La til manglende prop
    
    * Fiksing av tester
    
    * Legger tilbake ugyldige datoer i kalender
    
    * Fjerner unødige valideringsfunksjoner
    
    * Retter opp etter endring i prop

--- packages/prosess-vilkar-overstyring/src/components-periodisert/VilkarBegrunnelse.tsx
+++ packages/prosess-vilkar-overstyring/src/components-periodisert/VilkarBegrunnelse.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import { useIntl } from 'react-intl';
-
-import { TextAreaField } from '@fpsak-frontend/form';
-import { decodeHtmlEntity, hasValidText, maxLength, minLength, requiredIfNotPristine } from '@fpsak-frontend/utils';
-import { Aksjonspunkt } from '@k9-sak-web/types';
+import { decodeHtmlEntity } from '@fpsak-frontend/utils';
+import { Vilkarperiode } from '@k9-sak-web/types';
+import { TextAreaField } from '@navikt/ft-form-hooks';
+import { hasValidText, maxLength, minLength, required } from '@navikt/ft-form-validators';
+import { FunctionComponent } from 'react';
+import { VilkarBegrunnelseFormState } from './FormState';
 
 const minLength3 = minLength(3);
 const maxLength1500 = maxLength(1500);
 
 interface OwnProps {
-  begrunnelseLabel?: string;
   isReadOnly: boolean;
+}
+
+interface StaticFunctions {
+  transformValues: (values: VilkarBegrunnelseFormState) => VilkarBegrunnelseFormState;
+  buildInitialValues: (periode: Vilkarperiode) => VilkarBegrunnelseFormState;
 }
 
 /**
@@ -18,25 +22,22 @@ interface OwnProps {
  *
  * Presentasjonskomponent. Lar den NAV-ansatte skrive inn en begrunnelse før overstyring av vilkår eller beregning.
  */
-const VilkarBegrunnelse = ({ isReadOnly, begrunnelseLabel = 'VilkarBegrunnelse.Vilkar' }: OwnProps) => {
-  const intl = useIntl();
-  return (
-    <TextAreaField
-      name="begrunnelse"
-      label={intl.formatMessage({ id: begrunnelseLabel })}
-      validate={[requiredIfNotPristine, minLength3, maxLength1500, hasValidText]}
-      maxLength={1500}
-      readOnly={isReadOnly}
-      placeholder={intl.formatMessage({ id: 'VilkarBegrunnelse.BegrunnVurdering' })}
-    />
-  );
-};
+const VilkarBegrunnelse: FunctionComponent<OwnProps> & StaticFunctions = ({ isReadOnly }) => (
+  <TextAreaField
+    name="begrunnelse"
+    label="Vurdering"
+    validate={[required, minLength3, maxLength1500, hasValidText]}
+    maxLength={1500}
+    readOnly={isReadOnly}
+    placeholder="Begrunn vurderingen din"
+  />
+);
 
-VilkarBegrunnelse.buildInitialValues = (aksjonspunkt: Aksjonspunkt) => ({
-  begrunnelse: decodeHtmlEntity(aksjonspunkt && aksjonspunkt.begrunnelse ? aksjonspunkt.begrunnelse : ''),
+VilkarBegrunnelse.buildInitialValues = (periode: Vilkarperiode) => ({
+  begrunnelse: decodeHtmlEntity(periode && periode.begrunnelse ? periode.begrunnelse : ''),
 });
 
-VilkarBegrunnelse.transformValues = values => ({
+VilkarBegrunnelse.transformValues = (values: VilkarBegrunnelseFormState) => ({
   begrunnelse: values.begrunnelse,
 });
 
