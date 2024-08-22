423ba5c2ab1288230bba125df3d12939af783cfb
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2024-08-22 13:19:31 +0200

    Lag Lovreferanse-komponent, inkludert tester

---
+++ packages/v2/gui/src/shared/lovreferanse/Lovreferanse.stories.tsx
@@ -0,0 +1,85 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import withMaxWidth from '@k9-sak-web/gui/storybook/decorators/withMaxWidth.js';
+import { expect, within } from '@storybook/test';
+import { Lovreferanse } from './Lovreferanse';
+
+const meta: Meta<typeof Lovreferanse> = {
+  title: 'gui/shared/lovreferanse/Lovreferanse.tsx',
+  component: Lovreferanse,
+  decorators: [withMaxWidth(420)],
+};
+export default meta;
+
+const elemsfinder = (canvasElement: HTMLElement) => {
+  const canvas = within(canvasElement);
+  return {
+    canvas,
+    root: () => canvasElement,
+    linkEls: () => canvas.queryAllByRole('link'),
+  };
+};
+
+type Story = StoryObj<typeof Lovreferanse>;
+export const DefaultStory: Story = {
+  args: {
+    children: '§ 9-1',
+  },
+  play: async ({ canvasElement, step }) => {
+    const { linkEls } = elemsfinder(canvasElement);
+    await step('Enkeltparagrafer blir riktig lenket', async () => {
+      await expect(linkEls()).toHaveLength(1);
+      await expect(linkEls()[0]).toHaveTextContent('9-1');
+      await expect(linkEls()[0]).toHaveAttribute('href', 'https://lovdata.no/lov/1997-02-28-19/§9-1');
+    });
+  },
+};
+
+export const JamførParagraferBlirRiktigLenket: Story = {
+  args: {
+    children: '§ 9-1 jf 21-22',
+  },
+  play: async ({ canvasElement, step }) => {
+    const { linkEls } = elemsfinder(canvasElement);
+    await step('Enkeltparagrafer blir riktig lenket', async () => {
+      await expect(linkEls()).toHaveLength(2);
+      const [firstLink, secondLink] = linkEls();
+      await expect(firstLink).toHaveTextContent('9-1');
+      await expect(firstLink).toHaveAttribute('href', 'https://lovdata.no/lov/1997-02-28-19/§9-1');
+
+      await expect(secondLink).toHaveTextContent('21-22');
+      await expect(secondLink).toHaveAttribute('href', 'https://lovdata.no/lov/1997-02-28-19/§21-22');
+    });
+  },
+};
+
+export const TekstUtenParagraftegnBlirIkkeLenket: Story = {
+  args: {
+    children: '9-1 jf 21-22',
+  },
+  play: async ({ canvasElement, step }) => {
+    const { linkEls, root } = elemsfinder(canvasElement);
+    await step('Tekst uten paragraftegn blir ikke lenket', async () => {
+      await expect(linkEls()).toHaveLength(0);
+      await expect(root()).toHaveTextContent('9-1 jf 21-22');
+    });
+  },
+};
+
+export const DobbelparagraferBlirLenketRiktig: Story = {
+  args: {
+    children: '§§ 9-1 og 9-2',
+  },
+  play: async ({ canvasElement, step }) => {
+    const { linkEls } = elemsfinder(canvasElement);
+    await step('Dobbeltparagrafer blir lenket riktig', async () => {
+      await expect(linkEls()).toHaveLength(2);
+      const [firstLink, secondLink] = linkEls();
+      await expect(firstLink).toHaveTextContent('9-1');
+      await expect(firstLink).toHaveAttribute('href', 'https://lovdata.no/lov/1997-02-28-19/§9-1');
+
+      await expect(secondLink).toHaveTextContent('9-2');
+      await expect(secondLink).toHaveAttribute('href', 'https://lovdata.no/lov/1997-02-28-19/§9-2');
+    });
+  },
+};
---
+++ packages/v2/gui/src/shared/lovreferanse/Lovreferanse.tsx
@@ -0,0 +1,51 @@
+import { Link } from '@navikt/ds-react';
+import { Fragment } from 'react';
+type LovreferanseProps = {
+  /**
+   * Lovreferanseteksten.
+   *
+   * Denne teksten antas å være på formatet "§ 9-1", "§§ 9-2 og 9-3", "§ 8", "§ 9-3 jamfør 8-9" osv. */
+  children: string;
+};
+
+/**
+ * Tar inn en tekst, og lenker eventuelle lovreferanser til deres definisjon på lovdata sine sider
+ *
+ * ```tsx
+ * <Lovreferanse>§ 9-1</Lovreferanse>
+ * <Lovreferanse>§§ 9-1 og 9-2 jf. 22-21</Lovreferanse>
+ * ```
+ * */
+export const Lovreferanse = ({ children }: LovreferanseProps) => {
+  return <span>{berikMedLovdataLenker(children)}</span>;
+};
+
+const berikMedLovdataLenker = (lovreferanse: string) => {
+  const grunnUrlen = 'https://lovdata.no/lov/1997-02-28-19/';
+
+  const seksjonsRegex = /(\d+(-\d+)?)/g;
+
+  if (!lovreferanse.includes('§')) {
+    return lovreferanse;
+  }
+
+  return lovreferanse.split(' ').map((part, index) => {
+    const kapittelOgKanskjeParagraf = part.match(seksjonsRegex)?.[0];
+
+    if (kapittelOgKanskjeParagraf) {
+      const link = kapittelOgKanskjeParagraf.includes('-')
+        ? `${grunnUrlen}§${kapittelOgKanskjeParagraf}`
+        : `${grunnUrlen}§${kapittelOgKanskjeParagraf}-1`;
+
+      return (
+        <Fragment key={index}>
+          <Link href={link} title="Les mer på Lovdata.no">
+            {kapittelOgKanskjeParagraf}
+          </Link>{' '}
+        </Fragment>
+      );
+    }
+
+    return <span key={index}>{part} </span>;
+  });
+};
