195d494f6d30f62a74ae0f61909e1dbb8bbed5ef
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-09-05 15:11:56 +0200

    Legger til link til brreg.no for å søke fram tredjepartsmottaker.

---
+++ packages/v2/gui/src/sak/meldinger/TredjepartsmottakerCheckbox.tsx
@@ -0,0 +1,22 @@
+import { BodyShort, Checkbox, HStack, Link, Spacer } from '@navikt/ds-react';
+import { ExternalLinkIcon } from '@navikt/aksel-icons';
+
+export interface TredjepartsmottakerCheckboxProps {
+  readonly checked: boolean;
+  onChange(checked: boolean): void;
+  readonly disabled: boolean;
+}
+
+export const TredjepartsmottakerCheckbox = ({ checked, onChange, disabled }: TredjepartsmottakerCheckboxProps) => (
+  <HStack gap="1">
+    <Checkbox checked={checked} onChange={() => onChange(!checked)} size="small" disabled={disabled}>
+      Send til tredjepart
+    </Checkbox>
+    <Spacer />
+    <Link href="https://w2.brreg.no/enhet/sok" target="_blank" referrerPolicy="no-referrer">
+      <BodyShort size="small">
+        Søk på brreg.no <ExternalLinkIcon />
+      </BodyShort>
+    </Link>
+  </HStack>
+);
