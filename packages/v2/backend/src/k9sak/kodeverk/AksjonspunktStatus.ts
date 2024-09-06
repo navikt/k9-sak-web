0af47dd511dcf5fb19eace8a9dafb13b86c87318
Hallvard Andreas Stark <hallvard@scel.to>
2024-09-06 09:37:28 +0200

    Rydder

--- packages/v2/backend/src/k9sak/kodeverk/AksjonspunktStatus.ts
+++ packages/v2/backend/src/k9sak/kodeverk/AksjonspunktStatus.ts
@@ -1,9 +1,6 @@
 import type { AksjonspunktDto } from '@navikt/k9-sak-typescript-client';
-import type { Kodeverk } from '../../shared/Kodeverk.ts';
 
 export type AksjonspunktStatus = Exclude<AksjonspunktDto['status'], undefined>;
-
-export type AksjonspunktStatusKodeverk = Kodeverk<AksjonspunktStatus, 'AKSJONSPUNKT_TYPE'>;
 
 export type AksjonspunktStatusName = 'AVBRUTT' | 'OPPRETTET' | 'UTFORT';
 
