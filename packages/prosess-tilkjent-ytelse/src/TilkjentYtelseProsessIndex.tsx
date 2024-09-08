da92d3d643c61574dbdf7ef6cce5a867e399e5f4
Hallvard Andreas Stark <hallvard@scel.to>
2024-09-08 13:43:26 +0200

    Trenger visst intl her litt til

--- packages/prosess-tilkjent-ytelse/src/TilkjentYtelseProsessIndex.tsx
+++ packages/prosess-tilkjent-ytelse/src/TilkjentYtelseProsessIndex.tsx
@@ -1,20 +1,34 @@
 import {
   Aksjonspunkt,
   ArbeidsgiverOpplysningerPerId,
+  Behandling,
   BeregningsresultatUtbetalt,
+  Fagsak,
   KodeverkMedNavn,
 } from '@k9-sak-web/types';
+import { createIntl, createIntlCache, RawIntlProvider } from 'react-intl';
 import TilkjentYtelsePanel from './components/TilkjentYtelsePanel';
 
 interface OwnProps {
   arbeidsgiverOpplysningerPerId: ArbeidsgiverOpplysningerPerId;
   alleKodeverk: { [key: string]: KodeverkMedNavn[] };
+  behandling: Behandling;
   beregningsresultat: BeregningsresultatUtbetalt;
+  fagsak: Fagsak;
   aksjonspunkter: Aksjonspunkt[];
   isReadOnly: boolean;
   submitCallback: (data: any) => Promise<any>;
   readOnlySubmitButton: boolean;
 }
+
+const cache = createIntlCache();
+
+const intl = createIntl(
+  {
+    locale: 'nb-NO',
+  },
+  cache,
+);
 
 const TilkjentYtelseProsessIndex = ({
   beregningsresultat,
@@ -25,15 +39,17 @@ const TilkjentYtelseProsessIndex = ({
   readOnlySubmitButton,
   arbeidsgiverOpplysningerPerId,
 }: OwnProps) => (
-  <TilkjentYtelsePanel
-    beregningsresultat={beregningsresultat}
-    aksjonspunkter={aksjonspunkter}
-    alleKodeverk={alleKodeverk}
-    readOnly={isReadOnly}
-    submitCallback={submitCallback}
-    readOnlySubmitButton={readOnlySubmitButton}
-    arbeidsgiverOpplysningerPerId={arbeidsgiverOpplysningerPerId}
-  />
+  <RawIntlProvider value={intl}>
+    <TilkjentYtelsePanel
+      beregningsresultat={beregningsresultat}
+      aksjonspunkter={aksjonspunkter}
+      alleKodeverk={alleKodeverk}
+      readOnly={isReadOnly}
+      submitCallback={submitCallback}
+      readOnlySubmitButton={readOnlySubmitButton}
+      arbeidsgiverOpplysningerPerId={arbeidsgiverOpplysningerPerId}
+    />
+  </RawIntlProvider>
 );
 
 export default TilkjentYtelseProsessIndex;
