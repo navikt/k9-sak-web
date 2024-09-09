fffa9e128bc97956b4518a7648730956f1b2ef35
Thomas H. Wiberg <thomas@thawi.no>
2024-09-09 15:06:28 +0200

    Update PerioderMedMedlemskapFaktaPanel.spec.tsx

--- packages/fakta-medlemskap/src/components/oppholdInntektOgPerioder/PerioderMedMedlemskapFaktaPanel.spec.tsx
+++ packages/fakta-medlemskap/src/components/oppholdInntektOgPerioder/PerioderMedMedlemskapFaktaPanel.spec.tsx
@@ -203,7 +203,7 @@ describe('<PerioderMedMedlemskapFaktaPanel>', () => {
         },
       ],
       isPeriodAksjonspunktClosed: false,
-      medlemskapManuellVurderingType: { kode: 'manuellType', kodeverk: '' },
+      medlemskapManuellVurderingType: 'manuellType',
       fodselsdato: '2017-10-15',
       hasPeriodeAksjonspunkt: true,
     });
