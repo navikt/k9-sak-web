c17e0151d3ea932ddf48330e0e733ad514c14b0a
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2024-09-18 08:39:02 +0200

    TSFF-827 - Visning av opplysninger ved automatisk vurdert medlemskap (#6592)
    * Visning av detaljer ved automatisk vurdert medlemskap
    
    * Fiks test
    
    * Fiks test

--- packages/fakta-medlemskap/src/components/oppholdInntektOgPerioder/OppholdINorgeOgAdresserFaktaPanel.tsx
+++ packages/fakta-medlemskap/src/components/oppholdInntektOgPerioder/OppholdINorgeOgAdresserFaktaPanel.tsx
@@ -1,55 +1,14 @@
-import BostedSokerFaktaIndex from '@fpsak-frontend/fakta-bosted-soker';
 import aksjonspunktCodes from '@fpsak-frontend/kodeverk/src/aksjonspunktCodes';
 import { isAksjonspunktOpen } from '@fpsak-frontend/kodeverk/src/aksjonspunktStatus';
-import { FaktaGruppe, PeriodLabel, VerticalSpacer } from '@fpsak-frontend/shared-components';
-import { required } from '@fpsak-frontend/utils';
-import { BodyShort, Detail, HGrid } from '@navikt/ds-react';
-import { RadioGroupPanel } from '@navikt/ft-form-hooks';
-import countries from 'i18n-iso-countries';
-import norwegianLocale from 'i18n-iso-countries/langs/no.json';
 import { FunctionComponent } from 'react';
 
 import { Aksjonspunkt, KodeverkMedNavn, Personopplysninger } from '@k9-sak-web/types';
 import { useFormContext, useWatch } from 'react-hook-form';
 import { OppholdInntektOgPerioderFormState, OppholdINorgeOgAdresserFaktaPanelFormState } from './FormState';
 import { MerknaderFraBeslutter } from './MerknaderFraBeslutter';
-import { Opphold } from './Opphold';
+import OppholdINorgeOgAdresser from './OppholdINorgeOgAdresser';
 import { Periode } from './Periode';
 import { Soknad } from './Soknad';
-import styles from './oppholdINorgeOgAdresserFaktaPanel.module.css';
-
-countries.registerLocale(norwegianLocale);
-
-const capitalizeFirstLetter = (landNavn: string) => {
-  const string = landNavn.toLowerCase();
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
-
-const formatLandNavn = (landNavn: string) => {
-  if (landNavn.length === 2 || landNavn.length === 3) {
-    if (landNavn === 'XXK') {
-      return 'Kosovo';
-    }
-    return countries.getName(landNavn, 'no');
-  }
-  return landNavn;
-};
-
-const lagOppholdIUtland = (utlandsOpphold: Opphold['utlandsopphold']) =>
-  Array.isArray(utlandsOpphold) && utlandsOpphold.length > 0 ? (
-    utlandsOpphold.map(u => (
-      <div key={`${u.landNavn}${u.fom}${u.tom}`}>
-        <HGrid gap="1" columns={{ xs: '4fr 8fr' }}>
-          <BodyShort size="small">{capitalizeFirstLetter(formatLandNavn(u.landNavn))}</BodyShort>
-          <BodyShort size="small">
-            <PeriodLabel showTodayString dateStringFom={u.fom} dateStringTom={u.tom} />
-          </BodyShort>
-        </HGrid>
-      </div>
-    ))
-  ) : (
-    <BodyShort size="small">-</BodyShort>
-  );
 
 interface OppholdINorgeOgAdresserFaktaPanelProps {
   readOnly: boolean;
@@ -79,61 +38,15 @@ const OppholdINorgeOgAdresserFaktaPanel: FunctionComponent<OppholdINorgeOgAdress
     name: 'oppholdInntektOgPeriodeForm',
   });
   return (
-    <FaktaGruppe merknaderFraBeslutter={alleMerknaderFraBeslutter[aksjonspunktCodes.AVKLAR_OM_BRUKER_ER_BOSATT]}>
-      <HGrid gap="1" columns={{ xs: '6fr 6fr' }}>
-        <div>
-          <FaktaGruppe withoutBorder titleCode="Opplysninger oppgitt i søknaden" useIntl={false}>
-            <Detail>Opphold utenfor Norge</Detail>
-            <VerticalSpacer fourPx />
-            {!!opphold && lagOppholdIUtland(opphold.utlandsopphold)}
-          </FaktaGruppe>
-        </div>
-        <div>
-          <FaktaGruppe withoutBorder titleCode="Bostedsadresse fra folkeregisteret" useIntl={false}>
-            {foreldre.map(f => (
-              <div key={f.personopplysning.navn}>
-                {f.isApplicant && (
-                  <BostedSokerFaktaIndex personopplysninger={f.personopplysning} alleKodeverk={alleKodeverk} />
-                )}
-                {!f.isApplicant && (
-                  <BostedSokerFaktaIndex
-                    sokerTypeTextId="OppholdINorgeOgAdresserFaktaPanel.Parent"
-                    personopplysninger={f.personopplysning}
-                    alleKodeverk={alleKodeverk}
-                  />
-                )}
-              </div>
-            ))}
-          </FaktaGruppe>
-          {hasBosattAksjonspunkt && (
-            <div className={styles.ieFlex}>
-              <RadioGroupPanel
-                name="oppholdInntektOgPeriodeForm.bosattVurdering"
-                validate={[required]}
-                isReadOnly={readOnly}
-                isEdited={isBosattAksjonspunktClosed}
-                isHorizontal
-                isTrueOrFalseSelection
-                radios={[
-                  {
-                    value: 'true',
-                    label: 'Søker er bosatt i Norge',
-                  },
-                  {
-                    value: 'false',
-                    label: (
-                      <>
-                        Søker er <b>ikke</b> bosatt i Norge
-                      </>
-                    ),
-                  },
-                ]}
-              />
-            </div>
-          )}
-        </div>
-      </HGrid>
-    </FaktaGruppe>
+    <OppholdINorgeOgAdresser
+      alleKodeverk={alleKodeverk}
+      alleMerknaderFraBeslutter={alleMerknaderFraBeslutter}
+      foreldre={foreldre}
+      hasBosattAksjonspunkt={hasBosattAksjonspunkt}
+      isBosattAksjonspunktClosed={isBosattAksjonspunktClosed}
+      opphold={opphold}
+      readOnly={readOnly}
+    />
   );
 };
 const createParent = (isApplicant: boolean, personopplysning?: Personopplysninger) => ({
