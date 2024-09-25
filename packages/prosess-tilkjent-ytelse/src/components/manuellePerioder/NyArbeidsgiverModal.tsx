27bc292875cf4dd03d56d800751af50c0574641f
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2024-09-25 10:36:45 +0200

    Revert "Revert "TSFF-743 - refaktorering av prosess-tilkjent-ytelse (#6481)" …"
    This reverts commit 183343f16aa5d624a063a4a7b95b4426517c7f01.

--- packages/prosess-tilkjent-ytelse/src/components/manuellePerioder/NyArbeidsgiverModal.tsx
+++ packages/prosess-tilkjent-ytelse/src/components/manuellePerioder/NyArbeidsgiverModal.tsx
@@ -1,95 +1,73 @@
-import { behandlingForm, InputField } from '@fpsak-frontend/form';
 import { FlexColumn, FlexContainer, FlexRow, VerticalSpacer } from '@fpsak-frontend/shared-components';
 import { hasValidOrgNumber, required } from '@fpsak-frontend/utils';
 import { Button, Modal } from '@navikt/ds-react';
-import React from 'react';
-import { injectIntl, WrappedComponentProps } from 'react-intl';
-import { connect } from 'react-redux';
-import { InjectedFormProps } from 'redux-form';
+import { InputField } from '@navikt/ft-form-hooks';
+import { useFormContext } from 'react-hook-form';
+import { NyArbeidsgiverFormState, TilkjentYtelseFormState } from './FormState';
 import styles from './periode.module.css';
-
-const defaultArbeidsgiver = {
-  navn: '',
-  orgNr: '',
-};
 
 interface OwnProps {
   showModal?: boolean;
-  closeEvent: (...args: any[]) => any;
-  cancelEvent: (...args: any[]) => any;
+  closeEvent: (values: NyArbeidsgiverFormState) => void;
+  cancelEvent: () => void;
 }
 
-export const NyArbeidsgiverModal = ({
-  showModal = false,
-  closeEvent,
-  cancelEvent,
-  intl,
-  ...formProps
-}: OwnProps & WrappedComponentProps & InjectedFormProps) => (
-  <Modal className={styles.modal} open={showModal} aria-label="Ny arbeidsgiver" onClose={closeEvent}>
-    <Modal.Body>
-      <FlexContainer wrap>
-        <FlexRow>
-          <FlexColumn className={styles.fullWidth}>
-            <InputField
-              label={{ id: 'TilkjentYtelse.ArbeidsgiverNavn' }}
-              name="navn"
-              validate={[required]}
-              format={value => value}
-            />
+const NyArbeidsgiverModal = ({ showModal = false, closeEvent, cancelEvent }: OwnProps) => {
+  const formMethods = useFormContext<TilkjentYtelseFormState>();
+  const nyArbeidsgiverFormState = formMethods.watch('nyArbeidsgiverForm');
 
-            <InputField
-              label={{ id: 'TilkjentYtelse.ArbeidsgiverOrgnummer' }}
-              name="orgNr"
-              validate={[required, hasValidOrgNumber]}
-              format={value => value}
-            />
-          </FlexColumn>
-        </FlexRow>
-        <FlexRow>
-          <FlexColumn className={styles.right}>
-            <VerticalSpacer eightPx />
-            <Button
-              variant="primary"
-              size="small"
-              className={styles.button}
-              onClick={formProps.handleSubmit}
-              disabled={formProps.pristine}
-            >
-              {intl.formatMessage({ id: 'TilkjentYtelse.Ok' })}
-            </Button>
-            <Button
-              variant="secondary"
-              size="small"
-              onClick={() => {
-                cancelEvent();
-                formProps.destroy();
-              }}
-            >
-              {intl.formatMessage({ id: 'TilkjentYtelse.Avbryt' })}
-            </Button>
-          </FlexColumn>
-        </FlexRow>
-      </FlexContainer>
-    </Modal.Body>
-  </Modal>
-);
+  const handleSubmit = () => {
+    formMethods.trigger('nyArbeidsgiverForm').then(valid => {
+      if (valid) {
+        closeEvent(nyArbeidsgiverFormState);
+      }
+    });
+  };
 
-interface PureOwnProps {
-  closeEvent: (...args: any[]) => any;
-}
-
-const mapStateToPropsFactory = (_initialState: any, ownProps: PureOwnProps) => {
-  const onSubmit = (values: any) => ownProps.closeEvent(values);
-
-  return () => ({
-    initialValues: defaultArbeidsgiver,
-    onSubmit,
-  });
+  return (
+    <Modal className={styles.modal} open={showModal} aria-label="Ny arbeidsgiver" onClose={cancelEvent}>
+      <Modal.Body>
+        <FlexContainer wrap>
+          <FlexRow>
+            <FlexColumn className={styles.fullWidth}>
+              <InputField label="Navn" name="nyArbeidsgiverForm.navn" validate={[required]} format={value => value} />
+              <InputField
+                label="Organisasjonsnummer"
+                name="nyArbeidsgiverForm.orgNr"
+                validate={[required, hasValidOrgNumber]}
+                format={value => value}
+              />
+            </FlexColumn>
+          </FlexRow>
+          <FlexRow>
+            <FlexColumn className={styles.right}>
+              <VerticalSpacer eightPx />
+              <Button
+                variant="primary"
+                size="small"
+                className={styles.button}
+                disabled={!formMethods.formState.isDirty}
+                onClick={handleSubmit}
+                type="button"
+              >
+                Ok
+              </Button>
+              <Button
+                variant="secondary"
+                size="small"
+                onClick={() => {
+                  cancelEvent();
+                }}
+                type="button"
+              >
+                Avbryt
+              </Button>
+            </FlexColumn>
+          </FlexRow>
+        </FlexContainer>
+      </Modal.Body>
+    </Modal>
+  );
 };
 
-export default connect(mapStateToPropsFactory)(
-  behandlingForm({
-    form: 'nyArbeidsgiverForm',
-  })(injectIntl(NyArbeidsgiverModal)),
-);
+export default NyArbeidsgiverModal;
