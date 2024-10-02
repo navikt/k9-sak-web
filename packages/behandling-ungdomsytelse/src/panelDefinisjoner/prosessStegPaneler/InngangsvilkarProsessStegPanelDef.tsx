f8e110f577eaa71482888dab5908a8169c586345
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2024-10-02 13:38:48 +0200

    TSFF-854 - inngangsvilkår for ungdomsprogrammet (#6651)

--- packages/behandling-ungdomsytelse/src/panelDefinisjoner/prosessStegPaneler/InngangsvilkarProsessStegPanelDef.tsx
+++ packages/behandling-ungdomsytelse/src/panelDefinisjoner/prosessStegPaneler/InngangsvilkarProsessStegPanelDef.tsx
@@ -1,16 +1,22 @@
-import { prosessStegCodes } from '@k9-sak-web/konstanter';
 import { ProsessStegDef } from '@k9-sak-web/behandling-felles';
+import { prosessStegCodes } from '@k9-sak-web/konstanter';
 
-import SoknadsfristPanelDef from './inngangsvilkarPaneler/SoknadsfristPanelDef';
 import AlderPanelDef from './inngangsvilkarPaneler/AlderPanelDef';
 import OmsorgenForPanelDef from './inngangsvilkarPaneler/OmsorgenForPanelDef';
+import SoknadsfristPanelDef from './inngangsvilkarPaneler/SoknadsfristPanelDef';
+import UngdomsprogramPanelDef from './inngangsvilkarPaneler/UngdomsprogramPanelDef';
 
 class InngangsvilkarProsessStegPanelDef extends ProsessStegDef {
   getUrlKode = () => prosessStegCodes.INNGANGSVILKAR;
 
   getTekstKode = () => 'Behandlingspunkt.Inngangsvilkar';
 
-  getPanelDefinisjoner = () => [new SoknadsfristPanelDef(), new AlderPanelDef(), new OmsorgenForPanelDef()];
+  getPanelDefinisjoner = () => [
+    new SoknadsfristPanelDef(),
+    new AlderPanelDef(),
+    new OmsorgenForPanelDef(),
+    new UngdomsprogramPanelDef(),
+  ];
 }
 
 export default InngangsvilkarProsessStegPanelDef;
