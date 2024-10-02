f6cbd8af5f48df7dc85d7189b7fbc3830c18b3e4
Hallvard Andreas Stark <hallvard@scel.to>
2024-10-02 13:23:01 +0200

    TSFF-854 - inngangsvilkår for ungdomsprogrammet

---
+++ packages/behandling-ungdomsytelse/src/panelDefinisjoner/prosessStegPaneler/inngangsvilkarPaneler/UngdomsprogrammetPanelDef.tsx
@@ -0,0 +1,22 @@
+import vilkarType from '@fpsak-frontend/kodeverk/src/vilkarType';
+import { ProsessStegOverstyringPanelDef, ProsessStegPanelDef } from '@k9-sak-web/behandling-felles';
+
+class UngdomsprogrammetPanelDef extends ProsessStegPanelDef {
+  overstyringDef = new ProsessStegOverstyringPanelDef(this);
+
+  getId = () => 'UNGDOMSPROGRAMMET';
+
+  getTekstKode = () => 'Inngangsvilkar.Ungdomsprogrammet';
+
+  getKomponent = props => this.overstyringDef.getKomponent(props);
+
+  getAksjonspunktKoder = () => [];
+
+  getVilkarKoder = () => [vilkarType.UNGDOMSPROGRAMMET];
+
+  getOverstyrVisningAvKomponent = ({ vilkarForSteg }) => vilkarForSteg.length > 0;
+
+  getData = data => this.overstyringDef.getData(data);
+}
+
+export default UngdomsprogrammetPanelDef;
