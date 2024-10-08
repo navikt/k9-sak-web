dacce60727592215c395f47825e17095fe4c5b30
Hallvard Andreas Stark <hallvard@scel.to>
2024-10-08 11:51:42 +0200

    TSFF-854 - henter data om satser og uttak. Viser dette i to tabeller

--- packages/behandling-ungdomsytelse/src/panelDefinisjoner/prosessStegPaneler/BeregningProsessStegPanelDef.tsx
+++ packages/behandling-ungdomsytelse/src/panelDefinisjoner/prosessStegPaneler/BeregningProsessStegPanelDef.tsx
@@ -1,10 +1,21 @@
+import vilkarType from '@fpsak-frontend/kodeverk/src/vilkarType';
+import vilkarUtfallType from '@fpsak-frontend/kodeverk/src/vilkarUtfallType';
 import { ProsessStegDef, ProsessStegPanelDef } from '@k9-sak-web/behandling-felles';
 import UngBeregningIndex from '@k9-sak-web/gui/prosess/ung-beregning/UngBeregningIndex.js';
 import { prosessStegCodes } from '@k9-sak-web/konstanter';
+import { Vilkar } from '@k9-sak-web/types';
 
 class PanelDef extends ProsessStegPanelDef {
   getKomponent = props => <UngBeregningIndex {...props} />;
   getOverstyrVisningAvKomponent = () => true;
+
+  getOverstyrtStatus = ({ vilkar }: { vilkar: Vilkar[] }) => {
+    return vilkar
+      ?.find(v => v.vilkarType.kode === vilkarType.UNGDOMSPROGRAMVILKARET)
+      ?.perioder?.some(p => p.vilkarStatus.kode === vilkarUtfallType.OPPFYLT)
+      ? vilkarUtfallType.OPPFYLT
+      : vilkarUtfallType.IKKE_VURDERT;
+  };
 }
 
 class BeregningProsessStegPanelDef extends ProsessStegDef {
