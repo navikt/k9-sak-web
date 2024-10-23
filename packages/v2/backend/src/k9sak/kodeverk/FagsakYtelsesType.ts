c705ff61df3721709e855c5863b0be926be52863
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-10-23 16:11:53 +0200

    Bruk generert enum konstant for FagsakYtelsesType
    Så slepp ein å manuelt halde den i synk, og få kompileringsfeil i pipeline når ny verdi blir lagt til i k9-sak koden.

--- packages/v2/backend/src/k9sak/kodeverk/FagsakYtelsesType.ts
+++ packages/v2/backend/src/k9sak/kodeverk/FagsakYtelsesType.ts
@@ -1,30 +1,13 @@
-import { type FagsakDto } from '../generated';
+import {
+  type fagsakYtelseType as generatedFagsakYtelseTypeEnumUnion,
+  fagsakYtelseType as generatedFagsakYtelseType,
+} from '../generated';
 import type { Kodeverk } from '../../shared/Kodeverk.ts';
 
-export type FagsakYtelsesType = FagsakDto['sakstype'];
+export type FagsakYtelsesType = generatedFagsakYtelseTypeEnumUnion;
 
 // Oppretter Kodeverk type med verdier frå openapi generert union type
 export type FagsakYtelsesTypeKodeverk = Kodeverk<FagsakYtelsesType, 'FAGSAK_YTELSE'>;
 
-// Typesikker måte å lage ein "enum" frå union type generert frå backend.
-// Sikrer at vi er i synk med backend definisjoner.
-export const fagsakYtelsesType: Readonly<Record<FagsakYtelsesType, FagsakYtelsesType>> = {
-  DAG: 'DAG',
-  FRISINN: 'FRISINN',
-  SP: 'SP',
-  PSB: 'PSB',
-  PPN: 'PPN',
-  OMP: 'OMP',
-  OMP_KS: 'OMP_KS',
-  OMP_MA: 'OMP_MA',
-  OMP_AO: 'OMP_AO',
-  OLP: 'OLP',
-  AAP: 'AAP',
-  ES: 'ES',
-  FP: 'FP',
-  SVP: 'SVP',
-  EF: 'EF',
-  UNG: 'UNG',
-  OBSOLETE: 'OBSOLETE',
-  '-': '-',
-};
+// Eksporter generert enum konstant
+export const fagsakYtelsesType = generatedFagsakYtelseType;
