995c1542ecfd832aab2a497fff7dede04eec2399
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-10-23 16:53:45 +0200

    Bruk generert enum konstant for OpptjeningAktivitetType verdier.

--- packages/v2/backend/src/k9sak/kodeverk/OpptjeningAktivitetType.ts
+++ packages/v2/backend/src/k9sak/kodeverk/OpptjeningAktivitetType.ts
@@ -1,32 +1,13 @@
-import type { OpptjeningAktivitetDto } from '../generated';
-import type { Kodeverk } from '../../shared/Kodeverk.ts';
+import {
+  type opptjeningAktivitetType2 as generatedOpptjeningAktivitetTypeEnumUnion,
+  opptjeningAktivitetType2,
+} from '../generated';
+// ^- opptjeningAktivitetType (uten 2) er generert frå kalkulus type, så skal ikkje brukast her
+import type { Kodeverk } from '../../shared/Kodeverk.js';
 
-export type OpptjeningAktivitetType = Exclude<OpptjeningAktivitetDto['aktivitetType'], undefined | null>;
+export type OpptjeningAktivitetType = generatedOpptjeningAktivitetTypeEnumUnion;
 
 export type OpptjeningAktivitetTypeKodeverk = Kodeverk<OpptjeningAktivitetType, 'OPPTJENING_AKTIVITET_TYPE'>;
 
-// lager enum ut av typen så ein kan bruke den der det er ønskelig
-export const opptjeningAktivitetType: Readonly<Record<OpptjeningAktivitetType, OpptjeningAktivitetType>> = {
-  ARBEID: 'ARBEID',
-  FRISINN: 'FRISINN',
-  AAP: 'AAP',
-  DAGPENGER: 'DAGPENGER',
-  ETTERLØNN_SLUTTPAKKE: 'ETTERLØNN_SLUTTPAKKE',
-  FORELDREPENGER: 'FORELDREPENGER',
-  FRILANS: 'FRILANS',
-  MELLOM_ARBEID: 'MELLOM_ARBEID',
-  NÆRING: 'NÆRING',
-  MILITÆR_ELLER_SIVILTJENESTE: 'MILITÆR_ELLER_SIVILTJENESTE',
-  OMSORGSPENGER: 'OMSORGSPENGER',
-  OPPLÆRINGSPENGER: 'OPPLÆRINGSPENGER',
-  PLEIEPENGER: 'PLEIEPENGER',
-  PLEIEPENGER_AV_DAGPENGER: 'PLEIEPENGER_AV_DAGPENGER',
-  SVANGERSKAPSPENGER: 'SVANGERSKAPSPENGER',
-  SYKEPENGER: 'SYKEPENGER',
-  SYKEPENGER_AV_DAGPENGER: 'SYKEPENGER_AV_DAGPENGER',
-  UTDANNINGSPERMISJON: 'UTDANNINGSPERMISJON',
-  UTENLANDSK_ARBEIDSFORHOLD: 'UTENLANDSK_ARBEIDSFORHOLD',
-  VENTELØNN_VARTPENGER: 'VENTELØNN_VARTPENGER',
-  VIDERE_ETTERUTDANNING: 'VIDERE_ETTERUTDANNING',
-  '-': '-',
-};
+// Re-eksporterer generert enum const direkte
+export { opptjeningAktivitetType2 as opptjeningAktivitetType };
