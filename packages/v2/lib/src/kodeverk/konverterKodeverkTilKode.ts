242ca64d9493de0f4448ba815ad2fd5208fdbdc8
Thomas H. Wiberg <thomas@thawi.no>
2024-10-25 17:51:30 +0200

    Kodeverk: Skriv om for SKJERMLENKE_TYPER også

--- packages/v2/lib/src/kodeverk/konverterKodeverkTilKode.ts
+++ packages/v2/lib/src/kodeverk/konverterKodeverkTilKode.ts
@@ -16,6 +16,7 @@ export const konverterKodeverkTilKode = (data: any, erTilbakekreving: boolean) =
           'AKSJONSPUNKT_DEF', // Skrive om denne foreløpig, de ekstra attributtene skal fjernes i backend
           'BEHANDLING_RESULTAT_TYPE', // Skrive om denne foreløpig, de ekstra attributtene skal fjernes i backend
           'HISTORIKKINNSLAG_TYPE', // Inneholder .mal, men mal brukes ikke. Malen utledes fra .kode
+          'SKJERMLENKE_TYPE', // Denne er anderledes fra de andre, og har tre attributter selv om det ikke er fra tilbakekreving
         ].includes(data[key]?.kodeverk)
       ) {
         data[key] = data[key].kode; // eslint-disable-line no-param-reassign
