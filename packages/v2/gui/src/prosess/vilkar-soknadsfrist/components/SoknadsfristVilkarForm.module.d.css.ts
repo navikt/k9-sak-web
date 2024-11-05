65694178fa17b167433ee14d8857ef5133197123
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2024-11-05 13:44:56 +0100

    Lager prosess-søknadsfrist i v2-versjon. (#6722)
    * Lager prosess-søknadsfrist i v2-versjon.
    
    * Rydding
    
    * Legger inn bruk av object-hash igjen
    
    * Flytter tester inn i story
    
    * rydding
    
    * rydding
    
    * Bruker panel for ungdomsytelse
    
    * Fiks imports
    
    * Fikset diverse

---
+++ packages/v2/gui/src/prosess/vilkar-soknadsfrist/components/SoknadsfristVilkarForm.module.d.css.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly "aksjonspunkt": string;
+  readonly "apentAksjonspunkt": string;
+  readonly "key": string;
+  readonly "keyWithoutCursor": string;
+  readonly "status": string;
+};
+export = styles;
+
---
+++ packages/v2/gui/src/prosess/vilkar-soknadsfrist/components/SoknadsfristVilkarHeader.tsx
@@ -0,0 +1,118 @@
+import { vilkårStatus } from '@k9-sak-web/backend/k9sak/kodeverk/behandling/VilkårStatus.js';
+import { Lovreferanse } from '@k9-sak-web/gui/shared/lovreferanse/Lovreferanse.js';
+import { CheckmarkCircleFillIcon, KeyHorizontalIcon, XMarkOctagonFillIcon } from '@navikt/aksel-icons';
+import { Button, Detail, Heading, HStack, Label } from '@navikt/ds-react';
+import type { SetStateAction } from 'react';
+import type { SoknadsfristAksjonspunktType } from '../types/SoknadsfristAksjonspunktType';
+
+const isOverridden = (aksjonspunktCode: string, aksjonspunktCodes?: string[]) =>
+  aksjonspunktCodes?.some(code => code === aksjonspunktCode);
+const isHidden = (kanOverstyre: boolean, aksjonspunktCode: string, aksjonspunktCodes?: string[]) =>
+  !isOverridden(aksjonspunktCode, aksjonspunktCodes) && !kanOverstyre;
+
+const VilkarOkMessage = ({ originalErVilkarOk }: { originalErVilkarOk: boolean }) => {
+  let message = 'Ikke behandlet';
+  if (originalErVilkarOk) {
+    message = 'Vilkåret er oppfylt for hele perioden';
+  } else if (originalErVilkarOk === false) {
+    message = 'Vilkåret er avslått';
+  }
+
+  return (
+    <Label size="small" as="p">
+      {message}
+    </Label>
+  );
+};
+
+interface SoknadsfristVilkarHeaderProps {
+  aksjonspunkter: SoknadsfristAksjonspunktType[];
+  erOverstyrt?: boolean;
+  kanOverstyreAccess?: {
+    employeeHasAccess: boolean;
+    isEnabled: boolean;
+  };
+  lovReferanse?: string;
+  overrideReadOnly: boolean;
+  overstyringApKode: string;
+  panelTittelKode: string;
+  toggleOverstyring: (overstyrtPanel: SetStateAction<string[]>) => void;
+  status: string;
+}
+
+const SoknadsfristVilkarHeader = ({
+  panelTittelKode,
+  erOverstyrt,
+  overstyringApKode,
+  lovReferanse,
+  overrideReadOnly,
+  kanOverstyreAccess,
+  aksjonspunkter,
+  status,
+  toggleOverstyring,
+}: SoknadsfristVilkarHeaderProps) => {
+  const aksjonspunktCodes = aksjonspunkter.map(a => a.definisjon).filter(a => !!a);
+  const erOppfylt = vilkårStatus.OPPFYLT === status;
+  const originalErVilkarOk = vilkårStatus.IKKE_VURDERT !== status ? erOppfylt : undefined;
+  const togglePa = () => {
+    toggleOverstyring(oldArray => [...oldArray, overstyringApKode]);
+  };
+  return (
+    <>
+      <>
+        <HStack gap="4" align="center">
+          {!erOverstyrt && originalErVilkarOk !== undefined && (
+            <>
+              {originalErVilkarOk ? (
+                <CheckmarkCircleFillIcon fontSize={24} style={{ color: 'var(--a-surface-success)' }} />
+              ) : (
+                <XMarkOctagonFillIcon fontSize={24} style={{ color: 'var(--a-surface-danger)' }} />
+              )}
+            </>
+          )}
+          <Heading size="small" level="2">
+            {panelTittelKode}
+          </Heading>
+          {lovReferanse && (
+            <Detail>
+              <Lovreferanse>{lovReferanse}</Lovreferanse>
+            </Detail>
+          )}
+        </HStack>
+        <div className="flex">
+          <div className="px-2">
+            <div className="mt-2" />
+            <VilkarOkMessage originalErVilkarOk={!!originalErVilkarOk} />
+          </div>
+          {originalErVilkarOk !== undefined &&
+            kanOverstyreAccess?.employeeHasAccess &&
+            !isHidden(kanOverstyreAccess.isEnabled, overstyringApKode, aksjonspunktCodes) && (
+              <>
+                {!erOverstyrt && !overrideReadOnly && (
+                  <div className="px-2">
+                    <Button
+                      size="small"
+                      type="button"
+                      variant="tertiary"
+                      icon={<KeyHorizontalIcon className="-rotate-45 text-3xl" />}
+                      onClick={togglePa}
+                      aria-label="Overstyring av søknadsfristvilkåret"
+                    />
+                  </div>
+                )}
+                {(erOverstyrt || overrideReadOnly) && (
+                  <div className="px-2">
+                    <div className="mt-1" />
+                    <KeyHorizontalIcon className="-rotate-45 text-3xl text-[#b7b1a9]" />
+                  </div>
+                )}
+              </>
+            )}
+        </div>
+      </>
+      <div className="mt-2" />
+    </>
+  );
+};
+
+export default SoknadsfristVilkarHeader;
