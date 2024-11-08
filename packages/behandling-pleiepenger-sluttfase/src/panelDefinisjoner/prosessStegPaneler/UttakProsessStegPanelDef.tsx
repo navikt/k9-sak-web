df8149e1fe5b39838acefd8e9dd1f3f1f8881013
Hallvard Andreas Stark <hallvard@scel.to>
2024-11-08 12:54:05 +0100

    Sender ned props for alle relevante ytelser

--- packages/behandling-pleiepenger-sluttfase/src/panelDefinisjoner/prosessStegPaneler/UttakProsessStegPanelDef.tsx
+++ packages/behandling-pleiepenger-sluttfase/src/panelDefinisjoner/prosessStegPaneler/UttakProsessStegPanelDef.tsx
@@ -1,9 +1,8 @@
+import aksjonspunktCodes from '@fpsak-frontend/kodeverk/src/aksjonspunktCodes';
+import fagsakYtelseType from '@fpsak-frontend/kodeverk/src/fagsakYtelseType';
 import vilkarUtfallType from '@fpsak-frontend/kodeverk/src/vilkarUtfallType';
 import { ProsessStegDef, ProsessStegPanelDef } from '@k9-sak-web/behandling-felles';
 import { prosessStegCodes } from '@k9-sak-web/konstanter';
-import React from 'react';
-import aksjonspunktCodes from '@fpsak-frontend/kodeverk/src/aksjonspunktCodes';
-import fagsakYtelseType from '@fpsak-frontend/kodeverk/src/fagsakYtelseType';
 import AntallDagerLivetsSluttfaseIndex from '@k9-sak-web/prosess-uttak-antall-dager-sluttfase';
 import Uttak from '../../components/Uttak';
 import { PleiepengerSluttfaseBehandlingApiKeys } from '../../data/pleiepengerSluttfaseBehandlingApi';
@@ -20,6 +19,7 @@ class PanelDef extends ProsessStegPanelDef {
     erFagytelsetypeLivetsSluttfase,
     submitCallback,
     virkningsdatoUttakNyeRegler,
+    isReadOnly,
   }) => (
     <>
       <AntallDagerLivetsSluttfaseIndex kvoteInfo={kvoteInfo} />
@@ -33,6 +33,7 @@ class PanelDef extends ProsessStegPanelDef {
         erFagytelsetypeLivetsSluttfase={erFagytelsetypeLivetsSluttfase}
         submitCallback={submitCallback}
         virkningsdatoUttakNyeRegler={virkningsdatoUttakNyeRegler}
+        readOnly={isReadOnly}
       />
     </>
   );
