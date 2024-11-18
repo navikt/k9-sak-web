c5c0f2838a655413ed75e26fc706c69a4c2e5580
Hallvard Andreas Stark <hallvard@scel.to>
2024-11-18 14:37:45 +0100

    Endrer referanse

--- packages/v2/gui/src/prosess/ung-beregning/UngBeregningBackendApiType.ts
+++ packages/v2/gui/src/prosess/ung-beregning/UngBeregningBackendApiType.ts
@@ -1,4 +1,4 @@
-import type { UngdomsytelseSatsPeriodeDto } from '@k9-sak-web/backend/k9sak/generated';
+import type { UngdomsytelseSatsPeriodeDto } from '@k9-sak-web/backend/ungsak/generated';
 
 export type UngBeregningBackendApiType = {
   getSatser(behandlingUuid: string): Promise<UngdomsytelseSatsPeriodeDto[]>;
