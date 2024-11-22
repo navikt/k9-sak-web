7d8ee24ded35e149f0579eb41ce229281c1cf069
Hallvard Andreas Stark <hallvard@scel.to>
2024-11-22 14:44:06 +0100

    Tar bort fakta-felles som bare ble brukt ca et sted

--- packages/fakta-verge/src/components/FaktaBegrunnelseTextField.spec.tsx
+++ packages/fakta-verge/src/components/FaktaBegrunnelseTextField.spec.tsx
@@ -1,55 +1,39 @@
-import { renderWithIntlAndReduxForm } from '@fpsak-frontend/utils-test/test-utils';
-import { screen } from '@testing-library/react';
-import React from 'react';
-import messages from '../../i18n/nb_NO.json';
+import { render, screen } from '@testing-library/react';
 import FaktaBegrunnelseTextField from './FaktaBegrunnelseTextField';
 
 describe('<FaktaBegrunnelseTextField>', () => {
   it('skal ikke vise tekstfelt når en ikke har lov til å løse aksjonspunkt', () => {
-    renderWithIntlAndReduxForm(
-      <FaktaBegrunnelseTextField isReadOnly={false} isSubmittable={false} hasBegrunnelse={false} />,
-      { messages },
-    );
+    render(<FaktaBegrunnelseTextField isReadOnly={false} isSubmittable={false} hasBegrunnelse={false} />);
 
     expect(screen.queryByRole('textbox')).not.toBeInTheDocument();
   });
 
   it('skal vise tekstfelt når en har lov til å løse aksjonspunkt og en har gjort endringer', () => {
-    renderWithIntlAndReduxForm(<FaktaBegrunnelseTextField isReadOnly={false} isSubmittable hasBegrunnelse={false} />, {
-      messages,
-    });
+    render(<FaktaBegrunnelseTextField isReadOnly={false} isSubmittable hasBegrunnelse={false} />);
 
     expect(screen.getByRole('textbox', { name: 'Begrunn endringene' })).toBeInTheDocument();
   });
 
   it('skal ikke vise inputfelt når readOnly', () => {
-    renderWithIntlAndReduxForm(<FaktaBegrunnelseTextField isReadOnly isSubmittable hasBegrunnelse={false} />);
+    render(<FaktaBegrunnelseTextField isReadOnly isSubmittable hasBegrunnelse={false} />);
 
     expect(screen.queryByRole('textbox')).not.toBeInTheDocument();
   });
 
   it('skal vise standard-label når en ikke har valgt å vise vurderingstekst eller sende med tekstkode', () => {
-    renderWithIntlAndReduxForm(<FaktaBegrunnelseTextField isReadOnly={false} isSubmittable hasBegrunnelse={false} />, {
-      messages,
-    });
+    render(<FaktaBegrunnelseTextField isReadOnly={false} isSubmittable hasBegrunnelse={false} />);
 
     expect(screen.getByRole('textbox', { name: 'Begrunn endringene' })).toBeInTheDocument();
   });
 
   it('skal vise label for vurdering når dette er markert av prop', () => {
-    renderWithIntlAndReduxForm(
-      <FaktaBegrunnelseTextField isReadOnly={false} isSubmittable hasBegrunnelse={false} hasVurderingText />,
-      { messages },
-    );
+    render(<FaktaBegrunnelseTextField isReadOnly={false} isSubmittable hasBegrunnelse={false} hasVurderingText />);
 
     expect(screen.getByRole('textbox', { name: 'Vurdering' })).toBeInTheDocument();
   });
 
   it('skal vise medsendt label', () => {
-    renderWithIntlAndReduxForm(
-      <FaktaBegrunnelseTextField isReadOnly={false} isSubmittable hasBegrunnelse={false} label="Test" />,
-      { messages },
-    );
+    render(<FaktaBegrunnelseTextField isReadOnly={false} isSubmittable hasBegrunnelse={false} label="Test" />);
 
     expect(screen.getByRole('textbox', { name: 'Test' })).toBeInTheDocument();
   });
