4214fd5abbe7123f14c227ec89328e157fa2f51b
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2024-12-03 13:52:20 +0100

    Legg til future flags for v7

--- packages/sak-app/src/bootstrap.tsx
+++ packages/sak-app/src/bootstrap.tsx
@@ -7,10 +7,10 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter, createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from 'react-router-dom';
 
+import { ExtendedApiError } from '@k9-sak-web/backend/shared/instrumentation/ExtendedApiError.js';
 import AppIndex from './app/AppIndex';
 import configureStore from './configureStore';
 import { IS_DEV, VITE_SENTRY_RELEASE } from './constants';
-import { ExtendedApiError } from '@k9-sak-web/backend/shared/instrumentation/ExtendedApiError.js';
 
 import { isAlertInfo } from '@k9-sak-web/gui/app/alerts/AlertInfo.js';
 import { AxiosError } from 'axios';
@@ -100,7 +100,13 @@ const renderFunc = Component => {
     const root = createRoot(app);
     root.render(
       <Provider store={store}>
-        <BrowserRouter basename="/k9/web">
+        <BrowserRouter
+          basename="/k9/web"
+          future={{
+            v7_relativeSplatPath: true,
+            v7_startTransition: true,
+          }}
+        >
           <RestApiProvider>
             <RestApiErrorProvider>
               <Component />
