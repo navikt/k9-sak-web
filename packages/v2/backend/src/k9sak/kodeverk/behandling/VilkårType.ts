d2d366e5f399d85e412c947e289d1e18830373ef
Jostein Stuhaug <135307852+josstn@users.noreply.github.com>
2024-12-04 12:04:54 +0100

    Fiks Vilkårtype og bruken av den. (#6896)

--- packages/v2/backend/src/k9sak/kodeverk/behandling/VilkårType.ts
+++ packages/v2/backend/src/k9sak/kodeverk/behandling/VilkårType.ts
@@ -1,3 +1,31 @@
+/*
+import { type vilkarType as generatedVilkarTypeEnumUnion } from '@k9-sak-web/backend/k9sak/generated';
+
+export type VilkårType = generatedVilkarTypeEnumUnion;
+ */
+
+// Midlertidig kopiert inn for å få pipeline til å gå gjennom før ny generert kode er publisert. TODO Fjern denne og ta inn utkommentert kode over.
+export type VilkårType =
+  | 'FP_VK_0'
+  | 'FP_VK_2'
+  | 'K9_VK_1'
+  | 'K9_VK_3'
+  | 'K9_VK_5_3'
+  | 'K9_VK_2_a'
+  | 'K9_VK_2_b'
+  | 'FP_VK_3'
+  | 'FP_VK_34'
+  | 'FP_VK_21'
+  | 'FP_VK_23'
+  | 'FP_VK_41'
+  | 'K9_VK_16'
+  | 'K9_VK_20'
+  | 'K9_VK_21'
+  | 'K9_VK_22'
+  | 'K9_VK_17'
+  | 'K9_VK_9_6'
+  | '-';
+
 type VilkårTypeNames =
   | 'ALDERSVILKÅR_BARN'
   | 'ALDERSVILKÅR'
@@ -17,17 +45,16 @@ type VilkårTypeNames =
   | 'SØKERSOPPLYSNINGSPLIKT'
   | 'SØKNADSFRIST'
   | 'UDEFINERT'
-  | 'UNGDOMSPROGRAMVILKÅRET'
   | 'UTVIDETRETT';
 
-export const vilkarType: Readonly<Record<VilkårTypeNames, string>> = {
+export const vilkarType: Readonly<Record<VilkårTypeNames, VilkårType>> = {
+  K9_VILKÅRET: 'FP_VK_0',
   ALDERSVILKÅR_BARN: 'K9_VK_5_3',
   ALDERSVILKÅR: 'K9_VK_3',
   BEREGNINGSGRUNNLAGVILKÅR: 'FP_VK_41',
   GJENNOMGÅ_OPPLÆRING: 'K9_VK_22',
   GODKJENT_OPPLÆRINGSINSTITUSJON: 'K9_VK_21',
   I_LIVETS_SLUTTFASE: 'K9_VK_16',
-  K9_VILKÅRET: 'K9_VILKÅRET',
   LANGVARIG_SYKDOM: 'K9_VK_17',
   MEDISINSKEVILKÅR_18_ÅR: 'K9_VK_2_b',
   MEDISINSKEVILKÅR_UNDER_18_ÅR: 'K9_VK_2_a',
@@ -39,6 +66,5 @@ export const vilkarType: Readonly<Record<VilkårTypeNames, string>> = {
   SØKERSOPPLYSNINGSPLIKT: 'FP_VK_34',
   SØKNADSFRIST: 'FP_VK_3',
   UDEFINERT: '-',
-  UNGDOMSPROGRAMVILKÅRET: 'UNG_VK_XXX',
   UTVIDETRETT: 'K9_VK_9_6',
 };
