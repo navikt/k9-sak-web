a26dece5460857f3012e5a794fa3b79b2c2c29e3
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2024-12-05 15:49:04 +0100

    Tar bort ubrukt kode (#6901)

--- packages/behandling-pleiepenger/src/panelDefinisjoner/prosessStegPleiepengerPanelDefinisjoner.tsx
+++ packages/behandling-pleiepenger/src/panelDefinisjoner/prosessStegPleiepengerPanelDefinisjoner.tsx
@@ -1,18 +1,16 @@
-import VarselProsessStegPanelDef from './prosessStegPaneler/VarselProsessStegPanelDef';
-import InngangsvilkarFortsProsessStegPanelDef from './prosessStegPaneler/InngangsvilkarFortsProsessStegPanelDef';
-import MedisinskVilkarProsessStegPanelDef from './prosessStegPaneler/MedisinskVilkarProsessStegPanelDef';
-import SaksopplysningerProsessStegPanelDef from './prosessStegPaneler/SaksopplysningerProsessStegPanelDef';
-import InngangsvilkarProsessStegPanelDef from './prosessStegPaneler/InngangsvilkarProsessStegPanelDef';
 import BeregningsgrunnlagProsessStegPanelDef from './prosessStegPaneler/BeregningsgrunnlagProsessStegPanelDef';
-import UttakProsessStegPanelDef from './prosessStegPaneler/UttakProsessStegPanelDef';
-import TilkjentYtelseProsessStegPanelDef from './prosessStegPaneler/TilkjentYtelseProsessStegPanelDef';
-import SimuleringProsessStegPanelDef from './prosessStegPaneler/SimuleringProsessStegPanelDef';
-import VedtakProsessStegPanelDef from './prosessStegPaneler/VedtakProsessStegPanelDef';
 import FortsattMedlemskapProsessStegPanelDef from './prosessStegPaneler/FortsattMedlemskapProsessStegPanelDef';
+import InngangsvilkarFortsProsessStegPanelDef from './prosessStegPaneler/InngangsvilkarFortsProsessStegPanelDef';
+import InngangsvilkarProsessStegPanelDef from './prosessStegPaneler/InngangsvilkarProsessStegPanelDef';
+import MedisinskVilkarProsessStegPanelDef from './prosessStegPaneler/MedisinskVilkarProsessStegPanelDef';
+import SimuleringProsessStegPanelDef from './prosessStegPaneler/SimuleringProsessStegPanelDef';
+import TilkjentYtelseProsessStegPanelDef from './prosessStegPaneler/TilkjentYtelseProsessStegPanelDef';
+import UttakProsessStegPanelDef from './prosessStegPaneler/UttakProsessStegPanelDef';
+import VarselProsessStegPanelDef from './prosessStegPaneler/VarselProsessStegPanelDef';
+import VedtakProsessStegPanelDef from './prosessStegPaneler/VedtakProsessStegPanelDef';
 
 const prosessStegPanelDefinisjoner = [
   new VarselProsessStegPanelDef(),
-  new SaksopplysningerProsessStegPanelDef(),
   new InngangsvilkarProsessStegPanelDef(),
   new MedisinskVilkarProsessStegPanelDef(),
   new InngangsvilkarFortsProsessStegPanelDef(),
