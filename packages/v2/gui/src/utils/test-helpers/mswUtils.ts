c7bef9e11e4feace3856d4021fd091309ca64643
Hallvard Andreas Stark <hallvard@scel.to>
2024-12-17 17:08:26 +0100

    Kommentar for bruk av msw i testing

--- packages/v2/gui/src/utils/test-helpers/mswUtils.ts
+++ packages/v2/gui/src/utils/test-helpers/mswUtils.ts
@@ -1,6 +1,16 @@
 import '@testing-library/jest-dom/vitest';
 import { setupServer } from 'msw/node';
 
+/**
+ * For mocking av endepunkter i backend
+ * 
+ * @example
+ * server.use(
+      http.get('/k9/feature-toggle/toggles.json', () => {
+        return HttpResponse.json([{ key: 'LOS_MARKER_BEHANDLING_SUBMIT', value: 'true' }]);
+      }),
+    );
+ */
 export const server = setupServer();
 beforeAll(() => server.listen({ onUnhandledRequest: 'bypass' }));
 afterEach(() => server.resetHandlers());
