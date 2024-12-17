2ae5c4bf48e9f4ec024d3ea42356d7dc3d465374
Hallvard Andreas Stark <hallvard@scel.to>
2024-12-17 17:10:18 +0100

    Oppdatert kommentar

--- packages/v2/gui/src/utils/test-helpers/mswUtils.ts
+++ packages/v2/gui/src/utils/test-helpers/mswUtils.ts
@@ -5,11 +5,14 @@ import { setupServer } from 'msw/node';
  * For mocking av endepunkter i backend
  * 
  * @example
+ * it('en test som skal bruke msw', () => {
  * server.use(
       http.get('/k9/feature-toggle/toggles.json', () => {
         return HttpResponse.json([{ key: 'LOS_MARKER_BEHANDLING_SUBMIT', value: 'true' }]);
       }),
     );
+    ...
+    })
  */
 export const server = setupServer();
 beforeAll(() => server.listen({ onUnhandledRequest: 'bypass' }));
