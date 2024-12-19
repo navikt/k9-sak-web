fed84123f64e420e1587d6421116a46b0eba517a
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2024-12-19 12:39:08 +0100

    Bumper pakker (#6940)
    * Bumper pakker
    
    * Fjerner prop
    
    * Stiljusteringer
    
    * fiks test

--- packages/v2/gui/src/prosess/vilkar-overstyring/VilkarresultatMedOverstyringProsessIndex.tsx
+++ packages/v2/gui/src/prosess/vilkar-overstyring/VilkarresultatMedOverstyringProsessIndex.tsx
@@ -109,7 +109,6 @@ export const VilkarresultatMedOverstyringProsessIndex = ({
             label: `${periode.periode.fom && formatDate(periode.periode.fom)} - ${periode.periode.tom && formatDate(periode.periode.tom)}`,
           }))}
           onClick={setActiveTab}
-          theme="arrow"
           heading="Perioder"
         />
       </div>
