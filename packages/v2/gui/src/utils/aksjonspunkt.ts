7aeadf14fd501828c28c4aa41a581e8e25257780
Thomas H. Wiberg <thomas@thawi.no>
2024-12-23 14:36:49 +0100

    console.log

--- packages/v2/gui/src/utils/aksjonspunkt.ts
+++ packages/v2/gui/src/utils/aksjonspunkt.ts
@@ -2,7 +2,6 @@ import type { AksjonspunktDto } from '@k9-sak-web/backend/k9sak/generated';
 import { aksjonspunktStatus } from '@k9-sak-web/backend/k9sak/kodeverk/AksjonspunktStatus.js';
 
 export const kanAksjonspunktRedigeres = (aksjonspunkt: AksjonspunktDto): boolean => {
-  console.log('aksjonspunktet', aksjonspunkt);
   const { status, erAktivt, kanLoses } = aksjonspunkt;
   return status === aksjonspunktStatus.UTFORT && erAktivt === true && kanLoses === true;
 };
