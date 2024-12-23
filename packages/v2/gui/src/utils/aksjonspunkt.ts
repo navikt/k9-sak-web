f07ab9f5a85d8db2d65ebfef1e8e885a3c9a8461
Thomas H. Wiberg <thomas@thawi.no>
2024-12-23 14:36:36 +0100

    Skal ikke kunne redigere aksjonspunkt om det ikke kan løses

--- packages/v2/gui/src/utils/aksjonspunkt.ts
+++ packages/v2/gui/src/utils/aksjonspunkt.ts
@@ -2,7 +2,9 @@ import type { AksjonspunktDto } from '@k9-sak-web/backend/k9sak/generated';
 import { aksjonspunktStatus } from '@k9-sak-web/backend/k9sak/kodeverk/AksjonspunktStatus.js';
 
 export const kanAksjonspunktRedigeres = (aksjonspunkt: AksjonspunktDto): boolean => {
-  return aksjonspunkt.status === aksjonspunktStatus.UTFORT && aksjonspunkt.erAktivt === true;
+  console.log('aksjonspunktet', aksjonspunkt);
+  const { status, erAktivt, kanLoses } = aksjonspunkt;
+  return status === aksjonspunktStatus.UTFORT && erAktivt === true && kanLoses === true;
 };
 
 export const erAksjonspunktReadOnly = (aksjonspunkt: AksjonspunktDto): boolean => {
