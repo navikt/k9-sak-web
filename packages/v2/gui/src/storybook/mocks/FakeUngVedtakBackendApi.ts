0fd2f98a62a0a75a7641f71ba84e6fd11e5eba5a
Jostein Stuhaug <135307852+josstn@users.noreply.github.com>
2025-01-06 13:23:26 +0100

    Fikser slik at kall for brev forhåndsvisning fungerer med generert klient. (#6959)
    Generert klientkode var for gammal.

--- packages/v2/gui/src/storybook/mocks/FakeUngVedtakBackendApi.ts
+++ packages/v2/gui/src/storybook/mocks/FakeUngVedtakBackendApi.ts
@@ -1,7 +1,8 @@
 import type { ForhåndsvisVedtaksbrevResponse } from '@k9-sak-web/backend/ungsak/generated';
+import { fakePdf } from './fakePdf.js';
 
 export class FakeUngVedtakBackendApi {
   async forhåndsvisVedtaksbrev(): Promise<ForhåndsvisVedtaksbrevResponse> {
-    return;
+    return fakePdf();
   }
 }
