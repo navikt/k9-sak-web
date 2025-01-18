3983e15dd0972dd1087cd1c9f5c21241cf71bc79
Thomas H. Wiberg <thomas@thawi.no>
2025-01-18 12:52:32 +0100

    Skal ikke kunne redigere aksjonspunkt når sak hos beslutter

--- packages/v2/gui/src/utils/aksjonspunkt.ts
+++ packages/v2/gui/src/utils/aksjonspunkt.ts
@@ -2,8 +2,8 @@ import type { AksjonspunktDto } from '@k9-sak-web/backend/k9sak/generated';
 import { aksjonspunktStatus } from '@k9-sak-web/backend/k9sak/kodeverk/AksjonspunktStatus.js';
 
 export const kanAksjonspunktRedigeres = (aksjonspunkt: AksjonspunktDto): boolean => {
-  const { status, erAktivt } = aksjonspunkt;
-  return status === aksjonspunktStatus.UTFORT && erAktivt === true;
+  const { status, erAktivt, kanLoses } = aksjonspunkt;
+  return status === aksjonspunktStatus.UTFORT && erAktivt && kanLoses === true;
 };
 
 export const erAksjonspunktReadOnly = (aksjonspunkt: AksjonspunktDto): boolean => {
