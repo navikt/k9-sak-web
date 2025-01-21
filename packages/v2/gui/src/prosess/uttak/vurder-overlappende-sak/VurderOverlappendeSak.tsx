475d2d037ccb5f5b98686f4d1b953664769b90f4
Thomas H. Wiberg <thomas@thawi.no>
2025-01-21 10:46:03 +0100

    Default fastsattUttaksgrad til undefined

--- packages/v2/gui/src/prosess/uttak/vurder-overlappende-sak/VurderOverlappendeSak.tsx
+++ packages/v2/gui/src/prosess/uttak/vurder-overlappende-sak/VurderOverlappendeSak.tsx
@@ -70,7 +70,7 @@ const VurderOverlappendeSak: FC<Props> = ({ behandling, aksjonspunkt, api, oppda
       perioder:
         data?.perioderMedOverlapp.map(periode => ({
           periode: { fom: periode.periode.fom || '', tom: periode.periode.tom || '' },
-          søkersUttaksgrad: periode.fastsattUttaksgrad ?? 0,
+          søkersUttaksgrad: periode.fastsattUttaksgrad ?? undefined,
           saksnummer: periode.saksnummer.map(saksNr => saksNr || ''),
         })) || [],
     };
