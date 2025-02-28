5771718fd98c710622a92f484048e83006ca3925
Jostein Stuhaug <jostein.stuhaug@nav.no>
2025-02-28 13:07:19 +0100

    Moderniser story

--- packages/v2/gui/src/sak/meny/sett-paa-vent/MenySettPaVentIndex.stories.tsx
+++ packages/v2/gui/src/sak/meny/sett-paa-vent/MenySettPaVentIndex.stories.tsx
@@ -1,28 +1,30 @@
 import { behandlingType } from '@k9-sak-web/backend/k9sak/kodeverk/behandling/BehandlingType.js';
-import { KodeverkProvider } from '@k9-sak-web/gui/kodeverk/index.js';
-import alleKodeverkV2 from '@k9-sak-web/lib/kodeverk/mocks/alleKodeverkV2.json';
 import { action } from '@storybook/addon-actions';
 import { asyncAction } from '../../../storybook/asyncAction';
 import MenySettPaVentIndexV2 from './MenySettPaVentIndex';
+import withKodeverkContext from '../../../storybook/decorators/withKodeverkContext.js';
+import type { Meta, StoryObj } from '@storybook/react';
 
-export default {
+const meta = {
   title: 'gui/sak/meny/sett-pa-vent',
   component: MenySettPaVentIndexV2,
-};
+  decorators: [
+    withKodeverkContext({
+      behandlingType: behandlingType.FØRSTEGANGSSØKNAD,
+    }),
+  ],
+} satisfies Meta<typeof MenySettPaVentIndexV2>;
 
-export const visMenyForÅSetteBehandlingPåVent = () => (
-  <KodeverkProvider
-    behandlingType={behandlingType.FØRSTEGANGSSØKNAD}
-    kodeverk={alleKodeverkV2}
-    klageKodeverk={{}}
-    tilbakeKodeverk={{}}
-  >
-    <MenySettPaVentIndexV2
-      behandlingId={1}
-      behandlingVersjon={2}
-      settBehandlingPaVent={asyncAction('button-click')}
-      lukkModal={action('button-click')}
-      erTilbakekreving={false}
-    />
-  </KodeverkProvider>
-);
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const visMenyForÅSetteBehandlingPåVent: Story = {
+  args: {
+    behandlingId: 1,
+    behandlingVersjon: 2,
+    settBehandlingPaVent: asyncAction('sett behandling på vent'),
+    lukkModal: action('lukkModal'),
+    erTilbakekreving: false,
+  },
+};
