29aab52431f474e45e006597998b24f8276527f1
Hallvard Andreas Stark <hallvard@scel.to>
2025-03-13 13:49:11 +0100

    Fix story

--- packages/v2/gui/src/prosess/ung-beregning/UngBeregning.stories.tsx
+++ packages/v2/gui/src/prosess/ung-beregning/UngBeregning.stories.tsx
@@ -11,4 +11,4 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const DefaultStory: Story = { args: { behandling: { uuid: '123' }, api } };
+export const DefaultStory: Story = { args: { behandling: { uuid: '123' }, api, barn: [] } };
