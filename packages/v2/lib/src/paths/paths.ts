831e0862c373e0a09f9562817f3c0fa6f5c0c750
Hallvard Andreas Stark <hallvard@scel.to>
2025-03-20 12:28:22 +0100

    Retter bug i meny for å sette behandlinger på vent

--- packages/v2/lib/src/paths/paths.ts
+++ packages/v2/lib/src/paths/paths.ts
@@ -13,6 +13,10 @@ export const goToLos = () => {
   const path = getPathToK9Los();
   if (path) {
     window.location.assign(path);
+  } else if (isDev()) {
+    // I Q1 og prod havner man i LOS. Ved lokal utvikling må vi refreshe vindu for å få riktig vising,
+    // da enkelte skjermbilder ikke er lagt opp til å håndtere visning dersom man blir værende.
+    window.location.reload();
   }
 };
 
@@ -21,3 +25,5 @@ const getBackendUrl = () => (window.location.pathname.includes('/ung/web') ? 'un
 export const goToSearch = () => {
   window.location.assign(`/${getBackendUrl()}/web`);
 };
+
+export const isDev = () => window.location.hostname === 'localhost';
