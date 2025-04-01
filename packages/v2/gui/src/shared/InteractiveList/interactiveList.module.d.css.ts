eb2ed68aa5d4f5c3117074701d6787cd2812460a
vebnor <vebjorn.nordby@nav.no>
2025-04-01 11:34:05 +0200

    fiks type, fjern test

---
+++ packages/v2/gui/src/shared/InteractiveList/interactiveList.module.d.css.ts
@@ -0,0 +1,11 @@
+declare const styles: {
+  readonly interactiveList: string;
+  readonly interactiveListElement: string;
+  readonly 'interactiveListElement__chevron': string;
+  readonly 'interactiveListElement--inactive': string;
+  readonly 'interactiveListElement--active': string;
+  readonly 'interactiveListElement__button': string;
+  readonly 'interactiveListElement__button__contentContainer': string;
+};
+
+export default styles;
