799e0eeb80ddb146bd6d4186f0f1a057b683b3f4
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2025-04-01 13:42:43 +0200

    Utledning av hvilken status som skal vises for prosess-beregning i Ung (#7279)

--- packages/behandling-ungdomsytelse/src/panelDefinisjoner/prosessStegPaneler/BeregningProsessStegPanelDef.tsx
+++ packages/behandling-ungdomsytelse/src/panelDefinisjoner/prosessStegPaneler/BeregningProsessStegPanelDef.tsx
@@ -1,8 +1,11 @@
+import vilkarUtfallType from '@fpsak-frontend/kodeverk/src/vilkarUtfallType';
 import { aksjonspunktCodes } from '@k9-sak-web/backend/ungsak/kodeverk/AksjonspunktCodes.js';
 import { ProsessStegDef, ProsessStegPanelDef } from '@k9-sak-web/behandling-felles';
 import UngBeregningIndex from '@k9-sak-web/gui/prosess/ung-beregning/UngBeregningIndex.js';
+import { isAksjonspunktOpen } from '@k9-sak-web/gui/utils/aksjonspunktUtils.js';
 import { prosessStegCodes } from '@k9-sak-web/konstanter';
-import { PersonopplysningDto } from '@navikt/ung-sak-typescript-client';
+import { Aksjonspunkt } from '@k9-sak-web/types';
+import { KontrollerInntektDto, PersonopplysningDto } from '@navikt/ung-sak-typescript-client';
 
 class PanelDef extends ProsessStegPanelDef {
   getKomponent = props => <UngBeregningIndex {...props} />;
@@ -16,6 +19,21 @@ class PanelDef extends ProsessStegPanelDef {
         dødsdato: barn.dodsdato ? new Intl.DateTimeFormat('nb-NO').format(new Date(barn.dodsdato)) : '',
       })) || [],
   });
+
+  getOverstyrtStatus = ({
+    kontrollerInntekt,
+    aksjonspunkter,
+  }: {
+    kontrollerInntekt: KontrollerInntektDto;
+    aksjonspunkter: Aksjonspunkt[];
+  }) => {
+    const harInntektTilVurdering = kontrollerInntekt?.kontrollperioder?.some(p => p.erTilVurdering);
+    const harÅpneAksjonspunkt = aksjonspunkter
+      .filter(ap => ap.definisjon.kode === aksjonspunktCodes.KONTROLLER_INNTEKT)
+      .some(ap => isAksjonspunktOpen(ap.status.kode));
+
+    return harInntektTilVurdering && harÅpneAksjonspunkt ? vilkarUtfallType.IKKE_VURDERT : vilkarUtfallType.OPPFYLT;
+  };
 
   getAksjonspunktKoder = () => [aksjonspunktCodes.KONTROLLER_INNTEKT];
 }
