3e646ba2ad88e016b46cb16b96e71e0827d37e18
Vebjørn Nordby <vebjorn.nordby@nav.no>
2025-04-11 12:48:06 +0200

    Saksbehandlercontext vurdert av (#7287)
    * flytter institusjon
    
    * flytte inn i v2
    
    * kommenterer ut story
    
    * prøver å lage felleskomponent for navigasjon i vurderingsperioder
    
    * fix keys
    
    * flytte på ting
    
    * bump backend ts
    
    * sykdom og opplæring2
    
    * kan løse aksjonspunkt 9301
    
    * Vi bruker bare ICD10-diagnosekoder
    
    * nødvendig opplæring
    
    * Oppretter form for reisetid
    
    * fiksing på uperiodisert
    
    * felleskomponenter fra ft-frontend-saksbehandling
    
    * Ferdigvisning sykdom
    
    * kanke redigere vurderinger fra andre behandlinger
    
    * forbedre visning etter man har opprettet en ny sykdomsvurdering
    
    * Reisetid
    
    * redigering i alle steg
    
    * fikse redigering
    
    * trigge deploy til dev-next1
    
    * fjerner dev1-next branch midlertidig
    
    * pakkeoppdatering
    
    * yarn.lock
    
    * fjern gammel opplæring
    
    * fjerne gammel skit
    
    * fikse litt i prosesstegpanel
    
    * fikse typer
    
    * fix
    
    * test
    
    * fjerne importer fra gammel kode
    
    * fiks type, fjern test
    
    * reg-upd
    
    * fikse typer
    
    * y
    
    * gif strength
    
    * readonly
    
    * readonly
    
    * ikke vise warning hvis vi ikke har aksjonspunkt
    
    * noe validering
    
    * alt i small
    
    * fjerner ting som ikke ttrengs
    
    * dot annotation
    
    * Bytte ut SaksbehandlernavnContext og AssessedBy fra ft-frontend
    
    * Legger til storybook decorator for react-hook-form.
    
    Kan brukast til å rendre og teste komponent som bruker react-hook-form med useFormContext.
    
    * Story med test for DiagnosekodeVelger.
    
    * Flytter diagnosekodeSearcher.ts til v2.
    
    Så den kan brukast i ny kode også.
    
    * Bruker DiagnosekodeSearcher i DiagnosekodeVelger. Legger til støtte for ukjente koder.
    
    I tilfelle gamle data blir vist, med diagnosekoder som ikkje eksisterer i diagnosekode oppslag pakke.
    
    * Flytta diagnosekodeSearcher til v2, så den kan brukast derifrå og.
    
    * Fjerna unødvendig re-eksport, div småfiksing relatert til diagnosekodesøk.
    
    * Fjerna unødvendig re-eksport, div småfiksing relatert til diagnosekodesøk.
    
    * Bruker initDiagnosekodeSearcher istadenfor direkte oppretting.
    
    Litt tydlegare at det er ein litt tung operasjon då.
    
    * Fjerner ubrukt backend kommunikasjon. (#7288)
    
    * rename
    
    * fiksolini
    
    * fix
    
    * fiks
    
    * habib
    
    * Bruk unik id pr story i withFormProvider.
    
    Ser om dette fikser pipeline testfeil.
    
    * Revert "Bruk unik id pr story i withFormProvider."
    
    This reverts commit f8ade99912de7b3a7b7b9747fb04fb2e631e55b4.
    
    * waitFor i storybook test DiagnosekodeVelger.stories.tsx
    
    Sidan isValid er false først, må ein vente for å få den true når form state er initialisert.
    
    * Update packages/v2/gui/src/shared/detail-view/DetailView.tsx
    
    Co-authored-by: Jostein Stuhaug <135307852+josstn@users.noreply.github.com>
    
    * småfiks
    
    * lint
    
    * tittel
    
    * fjerne duplikat komponent
    
    * fjern ubrukt css2
    
    * slett ubrukt css-fil
    
    * dev-next1 deploy
    
    ---------
    
    Co-authored-by: Jostein Stuhaug <jostein.stuhaug@nav.no>
    Co-authored-by: Jostein Stuhaug <135307852+josstn@users.noreply.github.com>
    Co-authored-by: Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>

--- packages/fakta-etablert-tilsyn/src/ui/components/nattevåk/nattevåksperiode-vurderingsdetaljer/NattevåksperiodeVurderingsdetaljer.tsx
+++ packages/fakta-etablert-tilsyn/src/ui/components/nattevåk/nattevåksperiode-vurderingsdetaljer/NattevåksperiodeVurderingsdetaljer.tsx
@@ -1,13 +1,13 @@
 import { DetailView } from '@k9-sak-web/gui/shared/detailView/DetailView.js';
 import { LabelledContent } from '@k9-sak-web/gui/shared/labelled-content/LabelledContent.js';
 import { Box, Button } from '@navikt/ds-react';
-import { AssessedBy } from '@navikt/ft-plattform-komponenter';
 import Beskrivelse from '../../../../types/Beskrivelse';
 import Vurderingsperiode from '../../../../types/Vurderingsperiode';
 import Vurderingsresultat from '../../../../types/Vurderingsresultat';
 import BeskrivelserForPerioden from '../../beskrivelser-for-perioden/BeskrivelserForPerioden';
 import WriteAccessBoundContent from '../../write-access-bound-content/WriteAccessBoundContent';
 import styles from './nattevåksperiodeVurderingsdetaljer.module.css';
+import { VurdertAv } from '@k9-sak-web/gui/shared/vurdert-av/VurdertAv.js';
 
 interface NattevåksperiodeVurderingsdetaljerProps {
   nattevåksperiode: Vurderingsperiode;
@@ -43,7 +43,7 @@ const NattevåksperiodeVurderingsdetaljer = ({
           content={<span className="whitespace-pre-wrap">{nattevåksperiode.begrunnelse}</span>}
           indentContent
         />
-        <AssessedBy ident={opprettetAv} date={opprettetTidspunkt} />
+        <VurdertAv ident={opprettetAv} date={opprettetTidspunkt} />
       </Box>
       <Box marginBlock="8 0">
         <LabelledContent
