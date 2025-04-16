d1f3c05451e29d43ed1feebb5ce82deed3e4a250
Audun Eltvik <audun.eltvik@nav.no>
2025-04-16 11:10:12 +0200

    Legg til tekst for opplæringspenger i FatterVedtakApprovalModal

--- packages/sak-totrinnskontroll/src/components/modal/FatterVedtakApprovalModal.tsx
+++ packages/sak-totrinnskontroll/src/components/modal/FatterVedtakApprovalModal.tsx
@@ -105,6 +105,9 @@ const getModalDescriptionTextCode = (
   if (ytelseType === fagsakYtelsesType.PLEIEPENGER_SYKT_BARN) {
     return 'Pleiepenger er innvilget og vedtaket blir iverksatt. Du kommer nå til forsiden.';
   }
+  if(ytelseType === fagsakYtelsesType.OPPLAERINGSPENGER) {
+    return 'Opplæringspenger er innvilget og vedtaket blir iverksatt. Du kommer nå til forsiden.';
+  }
   if (erFagytelseTypeUtvidetRett(ytelseType)) {
     return 'Ekstra omsorgsdager er innvilget og vedtaket blir iverksatt. Du kommer nå til forsiden.';
   }
