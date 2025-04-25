ce8f09811246cb0d89c6a90e7f054081e06096a1
Hallvard Andreas Stark <hallvard@scel.to>
2025-04-25 14:38:19 +0200

    Ung: vedtak, rydder i navnebruk. Sender inn riktig params til backend mock

--- packages/v2/gui/src/prosess/ung-vedtak/UngVedtakBackendApiType.ts
+++ packages/v2/gui/src/prosess/ung-vedtak/UngVedtakBackendApiType.ts
@@ -1,6 +1,6 @@
 import type { ForhåndsvisVedtaksbrevResponse, VedtaksbrevValgResponse } from '@k9-sak-web/backend/ungsak/generated';
 
 export type UngVedtakBackendApiType = {
-  forhåndsvisVedtaksbrev(behandlingUuid: number): Promise<ForhåndsvisVedtaksbrevResponse>;
-  vedtaksbrevValg(behandlingUuid: number): Promise<VedtaksbrevValgResponse>;
+  forhåndsvisVedtaksbrev(behandlingId: number): Promise<ForhåndsvisVedtaksbrevResponse>;
+  vedtaksbrevValg(behandlingId: number): Promise<VedtaksbrevValgResponse>;
 };
