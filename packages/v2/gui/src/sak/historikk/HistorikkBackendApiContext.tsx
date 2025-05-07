456de1797e99e83cca2fd73f337bc7545131aefc
Jostein Stuhaug <jostein.stuhaug@nav.no>
2025-05-07 10:15:33 +0200

    Viser v2 historikk frå k9-klage også.
    Henter k9-klage historikkinnslag frå nytt v2 endepunkt, på samme måte som for k9-sak. Bruker ny generert klient, og tilhøyrande typer. Kombinerer disse med tilsvarande frå andre backends.
    
    Bruker nytt kodeverkoppslag for å berike historikkinnslag med navn på kodeverdier. Dette blir gjort i backend klient, så ein slepp forholde seg til det i gui koden.
    
    Viser feilmelding inline i historikkpanel viss henting frå ein/fleire backends feila. Slik at bruker blir klar over problemet, men kan fortsette å jobbe med andre ting.
    
     Legger til falske historikk-testdata for k9-klage, tilsvarande som for k9-sak, og bruker disse i storybook.

--- packages/v2/gui/src/sak/historikk/HistorikkBackendApiContext.tsx
+++ packages/v2/gui/src/sak/historikk/HistorikkBackendApiContext.tsx
@@ -1,7 +1,5 @@
 import { createContext } from 'react';
-import { HistorikkBackendClient } from './HistorikkBackendClient.js';
-import { getK9SakClient } from '@k9-sak-web/backend/k9sak/client';
 import type { HistorikkBackendApi } from './HistorikkBackendApi.js';
 
-const HistorikkBackendApiContext = createContext<HistorikkBackendApi>(new HistorikkBackendClient(getK9SakClient()));
+const HistorikkBackendApiContext = createContext<HistorikkBackendApi | null>(null);
 export default HistorikkBackendApiContext;
