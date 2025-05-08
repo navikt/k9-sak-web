d14f414501d12e05aebf603b2aa1d9580630052d
Jostein Stuhaug <135307852+josstn@users.noreply.github.com>
2025-05-08 13:58:16 +0200

    Errorhandling (#7393)
    * Flytter feilhandteringskode.
    
    Flytter "feilmeldinger" katalog frå v2/gui/sak til v2/gui/app. Gjev meir meining å ha det der sidan det omhandler feilmeldinger for heile appen.
    
    Flytter ErrorBoundary komponent til v2, fikser typescript feil.
    
    * Vis alle "unhandledrejection" (i q/dev)
    
    Som ein test prøver vi å vise alle unhandledrejections i gui. Disse har tidlegare berre blitt rapportert i console og i sentry.
    
    Viser dei no også til bruker når featuretoggle er aktiv. (i Q/dev i første omgang).
    
    Prøver å aktivere i prod også viss det ser ut til at det kan fungere bra.
    
    * Kommentar om bruk av useK9Kodeverkoppslag
    
    * Revert "Vis alle "unhandledrejection" (i q/dev)"
    
    This reverts commit e2d15d79da7c225efdfaa55ea968d8da2f19c433.
    
    * Vis feilmeldinger rapportert frå ErrorBoundary.
    
    Feil som har blitt rapportert frå ErrorBoundary har ikkje blitt vist slik intensjonen nok var, fordi visningskode har forventa at det var objekt av type ErrorData som vart sendt inn, men ErrorBoundary har sendt inn string.
    
    Fikse dette ved å legge til at DefaultFormatter.format vil returnere feilmelding når errorData som blir sendt inn er string.
    
    Fikser og litt ts feil og hook dependency feil i gammal feilhandteringskode.
    
    * Forbetra ErrorBoundary.tsx.
    
    ErrorBoundary vil no vise separat feilside viss det blir rapportert mange uhandterte feil, sjølv om doNotShowErrorPage er satt. Dette for å sikre mot evig loop viss feil blir kasta i re-rendering av children.
    
    Vil også alltid vise separat feilside viss errorMessageCallback, som no ikkje er påkrevd prop, ikkje er definert.
    
    Når det skal visast separat feilside visast det konkrete info sider for Unauthorized, Forbidden og NotFound viss rapportert feil er av type ExtendedApiError med tilsvarande status kode satt.
    
    Utelater også å rapportere "unauthorized" ExtendedApiError til sentry, sidan dette er forventa feil når brukersesjon er utgått.
    
    * Fleire ErrorBoundary på ulike nivå.
    
    Viss feil skjer i AppConfigResolver eller over er det ønskelig at separat feilmeldingsside blir vist, utan å forsøke å rendre opprinnelege komponenter, sidan feil på dette nivå sannsynlegvis vil vere så grunnleggande at ingenting vil fungere.
    
    Endre derfor ytterste ErrorBoundary til å alltid vise separat feilside.
    
    Legger til ErrorBoundary innanfor AppConfigResolver som fungerer slik som tidlegare, ved å vise feilmeldinger inne i gui, og fortsette å rendre komponent-treet.

--- packages/v2/gui/src/app/feilmeldinger/NotFoundPage.tsx
+++ packages/v2/gui/src/app/feilmeldinger/NotFoundPage.tsx
@@ -1,17 +1,16 @@
+import { BigError } from './BigError.js';
 import { Link } from 'react-router';
 
-import { BigError } from '@k9-sak-web/gui/sak/feilmeldinger/BigError.js';
-
 /**
- * ForbiddenPage
+ * NotFoundPage
  *
  * Presentasjonskomponent. Denne komponenten vises når den NAV-ansatte prøver å aksessere en url som ikke finnes.
  * Det blir presentert en generell feilmelding og en lenke som tar NAV-ansatt tilbake til hovedsiden.
  */
-const ForbiddenPage = () => (
-  <BigError title="Du har ikke tilgang til å slå opp denne personen">
+const NotFoundPage = () => (
+  <BigError title="Beklager, vi finner ikke siden du leter etter.">
     <Link to="/">Gå til forsiden</Link>
   </BigError>
 );
 
-export default ForbiddenPage;
+export default NotFoundPage;
