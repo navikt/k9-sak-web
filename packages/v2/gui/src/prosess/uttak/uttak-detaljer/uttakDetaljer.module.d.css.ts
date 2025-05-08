e863dc39c627dfbe0d64fd51c84752e505a1549e
Thomas H. Wiberg <thomas@thawi.no>
2025-05-08 11:56:49 +0200

    Create uttakDetaljer.module.d.css.ts

---
+++ packages/v2/gui/src/prosess/uttak/uttak-detaljer/uttakDetaljer.module.d.css.ts
@@ -0,0 +1,24 @@
+declare const styles: {
+  readonly "uttakDetaljer": string;
+  readonly "uttakDetaljerBeregningFirma": string;
+  readonly "uttakDetaljerBeregningStrek": string;
+  readonly "uttakDetaljerBeregningSum": string;
+  readonly "uttakDetaljerDataQuestionMark": string;
+  readonly "uttakDetaljerDetailItem": string;
+  readonly "uttakDetaljerDetailSum": string;
+  readonly "uttakDetaljerDetailtext": string;
+  readonly "uttakDetaljerExpandableDetailItem": string;
+  readonly "uttakDetaljerExpandableDetailItemContentCollapsed": string;
+  readonly "uttakDetaljerExpandableDetailItemHeader": string;
+  readonly "uttakDetaljerGraderingDetaljer": string;
+  readonly "uttakDetaljerGraderingDetaljerHighlight": string;
+  readonly "uttakDetaljerGraderingDetaljerNotHighlighted": string;
+  readonly "uttakDetaljerNyGradering": string;
+  readonly "uttakDetaljerNyTag": string;
+  readonly "uttakDetaljerTag": string;
+  readonly "uttakDetaljerTaptInntektAnnotasjon": string;
+  readonly "uttakDetaljerTaptInntektBeregning": string;
+  readonly "uttakDetaljerUtnullet": string;
+};
+export = styles;
+
