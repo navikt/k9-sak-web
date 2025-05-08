46244cd20a14b49de42fe042ecec602c4e1e428f
Jostein Stuhaug <jostein.stuhaug@nav.no>
2025-05-08 11:57:52 +0200

    Forbetra ErrorBoundary.tsx.
    ErrorBoundary vil no vise separat feilside viss det blir rapportert mange uhandterte feil, sjølv om doNotShowErrorPage er satt. Dette for å sikre mot evig loop viss feil blir kasta i re-rendering av children.
    
    Vil også alltid vise separat feilside viss errorMessageCallback, som no ikkje er påkrevd prop, ikkje er definert.
    
    Når det skal visast separat feilside visast det konkrete info sider for Unauthorized, Forbidden og NotFound viss rapportert feil er av type ExtendedApiError med tilsvarande status kode satt.
    
    Utelater også å rapportere "unauthorized" ExtendedApiError til sentry, sidan dette er forventa feil når brukersesjon er utgått.

--- packages/v2/gui/src/app/feilmeldinger/ErrorBoundary.tsx
+++ packages/v2/gui/src/app/feilmeldinger/ErrorBoundary.tsx
@@ -1,15 +1,20 @@
 import { Component, type ReactNode, type ErrorInfo } from 'react';
 import { captureException, withScope } from '@sentry/browser';
 import ErrorPage from './ErrorPage.js';
+import { ExtendedApiError } from '@k9-sak-web/backend/shared/instrumentation/ExtendedApiError.js';
+import UnauthorizedPage from './UnauthorizedPage.js';
+import ForbiddenPage from './ForbiddenPage.js';
+import NotFoundPage from './NotFoundPage.js';
 
 interface OwnProps {
-  errorMessageCallback: (error: any) => void;
+  errorMessageCallback?: (error: string) => void;
   children: ReactNode;
   doNotShowErrorPage?: boolean;
+  doNotShowErrorPageMaxCount?: number;
 }
 
 interface State {
-  hasError: boolean;
+  error: Error | null;
   sentryId: string | undefined;
 }
 
@@ -18,52 +23,110 @@ export class ErrorBoundary extends Component<OwnProps, State> {
     doNotShowErrorPage: false,
   };
 
+  private errorCount = 0;
+
   constructor(props: OwnProps) {
     super(props);
-    this.state = { hasError: false, sentryId: undefined };
+    this.state = { error: null, sentryId: undefined };
   }
 
-  static getDerivedStateFromError() {
-    // Update state so the next render will show the fallback UI.
-    return { hasError: true };
+  static ensureError(error: unknown): Error {
+    if (error instanceof Error) {
+      return error;
+    }
+    if (error == null) {
+      return new Error('Feil fanga, men feilobjekt var null/undefined');
+    }
+    return new Error(String(error));
   }
 
-  override componentDidCatch(error: Error, info: ErrorInfo): void {
+  // Vi ønsker ikkje å rapportere alle feil til Sentry, feks viss har utgått sesjon.
+  // Legg til fleire her ved behov.
+  protected shouldReportToSentry(): boolean {
+    const apiError = ExtendedApiError.findInError(this.state.error);
+    if (apiError != null) {
+      const doNotReport = apiError.isUnauthorized;
+      return !doNotReport;
+    }
+    return true;
+  }
+
+  override componentDidCatch(error: any, info: ErrorInfo): void {
     const { errorMessageCallback } = this.props;
-
-    withScope(scope => {
-      if (info.componentStack != null) {
-        scope.setExtra('componentStack', info.componentStack);
-      }
-      if (info.digest != null) {
-        scope.setExtra('digest', info.digest);
-      }
-      const sentryId = captureException(error);
-      this.setState({
-        ...this.state,
-        sentryId,
+    let sentryId: string | undefined = undefined;
+    if (this.shouldReportToSentry()) {
+      withScope(scope => {
+        if (info.componentStack != null) {
+          scope.setExtra('componentStack', info.componentStack);
+        }
+        if (info.digest != null) {
+          scope.setExtra('digest', info.digest);
+        }
+        sentryId = captureException(error);
       });
+    }
+    this.setState({
+      error: ErrorBoundary.ensureError(error),
+      sentryId,
     });
 
-    errorMessageCallback(
-      [
-        error.toString(),
-        info.componentStack
-          ?.split('\n')
-          .map(line => line.trim())
-          .find(line => !!line),
-      ].join(' '),
-    );
+    if (errorMessageCallback != null) {
+      errorMessageCallback(
+        [
+          error.toString(),
+          info.componentStack
+            ?.split('\n')
+            .map(line => line.trim())
+            .find(line => !!line),
+        ].join(' '),
+      );
+    }
+
+    if (error != null) {
+      this.errorCount++;
+    }
 
     // eslint-disable-next-line no-console
     console.error(error);
   }
 
   override render(): ReactNode {
-    const { children, doNotShowErrorPage } = this.props;
-    const { hasError, sentryId } = this.state;
+    const { children, doNotShowErrorPage, errorMessageCallback, doNotShowErrorPageMaxCount = 16 } = this.props;
+    const { error, sentryId } = this.state;
+    // I utgangspunktet visast feilside viss doNotShowErrorPage ikkje er true. Ellers blir berre errorMessageCallback
+    // kalla, og rendering fortsetter etter beste evne.
+    let showErrorPage = !doNotShowErrorPage;
+    if (!showErrorPage) {
+      // Dette kan likevel bli overstyrt slik at feilside visast viss errorMessageCallback ikkje er definert, eller viss
+      // det har oppstått meir enn 6 feil sidan sist ErrorBoundary vart resatt, for å unngå evig loop viss ein feil gjenoppstår
+      // for kvar rendering av children.
+      if (errorMessageCallback == null) {
+        console.info(`Ingen errorMessageCallback definert, vis separat feilside.`);
+        showErrorPage = true;
+      } else if (this.errorCount > doNotShowErrorPageMaxCount) {
+        console.info(`Meir enn ${doNotShowErrorPageMaxCount} feil på rad, vis separat feilside.`);
+        showErrorPage = true;
+      }
+    }
 
-    return hasError && !doNotShowErrorPage ? <ErrorPage sentryId={sentryId} /> : children;
+    if (error != null && showErrorPage) {
+      // Utled feilside som skal visast.
+      const apiError = ExtendedApiError.findInError(error);
+      if (apiError != null) {
+        if (apiError.isUnauthorized) {
+          return <UnauthorizedPage />;
+        }
+        if (apiError.isForbidden) {
+          return <ForbiddenPage />;
+        }
+        if (apiError.isNotFound) {
+          return <NotFoundPage />;
+        }
+      }
+      return <ErrorPage sentryId={sentryId} />;
+    }
+
+    return children;
   }
 }
 
