e498412954a2d5bd1675b16b60fdf5ab9d2ddd3a
Hallvard Andreas Stark <hallvard@scel.to>
2025-05-08 13:58:28 +0200

    Ung Viser ytelsestype i arbeid og inntekt Viser ikke fatt vedtakknapp dersom readOnly Viser ikke checkboxer for overstyring og hindring av brev TSFF-1480 TSFF-1476 TSFF-1477

--- packages/v2/gui/src/prosess/ung-vedtak/UngVedtakBehandlingDto.ts
+++ packages/v2/gui/src/prosess/ung-vedtak/UngVedtakBehandlingDto.ts
@@ -1,4 +1,4 @@
-import type { BehandlingsresultatDto } from '@k9-sak-web/backend/ungsak/generated';
+import type { BehandlingDto, BehandlingsresultatDto } from '@k9-sak-web/backend/ungsak/generated';
 
 type Behandlingsresultat = {
   type: BehandlingsresultatDto['type'];
@@ -7,4 +7,5 @@ type Behandlingsresultat = {
 export type UngVedtakBehandlingDto = {
   behandlingsresultat: Behandlingsresultat;
   id: number;
+  status: BehandlingDto['status'];
 };
