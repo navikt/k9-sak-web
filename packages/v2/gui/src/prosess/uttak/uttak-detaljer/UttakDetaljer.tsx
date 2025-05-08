2f8b07a3071f1aacd2eaa1bbfeb87d3a828acedd
Thomas H. Wiberg <thomas@thawi.no>
2025-05-08 12:43:40 +0200

    Flytt og oppdater Uttak Detaljer til v2 (#7391)
    * Flytt og oppdater Uttak Detaljer til v2
    
    - Flyttet komponenten med tilhørende komponenter
    - Oppdatert typer til å bruke typescript clienten
    - Tatt bort bruk av context i uttak-detaljer
    
    * Update GraderingMotArbeidstidDetaljer.tsx
    
    * Update GraderingMotArbeidstidDetaljer.tsx
    
    * Oppdater typer for arbeidsforhold
    
    * Flytt beregnDagerTimer til formatters
    
    * Manglende type
    
    * Update loosely-type-checked-files.json
    
    * Create uttakDetaljer.module.d.css.ts
    
    * Create uttakUtregning.module.d.css.ts
    
    * ts-check class-names

--- packages/v2/gui/src/prosess/uttak/uttak-detaljer/UttakDetaljer.tsx
+++ packages/v2/gui/src/prosess/uttak/uttak-detaljer/UttakDetaljer.tsx
@@ -1,105 +1,100 @@
+import type { JSX } from 'react';
 import classNames from 'classnames/bind';
-import * as React from 'react';
-
-import { BriefcaseClockIcon, CheckmarkIcon, HandHeartIcon, PersonFillIcon, SackKronerIcon } from '@navikt/aksel-icons';
+import {
+  UttaksperiodeInfoUtfall,
+  type UttaksperiodeInfoUtfall as UttaksperiodeInfoUtfallType,
+  UttaksperiodeInfoÅrsaker,
+  type UttaksperiodeInfoÅrsaker as UttaksperiodeInfoÅrsakerType,
+  type Utenlandsopphold,
+  type ArbeidsgiverOversiktDto,
+} from '@k9-sak-web/backend/k9sak/generated';
+import { KodeverkType, type KodeverkNavnFraKodeType } from '@k9-sak-web/lib/kodeverk/types.js';
 import { Alert, Box, Heading, HelpText, HGrid, HStack, Tag } from '@navikt/ds-react';
-import BarnetsDødsfallÅrsakerMedTekst from '../../../constants/BarnetsDødsfallÅrsakerMedTekst';
-import IkkeOppfylteÅrsakerMedTekst from '../../../constants/IkkeOppfylteÅrsakerMedTekst';
-import Utfall from '../../../constants/Utfall';
-import Årsaker from '../../../constants/Årsaker';
-import { UttaksperiodeMedInntektsgradering } from '../../../types/Uttaksperiode';
-import { harÅrsak } from '../../../util/årsakUtils';
-import ContainerContext from '../../context/ContainerContext';
+import { BriefcaseClockIcon, CheckmarkIcon, HandHeartIcon, SackKronerIcon } from '@navikt/aksel-icons';
+import { useKodeverkContext } from '@k9-sak-web/gui/kodeverk/index.js';
+import GraderingMotTilsynDetaljer from './GraderingMotTilsynDetaljer';
 import GraderingMotArbeidstidDetaljer from './GraderingMotArbeidstidDetaljer';
 import GraderingMotInntektDetaljer from './GraderingMotInntektDetaljer';
-import GraderingMotTilsynDetaljer from './GraderingMotTilsynDetaljer';
+import type { UttaksperiodeMedInntektsgradering } from '../types/UttaksperiodeMedInntektsgradering';
+import {
+  BarnetsDødsfallÅrsakerMedTekst,
+  IkkeOppfylteÅrsakerMedTekst,
+} from '../constants/UttaksperiodeInfoÅrsakerTekst';
 
 import styles from './uttakDetaljer.module.css';
 
-import type { JSX } from 'react';
-
 const cx = classNames.bind(styles);
 
-const getÅrsaksetiketter = (årsaker: Årsaker[]) => {
-  const funnedeÅrsaker = IkkeOppfylteÅrsakerMedTekst.filter(årsak => harÅrsak(årsaker, årsak.årsak));
+const getÅrsaksetiketter = (årsaker: UttaksperiodeInfoÅrsakerType[]) => {
+  const funnedeÅrsaker = IkkeOppfylteÅrsakerMedTekst.filter(årsak => årsaker.includes(årsak.årsak));
   return funnedeÅrsaker.map(årsak => (
-    <Tag variant="error" key={årsak.årsak} className={styles.uttakDetaljer__etikett}>
+    <Tag variant="error" key={årsak.årsak} className={styles.uttakDetaljer}>
       {årsak.tekst}
     </Tag>
   ));
 };
 
-const getTekstVedBarnetsDødsfall = (årsaker: Årsaker[]) => {
-  const funnedeÅrsaker = BarnetsDødsfallÅrsakerMedTekst.filter(årsak => harÅrsak(årsaker, årsak.årsak));
+const getTekstVedBarnetsDødsfall = (årsaker: UttaksperiodeInfoÅrsakerType[]) => {
+  const funnedeÅrsaker = BarnetsDødsfallÅrsakerMedTekst.filter(årsak => årsaker.includes(årsak.årsak));
   return funnedeÅrsaker.map(årsak => (
-    <div key={årsak.årsak} className={styles.uttakDetaljer__etikettBarnetsDødsfall}>
+    <div key={årsak.årsak} className={styles.uttakDetaljer}>
       {årsak.tekst}
     </div>
   ));
 };
 
-const utenlandsoppholdTekst = (utenlandsopphold, kodeverk) => {
+const utenlandsoppholdTekst = (utenlandsopphold: Utenlandsopphold, kodeverkNavnFraKode: KodeverkNavnFraKodeType) => {
   if (utenlandsopphold?.erEøsLand) {
     return 'Periode med utenlandsopphold i EØS-land, telles ikke i 8 uker.';
   }
 
-  return kodeverk?.find(v => v.kode === utenlandsopphold?.årsak)?.navn;
+  if (!utenlandsopphold.årsak) return 'Mangler årsak for utenlandsopphold';
+
+  return kodeverkNavnFraKode(utenlandsopphold.årsak, KodeverkType.UTLANDSOPPHOLD_AARSAK);
 };
 
-const utenlandsoppholdInfo = (utfall: Utfall, utenlandsopphold: { landkode: string }) => {
-  const { kodeverkUtenlandsoppholdÅrsak } = React.useContext(ContainerContext);
-
-  if (!utenlandsopphold?.landkode) {
+const utenlandsoppholdInfo = (
+  utfall: UttaksperiodeInfoUtfallType | undefined,
+  utenlandsopphold: Utenlandsopphold | undefined,
+  kodeverkNavnFraKode: KodeverkNavnFraKodeType,
+) => {
+  if (!utenlandsopphold?.landkode || utfall === undefined) {
     return null;
   }
 
-  if (utfall === Utfall.IKKE_OPPFYLT) {
+  if (utfall === UttaksperiodeInfoUtfall.IKKE_OPPFYLT) {
     return null;
   }
 
   return (
-    <Tag variant="success" className={styles.uttakDetaljer__etikett}>
-      {utenlandsoppholdTekst(utenlandsopphold, kodeverkUtenlandsoppholdÅrsak)}
+    <Tag variant="success" className={styles.uttakDetaljer}>
+      {utenlandsoppholdTekst(utenlandsopphold, kodeverkNavnFraKode)}
     </Tag>
   );
 };
 
-const shouldHighlight = (aktuellÅrsak: Årsaker, årsaker: Årsaker[]) => årsaker.some(årsak => årsak === aktuellÅrsak);
-const harBarnetsDødsfallÅrsak = (årsaker: Årsaker[]) =>
-  BarnetsDødsfallÅrsakerMedTekst.some(barnetsDødsfallÅrsak => harÅrsak(årsaker, barnetsDødsfallÅrsak.årsak));
+const shouldHighlight = (aktuellÅrsak: UttaksperiodeInfoÅrsakerType, årsaker: UttaksperiodeInfoÅrsakerType[]) =>
+  årsaker.some(årsak => årsak === aktuellÅrsak);
 
-const getSøkerBerOmMaksimalt = (søkerBerOmMaksimalt: number, årsaker: Årsaker[]) => {
-  const highlightSøkerBerOmMaksimalt =
-    søkerBerOmMaksimalt && shouldHighlight(Årsaker.AVKORTET_MOT_SØKERS_ØNSKE, årsaker);
-
-  const containerCls = cx('uttakDetaljer__oppsummering__container', {
-    'uttakDetaljer__oppsummering__container--highlighted': highlightSøkerBerOmMaksimalt,
-  });
-
-  return (
-    <div className={containerCls}>
-      {highlightSøkerBerOmMaksimalt && (
-        <div className={styles.uttakDetaljer__oppsummering__checkIcon}>
-          <CheckmarkIcon />
-        </div>
-      )}
-      <PersonFillIcon title="Søker" fontSize="1.5rem" style={{ color: 'var(--a-blue-500)' }} />
-      <p className={styles.uttakDetaljer__oppsummering__tekst}>{`Søker ber om maksimalt: ${søkerBerOmMaksimalt} %`}</p>
-    </div>
-  );
-};
-
-interface UttakDetaljerProps {
+export interface UttakDetaljerProps {
   uttak: UttaksperiodeMedInntektsgradering;
+  arbeidsforhold: ArbeidsgiverOversiktDto['arbeidsgivere'];
+  erFagytelsetypeLivetsSluttfase: boolean;
+
   manueltOverstyrt: boolean;
 }
 
-const NyUttakDetaljer = ({ uttak, manueltOverstyrt }: UttakDetaljerProps): JSX.Element => {
-  const { arbeidsforhold, erFagytelsetypeLivetsSluttfase } = React.useContext(ContainerContext);
+const UttakDetaljer = ({
+  uttak,
+  arbeidsforhold,
+  erFagytelsetypeLivetsSluttfase,
+  manueltOverstyrt,
+}: UttakDetaljerProps): JSX.Element => {
+  const { kodeverkNavnFraKode } = useKodeverkContext();
+
   const {
     utbetalingsgrader,
     graderingMotTilsyn,
-    søkerBerOmMaksimalt,
     årsaker,
     søkersTapteArbeidstid,
     pleiebehov,
@@ -117,24 +112,33 @@ const NyUttakDetaljer = ({ uttak, manueltOverstyrt }: UttakDetaljerProps): JSX.E
    */
   const shouldHighlightInntekt = !manueltOverstyrt && !!inntektsgradering;
   const shouldHighlightTilsyn =
-    !manueltOverstyrt && !shouldHighlightInntekt && shouldHighlight(Årsaker.GRADERT_MOT_TILSYN, årsaker);
+    !manueltOverstyrt &&
+    !shouldHighlightInntekt &&
+    årsaker &&
+    shouldHighlight(UttaksperiodeInfoÅrsaker.GRADERT_MOT_TILSYN, årsaker || []);
   const shouldHighlightArbeidstid =
-    !manueltOverstyrt && !shouldHighlightInntekt && shouldHighlight(Årsaker.AVKORTET_MOT_INNTEKT, årsaker);
+    !manueltOverstyrt &&
+    !shouldHighlightInntekt &&
+    årsaker &&
+    shouldHighlight(UttaksperiodeInfoÅrsaker.AVKORTET_MOT_INNTEKT, årsaker || []);
+
+  // Hvis en av årsakene fra uttaksdetaljene er en av årsakene for barnets dødsfall ...
+  const harBarnetsDødsfallÅrsak = årsaker?.some(årsak =>
+    BarnetsDødsfallÅrsakerMedTekst.some(barnetsDødsfallÅrsak => årsak === barnetsDødsfallÅrsak.årsak),
+  );
 
   return (
     <>
-      {getÅrsaksetiketter(årsaker)}
-      {getTekstVedBarnetsDødsfall(årsaker)}
-      {utenlandsoppholdInfo(utfall, utenlandsopphold)}
-      <div className={styles.uttakDetaljer__oppsummering}>
-        {søkerBerOmMaksimalt && getSøkerBerOmMaksimalt(søkerBerOmMaksimalt, årsaker)}
-      </div>
+      {getÅrsaksetiketter(årsaker || [])}
+      {getTekstVedBarnetsDødsfall(årsaker || [])}
+      {utenlandsoppholdInfo(utfall, utenlandsopphold, kodeverkNavnFraKode)}
+
       {manueltOverstyrt && (
         <Alert variant="info" size="small" className="mx-4">
           Uttaksgrad og/eller utbetalingsgrad er manuelt overstyrt av saksbehandler.
         </Alert>
       )}
-      <HGrid gap="8" columns={3} align="start" className={styles.uttakDetaljer}>
+      <HGrid gap="8" columns={3} align="start" className={styles['uttakDetaljer']}>
         {graderingMotTilsyn && !erFagytelsetypeLivetsSluttfase && (
           <Box
             className={cx({
@@ -145,7 +149,7 @@ const NyUttakDetaljer = ({ uttak, manueltOverstyrt }: UttakDetaljerProps): JSX.E
             title="Gradering mot tilsyn"
           >
             {shouldHighlightTilsyn && (
-              <Box className={styles.uttakDetaljer__tag}>
+              <Box className={styles.uttakDetaljerTag}>
                 <Tag size="medium" variant="alt3-moderate">
                   <CheckmarkIcon />
                   Gir lavest pleiepengegrad
@@ -155,13 +159,13 @@ const NyUttakDetaljer = ({ uttak, manueltOverstyrt }: UttakDetaljerProps): JSX.E
             <HStack>
               <HandHeartIcon className="!ml-[-4px]" />
               <Heading size="xsmall"> Gradering mot tilsyn</Heading>
-              {harBarnetsDødsfallÅrsak(årsaker) && (
-                <HelpText placement="right" wrapperClassName={styles.uttakDetaljer__data__questionMark}>
+              {harBarnetsDødsfallÅrsak && (
+                <HelpText placement="right" wrapperClassName={styles.uttakDetaljerDataQuestionMark}>
                   Gradering mot tilsyn blir ikke medregnet på grunn av barnets dødsfall.
                 </HelpText>
               )}
             </HStack>
-            <GraderingMotTilsynDetaljer graderingMotTilsyn={graderingMotTilsyn} pleiebehov={pleiebehov} />
+            <GraderingMotTilsynDetaljer graderingMotTilsyn={graderingMotTilsyn} pleiebehov={pleiebehov || 0} />
           </Box>
         )}
 
@@ -174,7 +178,7 @@ const NyUttakDetaljer = ({ uttak, manueltOverstyrt }: UttakDetaljerProps): JSX.E
           title="Gradering mot arbeidstid"
         >
           {shouldHighlightArbeidstid && (
-            <Box className={styles.uttakDetaljer__tag}>
+            <Box className={styles.uttakDetaljerTag}>
               <Tag size="medium" variant="alt3-moderate">
                 <CheckmarkIcon />
                 Gir lavest pleiepengegrad
@@ -186,8 +190,8 @@ const NyUttakDetaljer = ({ uttak, manueltOverstyrt }: UttakDetaljerProps): JSX.E
             <Heading size="xsmall">Gradering mot arbeidstid</Heading>
           </HStack>
           <GraderingMotArbeidstidDetaljer
-            alleArbeidsforhold={arbeidsforhold}
-            utbetalingsgrader={utbetalingsgrader}
+            alleArbeidsforhold={arbeidsforhold || {}}
+            utbetalingsgrader={utbetalingsgrader || []}
             søkersTapteArbeidstid={søkersTapteArbeidstid}
           />
         </Box>
@@ -202,7 +206,7 @@ const NyUttakDetaljer = ({ uttak, manueltOverstyrt }: UttakDetaljerProps): JSX.E
             title="Gradering mot inntekt"
           >
             {shouldHighlightInntekt && (
-              <Box className={styles.uttakDetaljer__tag}>
+              <Box className={styles.uttakDetaljerTag}>
                 <Tag size="medium" variant="alt3-moderate">
                   <CheckmarkIcon />
                   Gir lavest pleiepengegrad
@@ -213,11 +217,14 @@ const NyUttakDetaljer = ({ uttak, manueltOverstyrt }: UttakDetaljerProps): JSX.E
               <SackKronerIcon className="!ml-[-4px]" />
               <Heading size="xsmall">Gradering mot arbeidsinntekt</Heading>
             </HStack>
-            <GraderingMotInntektDetaljer alleArbeidsforhold={arbeidsforhold} inntektsgradering={inntektsgradering} />
+            <GraderingMotInntektDetaljer
+              alleArbeidsforhold={arbeidsforhold || {}}
+              inntektsgradering={inntektsgradering}
+            />
           </Box>
         )}
       </HGrid>
     </>
   );
 };
-export default NyUttakDetaljer;
+export default UttakDetaljer;
