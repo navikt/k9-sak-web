6700feb35d63d2dc1fc9435f211f85ad9bb251af
Thomas H. Wiberg <thomas@thawi.no>
2025-05-13 15:18:59 +0200

    De redaksjonelle endringene dette egentlig begynte med

--- packages/v2/gui/src/prosess/avregning/kontroller-etterbetaling/KontrollerEtterbetalingAlert.tsx
+++ packages/v2/gui/src/prosess/avregning/kontroller-etterbetaling/KontrollerEtterbetalingAlert.tsx
@@ -6,8 +6,8 @@ const KontrollerEtterbetalingAlert: FC = () => {
   return (
     <MaxWidth>
       <Alert variant="warning" size="small">
-        Ny inntektsmelding vil føre til en høy etterbetaling til bruker i en tidligere innvilget periode. Kontroller om
-        dette er riktig.
+        Endrede opplysninger vil føre til en høy etterbetaling til bruker i en tidligere innvilget periode. Kontroller
+        om dette er riktig.
       </Alert>
     </MaxWidth>
   );
