166360b00d99d0cb4f2419484722a491dfed484e
Thomas H. Wiberg <thomas@thawi.no>
2025-05-13 15:05:50 +0200

    To be continued

--- packages/v2/gui/src/shared/aksjonspunktBox/AksjonspunktBox.tsx
+++ packages/v2/gui/src/shared/aksjonspunktBox/AksjonspunktBox.tsx
@@ -1,5 +1,7 @@
 import classnames from 'classnames/bind';
 import { type ReactNode } from 'react';
+import { Box } from '@navikt/ds-react';
+import { K9MaxTextWidth } from '../../tokens/tokens';
 
 import styles from './aksjonspunktBox.module.css';
 
@@ -11,8 +13,20 @@ interface OwnProps {
   className?: string;
 }
 
-const AksjonspunktBox = ({ erAksjonspunktApent, className, children }: OwnProps) => (
-  <div className={classNames(className, 'aksjonspunkt', { erAksjonspunktApent })}>{children}</div>
+/*
+ * Legger til et flagg for maxWidth, som setter maxWidth til 43.5rem.
+ * 43.5rem er hentet fra Aksel, i Alert komponenten som hardkodet maksbredde.
+ */
+const AksjonspunktBox = ({ erAksjonspunktApent, className, children, maxWidth = false }: OwnProps) => (
+  <Box
+    className={classNames('aksjonspunkt', className, { erAksjonspunktApent })}
+    borderWidth={erAksjonspunktApent ? '3' : undefined}
+    borderRadius={erAksjonspunktApent ? 'large' : undefined}
+    padding={erAksjonspunktApent ? '4' : undefined}
+    maxWidth={maxWidth ? K9MaxTextWidth : undefined}
+  >
+    {children}
+  </Box>
 );
 
 export default AksjonspunktBox;
