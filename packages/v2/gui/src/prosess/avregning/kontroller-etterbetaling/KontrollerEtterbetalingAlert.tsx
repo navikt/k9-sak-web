b5d4efce4a1ef767fb316a2cc9103e15ee13e3a5
Thomas H. Wiberg <thomas@thawi.no>
2025-05-13 15:38:29 +0200

    Høy etterbetaling justering (#7419)
    * Redaksjonelle justeringer
    
    * Update KontrollerEtterbetaling.stories.tsx
    
    * To be continued
    
    * Bruk tokens
    
    * Bruk maxWidth for alert og ap-box
    
    * De redaksjonelle endringene dette egentlig begynte med
    
    * Update tokens.css

--- packages/v2/gui/src/prosess/avregning/kontroller-etterbetaling/KontrollerEtterbetalingAlert.tsx
+++ packages/v2/gui/src/prosess/avregning/kontroller-etterbetaling/KontrollerEtterbetalingAlert.tsx
@@ -1,12 +1,15 @@
 import { Alert } from '@navikt/ds-react';
 import type { FC } from 'react';
+import MaxWidth from '@k9-sak-web/gui/shared/maxWidth/maxWidth.js';
 
 const KontrollerEtterbetalingAlert: FC = () => {
   return (
-    <Alert variant="warning" size="small" contentMaxWidth={false}>
-      Ny inntektsmelding vil føre til en høy etterbetaling til bruker i en tidligere innvilget periode. Kontroller om
-      dette er riktig.
-    </Alert>
+    <MaxWidth>
+      <Alert variant="warning" size="small">
+        Endrede opplysninger vil føre til en høy etterbetaling til bruker i en tidligere innvilget periode. Kontroller
+        om dette er riktig.
+      </Alert>
+    </MaxWidth>
   );
 };
 
--- packages/v2/gui/src/shared/aksjonspunktBox/AksjonspunktBox.tsx
+++ packages/v2/gui/src/shared/aksjonspunktBox/AksjonspunktBox.tsx
@@ -1,6 +1,7 @@
 import classnames from 'classnames/bind';
 import { type ReactNode } from 'react';
 import { Box } from '@navikt/ds-react';
+import { K9MaxTextWidth } from '@k9-sak-web/gui/tokens/tokens.js';
 
 import styles from './aksjonspunktBox.module.css';
 
@@ -13,17 +14,13 @@ interface OwnProps {
   className?: string;
 }
 
-/*
- * Legger til et flagg for maxWidth, som setter maxWidth til 43.5rem.
- * 43.5rem er hentet fra Aksel, i Alert komponenten som hardkodet maksbredde.
- */
 const AksjonspunktBox = ({ erAksjonspunktApent, className, children, maxWidth = false }: OwnProps) => (
   <Box
     className={classNames('aksjonspunkt', className, { erAksjonspunktApent })}
     borderWidth={erAksjonspunktApent ? '3' : undefined}
     borderRadius={erAksjonspunktApent ? 'large' : undefined}
     padding={erAksjonspunktApent ? '4' : undefined}
-    maxWidth={maxWidth ? '43.5rem' : undefined}
+    maxWidth={maxWidth ? K9MaxTextWidth : undefined}
   >
     {children}
   </Box>
