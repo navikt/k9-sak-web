5827c06da37cef9b22faa185726b64f82848bd4e
Thomas H. Wiberg <thomas@thawi.no>
2025-05-13 14:54:31 +0200

    Bruke token fra Aksel som sammenfaller
    Men gjør den semantisk slik at vi kan endre vår bruk av border for aksjonspunkt spesifikt om ønskelig.

--- packages/v2/gui/src/tokens/tokens.ts
+++ packages/v2/gui/src/tokens/tokens.ts
@@ -1,2 +1,4 @@
+import { AOrange400 } from '@navikt/ds-tokens/dist/tokens';
+
 export const K9MaxTextWidth = '43.5rem';
-export const K9AksjonspunktBorderColor = '#fa3';
+export const K9AksjonspunktBorderColor = AOrange400;
