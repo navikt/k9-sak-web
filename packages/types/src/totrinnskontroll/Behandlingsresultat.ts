8033d31b890a38f2af804c38e60b4f44d1770df4
Hallvard Andreas Stark <hallvard@scel.to>
2025-05-15 12:27:02 +0200

    rydder

--- packages/types/src/totrinnskontroll/Behandlingsresultat.ts
+++ packages/types/src/totrinnskontroll/Behandlingsresultat.ts
@@ -1,12 +1,9 @@
-import { BehandlingsresultatDto } from '@navikt/ung-sak-typescript-client';
-
 interface Type {
   kode: string;
 }
 
 export interface Behandlingsresultat {
   type: Type;
-  vilkårResultat: BehandlingsresultatDto['vilkårResultat'];
 }
 
 export default Behandlingsresultat;
