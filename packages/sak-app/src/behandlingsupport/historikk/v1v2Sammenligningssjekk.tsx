20ee5d7ddefb381c32134d7737db0bcac236d68e
Jostein Stuhaug <135307852+josstn@users.noreply.github.com>
2025-05-19 09:40:21 +0200

    Historikkv2/tsff 1514 (#7438)
    * historikk: Unngå feilrapport for ord i TILBAKEKR_VIDEREBEHANDLING
    
    Unntar ordet "satt", v2 historikk bruker "er endret fra" istadenfor "er statt til". Godtek denne differansen sidan det då er meir info i v2.
    
    * historikk: Unngå feilrapport for ord i SAK_RETUR
    
    Unntar nokre ord, pga v2 historikk bruker kolon istadenfor samanbindingsord.
    
    * historikk: Sjekk for samanlikningsfeil i story.
    
    Logger også samanlikningsfeil til console i dev mode, så det er lettare å sjå dei i storybook.
    
    * historikk: Reproduser testdata som fører til samanlikningsfeil.
    
    Reproduserer historikkinnslag som har ført til samanlikningsfeil, før fiksing av disse.
    
    * historikk: Ignorer alltid "er" i samanlikningssjekk.
    
    Sidan v2 av innslag bruker kolon istadenfor er som bindeledd mellom namn og verdi.
    
    * historikk: Fiks fake backend data til å tilsvare endringer i backend.
    
    * Fiks manglande import suffiks.
    
    ---------
    
    Co-authored-by: Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>

--- packages/sak-app/src/behandlingsupport/historikk/v1v2Sammenligningssjekk.tsx
+++ packages/sak-app/src/behandlingsupport/historikk/v1v2Sammenligningssjekk.tsx
@@ -121,15 +121,27 @@ historikkInnslagMissingWordsExemptions.set('KLAGE_BEH_NFP', ['endret', 'fra']);
 historikkInnslagMissingWordsExemptions.set(KodeverdiSomObjektHistorikkinnslagTypeKilde.FAKTA_ENDRET, [
   'Aldersvilkåret',
 ]);
+historikkInnslagMissingWordsExemptions.set(
+  KodeverdiSomObjektHistorikkinnslagTypeKilde.TILBAKEKREVING_VIDEREBEHANDLING,
+  ['satt'],
+);
+historikkInnslagMissingWordsExemptions.set(KodeverdiSomObjektHistorikkinnslagTypeKilde.SAK_RETUR, [
+  'Vurdering',
+  'eller',
+  'Fastsatt',
+]);
+
+// Nokre enkle samanbindingsord utelatast frå samanlikning for alle historikkinnslag typer
+const wordsAlwaysExempted = ['er', 'Er'];
 
 const allMissingWordsAreExempted = (historikkInnslagV1Type: string, wordsMissing: string[]): boolean => {
-  const wordsKnownMissing = historikkInnslagMissingWordsExemptions.get(historikkInnslagV1Type);
-  if (wordsKnownMissing != null) {
-    return wordsMissing.every(wordMissing =>
-      wordsKnownMissing.some(wordKnownMissing => wordKnownMissing === wordMissing),
-    );
-  }
-  return wordsMissing.length === 0;
+  const wordsKnownMissing = [
+    ...wordsAlwaysExempted,
+    ...(historikkInnslagMissingWordsExemptions.get(historikkInnslagV1Type) ?? []),
+  ];
+  return wordsMissing.every(wordMissing =>
+    wordsKnownMissing.some(wordKnownMissing => wordKnownMissing === wordMissing),
+  );
 };
 
 export const compareRenderedElementTexts = async (
