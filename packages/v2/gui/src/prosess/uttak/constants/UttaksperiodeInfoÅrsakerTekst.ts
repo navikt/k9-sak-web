7c3e6315ff3f18e9e1fb0973251746da4532a0fe
vebnor <vebjorn.nordby@nav.no>
2025-06-02 09:48:45 +0200

    pleie- og/eller opplæringspenger

--- packages/v2/gui/src/prosess/uttak/constants/UttaksperiodeInfoÅrsakerTekst.ts
+++ packages/v2/gui/src/prosess/uttak/constants/UttaksperiodeInfoÅrsakerTekst.ts
@@ -59,7 +59,7 @@ export const IkkeOppfylteÅrsakerMedTekst: UttaksperiodeInfoÅrsakerTekstType[] 
   },
   {
     årsak: UttaksperiodeInfoÅrsaker.FOR_MANGE_DAGER_UTENLANDSOPPHOLD,
-    tekst: 'Årsak for avslag: Søker har mottatt ytelser i utlandet i 8 uker.',
+    tekst: 'Årsak for avslag: Søker har mottatt pleie- og/eller opplæringspenger i utlandet i 8 uker.',
   },
   {
     årsak: UttaksperiodeInfoÅrsaker.BARNETS_DØDSFALL,
