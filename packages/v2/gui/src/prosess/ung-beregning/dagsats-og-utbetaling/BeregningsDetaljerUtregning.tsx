5a5be598391f492554efabe80389848078dfa959
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2025-06-02 15:20:40 +0200

    TSFF-1578 - Utregning viser også barnetillegg (#7489)
    * TSFF-1578 - Utregning viser også barnetillegg
    
    * Flytter key
    
    * Ung - tekstendringer for siste utbetaling

--- packages/v2/gui/src/prosess/ung-beregning/dagsats-og-utbetaling/BeregningsDetaljerUtregning.tsx
+++ packages/v2/gui/src/prosess/ung-beregning/dagsats-og-utbetaling/BeregningsDetaljerUtregning.tsx
@@ -1,6 +1,7 @@
 import type { UngdomsytelseSatsPeriodeDto } from '@k9-sak-web/backend/ungsak/generated';
 import { formatCurrencyWithKr } from '@k9-sak-web/gui/utils/formatters.js';
 import { BodyShort, Box, HStack, VStack } from '@navikt/ds-react';
+import React from 'react';
 import styles from './dagsatsOgUtbetaling.module.css';
 interface BeregningsDetaljerUtregningProps {
   rapportertInntekt?: number;
@@ -17,15 +18,27 @@ export const BeregningsDetaljerUtregning = ({
 }: BeregningsDetaljerUtregningProps) => (
   <>
     <VStack gap="4">
-      {satsperioder?.map(({ dagsats, antallDager, fom, tom }) => (
-        <HStack justify="space-between" key={`${fom}-${tom}`}>
-          <BodyShort size="small">
-            Beregnet ytelse ({formatCurrencyWithKr(dagsats)} x {antallDager} dager)
-          </BodyShort>
-          <BodyShort size="small" weight="semibold">
-            {formatCurrencyWithKr(dagsats * antallDager)}
-          </BodyShort>
-        </HStack>
+      {satsperioder?.map(({ dagsats, antallDager, fom, tom, dagsatsBarnetillegg, antallBarn }) => (
+        <React.Fragment key={`${fom}-${tom}`}>
+          <HStack justify="space-between">
+            <BodyShort size="small">
+              Beregnet ytelse ({formatCurrencyWithKr(dagsats)} x {antallDager} dager)
+            </BodyShort>
+            <BodyShort size="small" weight="semibold">
+              {formatCurrencyWithKr(dagsats * antallDager)}
+            </BodyShort>
+          </HStack>
+          {antallBarn != undefined && antallBarn > 0 && (
+            <HStack justify="space-between">
+              <BodyShort size="small">
+                Barnetillegg ({formatCurrencyWithKr(dagsatsBarnetillegg)} x {antallDager} dager)
+              </BodyShort>
+              <BodyShort size="small" weight="semibold">
+                {formatCurrencyWithKr(dagsatsBarnetillegg * antallDager)}
+              </BodyShort>
+            </HStack>
+          )}
+        </React.Fragment>
       ))}
 
       <Box borderColor="border-subtle" borderWidth="0 0 1 0" />
