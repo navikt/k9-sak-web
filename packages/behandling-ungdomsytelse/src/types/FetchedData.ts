6d8c561532f1da3b4ac09c3fedc7f80b8d1f7c8f
Hallvard Andreas Stark <hallvard@scel.to>
2025-06-13 15:20:11 +0200

    fix

--- packages/behandling-ungdomsytelse/src/types/FetchedData.ts
+++ packages/behandling-ungdomsytelse/src/types/FetchedData.ts
@@ -1,18 +1,10 @@
-import {
-  Aksjonspunkt,
-  BeregningsresultatUtbetalt,
-  Personopplysninger,
-  SimuleringResultat,
-  Soknad,
-  Vilkar,
-} from '@k9-sak-web/types';
+import { Aksjonspunkt, Personopplysninger, SimuleringResultat, Soknad, Vilkar } from '@k9-sak-web/types';
 
 export interface FetchedData {
   aksjonspunkter: Aksjonspunkt[];
   vilkar: Vilkar[];
   personopplysninger: Personopplysninger;
   soknad: Soknad;
-  beregningsresultatUtbetaling: BeregningsresultatUtbetalt;
   simuleringResultat: SimuleringResultat;
 }
 
