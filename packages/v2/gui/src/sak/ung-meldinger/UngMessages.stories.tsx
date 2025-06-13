2776184302c33e08c436849eca76655c5c361334
Hallvard Andreas Stark <hallvard@scel.to>
2025-06-13 15:51:31 +0200

    Story

---
+++ packages/v2/gui/src/sak/ung-meldinger/UngMessages.stories.tsx
@@ -0,0 +1,55 @@
+import type {
+  InformasjonsbrevMottakerValgResponseIdType,
+  KodeverdiSomObjektDokumentMalTypeKilde,
+} from '@k9-sak-web/backend/ungsak/generated';
+import type { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+import { FakeUngMessagesBackendApi } from '../../storybook/mocks/FakeUngMessagesBackendApi';
+import { UngMessages } from './UngMessages';
+import type { UngMessagesFormState } from './UngMessagesFormState';
+
+const meta: Meta<typeof UngMessages> = {
+  title: 'gui/sak/ung-meldinger/UngMessages',
+  component: UngMessages,
+};
+export default meta;
+
+type Story = StoryObj<typeof UngMessages>;
+
+export const Default: Story = {
+  render: () => {
+    const [formValues, setFormValues] = useState<UngMessagesFormState | undefined>(undefined);
+    const mockApi = new FakeUngMessagesBackendApi();
+    const brevmaler = [
+      {
+        malType: {
+          kode: 'GENERELT_FRITEKSTBREV',
+          kodeverk: 'DOKUMENT_MAL_TYPE',
+          navn: 'Fritekst generelt brev',
+          kilde: 'GENERELT_FRITEKSTBREV' as KodeverdiSomObjektDokumentMalTypeKilde,
+        },
+        mottakere: [
+          {
+            id: '9904458010078',
+            idType: 'AKTØRID' as InformasjonsbrevMottakerValgResponseIdType,
+            navn: 'Kolibir Nina',
+            fnr: '04458010078',
+          },
+        ],
+        støtterFritekst: false,
+        støtterTittelOgFritekst: true,
+      },
+    ];
+    return (
+      <UngMessages
+        api={mockApi}
+        behandlingId={1}
+        språkkode="nb"
+        onMessageSent={() => alert('onMessageSent fired!')}
+        brevmaler={brevmaler}
+        ungMessagesFormValues={formValues}
+        setUngMessagesFormValues={setFormValues}
+      />
+    );
+  },
+};
