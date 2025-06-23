12a64d662a95d8e6d580df0a99ac689ca207e97d
Vebjørn Nordby <vebjorn.nordby@nav.no>
2025-06-23 13:04:40 +0200

    TSFF-1591: flipper rekkefølge for reisetid (#7568)
    * flipper rekkefølge for reisetid
    
    * TSFF-1590/chevron skal alltid peke til høyre

--- packages/v2/gui/src/fakta/sykdom-og-opplæring/4-reisetid/ReisetidIndex.tsx
+++ packages/v2/gui/src/fakta/sykdom-og-opplæring/4-reisetid/ReisetidIndex.tsx
@@ -39,6 +39,7 @@ const ReisetidIndex = () => {
               valgtPeriode={valgtVurdering}
               perioder={vurderingsliste || []}
               onPeriodeClick={setValgtVurdering}
+              nyesteFørst={false}
             />
           </>
         )}
