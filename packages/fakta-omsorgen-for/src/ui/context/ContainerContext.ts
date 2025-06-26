c539f5d9113c402f43e822396eab4d53980812d5
vebnor <vebjorn.nordby@nav.no>
2025-06-26 11:35:23 +0200

    kaster error hvis null, så får vi implicit type

--- packages/fakta-omsorgen-for/src/ui/context/ContainerContext.ts
+++ packages/fakta-omsorgen-for/src/ui/context/ContainerContext.ts
@@ -4,6 +4,10 @@ import { ContainerContract } from '../../types/ContainerContract';
 const ContainerContext = React.createContext<ContainerContract | null>(null);
 export const useOmsorgenForContext = () => {
   const context = React.useContext(ContainerContext);
-  return context as ContainerContract;
+  if (!context) {
+    throw new Error('useOmsorgenForContext must be used within a ContainerContext');
+  }
+  return context;
 };
+
 export default ContainerContext;
