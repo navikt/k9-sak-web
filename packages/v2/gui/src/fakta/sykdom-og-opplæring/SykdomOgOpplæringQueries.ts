31a31b2f22c09c5dd19243936965351e4a91750c
vebnor <vebjorn.nordby@nav.no>
2025-07-22 13:09:20 +0200

    fjern feil

--- packages/v2/gui/src/fakta/sykdom-og-opplæring/SykdomOgOpplæringQueries.ts
+++ packages/v2/gui/src/fakta/sykdom-og-opplæring/SykdomOgOpplæringQueries.ts
@@ -1,6 +1,5 @@
 import { useMutation, useQuery } from '@tanstack/react-query';
 import {
-  ApiError,
   type GetBrevMottakerinfoEregData,
   type GetBrevMottakerinfoEregResponse,
   type OppdaterLangvarigSykdomsVurderingData,
