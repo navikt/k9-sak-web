9b11707a9a2d4ae359f89edab272097cf2a8c906
vebnor <vebjorn.nordby@nav.no>
2025-07-23 15:24:48 +0200

    migrering

--- vite.config.js
+++ vite.config.js
@@ -102,15 +102,13 @@ export default ({ mode }) => {
       outDir: './dist/k9/web',
       sourcemap: true,
       rollupOptions: {
-        external: [
-          "mockServiceWorker.js"
-        ],
+        external: ['mockServiceWorker.js'],
         plugins: [sourcemaps({ exclude: /@sentry/ })],
         output: {
           manualChunks: {
-            diagnosekoder: ['@navikt/diagnosekoder']
-          }
-        }
+            diagnosekoder: ['@navikt/diagnosekoder'],
+          },
+        },
       },
     },
     test: {
