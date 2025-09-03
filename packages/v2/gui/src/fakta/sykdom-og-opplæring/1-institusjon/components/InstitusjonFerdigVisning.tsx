715853e448c30269ed105b30f81c654510899bbc
vebnor <vebjorn.nordby@nav.no>
2025-09-03 12:57:40 +0200

    teksteendringer og nye lenker til lovdata

--- packages/v2/gui/src/fakta/sykdom-og-opplæring/1-institusjon/components/InstitusjonFerdigVisning.tsx
+++ packages/v2/gui/src/fakta/sykdom-og-opplæring/1-institusjon/components/InstitusjonFerdigVisning.tsx
@@ -1,10 +1,11 @@
 import { k9_sak_web_app_tjenester_behandling_opplæringspenger_visning_institusjon_InstitusjonResultat as InstitusjonResultat } from '@k9-sak-web/backend/k9sak/generated/types.js';
 import { CogIcon, PersonPencilFillIcon } from '@navikt/aksel-icons';
-import { BodyShort, Box, Label, Skeleton, Tag } from '@navikt/ds-react';
+import { BodyLong, BodyShort, Box, Label, Skeleton, Tag } from '@navikt/ds-react';
 import { LabelledContent } from '../../../../shared/labelled-content/LabelledContent.js';
 import { VurdertAv } from '../../../../shared/vurdert-av/VurdertAv.js';
 import { useAlleInstitusjoner } from '../../SykdomOgOpplæringQueries.js';
 import type { InstitusjonVurderingDtoMedPerioder } from '../types/InstitusjonVurderingDtoMedPerioder.js';
+import { Lovreferanse } from '../../../../shared/lovreferanse/Lovreferanse.js';
 
 interface OwnProps {
   vurdering: InstitusjonVurderingDtoMedPerioder;
@@ -28,7 +29,12 @@ const InstitusjonFerdigVisning = ({ vurdering }: OwnProps) => {
       </Box.New>
       <div className="flex flex-col gap-6 mt-6">
         <LabelledContent
-          label="Er opplæringen ved en godkjent helseinstitusjon eller kompetansesenter?"
+          label={
+            <BodyLong size="small">
+              Er institusjonen en godkjent helseinstitusjon eller kompetansesenter, jf{' '}
+              <Lovreferanse>§ 9-14</Lovreferanse>?
+            </BodyLong>
+          }
           size="small"
           content={
             ((vurdering.resultat === InstitusjonResultat.GODKJENT_AUTOMATISK ||
@@ -40,7 +46,12 @@ const InstitusjonFerdigVisning = ({ vurdering }: OwnProps) => {
         {vurdering.begrunnelse && (
           <div>
             <LabelledContent
-              label="Gjør en vurdering av om opplæringen gjennomgås ved en godkjent helseinstitusjon eller et offentlig spesialpedagogisk kompetansesenter etter § 9-14, første ledd."
+              label={
+                <BodyLong size="small">
+                  Skriv din vurdering av om institusjonen er en godkjent helseinstitusjon eller kompetansesenter etter
+                  reglene i <Lovreferanse>§ 9-14, første ledd</Lovreferanse>
+                </BodyLong>
+              }
               indentContent
               size="small"
               content={
--- packages/v2/gui/src/fakta/sykdom-og-opplæring/4-reisetid/ReisetidFerdigvisning.tsx
+++ packages/v2/gui/src/fakta/sykdom-og-opplæring/4-reisetid/ReisetidFerdigvisning.tsx
@@ -2,7 +2,7 @@ import type { k9_sak_web_app_tjenester_behandling_opplæringspenger_visning_reis
 import OppgittReisetid from './OppgittReisetid';
 import type { Period } from '@navikt/ft-utils';
 import { LabelledContent } from '../../../shared/labelled-content/LabelledContent';
-import { BodyLong } from '@navikt/ds-react';
+import { BodyLong, Link } from '@navikt/ds-react';
 import { VurdertAv } from '../../../shared/vurdert-av/VurdertAv';
 import { resultatTilJaNei } from './utils';
 
@@ -17,23 +17,44 @@ const ReisetidFerdigvisning = ({ vurdering }: { vurdering: ReisetidVurderingDto 
       />
       <div>
         <LabelledContent
-          label="Vurdering"
+          label="Vurder om det er nødvendig å reise en annen dag enn kursdagene"
           indentContent={true}
           size="small"
-          content={<BodyLong className="whitespace-pre-wrap">{vurdering.reisetid.begrunnelse}</BodyLong>}
+          description={
+            <BodyLong size="small">
+              Ta utgangspunkt i{' '}
+              <Link target="_blank" href="https://lovdata.no/pro/lov/1997-02-28-19/§9-14">
+                lovtekst
+              </Link>{' '}
+              og{' '}
+              <Link target="_blank" href="https://lovdata.no/pro/#document/NAV/rundskriv/r09-00/KAPITTEL_4-5">
+                rundskriv
+              </Link>{' '}
+              til §9-14
+            </BodyLong>
+          }
+          content={
+            <BodyLong size="small" className="whitespace-pre-wrap">
+              {vurdering.reisetid.begrunnelse}
+            </BodyLong>
+          }
         />
         <VurdertAv ident={vurdering.reisetid.vurdertAv} date={vurdering.reisetid.vurdertTidspunkt} size="small" />
       </div>
       <LabelledContent
         label={vurderingGjelderEnkeltdag ? 'Innvilges reisedag?' : 'Innvilges reisedager?'}
         size="small"
-        content={<BodyLong className="capitalize">{resultatTilJaNei(vurdering.reisetid.resultat)}</BodyLong>}
+        content={
+          <BodyLong size="small" className="capitalize">
+            {resultatTilJaNei(vurdering.reisetid.resultat)}
+          </BodyLong>
+        }
       />
       {!vurderingGjelderEnkeltdag && (
         <LabelledContent
           label={`I hvilken periode ${innvilgesEllerAvslås} reisetid?`}
           size="small"
-          content={<BodyLong>{vurdering.perioder[0]?.prettifyPeriod()}</BodyLong>}
+          content={<BodyLong size="small">{vurdering.perioder[0]?.prettifyPeriod()}</BodyLong>}
         />
       )}
     </div>
