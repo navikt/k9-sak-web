c385d19465e80e68df61363bf3ebacad9de88f1c
vebnor <vebjorn.nordby@nav.no>
2025-09-18 09:45:52 +0200

    forenkler og gjør det litt mer elegant

--- packages/v2/gui/src/shared/statusstriper/andreSakerPaSokerStripe/relevanteYtelserForYtelse.ts
+++ packages/v2/gui/src/shared/statusstriper/andreSakerPaSokerStripe/relevanteYtelserForYtelse.ts
@@ -3,43 +3,37 @@ import { fagsakYtelsesType, type FagsakYtelsesType } from '@k9-sak-web/backend/k
 export const finnRelevanteYtelserForYtelse = (fagsakYtelseType: FagsakYtelsesType) => {
   const ytelser: FagsakYtelsesType[] = [fagsakYtelseType];
 
-  const relevanteYtelserForType = andreRelevanteYtelserMap.get(fagsakYtelseType);
-  if (relevanteYtelserForType) {
-    relevanteYtelserForType.forEach(ytelse => {
-      ytelser.push(ytelse);
+  const ytelseGruppe = ytelsesGrupper[fagsakYtelseType];
+  if (ytelseGruppe) {
+    ytelseGruppe.forEach(ytelse => {
+      if (ytelse !== fagsakYtelseType) {
+        ytelser.push(ytelse);
+      }
     });
   }
 
   return ytelser;
 };
 
-const andreRelevanteYtelserMap = new Map<FagsakYtelsesType, FagsakYtelsesType[]>([
-  [
-    fagsakYtelsesType.PLEIEPENGER_SYKT_BARN,
-    [fagsakYtelsesType.PLEIEPENGER_NÆRSTÅENDE, fagsakYtelsesType.OPPLÆRINGSPENGER],
-  ],
-  [
-    fagsakYtelsesType.PLEIEPENGER_NÆRSTÅENDE,
-    [fagsakYtelsesType.PLEIEPENGER_SYKT_BARN, fagsakYtelsesType.OPPLÆRINGSPENGER],
-  ],
-  [
-    fagsakYtelsesType.OMSORGSPENGER,
-    [fagsakYtelsesType.OMSORGSPENGER_AO, fagsakYtelsesType.OMSORGSPENGER_MA, fagsakYtelsesType.OMSORGSPENGER_KS],
-  ],
-  [
-    fagsakYtelsesType.OMSORGSPENGER_AO,
-    [fagsakYtelsesType.OMSORGSPENGER, fagsakYtelsesType.OMSORGSPENGER_MA, fagsakYtelsesType.OMSORGSPENGER_KS],
-  ],
-  [
-    fagsakYtelsesType.OMSORGSPENGER_MA,
-    [fagsakYtelsesType.OMSORGSPENGER, fagsakYtelsesType.OMSORGSPENGER_AO, fagsakYtelsesType.OMSORGSPENGER_MA],
-  ],
-  [
-    fagsakYtelsesType.OMSORGSPENGER_KS,
-    [fagsakYtelsesType.OMSORGSPENGER, fagsakYtelsesType.OMSORGSPENGER_AO, fagsakYtelsesType.OMSORGSPENGER_MA],
-  ],
-  [
-    fagsakYtelsesType.OPPLÆRINGSPENGER,
-    [fagsakYtelsesType.PLEIEPENGER_SYKT_BARN, fagsakYtelsesType.PLEIEPENGER_NÆRSTÅENDE],
-  ],
-]);
+const pleiepengerGruppe = [
+  fagsakYtelsesType.PLEIEPENGER_SYKT_BARN,
+  fagsakYtelsesType.PLEIEPENGER_NÆRSTÅENDE,
+  fagsakYtelsesType.OPPLÆRINGSPENGER,
+];
+
+const omsorgspengerGruppe = [
+  fagsakYtelsesType.OMSORGSPENGER,
+  fagsakYtelsesType.OMSORGSPENGER_AO,
+  fagsakYtelsesType.OMSORGSPENGER_MA,
+  fagsakYtelsesType.OMSORGSPENGER_KS,
+];
+
+const ytelsesGrupper: Partial<Record<FagsakYtelsesType, FagsakYtelsesType[]>> = {
+  [fagsakYtelsesType.PLEIEPENGER_SYKT_BARN]: pleiepengerGruppe,
+  [fagsakYtelsesType.PLEIEPENGER_NÆRSTÅENDE]: pleiepengerGruppe,
+  [fagsakYtelsesType.OPPLÆRINGSPENGER]: pleiepengerGruppe,
+  [fagsakYtelsesType.OMSORGSPENGER]: omsorgspengerGruppe,
+  [fagsakYtelsesType.OMSORGSPENGER_AO]: omsorgspengerGruppe,
+  [fagsakYtelsesType.OMSORGSPENGER_MA]: omsorgspengerGruppe,
+  [fagsakYtelsesType.OMSORGSPENGER_KS]: omsorgspengerGruppe,
+};
