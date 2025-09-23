db6f2858ab9dd0818440cbc49c18a85c79f461b9
Audun Eltvik <audun.eltvik@nav.no>
2025-09-23 13:14:06 +0200

    Legg til mulighet for å vurdere flere perioder i samme opplæringsvurdering

--- packages/v2/gui/src/fakta/sykdom-og-opplæring/3-nødvendig-opplæring/NødvendigOpplæringIndex.tsx
+++ packages/v2/gui/src/fakta/sykdom-og-opplæring/3-nødvendig-opplæring/NødvendigOpplæringIndex.tsx
@@ -2,7 +2,7 @@ import Vurderingsnavigasjon, {
   type Vurderingselement,
 } from '../../../shared/vurderingsperiode-navigasjon/VurderingsperiodeNavigasjon';
 import { useVurdertOpplæring } from '../SykdomOgOpplæringQueries';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { SykdomOgOpplæringContext } from '../FaktaSykdomOgOpplæringIndex';
 import { Period } from '@navikt/ft-utils';
 import { type k9_sak_web_app_tjenester_behandling_opplæringspenger_visning_opplæring_OpplæringVurderingDto as OpplæringVurderingDto } from '@k9-sak-web/backend/k9sak/generated/types.js';
@@ -23,6 +23,16 @@ const NødvendigOpplæring = () => {
     ...vurdering,
     perioder: [new Period(vurdering.opplæring.fom, vurdering.opplæring.tom)],
   }));
+  const [andrePerioderTilVurdering, setAndrePerioderTilVurdering] = useState<{ fom: string; tom: string }[]>([]);
+  useEffect(() => {
+    if (vurderingsliste && valgtVurdering) {
+      setAndrePerioderTilVurdering(
+        vurderingsliste
+          .filter(v => !v.perioder.some(p => valgtVurdering.perioder.some(vp => p.fom === vp.fom && p.tom === vp.tom)))
+          .flatMap(v => v.perioder.map(p => ({ fom: p.fom, tom: p.tom }))),
+      );
+    }
+  }, [valgtVurdering]);
 
   if (isLoadingVurdertOpplæring) {
     return <CenteredLoader />;
@@ -41,7 +51,14 @@ const NødvendigOpplæring = () => {
           </>
         )}
         showDetailSection
-        detailSection={() => (valgtVurdering ? <NødvendigOpplæringContainer vurdering={valgtVurdering} /> : null)}
+        detailSection={() =>
+          valgtVurdering ? (
+            <NødvendigOpplæringContainer
+              vurdering={valgtVurdering}
+              andrePerioderTilVurdering={andrePerioderTilVurdering}
+            />
+          ) : null
+        }
       />
     </div>
   );
