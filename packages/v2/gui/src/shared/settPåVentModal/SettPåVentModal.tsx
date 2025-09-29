948e305c4e9a0033e252cd9b46c487b01f15f89b
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2025-09-29 14:20:14 +0200

    send navigering etter endre frist som prop (#7900)

--- packages/v2/gui/src/shared/settPåVentModal/SettPåVentModal.tsx
+++ packages/v2/gui/src/shared/settPåVentModal/SettPåVentModal.tsx
@@ -2,7 +2,6 @@ import { VenteÅrsakType } from '@k9-sak-web/backend/k9sak/kodeverk/VenteÅrsakT
 import { useKodeverkContext } from '@k9-sak-web/gui/kodeverk/index.js';
 import { formatDate } from '@k9-sak-web/lib/dateUtils/dateUtils.js';
 import { type KodeverkObject, KodeverkType } from '@k9-sak-web/lib/kodeverk/types.js';
-import { goToLos } from '@k9-sak-web/lib/paths/paths.js';
 import { CheckmarkCircleFillIcon } from '@navikt/aksel-icons';
 import { BodyShort, Box, Button, Label, Modal, Select } from '@navikt/ds-react';
 import { RhfDatepicker, RhfForm, RhfSelect, RhfTextarea } from '@navikt/ft-form-hooks';
@@ -100,6 +99,7 @@ interface PureOwnProps {
   frist?: string;
   ventearsak?: string;
   ventearsakVariant?: string;
+  navigerEtterEndreFrist: () => void;
 }
 
 export interface FormState {
@@ -124,6 +124,7 @@ export const SettPåVentModal = ({
   visBrevErBestilt = false,
   hasManualPaVent,
   ventearsakVariant: originalVentearsakVariant,
+  navigerEtterEndreFrist,
 }: PureOwnProps) => {
   const formMethods = useForm<FormState>({
     defaultValues: buildInitialValues(originalVentearsak, originalFrist, hasManualPaVent, originalVentearsakVariant),
@@ -179,7 +180,7 @@ export const SettPåVentModal = ({
     if (erVenterEtterlysInntektsmelding && showEndreFrist) {
       toggleEndreFrist();
     } else if (!showAvbryt) {
-      goToLos();
+      navigerEtterEndreFrist();
     }
   };
 
