ac89594a4a3e0776910c6ad341be69b4cfc5ddca
Hallvard Andreas Stark <hallvard@scel.to>
2025-09-29 13:14:23 +0200

    Typerydding

--- packages/v2/gui/src/prosess/ung-vedtak/UngVedtakBackendApiType.ts
+++ packages/v2/gui/src/prosess/ung-vedtak/UngVedtakBackendApiType.ts
@@ -1,7 +1,7 @@
 import type {
   ForhåndsvisVedtaksbrevResponse,
+  LagreVedtaksbrevValgResponses,
   ung_sak_kontrakt_formidling_vedtaksbrev_VedtaksbrevValgRequest,
-  ung_sak_kontrakt_formidling_vedtaksbrev_VedtaksbrevValgResponse,
   VedtaksbrevValgResponse,
 } from '@k9-sak-web/backend/ungsak/generated/types.js';
 
@@ -10,5 +10,5 @@ export type UngVedtakBackendApiType = {
   vedtaksbrevValg(behandlingId: number): Promise<VedtaksbrevValgResponse>;
   lagreVedtaksbrev(
     data: ung_sak_kontrakt_formidling_vedtaksbrev_VedtaksbrevValgRequest,
-  ): Promise<ung_sak_kontrakt_formidling_vedtaksbrev_VedtaksbrevValgResponse>;
+  ): Promise<LagreVedtaksbrevValgResponses>;
 };
