ccdc222ae59fd83940d1110c1576a21778a23bdc
Hallvard Andreas Stark <hallvard@scel.to>
2025-10-02 12:47:11 +0200

    Brevhistorie

--- packages/v2/gui/src/prosess/ung-vedtak/UngVedtak.stories.tsx
+++ packages/v2/gui/src/prosess/ung-vedtak/UngVedtak.stories.tsx
@@ -1,6 +1,7 @@
 import {
   ung_kodeverk_behandling_BehandlingStatus as BehandlingDtoStatus,
   ung_kodeverk_behandling_BehandlingType as BehandlingDtoType,
+  ung_kodeverk_dokument_DokumentMalType,
 } from '@k9-sak-web/backend/ungsak/generated/types.js';
 import alleKodeverkV2 from '@k9-sak-web/lib/kodeverk/mocks/alleKodeverkV2.json';
 import type { Meta, StoryObj } from '@storybook/react';
@@ -100,5 +101,69 @@ export const AvslåttStory: Story = {
       status: BehandlingDtoStatus.AVSLUTTET,
     },
     readOnly: false,
+  },
+};
+
+export const MedBrev: Story = {
+  args: {
+    behandling: {
+      behandlingsresultat: {
+        type: 'INNVILGET',
+      },
+      id: 3000002,
+      status: BehandlingDtoStatus.OPPRETTET,
+    },
+    vilkår: [
+      {
+        vilkarType: 'UNG_VK_2',
+        perioder: [
+          {
+            avslagKode: undefined,
+            vilkarStatus: 'OPPFYLT',
+          },
+        ],
+      },
+      {
+        vilkarType: 'UNG_VK_1',
+        perioder: [
+          {
+            avslagKode: undefined,
+            vilkarStatus: 'OPPFYLT',
+          },
+        ],
+      },
+    ],
+    readOnly: false,
+    aksjonspunkter: [
+      {
+        kanLoses: true,
+      },
+    ],
+    vedtaksbrevValgResponse: {
+      harBrev: true,
+      vedtaksbrevValg: [
+        {
+          dokumentMalType: {
+            kilde: ung_kodeverk_dokument_DokumentMalType.ENDRING_INNTEKT,
+            navn: 'Endre inntekt',
+            kode: 'ENDRE_INNTEKT',
+            kodeverk: '',
+          },
+        },
+        {
+          dokumentMalType: {
+            kilde: ung_kodeverk_dokument_DokumentMalType.ENDRING_HØY_SATS,
+            navn: 'Endring høy sats',
+            kode: 'ENDRE_HØY_SATS',
+            kodeverk: '',
+          },
+        },
+        // {
+        //   brevMal: 'AVSLAG_UNG',
+        //   navn: 'Avslag ungdomsytelse',
+        //   støtterTredjepartsmottaker: false,
+        // },
+      ],
+    },
   },
 };
