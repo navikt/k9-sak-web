44ae8447d6dd7ced91c416a6a41122ac7c0b2ca0
Hallvard Andreas Stark <hallvard@scel.to>
2025-10-07 22:13:57 +0200

    Fjerner lasting av init-fetch for tilbake

--- packages/ung/sak-app/app/useHentInitLenker.tsx
+++ packages/ung/sak-app/app/useHentInitLenker.tsx
@@ -11,17 +11,13 @@ const LINKS_TEMPLATE = {
 const useHentInitLenker = (): boolean[] => {
   const { data: initFetchLinksUngSak = LINKS_TEMPLATE, state: initFetchStateUngSak } =
     restApiHooks.useGlobalStateRestApi<InitLinks>(UngSakApiKeys.INIT_FETCH);
-  const { data: initFetchLinksTilbake = LINKS_TEMPLATE, state: initFetchStateTilbake } =
-    restApiHooks.useGlobalStateRestApi<InitLinks>(UngSakApiKeys.INIT_FETCH_TILBAKE);
 
   const harUngsakInitKallFeilet = initFetchStateUngSak === RestApiState.ERROR;
-  const harHentetFerdigInitLenker =
-    initFetchStateUngSak === RestApiState.SUCCESS &&
-    (initFetchStateTilbake === RestApiState.SUCCESS || initFetchStateTilbake === RestApiState.ERROR);
+  const harHentetFerdigInitLenker = initFetchStateUngSak === RestApiState.SUCCESS;
 
   if (harHentetFerdigInitLenker) {
-    requestApi.setLinks(initFetchLinksUngSak.links.concat(initFetchLinksTilbake.links), LinkCategory.INIT_DATA);
-    requestApi.setLinks(initFetchLinksUngSak.sakLinks.concat(initFetchLinksTilbake.sakLinks), LinkCategory.FAGSAK);
+    requestApi.setLinks(initFetchLinksUngSak.links, LinkCategory.INIT_DATA);
+    requestApi.setLinks(initFetchLinksUngSak.sakLinks, LinkCategory.FAGSAK);
   }
 
   return [harHentetFerdigInitLenker, harUngsakInitKallFeilet];
