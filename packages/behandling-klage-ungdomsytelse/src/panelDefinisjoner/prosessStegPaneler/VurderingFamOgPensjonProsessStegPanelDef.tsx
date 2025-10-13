8b7aa4acf6e067c6b4b9fab8d5da0bbf0dc64d43
Hallvard Andreas Stark <hallvard@scel.to>
2025-10-13 12:29:25 +0200

    Kodedverktransformering

--- packages/behandling-klage-ungdomsytelse/src/panelDefinisjoner/prosessStegPaneler/VurderingFamOgPensjonProsessStegPanelDef.tsx
+++ packages/behandling-klage-ungdomsytelse/src/panelDefinisjoner/prosessStegPaneler/VurderingFamOgPensjonProsessStegPanelDef.tsx
@@ -2,9 +2,14 @@ import aksjonspunktCodes from '@fpsak-frontend/kodeverk/src/aksjonspunktCodes';
 import { ProsessStegDef, ProsessStegPanelDef } from '@k9-sak-web/behandling-felles';
 import { KlagevurderingProsessIndex } from '@k9-sak-web/gui/prosess/klagevurdering/KlagevurderingProsessIndex.js';
 import { prosessStegCodes } from '@k9-sak-web/konstanter';
+import { konverterKodeverkTilKode } from '@k9-sak-web/lib/kodeverk/konverterKodeverkTilKode.js';
 
 class PanelDef extends ProsessStegPanelDef {
-  getKomponent = props => <KlagevurderingProsessIndex {...props} />;
+  getKomponent = props => {
+    const deepCopyProps = JSON.parse(JSON.stringify(props));
+    konverterKodeverkTilKode(deepCopyProps, false);
+    return <KlagevurderingProsessIndex {...props} {...deepCopyProps} />;
+  };
 
   getAksjonspunktKoder = () => [aksjonspunktCodes.BEHANDLE_KLAGE_NFP];
 
--- packages/behandling-klage-ungdomsytelse/src/panelDefinisjoner/prosessStegPaneler/VurderingKlageInstansProsessStegPanelDef.tsx
+++ packages/behandling-klage-ungdomsytelse/src/panelDefinisjoner/prosessStegPaneler/VurderingKlageInstansProsessStegPanelDef.tsx
@@ -3,10 +3,15 @@ import { fagsakYtelsesType } from '@k9-sak-web/backend/k9sak/kodeverk/FagsakYtel
 import { ProsessStegDef, ProsessStegPanelDef } from '@k9-sak-web/behandling-felles';
 import { KlagevurderingProsessIndex } from '@k9-sak-web/gui/prosess/klagevurdering/KlagevurderingProsessIndex.js';
 import { prosessStegCodes } from '@k9-sak-web/konstanter';
+import { konverterKodeverkTilKode } from '@k9-sak-web/lib/kodeverk/konverterKodeverkTilKode.js';
 import { Fagsak, FeatureToggles } from '@k9-sak-web/types';
 
 class PanelDef extends ProsessStegPanelDef {
-  getKomponent = props => <KlagevurderingProsessIndex {...props} />;
+  getKomponent = props => {
+    const deepCopyProps = JSON.parse(JSON.stringify(props));
+    konverterKodeverkTilKode(deepCopyProps, false);
+    return <KlagevurderingProsessIndex {...props} {...deepCopyProps} />;
+  };
 
   getAksjonspunktKoder = () => [aksjonspunktCodes.BEHANDLE_KLAGE_NK];
 
