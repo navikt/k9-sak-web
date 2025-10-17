46ec10523dc60f06da5e6c4da0ca6c02fa6e3061
Jostein Stuhaug <jostein.stuhaug@nav.no>
2025-10-17 12:47:22 +0200

    Legg til visning av avbrutt autentiseringsfeil.
    For å prøve å informere systembruker litt betre om kva som har gått gale.

---
+++ packages/v2/gui/src/app/auth/AuthAbortedPage.tsx
@@ -0,0 +1,32 @@
+import { Alert, BodyLong, BodyShort, Heading, HStack, Link, VStack } from '@navikt/ds-react';
+import { XMarkOctagonIcon } from '@navikt/aksel-icons';
+
+interface AuthAbortedPageProps {
+  readonly retryURL: URL | null;
+}
+
+export const AuthAbortedPage = ({ retryURL }: AuthAbortedPageProps) => {
+  return (
+    <HStack justify="center" padding="space-48">
+      <VStack justify="center" gap="space-16">
+        <HStack align="center">
+          <XMarkOctagonIcon fontSize="4rem" style={{ color: 'var(--ax-text-danger-subtle)' }} />
+          <Heading size="medium">Innlogging avbrutt</Heading>{' '}
+        </HStack>
+        <Alert variant="error">Automatisk innlogging ble avbrutt før den var fullført.</Alert>
+        <BodyLong>
+          Mulige årsaker:
+          <ul>
+            <li>Du lukket popupvinduet før innlogging var ferdig</li>
+            <li>Nettleser blokkerte automatisk åpning av popup vinduet for innlogging</li>
+            <li>Teknisk feil i autentiseringsflyt</li>
+          </ul>
+        </BodyLong>
+        <BodyShort>Hvis du ønsker å logge inn, prøv på nytt med linken under.</BodyShort>
+        <BodyShort weight="semibold">
+          <Link href={retryURL?.href ?? '/'}>Logg inn</Link>
+        </BodyShort>
+      </VStack>
+    </HStack>
+  );
+};
