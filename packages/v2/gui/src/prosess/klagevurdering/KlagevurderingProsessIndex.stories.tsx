efb904e7c840e3453389d8d34815e1dd3288be55
Hallvard Andreas Stark <hallvard@scel.to>
2025-10-27 15:04:10 +0100

    fix story

--- packages/v2/gui/src/prosess/klagevurdering/KlagevurderingProsessIndex.stories.tsx
+++ packages/v2/gui/src/prosess/klagevurdering/KlagevurderingProsessIndex.stories.tsx
@@ -1,12 +1,14 @@
 import {
   type ung_kodeverk_behandling_aksjonspunkt_AksjonspunktDefinisjon,
+  ung_kodeverk_behandling_BehandlingStatus,
+  ung_kodeverk_behandling_BehandlingType,
   ung_kodeverk_behandling_FagsakYtelseType,
   ung_kodeverk_klage_KlageVurderingType,
 } from '@k9-sak-web/backend/ungsak/generated/types.js';
 import AksjonspunktCodes from '@k9-sak-web/lib/kodeverk/types/AksjonspunktCodes.js';
 import type { Meta, StoryObj } from '@storybook/react';
-import { delay, http, HttpResponse } from 'msw';
-import { expect, userEvent } from 'storybook/test';
+import { http, HttpResponse } from 'msw';
+import { expect, userEvent, waitFor } from 'storybook/test';
 import { asyncAction } from '../../storybook/asyncAction';
 import { KlagevurderingProsessIndex } from './KlagevurderingProsessIndex';
 
@@ -37,19 +39,41 @@ export const VisPanelForKlagevurderingMedAksjonspunktNk: Story = {
         definisjon: AksjonspunktCodes.BEHANDLE_KLAGE_NK as ung_kodeverk_behandling_aksjonspunkt_AksjonspunktDefinisjon,
       },
     ],
-    klageVurdering: {
-      klageVurderingResultatNK: {
-        klageVurdertAv: 'NK',
-        klageVurdering: ung_kodeverk_klage_KlageVurderingType.STADFESTE_YTELSESVEDTAK,
-        fritekstTilBrev: 'test',
-        klageMedholdArsakNavn: 'TEST',
-        godkjentAvMedunderskriver: false,
-      },
+    behandling: {
+      opprettet: '123',
+      sakstype: ung_kodeverk_behandling_FagsakYtelseType.UNGDOMSYTELSE,
+      status: ung_kodeverk_behandling_BehandlingStatus.OPPRETTET,
+      type: ung_kodeverk_behandling_BehandlingType.KLAGE,
+      uuid: '123',
+      versjon: 1,
+    },
+  },
+  parameters: {
+    msw: {
+      handlers: [
+        http.get('/api/klage-v2', async () => {
+          // await delay(250);
+          return HttpResponse.json(
+            {
+              klageVurderingResultatNK: {
+                klageVurdertAv: 'NK',
+                klageVurdering: ung_kodeverk_klage_KlageVurderingType.STADFESTE_YTELSESVEDTAK,
+                fritekstTilBrev: 'test',
+                klageMedholdArsakNavn: 'TEST',
+                godkjentAvMedunderskriver: false,
+              },
+            },
+            { status: 200 },
+          );
+        }),
+      ],
     },
   },
   play: async ({ canvas, step }) => {
     await step('skal vise fire options når klage stadfestet', async () => {
-      await expect(canvas.getByRole('radio', { name: 'Stadfest vedtaket' })).toBeInTheDocument();
+      await waitFor(async () => {
+        await expect(canvas.getByRole('radio', { name: 'Stadfest vedtaket' })).toBeInTheDocument();
+      });
       await expect(canvas.getByRole('radio', { name: 'Omgjør vedtaket' })).toBeInTheDocument();
       await expect(canvas.getByRole('radio', { name: 'Hjemsend vedtaket' })).toBeInTheDocument();
       await expect(canvas.getByRole('radio', { name: 'Opphev og hjemsend vedtaket' })).toBeInTheDocument();
@@ -66,33 +90,19 @@ export const KlagevurderingMedAksjonspunktNfpKlageOpprettholdt: Story = {
         definisjon: AksjonspunktCodes.BEHANDLE_KLAGE_NFP as ung_kodeverk_behandling_aksjonspunkt_AksjonspunktDefinisjon,
       },
     ],
-    // klageVurdering: {
-    //   klageVurderingResultatNFP: {
-    //     klageVurdertAv: 'NK',
-    //     klageVurdering: ung_kodeverk_klage_KlageVurderingType.STADFESTE_YTELSESVEDTAK,
-    //     fritekstTilBrev: 'test',
-    //     klageMedholdArsakNavn: 'TEST',
-    //     godkjentAvMedunderskriver: false,
-    //   },
-    // },
-  },
-  play: async ({ canvas, step }) => {
-    await step('skal vise to options når klage opprettholdt', async () => {
-      await expect(canvas.getByRole('radio', { name: 'Omgjør vedtaket' })).toBeInTheDocument();
-      await expect(canvas.getByRole('radio', { name: 'Oppretthold vedtaket' })).toBeInTheDocument();
-      await userEvent.click(canvas.getByRole('radio', { name: 'Oppretthold vedtaket' }));
-      await expect(canvas.getByRole('combobox', { name: 'Hjemmel' })).toBeInTheDocument();
-    });
-
-    await step('skal vise lenke til forhåndsvis brev når fritekst er fylt, og klagevurdering valgt', async () => {
-      await expect(canvas.getByRole('button', { name: 'Lagre og forhåndsvis brev' })).toBeInTheDocument();
-    });
+    behandling: {
+      opprettet: '123',
+      sakstype: ung_kodeverk_behandling_FagsakYtelseType.UNGDOMSYTELSE,
+      status: ung_kodeverk_behandling_BehandlingStatus.OPPRETTET,
+      type: ung_kodeverk_behandling_BehandlingType.KLAGE,
+      uuid: '123',
+      versjon: 1,
+    },
   },
   parameters: {
     msw: {
       handlers: [
-        http.get('/ung/sak/api/klage-v2', async () => {
-          await delay(250);
+        http.get('/api/klage-v2', async () => {
           return HttpResponse.json(
             {
               klageVurderingResultatNFP: {
@@ -109,26 +119,63 @@ export const KlagevurderingMedAksjonspunktNfpKlageOpprettholdt: Story = {
       ],
     },
   },
+  play: async ({ canvas, step }) => {
+    await step('skal vise to options når klage opprettholdt', async () => {
+      await waitFor(async () => {
+        await expect(canvas.getByRole('radio', { name: 'Omgjør vedtaket' })).toBeInTheDocument();
+      });
+      await expect(canvas.getByRole('radio', { name: 'Oppretthold vedtaket' })).toBeInTheDocument();
+      await userEvent.click(canvas.getByRole('radio', { name: 'Oppretthold vedtaket' }));
+      await expect(canvas.getByRole('combobox', { name: 'Hjemmel' })).toBeInTheDocument();
+    });
+
+    await step('skal vise lenke til forhåndsvis brev når fritekst er fylt, og klagevurdering valgt', async () => {
+      await expect(canvas.getByRole('button', { name: 'Lagre og forhåndsvis brev' })).toBeInTheDocument();
+    });
+  },
 };
 
 export const KlagevurderingMedAksjonspunktNfpKlageMedhold: Story = {
   args: {
     ...KlagevurderingMedAksjonspunktNfpKlageOpprettholdt.args,
-    klageVurdering: {
-      ...KlagevurderingMedAksjonspunktNfpKlageOpprettholdt.args.klageVurdering,
-      klageVurderingResultatNFP: {
-        ...KlagevurderingMedAksjonspunktNfpKlageOpprettholdt.args.klageVurdering.klageVurderingResultatNK,
-        klageVurdering: ung_kodeverk_klage_KlageVurderingType.MEDHOLD_I_KLAGE,
-      },
+    behandling: {
+      opprettet: '123',
+      sakstype: ung_kodeverk_behandling_FagsakYtelseType.UNGDOMSYTELSE,
+      status: ung_kodeverk_behandling_BehandlingStatus.OPPRETTET,
+      type: ung_kodeverk_behandling_BehandlingType.KLAGE,
+      uuid: '123',
+      versjon: 1,
+    },
+  },
+  parameters: {
+    msw: {
+      handlers: [
+        http.get('/api/klage-v2', async () => {
+          return HttpResponse.json(
+            {
+              klageVurderingResultatNFP: {
+                klageVurdertAv: 'NK',
+                fritekstTilBrev: 'test',
+                klageMedholdArsakNavn: 'TEST',
+                godkjentAvMedunderskriver: false,
+                klageVurdering: ung_kodeverk_klage_KlageVurderingType.MEDHOLD_I_KLAGE,
+              },
+            },
+            { status: 200 },
+          );
+        }),
+      ],
     },
   },
   play: async ({ canvas, step }) => {
     await step('skal vise to options når klage opprettholdt', async () => {
-      await expect(canvas.getByRole('radio', { name: 'Omgjør vedtaket' })).toBeInTheDocument();
+      await waitFor(async () => {
+        await expect(canvas.getByRole('radio', { name: 'Omgjør vedtaket' })).toBeInTheDocument();
+      });
       await expect(canvas.getByRole('radio', { name: 'Oppretthold vedtaket' })).toBeInTheDocument();
+      await userEvent.click(canvas.getByRole('radio', { name: 'Omgjør vedtaket' }));
       await expect(canvas.getByRole('radio', { name: 'Til gunst' })).toBeInTheDocument();
       await expect(canvas.getByRole('radio', { name: 'Til ugunst' })).toBeInTheDocument();
-      await userEvent.click(canvas.getByRole('radio', { name: 'Omgjør vedtaket' }));
       await expect(canvas.getByRole('radio', { name: 'Delvis omgjør, til gunst' })).toBeInTheDocument();
     });
   },
