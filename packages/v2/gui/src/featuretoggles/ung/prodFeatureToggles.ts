7459280c9744ead931f0c307d73c7b2c02c171c6
Jostein Stuhaug <135307852+josstn@users.noreply.github.com>
2025-10-31 11:24:51 +0100

    Show all unhandled rejections (#8085)
    * Vis feilmelding for alle async feil som oppstår.
    
    * featuretoggle på ny UnhandledRejectionCatcher kode.
    
    Ønsker å berre køyre dette i Q ei stund for å kartlegge kor mange og kva type feilmeldinger som kjem.
    
    * prod feature toggle skulle vere false. 😬
    
    ---------
    
    Co-authored-by: Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>

--- packages/v2/gui/src/featuretoggles/ung/prodFeatureToggles.ts
+++ packages/v2/gui/src/featuretoggles/ung/prodFeatureToggles.ts
@@ -25,4 +25,5 @@ export const prodFeatureToggles = {
   BRUK_MANGLER_LEGEERKLÆRING_I_TILSYN_OG_PLEIE: false,
   MARKERING_UTENLANDSTILSNITT: false,
   AKTIVER_AVSLAG_IKKE_INNTEKTSTAP: false,
+  VIS_ALLE_ASYNC_ERRORS: false,
 } satisfies FeatureToggles;
