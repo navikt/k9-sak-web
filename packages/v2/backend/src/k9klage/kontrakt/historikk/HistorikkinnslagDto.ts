80757b32cd17a320b9bef118dd6149fa531f1914
Jostein Stuhaug <135307852+josstn@users.noreply.github.com>
2025-11-07 12:02:19 +0100

    Historikkv2/ung (#8115)
    * Forenkling av historikk type beriker.
    
    * Forbetring av berikede historikk typer.
    
    * Flytter historikk api inn i sak/historikk.
    
    * Fiks HistorikkIndex story title.
    
    * Endra slik at HistorikkBackendApi har eit kall for alle backends.
    
    Enkapsulerer forskjeller mellom k9 og ung, gjere komponent koden enklare.
    
    * backend id på HistorikkBackendApi.
    
    Slik at react query kan oppdage det viss den endra seg.
    
    * Normalisert HistorikkinnslagDto type i backend.
    
    Enkapsulerer forskjellane i pakke/typenamn frå ulike backends.
    
    * Legger til UngHistorikkInnslagBeriker og UngHistorikkBackendClient.
    
    Rydder samtidig litt i typer og importnavn ellers.
    
    * Ta i bruk v2/ Historikk visning for ung.
    
    * Fjern gammal HistorikkIndex for ung.
    
    * Normalisert og "combined" HistorikkAktør type i backend.
    
    Enkapsulerer forskjellane i pakke/typenamn frå ulike backends.
    
    * Rydder i historikk gui kode etter modernisering.
    
    * La til ung-tilbake HistorikkInnslagDokumentLinkDto i combined for DokumentLink.
    
    * Beriker historikk dokumentlenker med serverUrl.
    
    Slik at vi slepp å ha backend forskjeller i DokumentLenke.tsx
    
    * Sletter gammal historikk kode.
    
    * historikkAktor.ts er ikkje brukt lenger.
    
    * Fjerner referanse til sletta sak-historikk pakke.
    
    * Fjerner no ubrukte historikk api keys.
    
    * Fjerner no ubrukte historikk kodeverkoppslag.
    
    * Fjerner no ubrukte historikk kodeverksreferanser.
    
    * Fiks kompileringsfeil etter kodefjerning.
    
    * Legg til manglande key.
    
    ---------
    
    Co-authored-by: Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>

---
+++ packages/v2/backend/src/k9klage/kontrakt/historikk/HistorikkinnslagDto.ts
@@ -0,0 +1,2 @@
+export type { k9_klage_kontrakt_historikk_v2_HistorikkinnslagDtoV2 as HistorikkinnslagDto } from '../../generated/types.js';
+export type { k9_klage_kontrakt_historikk_v2_HistorikkinnslagDtoV2_Linje as HistorikkinnslagDtoLinje } from '../../generated/types.js';
---
+++ packages/v2/backend/src/ungsak/kontrakt/historikk/HistorikkinnslagDto.ts
@@ -0,0 +1,2 @@
+export type { ung_sak_kontrakt_historikk_HistorikkinnslagDto as HistorikkinnslagDto } from '../../generated/types.js';
+export type { ung_sak_kontrakt_historikk_HistorikkinnslagDto_Linje as HistorikkinnslagDtoLinje } from '../../generated/types.js';
