548ddd575c1648d224b4fc9170b522d6b6f384be
Jostein Stuhaug <jostein.stuhaug@nav.no>
2025-11-10 15:29:03 +0100

    Forenkling med map groupby.

--- packages/v2/gui/src/prosess/tilkjent-ytelse/api/K9TilkjentYtelseBackendClient.ts
+++ packages/v2/gui/src/prosess/tilkjent-ytelse/api/K9TilkjentYtelseBackendClient.ts
@@ -1,7 +1,6 @@
-import type { k9_sak_kontrakt_beregningsresultat_FeriepengegrunnlagAndelDto as FeriepengegrunnlagAndel } from '@k9-sak-web/backend/k9sak/generated/types.js';
 import { beregningsresultat_hentFeriepengegrunnlag } from '@k9-sak-web/backend/k9sak/generated/sdk.js';
 import type { FeriepengerPrÅr } from '../components/feriepenger/FeriepengerPanel.js';
-import { finnListeMedOpptjeningsår, type TilkjentYtelseApi } from './TilkjentYtelseApi.js';
+import type { TilkjentYtelseApi } from './TilkjentYtelseApi.js';
 
 export default class K9TilkjentYtelseBackendClient implements TilkjentYtelseApi {
   /**
@@ -10,12 +9,6 @@ export default class K9TilkjentYtelseBackendClient implements TilkjentYtelseApi 
   async hentFeriepengegrunnlagPrÅr(behandlingUuid: string): Promise<FeriepengerPrÅr> {
     const feriepengegrunnlag =
       (await beregningsresultat_hentFeriepengegrunnlag({ query: { behandlingUuid } })).data ?? null;
-    const opptjeningsår = finnListeMedOpptjeningsår(feriepengegrunnlag.andeler);
-    const andelerPrÅr = new Map<number, FeriepengegrunnlagAndel[]>();
-    for (const år of opptjeningsår) {
-      const andelerIÅret = feriepengegrunnlag.andeler.filter(andel => andel.opptjeningsår == år);
-      andelerPrÅr.set(år, andelerIÅret);
-    }
-    return andelerPrÅr;
+    return Map.groupBy(feriepengegrunnlag.andeler, andel => andel.opptjeningsår);
   }
 }
--- packages/v2/gui/src/prosess/tilkjent-ytelse/api/TilkjentYtelseApi.ts
+++ packages/v2/gui/src/prosess/tilkjent-ytelse/api/TilkjentYtelseApi.ts
@@ -1,14 +1,5 @@
 import type { FeriepengerPrÅr } from '../components/feriepenger/FeriepengerPanel.js';
-import type { k9_sak_kontrakt_beregningsresultat_FeriepengegrunnlagAndelDto as FeriepengegrunnlagAndel } from '@navikt/k9-sak-typescript-client/types';
 
 export interface TilkjentYtelseApi {
   hentFeriepengegrunnlagPrÅr(behandlingUuid: string): Promise<FeriepengerPrÅr>;
 }
-
-/**
- * Hjelpefunksjon for implementering av TilkjentYtelseApi
- */
-export const finnListeMedOpptjeningsår = (andeler: FeriepengegrunnlagAndel[]): number[] => {
-  const årsliste = andeler.map(andel => andel.opptjeningsår).toSorted((a, b) => a - b);
-  return [...new Set(årsliste)];
-};
