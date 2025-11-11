f412a08bde85af08784e2123416fb079fd8988d0
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2025-11-11 13:13:19 +0100

    Bruk nytt vedtak-klage-panel for K9 (#8109)
    * Checkpoint from VS Code for coding agent session
    
    * Sender inn riktig valgt part
    
    * sender ned fagsak
    
    * Skjul forskjeller mellom backends meir i Backend Api. (#8111)
    
    * Skjul forskjeller mellom backends meir i Backend Api.
    
    Slik at komponent kode blir mindre kompleks, og vi unngår litt if/else der.
    
    * Bruk combined FagsakDto.
    
    * Flytter context
    
    ---------
    
    Co-authored-by: Jostein Stuhaug <135307852+josstn@users.noreply.github.com>

--- packages/v2/gui/src/prosess/vedtak-klage/api/VedtakKlageApi.ts
+++ packages/v2/gui/src/prosess/vedtak-klage/api/VedtakKlageApi.ts
@@ -1,11 +1,12 @@
 import type { KlagebehandlingDto } from '@k9-sak-web/backend/combined/kontrakt/klage/KlagebehandlingDto.js';
+import type { FagsakDto } from '@k9-sak-web/backend/combined/kontrakt/fagsak/FagsakDto.js';
+import type { BehandlingDto } from '@k9-sak-web/backend/combined/kontrakt/behandling/BehandlingDto.js';
 
 export interface VedtakKlageApi {
   /**
    * Brukast i queryKey i useQuery kall for å sikre at query blir oppfriska viss backend blir endra.
    */
-  readonly backend: 'ung' /* | 'k9' */; // k9 backend kan leggast til i framtida. typer brukt i interface må då vere union av ung og k9 sine typer
-
-  forhåndsvisKlageVedtaksbrev(behandlingId: number): Promise<Blob>;
+  readonly backend: 'ung' | 'k9klage';
+  forhåndsvisKlageVedtaksbrev(behandling: BehandlingDto, fagsak: FagsakDto): Promise<Blob>;
   getKlageVurdering(behandlingUuid: string): Promise<KlagebehandlingDto>;
 }
