43c251e77a3d88418f34654f083feed7a3bd4033
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2025-11-28 13:16:43 +0100

    Chore/slett prosess klagevurdering (#8194)
    * fikser feil import av prosess-klagevurdering
    
    * Fjerner gammel prosess-klagevurdering
    Bruker v2 nå
    
    * oppdatert lus
    
    * sletter ubrukt fil
    
    ---------
    
    Co-authored-by: Vebjørn Nordby <vebjorn.nordby@nav.no>

--- packages/kodeverk/src/klageVurderingOmgjoer.ts
+++
@@ -1,8 +0,0 @@
-const klageVurderingOmgjoer = {
-  DELVIS_MEDHOLD_I_KLAGE: 'DELVIS_MEDHOLD_I_KLAGE',
-  GUNST_MEDHOLD_I_KLAGE: 'GUNST_MEDHOLD_I_KLAGE',
-  UGUNST_MEDHOLD_I_KLAGE: 'UGUNST_MEDHOLD_I_KLAGE',
-  UDEFINERT: '-',
-};
-
-export default klageVurderingOmgjoer;
