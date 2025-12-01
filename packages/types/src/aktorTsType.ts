372caa04200dd5aea1dee5497ad0e4d46ee89b9a
Jostein Stuhaug <135307852+josstn@users.noreply.github.com>
2025-12-01 12:54:11 +0100

    Fjern ubrukte typedefinisjoner (#8205)
    * Fjerna ubrukte filer.
    
    * Fjerna fleire ubrukte filer.

--- packages/types/src/aktorTsType.ts
+++
@@ -1,9 +0,0 @@
-import Fagsak from './fagsakTsType';
-import FagsakPerson from './fagsakPersonTsType';
-
-export type Aktor = Readonly<{
-  fagsaker: Fagsak[];
-  person: FagsakPerson;
-}>;
-
-export default Aktor;
