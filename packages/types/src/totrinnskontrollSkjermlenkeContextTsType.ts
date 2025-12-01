823417faf31fc06e423678fdba6a0944303743e2
Jostein Stuhaug <jostein.stuhaug@nav.no>
2025-12-01 12:33:38 +0100

    Fjerna ubrukte filer.

--- packages/types/src/totrinnskontrollSkjermlenkeContextTsType.ts
+++
@@ -1,9 +0,0 @@
-import TotrinnskontrollAksjonspunkt from './totrinnskontrollAksjonspunktTsType';
-
-/** @deprecated Bruk generert type istaden */
-export type TotrinnskontrollSkjermlenkeContext = Readonly<{
-  skjermlenkeType: string;
-  totrinnskontrollAksjonspunkter: TotrinnskontrollAksjonspunkt[];
-}>;
-
-export default TotrinnskontrollSkjermlenkeContext;
