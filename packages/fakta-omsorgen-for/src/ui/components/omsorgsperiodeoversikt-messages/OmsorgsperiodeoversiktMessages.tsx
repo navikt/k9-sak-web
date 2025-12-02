6191a922c2163d3c2bf7353c29754caf4d70ec1f
vebnor <vebjorn.nordby@nav.no>
2025-12-02 14:34:56 +0100

    TSFF-2247: Viser ikke alert om at du må vurdere omsorgen for dersom du ikke har et aksjonspunkt med status OPPR

--- packages/fakta-omsorgen-for/src/ui/components/omsorgsperiodeoversikt-messages/OmsorgsperiodeoversiktMessages.tsx
+++ packages/fakta-omsorgen-for/src/ui/components/omsorgsperiodeoversikt-messages/OmsorgsperiodeoversiktMessages.tsx
@@ -4,6 +4,8 @@ import { FormattedMessage } from 'react-intl';
 import Omsorgsperiodeoversikt from '../../../types/Omsorgsperiodeoversikt';
 import { getStringMedPerioder } from '../../../util/periodUtils';
 import styles from './omsorgsperiodeoversiktMessages.module.css';
+import { useOmsorgenForContext } from '../../context/ContainerContext';
+import { AksjonspunktStatus } from '@k9-sak-web/gui/storybook/mocks/uttak/uttakStoryMocks.js';
 
 interface OmsorgsperiodeoversiktMessagesProps {
   omsorgsperiodeoversikt: Omsorgsperiodeoversikt;
@@ -11,8 +13,13 @@ interface OmsorgsperiodeoversiktMessagesProps {
 
 const OmsorgsperiodeoversiktMessages = ({
   omsorgsperiodeoversikt,
-}: OmsorgsperiodeoversiktMessagesProps): JSX.Element => {
-  if (omsorgsperiodeoversikt.harPerioderTilVurdering()) {
+}: OmsorgsperiodeoversiktMessagesProps): JSX.Element | null => {
+  const { readOnly, omsorgenForAksjonspunkt } = useOmsorgenForContext();
+  if (
+    omsorgsperiodeoversikt.harPerioderTilVurdering() &&
+    !readOnly &&
+    omsorgenForAksjonspunkt?.status?.kode === AksjonspunktStatus.OPPRETTET
+  ) {
     const perioderTilVurdering = omsorgsperiodeoversikt.finnPerioderTilVurdering().map(({ periode }) => periode);
     return (
       <Box.New marginBlock="0 6">
