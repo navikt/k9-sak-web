3b267691ea0bb9684453ffb7ae9adc87f410284d
Jostein Stuhaug <jostein.stuhaug@nav.no>
2025-12-02 15:15:17 +0100

    Bugfix generering av behandlingPath.
    Slik at det fungerer både med behandlingId og behandlingUuid

--- packages/v2/gui/src/utils/paths.ts
+++ packages/v2/gui/src/utils/paths.ts
@@ -2,9 +2,9 @@ import { buildPath } from './urlUtils.js';
 
 export const fagsakPath = '/fagsak/:saksnummer/';
 export const behandlingerPath = `${fagsakPath}behandling/`;
-export const behandlingPath = `${behandlingerPath}:behandlingId(\\d+)/`;
+export const behandlingPath = `${behandlingerPath}:behandlingIdOrUuid/`;
 
 export const pathToFagsak = (saksnummer: string): string => buildPath(fagsakPath, { saksnummer });
 export const pathToBehandlinger = (saksnummer: string): string => buildPath(behandlingerPath, { saksnummer });
-export const pathToBehandling = (saksnummer: string, behandlingId: number): string =>
-  buildPath(behandlingPath, { saksnummer, behandlingId });
+export const pathToBehandling = (saksnummer: string, behandlingIdOrUuid: number | string): string =>
+  buildPath(behandlingPath, { saksnummer, behandlingIdOrUuid });
