41c43c13b54a633d5cf0c7ab0572fe37fc270e04
Hallvard Andreas Stark <hallvard@scel.to>
2025-12-15 13:49:53 +0100

    Update ung-sak-typescript-client version and add gjelderDelerAvMåned prop to BeregningsDetaljer components

--- packages/v2/gui/src/prosess/ung-beregning/dagsats-og-utbetaling/BeregningsDetaljerUtregning.tsx
+++ packages/v2/gui/src/prosess/ung-beregning/dagsats-og-utbetaling/BeregningsDetaljerUtregning.tsx
@@ -9,6 +9,7 @@ interface BeregningsDetaljerUtregningProps {
   utbetaling?: number;
   satsperioder?: Array<UngdomsytelseSatsPeriodeDto>;
   reduksjonsgrunnlag?: number;
+  gjelderDelerAvMåned?: boolean;
 }
 
 export const BeregningsDetaljerUtregning = ({
@@ -17,63 +18,69 @@ export const BeregningsDetaljerUtregning = ({
   utbetaling,
   satsperioder,
   reduksjonsgrunnlag,
-}: BeregningsDetaljerUtregningProps) => (
-  <>
-    <VStack gap="space-16">
-      {satsperioder?.map(({ dagsats, antallDager, fom, tom, dagsatsBarnetillegg, antallBarn }) => (
-        <React.Fragment key={`${fom}-${tom}`}>
-          <HStack justify="space-between">
-            <BodyShort size="small">
-              Beregnet ytelse ({formatCurrencyWithKr(dagsats)} x {antallDager} dager)
+  gjelderDelerAvMåned,
+}: BeregningsDetaljerUtregningProps) => {
+  const skalViseTekstOmSisteMåned = gjelderDelerAvMåned && reduksjonsgrunnlag !== undefined;
+
+  return (
+    <>
+      <VStack gap="space-16">
+        {satsperioder?.map(({ dagsats, antallDager, fom, tom, dagsatsBarnetillegg, antallBarn }) => (
+          <React.Fragment key={`${fom}-${tom}`}>
+            <HStack justify="space-between">
+              <BodyShort size="small">
+                Beregnet ytelse ({formatCurrencyWithKr(dagsats)} x {antallDager} dager)
+              </BodyShort>
+              <BodyShort size="small" weight="semibold">
+                {formatCurrencyWithKr(dagsats * antallDager)}
+              </BodyShort>
+            </HStack>
+            {antallBarn != undefined && antallBarn > 0 && (
+              <HStack justify="space-between">
+                <BodyShort size="small">
+                  Barnetillegg ({formatCurrencyWithKr(dagsatsBarnetillegg)} x {antallDager} dager)
+                </BodyShort>
+                <BodyShort size="small" weight="semibold">
+                  {formatCurrencyWithKr(dagsatsBarnetillegg * antallDager)}
+                </BodyShort>
+              </HStack>
+            )}
+          </React.Fragment>
+        ))}
+
+        <Box.New borderWidth="0 0 1 0" />
+
+        {reduksjon && rapportertInntekt ? (
+          <>
+            <VStack gap="space-8">
+              <HStack justify="space-between">
+                <BodyShort size="small">
+                  Reduksjon på grunn av inntekt (66 % av {formatCurrencyWithKr(reduksjonsgrunnlag || rapportertInntekt)}
+                  )
+                </BodyShort>
+                <BodyShort size="small" weight="semibold">
+                  - {formatCurrencyWithKr(reduksjon)}
+                </BodyShort>
+              </HStack>
+              {skalViseTekstOmSisteMåned && (
+                <BodyShort size="small">Inntekten er periodisert siden det er siste måned</BodyShort>
+              )}
+            </VStack>
+            <Box.New borderWidth="0 0 1 0" />
+          </>
+        ) : null}
+
+        {utbetaling != undefined && (
+          <HStack justify="space-between" align="center">
+            <BodyShort weight="semibold" size="small" className={styles.utbetalingText} as="p">
+              Til utbetaling før skatt
             </BodyShort>
-            <BodyShort size="small" weight="semibold">
-              {formatCurrencyWithKr(dagsats * antallDager)}
+            <BodyShort size="small" className={styles.sum} weight="semibold">
+              {formatCurrencyWithKr(utbetaling)}
             </BodyShort>
           </HStack>
-          {antallBarn != undefined && antallBarn > 0 && (
-            <HStack justify="space-between">
-              <BodyShort size="small">
-                Barnetillegg ({formatCurrencyWithKr(dagsatsBarnetillegg)} x {antallDager} dager)
-              </BodyShort>
-              <BodyShort size="small" weight="semibold">
-                {formatCurrencyWithKr(dagsatsBarnetillegg * antallDager)}
-              </BodyShort>
-            </HStack>
-          )}
-        </React.Fragment>
-      ))}
-
-      <Box.New borderWidth="0 0 1 0" />
-
-      {reduksjon && rapportertInntekt ? (
-        <>
-          <VStack gap="space-8">
-            <HStack justify="space-between">
-              <BodyShort size="small">
-                Reduksjon på grunn av inntekt (66 % av {formatCurrencyWithKr(reduksjonsgrunnlag || rapportertInntekt)})
-              </BodyShort>
-              <BodyShort size="small" weight="semibold">
-                - {formatCurrencyWithKr(reduksjon)}
-              </BodyShort>
-            </HStack>
-            {reduksjonsgrunnlag && (
-              <BodyShort size="small">Inntekten er periodisert siden det er siste måned</BodyShort>
-            )}
-          </VStack>
-          <Box.New borderWidth="0 0 1 0" />
-        </>
-      ) : null}
-
-      {utbetaling != undefined && (
-        <HStack justify="space-between" align="center">
-          <BodyShort weight="semibold" size="small" className={styles.utbetalingText} as="p">
-            Til utbetaling før skatt
-          </BodyShort>
-          <BodyShort size="small" className={styles.sum} weight="semibold">
-            {formatCurrencyWithKr(utbetaling)}
-          </BodyShort>
-        </HStack>
-      )}
-    </VStack>
-  </>
-);
+        )}
+      </VStack>
+    </>
+  );
+};
