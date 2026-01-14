73f6cbea3a9013a5b363e0712b26019fe3eb6bdc
Jostein Stuhaug <135307852+josstn@users.noreply.github.com>
2026-01-14 16:08:10 +0100

    Støtte for meldingssending på klagebehandling (#8294)
    * K9KlageMeldingerBackendClient.ts lagt til.
    
    Delt funksjonalitet mellom K9SakMeldingerBackendClient.ts og K9KlageMeldingerBackendClient.ts lagt til felles ny BaseMeldingerBackendClient.ts
    
    For å støtte at det skal kunne sendast brev for klage behandlinger også.
    
    * Korrekte backend typer for MessagesApi.
    
    Bruker combined for det som skal vere likt mellom backends.
    
    * Oppdater k9-klage-typescript-client for sending av brev.

---
+++ packages/v2/backend/src/k9klage/kontrakt/dokument/BestillBrevDto.ts
@@ -0,0 +1,1 @@
+export type { k9_klage_kontrakt_dokument_BestillBrevDto as BestillBrevDto } from '@k9-sak-web/backend/k9klage/generated/types.js';
--- packages/v2/gui/src/sak/meldinger/api/MessagesApi.ts
+++ packages/v2/gui/src/sak/meldinger/api/MessagesApi.ts
@@ -1,24 +1,20 @@
 import type { BackendApi as TredjepartsmottakerBackendApi } from '../TredjepartsmottakerInput.js';
-import type { FagsakYtelsesType } from '@k9-sak-web/backend/k9sak/kodeverk/FagsakYtelsesType.js';
-import type { AvsenderApplikasjon } from '@k9-sak-web/backend/k9formidling/models/AvsenderApplikasjon.js';
+import type { FagsakYtelseType } from '@k9-sak-web/backend/combined/kodeverk/behandling/FagsakYtelseType.js';
 import type { FritekstbrevDokumentdata } from '@k9-sak-web/backend/k9formidling/models/FritekstbrevDokumentdata.js';
-import type { BestillBrevDto } from '@k9-sak-web/backend/k9sak/kontrakt/dokument/BestillBrevDto.js';
+import type { BestillBrevDto } from '@k9-sak-web/backend/combined/kontrakt/dokument/BestillBrevDto.js';
 import type { ForhåndsvisDto } from '@k9-sak-web/backend/k9formidling/models/ForhåndsvisDto.ts';
 import type { Template } from '@k9-sak-web/backend/k9formidling/models/Template.js';
 
+export type LagForhåndsvisningRequest = Omit<ForhåndsvisDto, 'avsenderApplikasjon'>;
+
 export interface MessagesApi extends TredjepartsmottakerBackendApi {
-  backend: 'k9sak';
+  backend: 'k9sak' | 'k9klage';
   hentInnholdBrevmal(
-    fagsakYtelsestype: FagsakYtelsesType,
+    fagsakYtelsestype: FagsakYtelseType,
     eksternReferanse: string,
-    avsenderApplikasjon: AvsenderApplikasjon,
     maltype: string,
   ): Promise<FritekstbrevDokumentdata[]>;
   bestillDokument(bestilling: BestillBrevDto): Promise<void>;
-  lagForhåndsvisningPdf(data: ForhåndsvisDto): Promise<Blob>;
-  hentMaler(
-    fagsakYtelsestype: FagsakYtelsesType,
-    behandlingUuid: string,
-    avsenderApplikasjon: AvsenderApplikasjon,
-  ): Promise<Template[]>;
+  lagForhåndsvisningPdf(data: LagForhåndsvisningRequest): Promise<Blob>;
+  hentMaler(fagsakYtelsestype: FagsakYtelseType, behandlingUuid: string): Promise<Template[]>;
 }
