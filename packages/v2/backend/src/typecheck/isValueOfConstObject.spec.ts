1e9fb8f9c6de798257a0028d0d1dd5925e4884ca
Jostein Stuhaug <jostein.stuhaug@nav.no>
2026-02-06 09:05:16 +0100

    rename isValueOfStringConstObject => isValueOfConstObject.
    Litt betre namn.

--- packages/v2/backend/src/typecheck/isValueOfConstObject.spec.ts
+++ packages/v2/backend/src/typecheck/isValueOfConstObject.spec.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { safeConstCombine } from './safeConstCombine.js';
-import { isValueOfStringConstObject } from './isValueOfStringConstObject.js';
+import { isValueOfConstObject } from './isValueOfConstObject.js';
 
 const one = {
   a: 'a',
@@ -19,7 +19,7 @@ type CombinedVals = (typeof combined)[keyof typeof combined];
 
 const requiresOne = (o: OneVals) => o.length;
 const toOneRequired = (o: CombinedVals) => {
-  if (isValueOfStringConstObject(o, one)) {
+  if (isValueOfConstObject(o, one)) {
     requiresOne(o);
     return true;
   }
@@ -28,26 +28,26 @@ const toOneRequired = (o: CombinedVals) => {
 
 describe('isValueOfStringConstObject', () => {
   it('should return true when called with prop from combined object that exists in given object', () => {
-    expect(isValueOfStringConstObject(combined.a, one)).toBe(true);
-    expect(isValueOfStringConstObject(combined.b, one)).toBe(true);
+    expect(isValueOfConstObject(combined.a, one)).toBe(true);
+    expect(isValueOfConstObject(combined.b, one)).toBe(true);
 
-    expect(isValueOfStringConstObject(combined.a, two)).toBe(true);
-    expect(isValueOfStringConstObject(combined.c, two)).toBe(true);
+    expect(isValueOfConstObject(combined.a, two)).toBe(true);
+    expect(isValueOfConstObject(combined.c, two)).toBe(true);
   });
   it('should return false when called with prop from combined object that does NOT exist in given object', () => {
-    expect(isValueOfStringConstObject(combined.c, one)).toBe(false);
-    expect(isValueOfStringConstObject(combined.b, two)).toBe(false);
+    expect(isValueOfConstObject(combined.c, one)).toBe(false);
+    expect(isValueOfConstObject(combined.b, two)).toBe(false);
   });
 
   it('should return true when called with value that exists in given object', () => {
-    expect(isValueOfStringConstObject('a', one)).toBe(true);
-    expect(isValueOfStringConstObject('b', one)).toBe(true);
-    expect(isValueOfStringConstObject('a', two)).toBe(true);
-    expect(isValueOfStringConstObject('c', two)).toBe(true);
+    expect(isValueOfConstObject('a', one)).toBe(true);
+    expect(isValueOfConstObject('b', one)).toBe(true);
+    expect(isValueOfConstObject('a', two)).toBe(true);
+    expect(isValueOfConstObject('c', two)).toBe(true);
   });
   it('should return false when called with value that does not exist in given object', () => {
-    expect(isValueOfStringConstObject('e', one)).toBe(false);
-    expect(isValueOfStringConstObject('v', one)).toBe(false);
+    expect(isValueOfConstObject('e', one)).toBe(false);
+    expect(isValueOfConstObject('v', one)).toBe(false);
   });
 
   it('should work when used as type guard', () => {
--- packages/v2/backend/src/typecheck/isValueOfStringConstObject.ts
+++
@@ -1,6 +0,0 @@
-export const isValueOfStringConstObject = <const Obj extends Readonly<Record<string, string>>>(
-  prop: string,
-  obj: Obj,
-): prop is Obj[keyof Obj] => {
-  return Object.values(obj).some(v => v === prop);
-};
