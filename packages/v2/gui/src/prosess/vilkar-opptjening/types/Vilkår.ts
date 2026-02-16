97f083dcb3129e57e17046981571539844b8b9c9
Jostein Stuhaug <jostein.stuhaug@nav.no>
2026-02-16 18:39:09 +0100

    Fjerna ubrukt type.

--- packages/v2/gui/src/prosess/vilkar-opptjening/types/Vilkår.ts
+++
@@ -1,5 +0,0 @@
-import type { k9_sak_kontrakt_vilkår_VilkårPeriodeDto as VilkårPeriodeDto } from '@k9-sak-web/backend/k9sak/generated/types.js';
-
-export type Vilkår = {
-  perioder: Array<VilkårPeriodeDto>;
-};
