1458c203a7491d9bda807f6d1f4ea5055cb904f6
vebnor <vebjorn.nordby@nav.no>
2026-02-19 12:05:24 +0100

    fjern testfeil

--- server/src/config.ts
+++ server/src/config.ts
@@ -37,7 +37,6 @@ const proxyApis: ProxyApi[] =
         { path: '/k9/tilbake', url: env('APP_URL_K9TILBAKE') },
         { path: '/k9/fordel', url: env('APP_URL_K9FORDEL') },
         { path: '/k9/endringslogg', url: env('ENDRINGSLOGG_URL'), stripPrefix: true },
-        { path: '/k9/testolini123', url: env('DUMMY') },
       ];
 
 export default {
