2ae7b4177d0f33d32c3896c7004a636f57bafbd8
Hallvard Andreas Stark <hallvard@scel.to>
2026-02-25 13:53:06 +0100

    TSFF-2521 - skal kunne velge opphør for AOO flere år tilbake i tid

--- packages/prosess-omsorgsdager/src/util/stringUtils.ts
+++ packages/prosess-omsorgsdager/src/util/stringUtils.ts
@@ -24,20 +24,24 @@ export const formatereDatoTilLesemodus = (dato: string): string => dayjs(dato).f
 
 /**
  * Utleder tilgjengelige år basert på fraDato og eventuelle relative år
- * @param {number} fraDato: Datoen som angir startpunktet for tilgjengelige år.
+ * @param {string} fraDato: Datoen som angir startpunktet for tilgjengelige år.
  * @param {number | undefined} relativtFraÅr: Antall år før fraDato som skal inkluderes (valgfritt).
  * @param {number | undefined} relativtTilÅr: Antall år etter fraDato som skal inkluderes (valgfritt).
+ * @param {number | undefined} tidligsteMuligeÅrFørNårværendeÅr: Antall år før nåværende år som skal inkluderes (valgfritt).
  * @returns En liste av objekter med tilgjengelige år.
  */
 export const utledTilgjengeligeÅr = (
   fraDato: string,
   relativtFraÅr?: number | undefined,
   relativtTilÅr: number | undefined = 19,
+  tidligsteMuligeÅrFørNårværendeÅr?: number,
 ): TilgjengeligÅrOption[] => {
   const nåværendeÅr = dayjs().year();
   const årFraDato = dayjs(fraDato).year();
   let tidligsteMuligeÅr = årFraDato > nåværendeÅr ? årFraDato : nåværendeÅr - 1;
-  if (relativtFraÅr !== undefined) {
+  if (tidligsteMuligeÅrFørNårværendeÅr) {
+    tidligsteMuligeÅr = nåværendeÅr - tidligsteMuligeÅrFørNårværendeÅr;
+  } else if (relativtFraÅr !== undefined) {
     tidligsteMuligeÅr = årFraDato - relativtFraÅr;
   }
   const år: TilgjengeligÅrOption[] = [{ value: '0', title: 'Dato for opphør', disabled: true }];
