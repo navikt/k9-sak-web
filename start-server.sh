b6e143eb8b66e0d0f8db4f4fb7cf9a6476c6a147
vebnor <vebjorn.nordby@nav.no>
2026-02-25 15:07:25 +0100

    Revert "Bytte fra NGINX til Express (#8390)"
    This reverts commit 1cf4fd770e05605f64e6e3f35d4769512aedc6e4.

---
+++ start-server.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+_shutdown_() {
+  # https://github.com/kubernetes/contrib/issues/1140
+  # https://github.com/kubernetes/kubernetes/issues/43576
+  # https://github.com/kubernetes/kubernetes/issues/64510
+  # https://nav-it.slack.com/archives/C5KUST8N6/p1543497847341300
+  echo "shutdown initialized, allowing incoming requests for 5 seconds before continuing"
+  sleep 5
+  nginx -s quit
+  wait "$pid"
+}
+trap _shutdown_ SIGTERM
+
+export APP_HOSTNAME="${HOSTNAME:-localhost}"
+export APP_PORT="${APP_PORT:-443}"
+export APP_NAME="${APP_NAME:-devimg}"
+export APP_VERSION="${APP_VERSION:-localhost}"
+
+envsubst '$APP_URL $APP_PORT $APP_HOSTNAME $APP_NAME $APP_VERSION $APP_PATH_PREFIX $APP_URL_K9FORMIDLING $APP_URL_K9FORMIDLING_DD $APP_URL_K9OPPDRAG $APP_URL_KLAGE $APP_URL_K9TILBAKE $APP_URL_K9FORDEL $ENDRINGSLOGG_URL $APP_URL_UNG_TILBAKE' < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/default.conf
+
+echo "### Nginx conf ###"
+cat /etc/nginx/conf.d/default.conf
+
+nginx -g "daemon off;" &
+pid=$!
+wait "$pid"
