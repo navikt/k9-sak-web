47b88f5d9703bc0ae977c2bcfcb224d2b77539c9
vebnor <vebjorn.nordby@nav.no>
2026-02-25 16:17:36 +0100

    propagere api-feil videre fra queryClient

--- packages/v2/gui/src/app/root/RootLayout.tsx
+++ packages/v2/gui/src/app/root/RootLayout.tsx
@@ -1,18 +1,36 @@
 import ErrorBoundary from '../feilmeldinger/ErrorBoundary.js';
 import { Theme } from '@navikt/ds-react';
 import { Outlet } from 'react-router';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MutationCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RootSuspense } from './suspense/RootSuspense.js';
+import { isErrorWithAlertInfo } from '../alerts/AlertInfo.js';
+import GeneralAsyncError from '../alerts/GeneralAsyncError.js';
+
+const queryClient = new QueryClient({
+  mutationCache: new MutationCache({
+    onError: error => {
+      // Lager en unhandled rejection, slik at feilene som fanges av @tanstack/react-query
+      // kan propageres videre opp og fanges av UnhandledRejectionCatcher, og vises i TopplinjeAlerts.tsx
+      // Hvis feilen ikke skal dyttes videre opp, så må man bruke
+      // const mutation = useMutation({
+      //    mutationFn: ...,
+      //    meta: { suppressGlobalError: true }
+      // })
+      if (isErrorWithAlertInfo(error)) {
+        void Promise.reject(error);
+      } else if (error instanceof Error) {
+        void Promise.reject(new GeneralAsyncError(error.message, error));
+      }
+    },
+  }),
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export const RootLayout = () => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-      },
-    },
-  });
-
   return (
     <Theme theme="light">
       <ErrorBoundary>
