529d121f59da765caa41fbce61b1b2cb3463aecd
Hallvard Andreas Stark <hallvard@scel.to>
2026-03-09 13:29:22 +0100

    Rydder

--- packages/v2/backend/src/ungsak/kodeverk/FagsakYtelsesType.ts
+++ packages/v2/backend/src/ungsak/kodeverk/FagsakYtelsesType.ts
@@ -1,10 +1,3 @@
-import {
-  type ung_kodeverk_behandling_FagsakYtelseType as generatedFagsakYtelseTypeEnumUnion,
-  ung_kodeverk_behandling_FagsakYtelseType as generatedFagsakYtelseType,
-} from '../generated/types.js';
+import { type ung_kodeverk_behandling_FagsakYtelseType as generatedFagsakYtelseTypeEnumUnion } from '../generated/types.js';
 
 export type FagsakYtelsesType = generatedFagsakYtelseTypeEnumUnion;
-
-// Eksporter generert enum konstant
-// NB: Vurder å bruke combined versjonen av denne istadenfor.
-export const fagsakYtelsesType = generatedFagsakYtelseType;
