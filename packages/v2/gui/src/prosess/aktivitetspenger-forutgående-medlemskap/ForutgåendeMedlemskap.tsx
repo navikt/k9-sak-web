1f13448f3a0cc20566127396d4b2aed3d0d721ae
Hallvard Andreas Stark <hallvard@scel.to>
2026-03-26 11:22:13 +0100

    Typing

--- packages/v2/gui/src/prosess/aktivitetspenger-forutgående-medlemskap/ForutgåendeMedlemskap.tsx
+++ packages/v2/gui/src/prosess/aktivitetspenger-forutgående-medlemskap/ForutgåendeMedlemskap.tsx
@@ -1,5 +1,5 @@
-import type { ung_sak_kontrakt_vilkår_medlemskap_MedlemskapsPeriodeDto } from '@k9-sak-web/backend/ungsak/generated/types.js';
 import type { AksjonspunktDto } from '@k9-sak-web/backend/ungsak/kontrakt/aksjonspunkt/AksjonspunktDto.js';
+import type { MedlemskapsPeriodeDto } from '@k9-sak-web/backend/ungsak/kontrakt/vilkår/medlemskap/MedlemskapsPeriodeDto.js';
 import { Box, Button, Heading, Radio, ReadMore, VStack } from '@navikt/ds-react';
 import { RhfForm, RhfRadioGroup } from '@navikt/ft-form-hooks';
 import { required } from '@navikt/ft-form-validators';
@@ -7,10 +7,13 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 interface Props {
-  submitCallback: (data: any, aksjonspunkt: AksjonspunktDto[]) => Promise<any>;
+  submitCallback: (
+    data: Array<{ kode: AksjonspunktDto['definisjon']; begrunnelse: string; erVilkarOk: boolean }>,
+    aksjonspunkt: Array<Pick<AksjonspunktDto, 'definisjon'>>,
+  ) => Promise<unknown>;
   aksjonspunkt: Pick<AksjonspunktDto, 'definisjon'>;
   readOnly: boolean;
-  forutgåendeMedlemskap: ung_sak_kontrakt_vilkår_medlemskap_MedlemskapsPeriodeDto[];
+  forutgåendeMedlemskap: MedlemskapsPeriodeDto[];
 }
 
 interface FormData {
@@ -28,9 +31,8 @@ export const ForutgåendeMedlemskap = ({ submitCallback, aksjonspunkt, readOnly 
   const onSubmit = async (data: FormData) => {
     setIsSubmitting(true);
     const erVilkarOk = data.erGodkjent === 'true';
-    const aksjonspunktKode = aksjonspunkt.definisjon ?? '';
     const payload = {
-      kode: aksjonspunktKode,
+      kode: aksjonspunkt.definisjon,
       begrunnelse: erVilkarOk ? 'Forutgående medlemskap er godkjent.' : 'Forutgående medlemskap er ikke godkjent.',
       erVilkarOk,
     };
