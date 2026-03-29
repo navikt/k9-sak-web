232794a211c82a9ceba18928429cf0443f26e944
Hallvard Andreas Stark <hallvard@scel.to>
2026-03-29 14:59:56 +0200

    Inngangsvilkår for aktivitetspenger

---
+++ packages/v2/gui/src/prosess/aktivitetspenger-inngangsvilkår/Søknadsfrist.tsx
@@ -0,0 +1,31 @@
+import { BodyShort, Label, VStack } from '@navikt/ds-react';
+import { useState } from 'react';
+import { VilkårSplittPanel } from './VilkårSplittPanel';
+
+export const Søknadsfrist = () => {
+  const [selectedId, setSelectedId] = useState('1');
+
+  return (
+    <VilkårSplittPanel
+      items={[{ id: '1', status: 'success', label: '01.11.2026' }]}
+      selectedItemId={selectedId}
+      onItemSelect={setSelectedId}
+      detailHeading="Vurdering av søknadsfrist"
+    >
+      <VStack gap="space-24">
+        <VStack gap="space-8">
+          <Label size="small" as="p">
+            Søknad innsendt
+          </Label>
+          <BodyShort size="small">01.11.2026</BodyShort>
+        </VStack>
+        <VStack gap="space-8">
+          <Label size="small" as="p">
+            Er vilkår om søknadsfrist oppfylt?
+          </Label>
+          <BodyShort size="small">Vilkåret er oppfylt</BodyShort>
+        </VStack>
+      </VStack>
+    </VilkårSplittPanel>
+  );
+};
