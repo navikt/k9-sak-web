cd20a2f3f4beb798cc4d340d697a4e7c0dd30339
Jostein Stuhaug <jostein.stuhaug@nav.no>
2026-04-08 14:10:19 +0200

    Test createQueryClient, juster defaultOptionsOverride merge.

--- packages/v2/gui/src/shared/query/queryClient.ts
+++ packages/v2/gui/src/shared/query/queryClient.ts
@@ -4,22 +4,23 @@ import { type DefaultOptions, QueryClient } from '@tanstack/react-query';
  * Det er denne funksjon som skal brukast over alt der ein ny QueryClient instans initialiserast. Slik at vi har ein stad
  * å sette felles options.
  *
- * @param defaultOptionsOverride Viss ein trenger å overstyre felles standard (feks fjerne retries i test), sett denne.
+ * @param defaultOptionsOverride Viss ein trenger å overstyre felles standard (feks fjerne retries i test), sett denne. Avgrensa til queries og mutations no, utvid viss behov i framtida.
  */
-export const createQueryClient = (defaultOptionsOverride?: DefaultOptions) =>
+export const createQueryClient = (defaultOptionsOverride?: Pick<DefaultOptions, 'queries' | 'mutations'>) =>
   new QueryClient({
     defaultOptions: {
       // Options satt her blir som standard gjeldande for heile k9-sak-web
       queries: {
         refetchOnWindowFocus: false,
+        // ...Men kan overstyrast med defaultOptionsOverride, feks når query client blir oppretta for tester.
+        ...defaultOptionsOverride?.queries,
       },
       // TODO Juster/sjekk denne for ønska feilhandtering
       mutations: {
         onError: err => {
           throw err;
         },
+        ...defaultOptionsOverride?.mutations,
       },
-      // ...Men kan overstyrast med defaultOptionsOverride, feks når query client blir oppretta for tester.
-      ...defaultOptionsOverride,
     },
   });
