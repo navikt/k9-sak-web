a137ce1ec1f53e249b197c572ce8028bca0314f9
Jostein Stuhaug <jostein.stuhaug@nav.no>
2026-04-08 15:09:52 +0200

    story for UnhandledRejectionCatcher lagt til.
    Viser eksempel på at feil kasta ved kall til server med tanstack useMutation som standard no blir propagert til UnhandledRejectionCatcher. Blir ikkje lenger ignorert eller sendt til ErrorBoundary som standard.

---
+++ packages/v2/gui/src/app/UnhandledRejectionCatcher.stories.tsx
@@ -0,0 +1,61 @@
+import { UnhandledRejectionCatcher } from './UnhandledRejectionCatcher.js';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { useMutation } from '@tanstack/react-query';
+import withFeatureToggles from '../storybook/decorators/withFeatureToggles.js';
+import { expect, userEvent } from 'storybook/test';
+
+const meta = {
+  title: 'gui/app/UnhandledRejectionCatcher',
+  component: UnhandledRejectionCatcher,
+  decorators: [withFeatureToggles({ VIS_ALLE_ASYNC_ERRORS: true })],
+  parameters: {
+    test: {
+      dangerouslyIgnoreUnhandledErrors: true,
+    },
+  },
+} satisfies Meta<typeof UnhandledRejectionCatcher>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const FailingMutationChild = () => {
+  const simulatedMutation = useMutation({
+    mutationFn: async () => {
+      throw new Error('BOOM');
+    },
+  });
+  return (
+    <>
+      <form>
+        <input type="text" />
+        <button type="button" onClick={() => simulatedMutation.mutate()}>
+          Trigger mutation fail
+        </button>
+      </form>
+    </>
+  );
+};
+
+export const MutationFailureDefaultUnhandled: Story = {
+  render: () => {
+    return (
+      <>
+        <UnhandledRejectionCatcher />
+        <FailingMutationChild />
+      </>
+    );
+  },
+  play: async ({ canvas, step }) => {
+    await step('user types something in input', async () => {
+      await userEvent.type(canvas.getByRole('textbox'), 'userinput');
+    });
+    await step('user submits, request triggers error', async () => {
+      await userEvent.click(canvas.getByRole('button'));
+      expect(canvas.getByText('Bakgrunnsprosess feilet', { exact: false }));
+    });
+    await step('input should still have typed value at this point (not be re-rendered)', async () => {
+      await expect(canvas.getByRole('textbox')).toHaveValue('userinput');
+    });
+  },
+};
