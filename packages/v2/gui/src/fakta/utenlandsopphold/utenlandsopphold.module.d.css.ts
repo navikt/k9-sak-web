76842f1b57249bf6c4cff9ca742057ccfa3d46ed
vebnor <vebjorn.nordby@nav.no>
2026-04-15 10:51:12 +0200

    typegen

---
+++ packages/v2/gui/src/fakta/utenlandsopphold/utenlandsopphold.module.d.css.ts
@@ -0,0 +1,8 @@
+declare const styles: {
+  readonly "alertstripe": string;
+  readonly "alertstripeTekst": string;
+  readonly "utenlandsopphold": string;
+  readonly "utenlandsoppholdInfo": string;
+};
+export = styles;
+
