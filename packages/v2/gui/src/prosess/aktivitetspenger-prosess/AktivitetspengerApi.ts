5260c041d5f1993f970b97a7e991273f1e52ba35
Hallvard Andreas Stark <hallvard@scel.to>
2026-04-16 15:13:22 +0200

    Bruker genererte imports

--- packages/v2/gui/src/prosess/aktivitetspenger-prosess/AktivitetspengerApi.ts
+++ packages/v2/gui/src/prosess/aktivitetspenger-prosess/AktivitetspengerApi.ts
@@ -1,38 +1,30 @@
-import type {
-  ung_sak_kontrakt_aksjonspunkt_AksjonspunktDto,
-  ung_sak_kontrakt_aksjonspunkt_BekreftedeAksjonspunkterDto,
-  ung_sak_kontrakt_aksjonspunkt_BekreftetAksjonspunktDto,
-  ung_sak_kontrakt_aksjonspunkt_BekreftetOgOverstyrteAksjonspunkterDto,
-  ung_sak_kontrakt_aktivitetspenger_ytelse_AktivitetspengerUtbetaltMånedDto,
-  ung_sak_kontrakt_AsyncPollingStatus,
-  ung_sak_kontrakt_vilkår_medlemskap_ForutgåendeMedlemskapResponse,
-  ung_sak_kontrakt_vilkår_VilkårMedPerioderDto,
-} from '@k9-sak-web/backend/ungsak/generated/types.js';
+import type { AksjonspunktDto } from '@k9-sak-web/backend/ungsak/kontrakt/aksjonspunkt/AksjonspunktDto.js';
+import type { BekreftedeAksjonspunkterDto } from '@k9-sak-web/backend/ungsak/kontrakt/aksjonspunkt/BekreftedeAksjonspunkterDto.js';
+import type { BekreftetAksjonspunktDto } from '@k9-sak-web/backend/ungsak/kontrakt/aksjonspunkt/BekreftetAksjonspunktDto.js';
+import type { BekreftetOgOverstyrteAksjonspunkterDto } from '@k9-sak-web/backend/ungsak/kontrakt/aksjonspunkt/BekreftetOgOverstyrteAksjonspunkterDto.js';
+import type { AktivitetspengerUtbetaltMånedDto } from '@k9-sak-web/backend/ungsak/kontrakt/aktivitetspenger/AktivitetspengerUtbetaltMånedDto.js';
 import type { BeregningsgrunnlagDto } from '@k9-sak-web/backend/ungsak/kontrakt/aktivitetspenger/BeregningsgrunnlagDto.js';
+import type { AsyncPollingStatus } from '@k9-sak-web/backend/ungsak/kontrakt/AsyncPollingStatus.js';
 import { type BehandlingDto } from '@k9-sak-web/backend/ungsak/kontrakt/behandling/BehandlingDto.js';
 import { type InnloggetAnsattUngV2Dto } from '@k9-sak-web/backend/ungsak/kontrakt/nav-ansatt/InnloggetAnsattUngV2Dto.js';
+import type { ForutgåendeMedlemskapResponse } from '@k9-sak-web/backend/ungsak/kontrakt/vilkår/medlemskap/ForutgåendeMedlemskapResponse.js';
+import type { VilkårMedPerioderDto } from '@k9-sak-web/backend/ungsak/kontrakt/vilkår/VilkårMedPerioderDto.js';
 
 export interface AktivitetspengerApi {
   readonly backend: 'ungsak';
-  getAksjonspunkter(behandlingId: string): Promise<ung_sak_kontrakt_aksjonspunkt_AksjonspunktDto[]>;
-  lagreAksjonspunkt(props: ung_sak_kontrakt_aksjonspunkt_BekreftedeAksjonspunkterDto): Promise<unknown>;
-  lagreAksjonspunktOverstyr(
-    props: ung_sak_kontrakt_aksjonspunkt_BekreftetOgOverstyrteAksjonspunkterDto,
-  ): Promise<unknown>;
-  getVilkår(behandlingUuid: string): Promise<ung_sak_kontrakt_vilkår_VilkårMedPerioderDto[]>;
+  getAksjonspunkter(behandlingId: string): Promise<AksjonspunktDto[]>;
+  lagreAksjonspunkt(props: BekreftedeAksjonspunkterDto): Promise<unknown>;
+  lagreAksjonspunktOverstyr(props: BekreftetOgOverstyrteAksjonspunkterDto): Promise<unknown>;
+  getVilkår(behandlingUuid: string): Promise<VilkårMedPerioderDto[]>;
   getBehandling(behandlingUuid: string): Promise<BehandlingDto>;
-  hentBehandlingMidlertidigStatus(behandlingUuid: string): Promise<ung_sak_kontrakt_AsyncPollingStatus>;
-  hentMedlemskapFraSøknad(
-    behandlingUuid: string,
-  ): Promise<ung_sak_kontrakt_vilkår_medlemskap_ForutgåendeMedlemskapResponse>;
+  hentBehandlingMidlertidigStatus(behandlingUuid: string): Promise<AsyncPollingStatus>;
+  hentMedlemskapFraSøknad(behandlingUuid: string): Promise<ForutgåendeMedlemskapResponse>;
   getBeregningsgrunnlag(behandlingUuid: string): Promise<BeregningsgrunnlagDto>;
   getInnloggetBruker(): Promise<InnloggetAnsattUngV2Dto>;
-  getSatsOgUtbetalingPerioder(
-    behandlingUuid: string,
-  ): Promise<ung_sak_kontrakt_aktivitetspenger_ytelse_AktivitetspengerUtbetaltMånedDto[]>;
+  getSatsOgUtbetalingPerioder(behandlingUuid: string): Promise<AktivitetspengerUtbetaltMånedDto[]>;
   bekreftAksjonspunkt(
     behandlingUuid: string,
     behandlingVersjon: number,
-    bekreftedeAksjonspunktDtoer: ung_sak_kontrakt_aksjonspunkt_BekreftetAksjonspunktDto[],
+    bekreftedeAksjonspunktDtoer: BekreftetAksjonspunktDto[],
   ): Promise<void>;
 }
