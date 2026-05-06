9ffec094a9d588aeb59a294e6cba4abb377a9d6f
Jostein Stuhaug <jostein.stuhaug@nav.no>
2026-05-06 16:45:23 +0200

    SentryReportedError skal aldri rapporterast til sentry.

--- packages/v2/gui/src/app/errorhandling/sentry.ts
+++ packages/v2/gui/src/app/errorhandling/sentry.ts
@@ -1,8 +1,12 @@
 import { ExtendedApiError } from '@k9-sak-web/backend/shared/errorhandling/ExtendedApiError.js';
+import { SentryReportedError } from './SentryReportedError.js';
 
 // Vi ønsker ikkje å rapportere alle feil til Sentry, feks viss har utgått sesjon.
 // Legg til fleire her ved behov.
 export const shouldReportToSentry = (error: Error | null): boolean => {
+  if (error instanceof SentryReportedError) {
+    return false;
+  }
   const apiError = ExtendedApiError.findInError(error);
   if (apiError != null) {
     const doNotReport = apiError.isUnauthorized;
