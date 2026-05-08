34dfa742b02f3d80dfb8256829177f8f91b016ac
Jostein Stuhaug <jostein.stuhaug@nav.no>
2026-05-08 21:42:50 +0200

    Legger til NavCallid i feilrapporteringstekst, for feiltyper som har dette.
    Slik at vi kan søke opp request feil direkte i serverlogger.

--- packages/v2/gui/src/app/errorhandling/ui/ErrorInfoCopy.tsx
+++ packages/v2/gui/src/app/errorhandling/ui/ErrorInfoCopy.tsx
@@ -2,14 +2,25 @@ import { isAlertInfo } from '../AlertInfo.js';
 import { Button } from '@navikt/ds-react';
 import { type ComponentProps, useCallback } from 'react';
 import { FilesIcon } from '@navikt/aksel-icons';
+import { ExtendedApiError } from '@k9-sak-web/backend/shared/errorhandling/ExtendedApiError.js';
+import { AxiosError } from 'axios';
 
 const prepareCopyText = (errors: ReadonlyArray<Error>): string => {
   const errLines: string[] = [];
   for (const error of errors) {
     const errorId = isAlertInfo(error) ? error.errorId : null;
-    const header = `**** ${error.name} (id: ${errorId}) ****`;
-    const message = `${error.message}`;
-    errLines.push(header, message, '/');
+    errLines.push(`**** ${error.name} (id: ${errorId}) ****`);
+    errLines.push(`${error.message}`);
+    if (error instanceof ExtendedApiError) {
+      errLines.push(`NavCallid:${error.navCallid}`);
+    }
+    if (error instanceof AxiosError) {
+      const callId = error.config?.headers.get('Nav-Callid');
+      if (typeof callId === 'string') {
+        errLines.push(`NavCallid:${callId}`);
+      }
+    }
+    errLines.push('/');
   }
   return errLines.join('\n');
 };
