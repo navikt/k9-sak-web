a5a6bc63e611d01bd02eecb3eb449440d29876cc
Vilde.Zcimarzceck <Vilde.zcimarzceck@nav.no>
2026-05-13 09:37:47 +0200

    endret fra a til å

--- packages/fakta-medisinsk-vilkår/src/ui/components/avslått-av-innleggelsesperiode-melding/AvslåttAvInnleggelsesperiodeMelding.tsx
+++ packages/fakta-medisinsk-vilkår/src/ui/components/avslått-av-innleggelsesperiode-melding/AvslåttAvInnleggelsesperiodeMelding.tsx
@@ -1,12 +1,12 @@
 import { type JSX } from 'react';
 import { Alert } from '@navikt/ds-react';
 
-const AvslåtttAvInnleggelsesperiodeMelding = (): JSX.Element => (
+const AvslåttAvInnleggelsesperiodeMelding = (): JSX.Element => (
   <Alert size="small" variant="warning">
     Hele eller deler av perioden er avslått som følge av innleggelse. Vurderingen som ligger til grunn blir dermed ikke
     brukt for disse dagene.
   </Alert>
 );
 
-export default AvslåtttAvInnleggelsesperiodeMelding;
+export default AvslåttAvInnleggelsesperiodeMelding;
 
