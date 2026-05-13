38d0ff4ca26c52ceabea7cf7987e542520907f39
Vilde.Zcimarzceck <Vilde.zcimarzceck@nav.no>
2026-05-13 13:41:55 +0200

    la tilbake returverdien

--- packages/fakta-medisinsk-vilkår/src/ui/components/avslått-av-innleggelsesperiode-melding/AvslåttAvInnleggelsesperiodeMelding.tsx
+++ packages/fakta-medisinsk-vilkår/src/ui/components/avslått-av-innleggelsesperiode-melding/AvslåttAvInnleggelsesperiodeMelding.tsx
@@ -1,6 +1,7 @@
+import React, { type JSX } from 'react';
 import { Alert } from '@navikt/ds-react';
 
-const AvslåttAvInnleggelsesperiodeMelding = (
+const AvslåttAvInnleggelsesperiodeMelding = (): JSX.Element => (
   <Alert size="small" variant="warning">
     Hele eller deler av perioden er avslått som følge av innleggelse. Vurderingen som ligger til grunn blir dermed ikke
     brukt for disse dagene.
