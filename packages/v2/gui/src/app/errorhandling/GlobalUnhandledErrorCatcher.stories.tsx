b5db3fa6493a6cc742bb91bc7ad6485132b9fc20
Jostein Stuhaug <jostein.stuhaug@nav.no>
2026-05-20 13:17:47 +0200

    Forøk å unngå feilande storybook test.

--- packages/v2/gui/src/app/errorhandling/GlobalUnhandledErrorCatcher.stories.tsx
+++ packages/v2/gui/src/app/errorhandling/GlobalUnhandledErrorCatcher.stories.tsx
@@ -212,7 +212,8 @@ export const ErrorWhileDialogOpen: Story = {
     await userEvent.click(canvas.getByRole('button', { name: 'Open dialog' }));
     const dialog = await within(document.body).findByRole('dialog');
     const dialogInput = within(dialog).getByRole('textbox', { name: 'Dialog input:' });
-    await userEvent.type(dialogInput, 'Skjemadata');
+    await userEvent.click(dialogInput);
+    await userEvent.keyboard('Skjemadata');
     await userEvent.click(within(dialog).getByRole('button', { name: 'Utløys feil' }));
     const alertDialog = await within(document.body).findByRole('alertdialog');
     await expect(alertDialog).toBeInTheDocument();
