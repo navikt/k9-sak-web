e54433c6614d43e7dc66ddce7a366f69f09f7e73
Jostein Stuhaug <jostein.stuhaug@nav.no>
2026-05-20 11:08:20 +0200

    Bruk ny createQueryClient funksjon i withQueryClientProvider.tsx

--- packages/v2/gui/src/storybook/decorators/withQueryClientProvider.tsx
+++ packages/v2/gui/src/storybook/decorators/withQueryClientProvider.tsx
@@ -1,13 +1,12 @@
 import type { Decorator } from '@storybook/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { createQueryClient } from "../../shared/query/queryClient.js";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-    },
+const queryClient = createQueryClient({
+  queries: {
+    retry: false,
   },
-});
+})
 
 export const withQueryClientProvider: Decorator = Story => (
   <QueryClientProvider client={queryClient}>
