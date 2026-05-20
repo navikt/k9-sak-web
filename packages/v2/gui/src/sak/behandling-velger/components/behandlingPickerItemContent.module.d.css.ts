841f25f118a116473f974f0d91edfc34642ef951
Hallvard Andreas Stark <hallvard@scel.to>
2026-05-20 12:37:46 +0200

    fix

--- packages/v2/gui/src/sak/behandling-velger/components/behandlingPickerItemContent.module.d.css.ts
+++ packages/v2/gui/src/sak/behandling-velger/components/behandlingPickerItemContent.module.d.css.ts
@@ -1,10 +1,11 @@
 declare const styles: {
-  readonly behandlingPicker: string;
-  readonly indent: string;
-  readonly opprettetDatoContainer: string;
-  readonly smallerUndertittel: string;
-  readonly utfallImage: string;
-  readonly åpneChevron: string;
-  readonly åpneText: string;
+  readonly "behandlingPicker": string;
+  readonly "indent": string;
+  readonly "opprettetDatoContainer": string;
+  readonly "smallerUndertittel": string;
+  readonly "utfallImage": string;
+  readonly "åpneChevron": string;
+  readonly "åpneText": string;
 };
 export = styles;
+
