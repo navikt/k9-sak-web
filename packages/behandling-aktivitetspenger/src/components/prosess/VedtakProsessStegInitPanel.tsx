2d5e838fc8169ca25ad0a72471d9b65072ceada3
Hallvard Andreas Stark <hallvard@scel.to>
2026-05-22 20:33:58 +0200

    Lesevisning i prosesspanel dersom behandling har status fatter vedtak

--- packages/behandling-aktivitetspenger/src/components/prosess/VedtakProsessStegInitPanel.tsx
+++ packages/behandling-aktivitetspenger/src/components/prosess/VedtakProsessStegInitPanel.tsx
@@ -51,7 +51,8 @@ export function VedtakProsessStegInitPanel({ api, behandling, onVedtakAksjonspun
     return (
       (!innloggetBruker.aktivitetspengerDel2SaksbehandlerTilgang?.kanBeslutte &&
         !innloggetBruker.aktivitetspengerDel2SaksbehandlerTilgang?.kanSaksbehandle) ||
-      behandling.status === BehandlingStatus.AVSLUTTET
+      behandling.status === BehandlingStatus.AVSLUTTET ||
+      behandling.status === BehandlingStatus.FATTER_VEDTAK
     );
   }, [innloggetBruker, behandling]);
   const erValgt = prosessPanelContext?.erValgt(PANEL_ID);
