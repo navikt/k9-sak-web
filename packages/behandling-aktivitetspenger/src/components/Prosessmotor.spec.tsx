b15941c5ce6ac8ca38850a9016a15918e7b4478f
Hallvard Andreas Stark <hallvard@scel.to>
2026-05-22 10:31:03 +0200

    renamer prosessmotor pga skrivefeil

--- packages/behandling-aktivitetspenger/src/components/Prosessmotor.spec.tsx
+++ packages/behandling-aktivitetspenger/src/components/Prosessmotor.spec.tsx
@@ -9,7 +9,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { renderHook, waitFor } from '@testing-library/react';
 import type { PropsWithChildren } from 'react';
 import { beforeEach, describe, expect, test } from 'vitest';
-import { useProsessmotor } from './Prossesmotor';
+import { useProsessmotor } from './Prosessmotor';
 
 const createWrapper = (queryClient: QueryClient) => {
   return ({ children }: PropsWithChildren) => (
