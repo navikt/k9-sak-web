64bfffa226050210760b75a87cecf1594f13220b
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2026-06-09 10:48:18 +0200

    fix: håndterer at simulering vises riktig dersom det er avslag i utta… (#8744)
    …k og man får AP
    
    ### Behov / Bakgrunn
    I prosessmeny v2 får man ikke riktig status i menyen når uttak er
    avslått og man får aksjonspunkt i simulering.
    
    ### Løsning
    Sjekker om uttak er avslått og man har aksjonspunkt
    
    ### Andre endringer
    
    ---------
    
    Co-authored-by: Vebjørn Nordby <vebjorn.nordby@nav.no>

---
+++ packages/v2/backend/src/k9sak/kontrakt/beregningsresultat/BeregningsresultatMedUtbetaltePeriodeDto.ts
@@ -0,0 +1,1 @@
+export type { k9_sak_kontrakt_beregningsresultat_BeregningsresultatMedUtbetaltePeriodeDto as BeregningsresultatMedUtbetaltePeriodeDto } from '@k9-sak-web/backend/k9sak/generated/types.js';
