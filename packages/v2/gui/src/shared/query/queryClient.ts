b6a364cc903b9719305f07fc0e693edf07a915e3
Jostein Stuhaug <jostein.stuhaug@nav.no>
2026-06-10 15:39:05 +0200

    Sett standard feilhandtering på QueryClient.
    For queries har vi throwOnError true, som vil vise feil i nærmaste ErrorBoundary, viss det ikkje blir overstyrt på aktuelt useQuery kall.
    
    For mutations har vi onError => throw, som vil kaste unhandledpromiserejection når feil oppstår i useMutation kall. Disse feil vil då vise på "topplinja" viss ikkje onError blir satt til noko anna på aktuelt useMutation kall. Dette settast slik for å få oppførsel lik legacy, der ein unngår at UI blir re-rendra/erstatta av ErrorBoundary, for at evt utfylt skjema informasjon ikkje skal forsvinne når det oppstår feil.

--- packages/v2/gui/src/shared/query/queryClient.ts
+++ packages/v2/gui/src/shared/query/queryClient.ts
@@ -11,15 +11,17 @@ export const createQueryClient = (defaultOptionsOverride?: Pick<DefaultOptions, 
     defaultOptions: {
       // Options satt her blir som standard gjeldande for heile k9-sak-web
       queries: {
+        throwOnError: true, // Kast feil til nærmaste ErrorBoundary
         refetchOnWindowFocus: false,
         // ...Men kan overstyrast med defaultOptionsOverride, feks når query client blir oppretta for tester.
         ...defaultOptionsOverride?.queries,
       },
-      // TODO Juster/sjekk denne for ønska feilhandtering
+      // Ved å sette onError slik, blir alle feil frå useMutation i utgangspunktet kasta som unhandledrejection.
       mutations: {
         onError: err => {
           throw err;
         },
+        throwOnError: false, // onError kaster unhandledrejection, så skrur av at feil skal kastast til ErrorBoundary.
         ...defaultOptionsOverride?.mutations,
       },
     },
