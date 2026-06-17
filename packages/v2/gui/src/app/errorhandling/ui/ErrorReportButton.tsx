c6772dca1c09943a0d418c8aa24fa1b63d8d14cb
Jostein Stuhaug <jostein.stuhaug@nav.no>
2026-06-17 11:42:09 +0200

    Legg til rel noopener på porten link.
    Treng ikkje noreferrer, sidan det er link til nav tjeneste.

--- packages/v2/gui/src/app/errorhandling/ui/ErrorReportButton.tsx
+++ packages/v2/gui/src/app/errorhandling/ui/ErrorReportButton.tsx
@@ -3,7 +3,7 @@ import { ExternalLinkIcon } from "@navikt/aksel-icons";
 import type { FC } from "react";
 
 export const ErrorReportButton: FC<{reportLink: string}> = ({reportLink}) => {
-  return <Button variant="secondary" data-color="neutral" size="small" as="a" href={reportLink} target="_blank" icon={<ExternalLinkIcon />} iconPosition="right">
+  return <Button variant="secondary" data-color="neutral" size="small" as="a" href={reportLink} target="_blank" rel="noopener" icon={<ExternalLinkIcon />} iconPosition="right">
     Meld feil i porten
   </Button>
 }
