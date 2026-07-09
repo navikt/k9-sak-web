1ad3b14d026dad718bf2dd84f2acdb4681457d06
Hallvard Andreas Stark <hallvard@scel.to>
2026-07-09 14:54:25 +0200

    Revert "Feature/meir info 500 feil (#8830)"
    This reverts commit c5b9c6deb09c325de29efa62672b8bff492612f0.

--- packages/utils/src/http-utils/axiosHttpUtils.ts
+++ packages/utils/src/http-utils/axiosHttpUtils.ts
@@ -2,15 +2,6 @@ import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 import { addLegacySerializerOption } from '@k9-sak-web/gui/utils/axios/axiosUtils.js';
 import { blobToText, isOfTypeBlob } from '@k9-sak-web/gui/app/errorhandling/legacycompat/blobResponseHelper.js';
 import { BlobResponseAxiosError } from '@k9-sak-web/gui/app/errorhandling/legacycompat/BlobResponseAxiosError.js';
-import { generateNavCallidHeader } from '@k9-sak-web/backend/shared/instrumentation/navCallid.js';
-
-// Legg til Nav-Callid-header (i tillegg til legacy serializer-option) slik at forespørslar herifrå
-// kan sporast på tvers av tenester på lik linje med forespørslar via rest-api-klienten.
-const buildRequestConfig = (requestConfig?: AxiosRequestConfig): AxiosRequestConfig => {
-  const { headerName, headerValue } = generateNavCallidHeader();
-  const config = addLegacySerializerOption(requestConfig);
-  return { ...config, headers: { ...config.headers, [headerName]: headerValue } };
-};
 
 async function resolveErrorType(error: unknown): Promise<Error> {
   if (error instanceof AxiosError) {
@@ -40,7 +31,7 @@ export async function get<T>(
   requestConfig?: AxiosRequestConfig,
 ): Promise<T> {
   try {
-    const response: AxiosResponse<T> = await axios.get(url, buildRequestConfig(requestConfig));
+    const response: AxiosResponse<T> = await axios.get(url, addLegacySerializerOption(requestConfig));
     return response.data;
   } catch (error) {
     const resolved = await resolveErrorType(error);
@@ -56,7 +47,7 @@ export async function post<T>(
   requestConfig?: AxiosRequestConfig,
 ): Promise<any> {
   try {
-    const response: AxiosResponse = await axios.post(url, body, buildRequestConfig(requestConfig));
+    const response: AxiosResponse = await axios.post(url, body, addLegacySerializerOption(requestConfig));
     return response.data;
   } catch (error) {
     const resolved = await resolveErrorType(error);
