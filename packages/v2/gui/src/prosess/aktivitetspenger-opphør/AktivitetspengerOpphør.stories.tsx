ad8e7ce8149f79fa90ce1e719d8265d8f71305b0
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2026-09-08 14:09:29 +0200

    TSFF-3031 - justeringer på tekster (#8980)
    ### Behov / Bakgrunn
    Ønsker å justere på noen tekster i henhold til skissene.
    
    ### Løsning
    Justerer på tekster.
    
    ### Andre endringer

--- packages/v2/gui/src/prosess/aktivitetspenger-opphør/AktivitetspengerOpphør.stories.tsx
+++ packages/v2/gui/src/prosess/aktivitetspenger-opphør/AktivitetspengerOpphør.stories.tsx
@@ -224,7 +224,7 @@ export const ÅrsakOgVarselKildeAnnetKreverFritekst: Story = {
     });
 
     await step('Fritekstfeltet er skjult før "Annet" er valgt', async () => {
-      await expect(canvas.queryByRole('textbox', { name: /beskriv hvor opplysningene kommer fra/i })).toBeNull();
+      await expect(canvas.queryByRole('textbox', { name: /skriv inn hvor du har fått opplysningene fra/i })).toBeNull();
     });
 
     await step('Velg kilde "Annet"', async () => {
@@ -236,7 +236,7 @@ export const ÅrsakOgVarselKildeAnnetKreverFritekst: Story = {
 
     await step('Fyll inn hvor opplysningene kommer fra', async () => {
       await userEvent.type(
-        await canvas.findByRole('textbox', { name: /beskriv hvor opplysningene kommer fra/i }),
+        await canvas.findByRole('textbox', { name: /skriv inn hvor du har fått opplysningene fra/i }),
         'Opplyst av veileder ved lokalkontoret',
       );
     });
@@ -444,7 +444,7 @@ export const ÅrsakOgVarselAvslå: Story = {
     await step('Velg kilde til opplysningene', async () => {
       await userEvent.selectOptions(
         canvas.getByRole('combobox', { name: /hvor har du fått opplysningene fra/i }),
-        'Folkeregisteret',
+        'Register',
       );
     });
 
