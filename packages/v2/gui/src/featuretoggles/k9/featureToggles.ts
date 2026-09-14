1fce24fed719cbda67a0b797e7bc042e145f38ba
Audun Eltvik <audun.eltvik@nav.no>
2026-09-14 14:03:15 +0200

    Skru på toggle for delvis manuell revurdering i prod

--- packages/v2/gui/src/featuretoggles/k9/featureToggles.ts
+++ packages/v2/gui/src/featuretoggles/k9/featureToggles.ts
@@ -41,4 +41,5 @@ export const qFeatureToggles = initQFeatureToggles(k9SpecificFeatureToggles)({
 export const prodFeatureToggles = initProdFeatureToggles(k9SpecificFeatureToggles)({
   isFor: 'prod',
   SKJUL_PROSESS_MENY_V2_VELGER: true,
+  REVURDERING_FRA_STEG_V2: true,
 });
