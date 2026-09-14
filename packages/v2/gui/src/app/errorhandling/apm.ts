153f4a12784967cffc705e03d2ffa6ea3a28483d
Jostein Stuhaug <jostein.stuhaug@nav.no>
2026-09-14 11:32:33 +0200

    shouldReportToApm => shouldNotReportToApm
    Og relatert omskriving av bruken.

--- packages/v2/gui/src/app/errorhandling/apm.ts
+++ packages/v2/gui/src/app/errorhandling/apm.ts
@@ -2,17 +2,6 @@ import { init, type InitOptions } from '@nais/apm';
 import { ExtendedApiError } from '@k9-sak-web/backend/shared/errorhandling/ExtendedApiError.js';
 import { AxiosError } from 'axios';
 
-// Vi ønsker ikkje å rapportere alle feil til apm, feks viss har utgått sesjon.
-// Legg til fleire her ved behov.
-export const shouldReportToApm = (error: Error | null): boolean => {
-  const apiError = ExtendedApiError.findInError(error);
-  if (apiError != null) {
-    const doNotReport = apiError.isUnauthorized;
-    return !doNotReport;
-  }
-  return true;
-};
-
 // Sidan vi legger til preserveOriginalError i initApm skal feilhendelser rapportert ha originalError satt.
 type WithOriginalError = {
   originalError: Error;
@@ -35,6 +24,16 @@ export const hasOriginalError = (
   return originalError instanceof Error;
 };
 
+// Vi ønsker ikkje å rapportere alle feil til apm, feks viss har utgått sesjon.
+// Legg til fleire her ved behov.
+const shouldNotReportToApm = (error: Error | null): boolean => {
+  const apiError = ExtendedApiError.findInError(error);
+  if (apiError != null) {
+    return apiError.isUnauthorized;
+  }
+  return false;
+};
+
 const randomErrorId = (): string =>
   typeof crypto !== 'undefined' && typeof crypto.randomUUID === 'function'
     ? crypto.randomUUID()
@@ -55,17 +54,18 @@ const beforeSend: NonNullable<InitOptions['beforeSend']> = item => {
     payload.context = { ...payload.context, loadedErrorId };
     if (hasOriginalError(item)) {
       const { originalError } = item.payload;
+      if (shouldNotReportToApm(originalError)) {
+        return null;
+      }
       const extendedApiError = ExtendedApiError.findInError(originalError);
       if (extendedApiError != null) {
         if (extendedApiError.navCallid != null) {
           payload.context = { ...payload.context, navCallid: extendedApiError.navCallid };
         }
-        if (extendedApiError.status != 0) {
-          payload.context = { ...payload.context, status: `${extendedApiError.status}` };
-        }
+        payload.context = { ...payload.context, status: `${extendedApiError.status}` };
       } else if (originalError instanceof AxiosError) {
-        if (originalError.response?.status != 0) {
-          payload.context = { ...payload.context, status: `${originalError.response?.status}` };
+        if (originalError.response?.status != null) {
+          payload.context = { ...payload.context, status: `${originalError.response.status}` };
         }
       }
     }
