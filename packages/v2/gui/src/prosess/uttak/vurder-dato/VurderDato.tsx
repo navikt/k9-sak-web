3f7cd49c527aee2b7b192164ebddef02b0e590ec
vebnor <vebjorn.nordby@nav.no>
2026-09-21 15:42:28 +0200

    slår sammen alert

--- packages/v2/gui/src/prosess/uttak/vurder-dato/VurderDato.tsx
+++ packages/v2/gui/src/prosess/uttak/vurder-dato/VurderDato.tsx
@@ -1,5 +1,5 @@
 import { AksjonspunktDefinisjon } from '@k9-sak-web/backend/k9sak/kodeverk/behandling/aksjonspunkt/AksjonspunktDefinisjon.js';
-import { Accordion, Alert, BodyLong, Label } from '@navikt/ds-react';
+import { Alert, BodyLong, Label, ReadMore, VStack } from '@navikt/ds-react';
 import { useEffect } from 'react';
 import { useUttakContext } from '../context/UttakContext';
 import styles from './VurderDato.module.css';
@@ -54,46 +54,39 @@ const VurderDato = () => {
   };
 
   return (
-    <div className={styles['vurderDatoContainer']}>
+    <VStack className={styles['vurderDatoContainer']} gap="space-20">
       <Alert variant="warning" size="small">
         <Label size="small">Vurder hvilken dato endringer i uttak skal gjelde fra</Label>
-        <BodyLong size="small">
-          Det er lansert endringer for hvordan utbetalingsgrad settes for nye aktiviteter, ikke yrkesaktiv og kun
-          ytelse. Vurder hvilken dato endringene skal gjelde fra i denne saken. Dager før denne datoen vil følge gammel
-          praksis.
-        </BodyLong>
-      </Alert>
-      <Alert variant="info" className={styles['info']}>
-        <Accordion className={styles['alertAccordion']} indent={false}>
-          <Accordion.Item className="!shadow-none">
-            <Accordion.Header>
-              <Label size="small">Hva innebærer endringene i uttak?</Label>
-            </Accordion.Header>
-            <Accordion.Content>
-              <BodyLong as="div" size="small">
-                Før endring:
-                <ol>
-                  <li>Nye aktiviteter blir tatt med ved utregning av utbetalingsgrad og søkers uttaksgrad.</li>
-                  <li>{`"Ikke-yrkesaktiv" og "Kun ytelse" har samme utbetalingsgrad som andre aktiviteter.`}</li>
-                </ol>
-                Etter endring:
-                <ol>
-                  <li>
-                    Nye aktiviteter blir ikke tatt med i utregning av utbetalingsgrad eller søkers uttaksgrad. De vil
-                    alltid stå med 0% utbetalingsgrad.
-                  </li>
-                  <li>
-                    {`"Ikke-yrkesaktiv" og "Kun ytelse" får alltid 100% som utbetalingsgrad, hvis det ikke er
+        <VStack gap="space-12">
+          <BodyLong size="small">
+            Det er lansert endringer for hvordan utbetalingsgrad settes for nye aktiviteter, ikke yrkesaktiv og kun
+            ytelse. Vurder hvilken dato endringene skal gjelde fra i denne saken. Dager før denne datoen vil følge
+            gammel praksis.
+          </BodyLong>
+          <ReadMore size="small" header="Hva innebærer endringene i uttak?" data-color="accent">
+            <BodyLong size="small">
+              Før endring:
+              <ol>
+                <li>Nye aktiviteter blir tatt med ved utregning av utbetalingsgrad og søkers uttaksgrad.</li>
+                <li>{`"Ikke-yrkesaktiv" og "Kun ytelse" har samme utbetalingsgrad som andre aktiviteter.`}</li>
+              </ol>
+              Etter endring:
+              <ol>
+                <li>
+                  Nye aktiviteter blir ikke tatt med i utregning av utbetalingsgrad eller søkers uttaksgrad. De vil
+                  alltid stå med 0% utbetalingsgrad.
+                </li>
+                <li>
+                  {`"Ikke-yrkesaktiv" og "Kun ytelse" får alltid 100% som utbetalingsgrad, hvis det ikke er
                             reduksjon grunnet tilsyn.`}
-                  </li>
-                </ol>
-              </BodyLong>
-            </Accordion.Content>
-          </Accordion.Item>
-        </Accordion>
+                </li>
+              </ol>
+            </BodyLong>
+          </ReadMore>
+        </VStack>
       </Alert>
       <VurderDatoAksjonspunkt initialValues={initialValues} />
-    </div>
+    </VStack>
   );
 };
 
