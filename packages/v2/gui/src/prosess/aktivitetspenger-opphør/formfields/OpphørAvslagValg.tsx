489bb3ed8b0c140f2ae8276b3993f1f267cf57ee
Hallvard Andreas Stark <hallvard@scel.to>
2026-09-25 12:39:31 +0200

    TSFF-3141 - sjekk på at tom ikke er før fom

--- packages/v2/gui/src/prosess/aktivitetspenger-opphør/formfields/OpphørAvslagValg.tsx
+++ packages/v2/gui/src/prosess/aktivitetspenger-opphør/formfields/OpphørAvslagValg.tsx
@@ -1,8 +1,9 @@
 import type { VilkårMedPerioderDto } from '@k9-sak-web/backend/ungsak/kontrakt/vilkår/VilkårMedPerioderDto.js';
 import { HStack, Radio } from '@navikt/ds-react';
 import { RhfRadioGroup } from '@navikt/ft-form-hooks';
-import { required } from '@navikt/ft-form-validators';
+import { dateAfterOrEqual, required } from '@navikt/ft-form-validators';
 import type { Control, FieldValues } from 'react-hook-form';
+import { useWatch } from 'react-hook-form';
 import Datovelger from '../../../shared/datovelger/Datovelger.js';
 
 const dagensDato = new Date();
@@ -34,54 +35,59 @@ export const OpphørAvslagValg = <TForm extends FieldValues>({
   isFormLocked,
   opphøreEllerAvslå,
   dateRange,
-}: Props<TForm>) => (
-  <>
-    <RhfRadioGroup
-      key={`${selectedId}-opphørsdato`}
-      control={control}
-      name={`perioder.${selectedId}.opphøreEllerAvslå` as never}
-      legend="Hva skal du gjøre?"
-      validate={[required]}
-      readOnly={isFormLocked}
-    >
-      <Radio value="opphøre">Opphøre fra en dato</Radio>
-      <Radio value="avslå">Avslå en innvilget periode</Radio>
-    </RhfRadioGroup>
-    {opphøreEllerAvslå === 'opphøre' && (
-      <Datovelger
-        name={`perioder.${selectedId}.opphørsdato` as never}
-        label="Opphøre fra og med"
-        readOnly={isFormLocked}
+}: Props<TForm>) => {
+  const avslagFom = useWatch({ control, name: `perioder.${selectedId}.avslagFom` as never }) as unknown as
+    string | undefined;
+
+  return (
+    <>
+      <RhfRadioGroup
+        key={`${selectedId}-opphørsdato`}
+        control={control}
+        name={`perioder.${selectedId}.opphøreEllerAvslå` as never}
+        legend="Hva skal du gjøre?"
         validate={[required]}
-        defaultMonth={dagensDato}
-        fromDate={dateRange?.fromDate}
-        toDate={dateRange?.toDate}
-        disableWeekends
-      />
-    )}
-    {opphøreEllerAvslå === 'avslå' && (
-      <HStack gap="space-8">
+        readOnly={isFormLocked}
+      >
+        <Radio value="opphøre">Opphøre fra en dato</Radio>
+        <Radio value="avslå">Avslå en innvilget periode</Radio>
+      </RhfRadioGroup>
+      {opphøreEllerAvslå === 'opphøre' && (
         <Datovelger
-          name={`perioder.${selectedId}.avslagFom` as never}
-          label="Fra og med"
+          name={`perioder.${selectedId}.opphørsdato` as never}
+          label="Opphøre fra og med"
           readOnly={isFormLocked}
           validate={[required]}
+          defaultMonth={dagensDato}
           fromDate={dateRange?.fromDate}
           toDate={dateRange?.toDate}
-          defaultMonth={dagensDato}
           disableWeekends
         />
-        <Datovelger
-          name={`perioder.${selectedId}.avslagTom` as never}
-          label="Til og med"
-          readOnly={isFormLocked}
-          validate={[required]}
-          fromDate={dateRange?.fromDate}
-          toDate={dateRange?.toDate}
-          defaultMonth={dagensDato}
-          disableWeekends
-        />
-      </HStack>
-    )}
-  </>
-);
+      )}
+      {opphøreEllerAvslå === 'avslå' && (
+        <HStack gap="space-8">
+          <Datovelger
+            name={`perioder.${selectedId}.avslagFom` as never}
+            label="Fra og med"
+            readOnly={isFormLocked}
+            validate={[required]}
+            fromDate={dateRange?.fromDate}
+            toDate={dateRange?.toDate}
+            defaultMonth={dagensDato}
+            disableWeekends
+          />
+          <Datovelger
+            name={`perioder.${selectedId}.avslagTom` as never}
+            label="Til og med"
+            readOnly={isFormLocked}
+            validate={[required, dateAfterOrEqual(avslagFom ?? '')]}
+            fromDate={dateRange?.fromDate}
+            toDate={dateRange?.toDate}
+            defaultMonth={dagensDato}
+            disableWeekends
+          />
+        </HStack>
+      )}
+    </>
+  );
+};
